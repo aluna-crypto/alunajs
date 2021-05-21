3554a76e3a1b2d2f0bb43d49ea6ecdb13c144f3c
Torres-ssf <sergio.uft@gmail.com>
2021-05-21 09:24:43 -0300

    implement list method for ValrBalance

--- src/exchanges/valr/lib/modules/ValrBalance.ts
+++ src/exchanges/valr/lib/modules/ValrBalance.ts
@@ -3,47 +3,31 @@ import { IAlunaBalanceSchema } from '../../../../lib/schemas/IAlunaBalanceSchema
 import { ValrPrivateRequest } from '../requests/ValrPrivateRequest'
 import { IValrBalanceSchema } from '../schemas/IValrBalanceSchema'
 
-
-
 export class ValrBalance extends ValrPrivateRequest implements IAlunaBalance {
-
-  public async list (): Promise<IAlunaBalanceSchema[]> {
-
+  public async list(): Promise<IAlunaBalanceSchema[]> {
     const rawBalances = await this.post<IValrBalanceSchema[]>({
-      url: '/balances',
-      params: {},
+      url: 'https://api.valr.com/v1/account/balances',
+      path: '/v1/account/balances',
     })
 
     const parsedBalances = this.parseMany({ rawBalances })
 
     return parsedBalances
-
   }
 
-
-
-  public parse (
-    params: {
-      rawBalance: IValrBalanceSchema,
-    },
-  ): IAlunaBalanceSchema {
-
+  public parse(params: {
+    rawBalance: IValrBalanceSchema
+  }): IAlunaBalanceSchema {
     // TODO: implement me
     const x: any = params
     return x
-
   }
 
-
-
-  public parseMany (
-    params: {
-      rawBalances: IValrBalanceSchema[],
-    },
-  ): IAlunaBalanceSchema[] {
+  public parseMany(params: {
+    rawBalances: IValrBalanceSchema[]
+  }): IAlunaBalanceSchema[] {
     return params.rawBalances.map((rawBalance: IValrBalanceSchema) =>
-      this.parse({ rawBalance })
+      this.parse({ rawBalance }),
     )
   }
-
 }
