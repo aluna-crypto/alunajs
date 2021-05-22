25de74ef6954cc198f6a4f94529f239813959b12
Torres-ssf <sergio.uft@gmail.com>
2021-05-22 14:40:17 -0300

    define IValrPublicRequestParams at ValrPublicRequest class

--- src/exchanges/valr/lib/requests/ValrPublicRequest.ts
+++ src/exchanges/valr/lib/requests/ValrPublicRequest.ts
@@ -15,21 +15,32 @@ export type IValrPublicRequestParams = IAlunaPublicRequestParams
 export class ValrPublicRequest
   extends AAlunaPublicRequest
   implements IAlunaPublicRequest {
-  async get<T> (params: IAlunaPublicRequestParams): Promise<T> {
+
+  async get<T> (params: IValrPublicRequestParams): Promise<T> {
+
     const {
-      url, options,
+      url,
+      options,
     } = params
 
     try {
+
       const res = await axios.get<T>(url, options)
 
       return res.data
+
     } catch (error) {
+
       throw new Error(error)
+
     }
+
   }
 
   async post<T> (params: IValrPublicRequestParams): Promise<T> {
+
     throw new Error('Method not implemented.')
+
   }
+
 }
