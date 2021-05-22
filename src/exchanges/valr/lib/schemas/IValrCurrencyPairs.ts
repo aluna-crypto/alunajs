5ba7c3ae2f1bf789f92bb07d9d07c92e3a684eda
Torres-ssf <sergio.uft@gmail.com>
2021-05-22 14:40:59 -0300

    implement a schema for ValrCurrencyPairs

---
+++ src/exchanges/valr/lib/schemas/IValrCurrencyPairs.ts
@@ -0,0 +1,13 @@
+export interface IValrCurrencyPairs {
+  symbol: string
+  baseCurrency: string
+  quoteCurrency: string
+  shortName: string
+  active: string
+  minBaseAmount: string
+  maxBaseAmount: string
+  minQuoteAmount: string
+  maxQuoteAmount: string
+  tickSize: string
+  baseDecimalPlaces: string
+}
