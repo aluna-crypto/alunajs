19001883ae4d7bfc4c59a932fad208cb603d6066
Torres-ssf <sergio.uft@gmail.com>
2021-05-22 16:13:42 -0300

    update IValrKeySchema

--- src/exchanges/valr/lib/schemas/IValrKeySchema.ts
+++ src/exchanges/valr/lib/schemas/IValrKeySchema.ts
@@ -1,15 +1,5 @@
 export interface IValrKeySchema {
-  balance: {
-    read: boolean
-  }
-  orders: {
-    read: boolean
-    write: boolean
-  }
-  deposits: {
-    read: boolean
-  }
-  withdraw: {
-    write: boolean
-  }
+  canRead: boolean
+  canTrade: boolean
+  canWithraw: boolean
 }
