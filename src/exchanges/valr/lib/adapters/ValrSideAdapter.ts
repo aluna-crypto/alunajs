a293456876b4df5a63cff9e0ad8d99d9f9203837
Torres-ssf <sergio.uft@gmail.com>
2021-05-23 16:30:52 -0300

    implement ValrSideAdapter

---
+++ src/exchanges/valr/lib/adapters/ValrSideAdapter.ts
@@ -0,0 +1,66 @@
+import {
+  SideEnum,
+} from 'lib/enums/SideEnum'
+
+import {
+  ValrSideEnum,
+} from '../enums/ValrSideEnum'
+import {
+  ValrError,
+} from '../errors/ValrError'
+
+
+
+export class ValrSideAdapter {
+
+  static translateToAluna (
+    params: {
+      side: ValrSideEnum
+    },
+  ): SideEnum {
+
+    const {
+      side,
+    } = params
+
+    switch (side) {
+
+      case ValrSideEnum.BUY:
+        return SideEnum.LONG
+
+      case ValrSideEnum.SELL:
+        return SideEnum.SHORT
+
+      default:
+        throw new ValrError(`Order side not supported: ${side}`)
+
+    }
+
+  }
+
+  static translateToValr (
+    params: {
+      side: SideEnum
+    },
+  ): ValrSideEnum {
+
+    const {
+      side,
+    } = params
+
+    switch (side) {
+
+      case SideEnum.LONG:
+        return ValrSideEnum.BUY
+
+      case SideEnum.SHORT:
+        return ValrSideEnum.SELL
+
+      default:
+        throw new ValrError(`Order side not supported: ${side}`)
+
+    }
+
+  }
+
+}
