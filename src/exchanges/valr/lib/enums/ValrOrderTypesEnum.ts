896c00c805ac8c1d1eccbeaaa5655505a19de4aa
Torres-ssf <sergio.uft@gmail.com>
2021-05-23 16:55:08 -0300

    uncomment all valr order types

--- src/exchanges/valr/lib/enums/ValrOrderTypesEnum.ts
+++ src/exchanges/valr/lib/enums/ValrOrderTypesEnum.ts
@@ -1,7 +1,7 @@
 export enum ValrOrderTypesEnum {
-  // SIMPLE = 'simple',
+  SIMPLE = 'simple',
   LIMIT = 'limit',
-  // MARKET = 'market',
+  MARKET = 'market',
   TAKE_PROFIT_LIMIT = 'take-profit-limit',
   LIMIT_POST_ONLY = 'post-only limit',
   STOP_LOSS_LIMIT = 'stop-loss-limit'
