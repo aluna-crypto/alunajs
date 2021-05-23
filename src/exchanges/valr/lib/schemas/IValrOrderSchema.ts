80f1adbc8125be1e84504f7fcb8a0eaf3fff894d
Torres-ssf <sergio.uft@gmail.com>
2021-05-23 15:30:53 -0300

    add different Order responses types to IValrOrderSchema

--- src/exchanges/valr/lib/schemas/IValrOrderSchema.ts
+++ src/exchanges/valr/lib/schemas/IValrOrderSchema.ts
@@ -12,6 +12,21 @@ import {
 } from '../enums/ValrSideEnum'
 
 
+/**
+ * ISSUE: Valr has many different order responses types
+*/
+
+export interface IValrPlaceOrderSchema {
+  side: ValrSideEnum
+  quantity: string
+  price: string
+  pair: string
+  postOnly: boolean
+  customerOrderId: string
+  timeInForce: ValrOrderTimeInForceEnum
+}
+
+
 
 export interface IValrOrderSchema {
   orderId: string
@@ -26,5 +41,44 @@ export interface IValrOrderSchema {
   updatedAt: string
   status: ValrOrderStatusEnum
   type: ValrOrderTypesEnum
+  timeInForce: ValrOrderTimeInForceEnum
+}
+
+
+
+export interface IValrOrderStatusSchema {
+  orderId: string
+  orderStatusType: ValrOrderTypesEnum
+  currencyPair: string
+  originalPrice: string
+  remainingQuantity: string
+  originalQuantity: string
+  orderSide: ValrSideEnum
+  orderType: ValrOrderTypesEnum
+  failedReason: string
+  customerOrderId: string
+  orderUpdatedAt: Date
+  orderCreatedAt: Date
+}
+
+
+
+export interface IValrOderHistorySchema {
+  orderId: string
+  customerOrderId: string
+  orderStatusType: ValrOrderTypesEnum
+  currencyPair: string
+  averagePrice: string
+  originalPrice: string
+  remainingQuantity: string
+  originalQuantity: string
+  total: string
+  totalFee: string
+  feeCurrency: string
+  orderSide: ValrSideEnum
+  orderType: ValrOrderTypesEnum
+  failedReason: string
+  orderUpdatedAt: Date
+  orderCreatedAt: Date
   timeInForce: ValrOrderTimeInForceEnum
 }
