6f94f6a6fff63d3441095339c1df015f432eefff
Torres-ssf <sergio.uft@gmail.com>
2021-05-23 16:55:56 -0300

    implement ValrTypeAdapter

---
+++ src/exchanges/valr/lib/adapters/ValrTypeAdapter.ts
@@ -0,0 +1,79 @@
+import {
+  OrderTypesEnum,
+} from 'lib/enums/OrderTypeEnum'
+
+import {
+  ValrOrderTypesEnum,
+} from '../enums/ValrOrderTypesEnum'
+import {
+  ValrError,
+} from '../errors/ValrError'
+
+
+
+export class ValrTypeAdapter {
+
+  static translateToAluna (params: {
+    type: ValrOrderTypesEnum
+  }): OrderTypesEnum {
+
+    const {
+      type,
+    } = params
+
+    switch (type) {
+
+      case ValrOrderTypesEnum.LIMIT:
+      case ValrOrderTypesEnum.LIMIT_POST_ONLY:
+        return OrderTypesEnum.LIMIT
+
+      case ValrOrderTypesEnum.STOP_LOSS_LIMIT:
+        return OrderTypesEnum.STOP_LIMIT
+
+      case ValrOrderTypesEnum.MARKET:
+      case ValrOrderTypesEnum.SIMPLE:
+        return OrderTypesEnum.MARKET
+
+      case ValrOrderTypesEnum.TAKE_PROFIT_LIMIT:
+        return OrderTypesEnum.TAKE_PROFIT_LIMIT
+
+      default:
+        throw new ValrError(`Order type not supported: ${type}`)
+
+    }
+
+  }
+
+  static translateToValr (
+    params: {
+      type: OrderTypesEnum
+    },
+  ): ValrOrderTypesEnum {
+
+    const {
+      type,
+    } = params
+
+    switch (type) {
+
+      case OrderTypesEnum.LIMIT:
+        return ValrOrderTypesEnum.LIMIT
+
+      case OrderTypesEnum.MARKET:
+        return ValrOrderTypesEnum.MARKET
+
+      case OrderTypesEnum.STOP_LIMIT:
+        return ValrOrderTypesEnum.STOP_LOSS_LIMIT
+
+      case OrderTypesEnum.TAKE_PROFIT_LIMIT:
+        return ValrOrderTypesEnum.TAKE_PROFIT_LIMIT
+
+
+      default:
+        throw new ValrError(`Order type not supported: ${type}`)
+
+    }
+
+  }
+
+}
