f41f90ebfdb03514de985750f5cd7368fbb88059
Torres-ssf <sergio.uft@gmail.com>
2021-05-23 13:33:28 -0300

    implement ValrErrorEnum

--- src/exchanges/valr/lib/enums/ValrErrorEnum.ts
+++ src/exchanges/valr/lib/enums/ValrErrorEnum.ts
@@ -1,3 +1,5 @@
-export interface IValrErrorEnum {
-  // TODO: implement me
+export enum ValrErrorEnum {
+  UNAUTHORIZED = 'Unauthorized',
+  INVALID_REQUEST = 'Invalid Request, please check your request and try again',
+  INVALID_SIGNATURE = 'Request has an invalid signature'
 }
