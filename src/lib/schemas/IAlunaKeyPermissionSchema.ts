7e9e32c95325026d03df156e3ebb6bc935723b19
Torres-ssf <sergio.uft@gmail.com>
2021-05-25 15:52:13 -0300

    update IAlunaKeyPermissionSchema

--- src/lib/schemas/IAlunaKeyPermissionSchema.ts
+++ src/lib/schemas/IAlunaKeyPermissionSchema.ts
@@ -1,15 +1,5 @@
 export interface IAlunaKeyPermissionSchema {
-  balance: {
-    read: boolean
-  }
-  orders: {
-    read: boolean
-    write: boolean
-  }
-  deposits: {
-    read: boolean
-  }
-  withdraw: {
-    write: boolean
-  }
+  read: boolean
+  trade: boolean
+  withdraw: boolean
 }
