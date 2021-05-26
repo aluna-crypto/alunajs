3324ba6e284031765ebc3c980f73d03f9d413d9f
Torres-ssf <sergio.uft@gmail.com>
2021-05-26 10:38:43 -0300

    uncomment method get IAlunaSymbol

--- src/lib/modules/IAlunaSymbol.ts
+++ src/lib/modules/IAlunaSymbol.ts
@@ -5,7 +5,7 @@ import { IAlunaSymbolSchema } from '../schemas/IAlunaSymbolSchema'
 export interface IAlunaSymbol {
 
   list (): Promise<IAlunaSymbolSchema[]>
-  // get? (params: { id: string | number }): Promise<IAlunaSymbolSchema>
+  get? (params: { id: string | number }): Promise<IAlunaSymbolSchema>
   parse (params: { rawSymbol: any }): IAlunaSymbolSchema
   parseMany (parms: { rawSymbols: any[] }): IAlunaSymbolSchema[]
 
