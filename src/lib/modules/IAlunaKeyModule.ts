187c21175523c9abf7727e959aac8051768d76d8
Torres-ssf <sergio.uft@gmail.com>
2021-05-26 20:48:44 -0300

    remove unnecessary params at IAlunaKeyModule

--- src/lib/modules/IAlunaKeyModule.ts
+++ src/lib/modules/IAlunaKeyModule.ts
@@ -1,20 +1,13 @@
 import { IAlunaModule } from '@lib/abstracts/IAlunaModule'
 
 import { IAlunaKeyPermissionSchema } from '../schemas/IAlunaKeyPermissionSchema'
-import { IAlunaKeySecretSchema } from '../schemas/IAlunaKeySecretSchema'
 
 
 
 export interface IAlunaKeyModule extends IAlunaModule {
 
-  validate (
-    params: IAlunaKeySecretSchema,
-  ): Promise<boolean>
-
-  getPermissions (
-    params: IAlunaKeySecretSchema,
-  ): Promise<IAlunaKeyPermissionSchema>
-
+  validate (): Promise<boolean>
+  getPermissions (): Promise<IAlunaKeyPermissionSchema>
   parsePermissions (
     params: { rawKey: any },
   ): IAlunaKeyPermissionSchema
