8ea0ee4c31a6561ec97f3b0a067fd75f21f549d0
Torres-ssf <sergio.uft@gmail.com>
2021-05-26 14:26:54 -0300

    implement new ValrMarket

---
+++ src/exchanges/valr/lib/modules/market/ValrMarket.ts
@@ -0,0 +1,66 @@
+import { AAlunaPublicModule } from '@lib/abstracts/AAlunaPublicModule'
+import { IAlunaMarket } from '@lib/modules/IAlunaMarket'
+import { IAlunaMarketSchema } from '@lib/schemas/IAlunaMarketSchema'
+
+import { IValrMarketSchema } from '../../schemas/IValrMarketSchema'
+import { ValrCurrencyPairsParser } from './parsers/ValrCurrencyPairParser'
+import { ValrMarketParser } from './parsers/ValrMarketParser'
+import { ValrMarketList } from './ValrMarketList'
+
+
+
+export interface IMarketWithCurrency extends IValrMarketSchema {
+  baseCurrency: string
+  quoteCurrency: string
+}
+
+
+
+export class ValrMarket extends AAlunaPublicModule implements IAlunaMarket {
+
+  async list (): Promise<IAlunaMarketSchema[]> {
+
+    const {
+      rawMarkets,
+      rawSymbolPairs,
+    } = await new ValrMarketList({
+      publicRequest: this.requestHandler,
+    }).list()
+
+    const {
+      rawMarketsWithCurrency,
+      currencyVolumes,
+    } = ValrCurrencyPairsParser.parse({
+      rawMarkets,
+      rawSymbolPairs,
+    })
+
+    return this.parseMany({
+      rawMarkets: rawMarketsWithCurrency,
+      currencyVolumes,
+    })
+
+  }
+
+  parse (params: {
+    rawMarket: IMarketWithCurrency
+    currencyVolumes: Record<string, string>
+  }): IAlunaMarketSchema {
+
+    return ValrMarketParser.parse(params)
+
+  }
+
+  parseMany (params: {
+    rawMarkets: IMarketWithCurrency[]
+    currencyVolumes: Record<string, string>
+  }): IAlunaMarketSchema[] {
+
+    return params.rawMarkets.map((rawMarket) => this.parse({
+      rawMarket,
+      currencyVolumes: params.currencyVolumes,
+    }))
+
+  }
+
+}
