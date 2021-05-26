3a610b55c970b41a6a7d228a7d1df64b7682ba88
Torres-ssf <sergio.uft@gmail.com>
2021-05-26 14:27:52 -0300

    update Valr class to conform with changes made at public requests

--- src/exchanges/valr/Valr.ts
+++ src/exchanges/valr/Valr.ts
@@ -1,3 +1,5 @@
+import { IAlunaPrivateRequest } from '@lib/abstracts/IAlunaPrivateRequest'
+
 import { AAlunaExchange } from '../../lib/abstracts/AAlunaExchange'
 import { IAlunaExchange } from '../../lib/abstracts/IAlunaExchange'
 import { IAlunaBalance } from '../../lib/modules/IAlunaBalance'
@@ -5,35 +7,35 @@ import { IAlunaKey } from '../../lib/modules/IAlunaKey'
 import { IAlunaOrder } from '../../lib/modules/IAlunaOrder'
 import { IAlunaKeySecretSchema } from '../../lib/schemas/IAlunaKeySecretSchema'
 import { IAlunaSettingsSchema } from '../../lib/schemas/IAlunaSettingsSchema'
+import { ValrMarket } from './lib/modules/market/ValrMarket'
+import { ValrSymbol } from './lib/modules/symbol/ValrSymbol'
 import { ValrBalance } from './lib/modules/ValrBalance'
-import { ValrKey } from './lib/modules/ValrKey'
-import { ValrMarket } from './lib/modules/ValrMarket'
-import { ValrOrder } from './lib/modules/ValrOrder'
-import { ValrSymbol } from './lib/modules/ValrSymbol'
+import { ValrPublicRequest } from './lib/requests/ValrPublicRequest'
 
 
 
 export class Valr extends AAlunaExchange implements IAlunaExchange {
 
-  // exchange identifiers
   static readonly ID = 'valr'
 
-  static readonly Symbol = new ValrSymbol()
+  static readonly publicRequest = new ValrPublicRequest()
 
-  static readonly Market = new ValrMarket()
+  static readonly Symbol = new ValrSymbol(Valr.publicRequest)
+
+  static readonly Market = new ValrMarket(Valr.publicRequest)
 
 
 
-  // instance (public) methods
   Key: IAlunaKey
 
   Order: IAlunaOrder
 
   Balance: IAlunaBalance
 
+  privateRequest: IAlunaPrivateRequest
 
 
-  // constructor
+
   constructor (
     params: {
       keySecret: IAlunaKeySecretSchema
@@ -44,8 +46,9 @@ export class Valr extends AAlunaExchange implements IAlunaExchange {
     super(params)
 
     this.Balance = new ValrBalance(this)
-    this.Order = new ValrOrder(this)
-    this.Key = new ValrKey(this)
+    // this.Order = new ValrOrder(this)
+    // this.Key = new ValrKey(this)
+    // this.Balance = new ValrOrder2(this.privateRequest)
 
   }
 
