5c5d704b4853c09276cb0b4687cf580502866610
Torres-ssf <sergio.uft@gmail.com>
2021-05-26 14:26:08 -0300

    implement ValrCurrencyPairsParser

---
+++ src/exchanges/valr/lib/modules/market/parsers/ValrMarketParser.ts
@@ -0,0 +1,53 @@
+import { IAlunaMarketSchema } from '@lib/schemas/IAlunaMarketSchema'
+
+import { IMarketWithCurrency } from '../ValrMarket'
+
+
+
+export class ValrMarketParser {
+
+  static parse (params: {
+    rawMarket: IMarketWithCurrency
+    currencyVolumes: Record<string, string>
+  }): IAlunaMarketSchema {
+
+    const {
+      rawMarket: {
+        askPrice,
+        baseVolume,
+        bidPrice,
+        changeFromPrevious,
+        highPrice,
+        lastTradedPrice,
+        lowPrice,
+        baseCurrency,
+        quoteCurrency,
+      },
+      currencyVolumes,
+    } = params
+
+    const quoteVolume = Number(currencyVolumes[quoteCurrency]) || 0
+
+    const ticker = {
+      high: parseFloat(highPrice),
+      low: parseFloat(lowPrice),
+      bid: parseFloat(bidPrice),
+      ask: parseFloat(askPrice),
+      last: parseFloat(lastTradedPrice),
+      date: new Date(new Date().toDateString()),
+      change: parseFloat(changeFromPrevious) / 100,
+      baseVolume: parseFloat(baseVolume),
+      quoteVolume,
+    }
+
+    return {
+      pairSymbol: `${baseCurrency}/${quoteCurrency}`,
+      ticker,
+      spotEnabled: false,
+      marginEnabled: false,
+      derivativesEnabled: false,
+    }
+
+  }
+
+}
