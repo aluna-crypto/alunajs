1fe2e03ecd1c1490e2be3d2cd521dc1915b9ebdb
Torres-ssf <sergio.uft@gmail.com>
2021-05-27 20:26:32 -0300

    made place method not optional at IAlunaOrderModule

--- src/lib/modules/IAlunaOrderModule.ts
+++ src/lib/modules/IAlunaOrderModule.ts
@@ -29,7 +29,7 @@ export interface IAlunaOrderModule extends IAlunaModule {
 
   list (params?: IAlunaOrderListParams): Promise<IAlunaOrderSchema[]>
   get? (params?: IAlunaOrderGetParams): Promise<IAlunaOrderSchema>
-  place? (params: IAlunaOrderPlaceParams): Promise<IAlunaOrderSchema>
+  place (params: IAlunaOrderPlaceParams): Promise<IAlunaOrderSchema>
   parse (params: { rawOrder: any }): IAlunaOrderSchema
   parseMany (parms: { rawOrders: any[] }): IAlunaOrderSchema[]
 
