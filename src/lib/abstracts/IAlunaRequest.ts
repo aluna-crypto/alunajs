c41c385c5299ce2ded0e29e5d3282f46c429f877
Anderson Arboleya <anderson@arboleya.me>
2021-05-26 22:01:43 -0300

    Improving interface, adding flexibility w/ more options.

--- src/lib/abstracts/IAlunaRequest.ts
+++ src/lib/abstracts/IAlunaRequest.ts
@@ -8,15 +8,31 @@ export interface IAlunaRequestOptions {
   httpsAgent?: any
 }
 
-export interface IAlunaRequestParams {
+export interface IAlunaRequestPublicParams {
   url: string
   body?: any
   options?: IAlunaRequestOptions
-  keySecret?: IAlunaKeySecretSchema
+  keySecret?: IAlunaKeySecretSchema // optional
   settings?: IAlunaSettingsSchema
 }
 
+export interface IAlunaRequestPrivateParams extends IAlunaRequestPublicParams {
+  keySecret: IAlunaKeySecretSchema // required
+}
+
+
 export interface IAlunaRequest {
-  post<T>(params: IAlunaRequestParams): Promise<T>
-  get<T>(params: IAlunaRequestParams): Promise<T>
+
+  // public methods (if `keySecret` is passed, make it private)
+  post<T>(params: IAlunaRequestPublicParams): Promise<T>
+  get<T>(params: IAlunaRequestPublicParams): Promise<T>
+
+  // public methods (doesn't require `keySecret`)
+  publicPost<T>(params: IAlunaRequestPublicParams): Promise<T>
+  publicPet<T>(params: IAlunaRequestPublicParams): Promise<T>
+
+  // private methods (requires `keySecret`)
+  privatePost<T>(params: IAlunaRequestPrivateParams): Promise<T>
+  privateGet<T>(params: IAlunaRequestPrivateParams): Promise<T>
+
 }
