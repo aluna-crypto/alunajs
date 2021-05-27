d6d24bdec11bab6cf3b8382ae8cb5edbd51dd490
Torres-ssf <sergio.uft@gmail.com>
2021-05-27 20:46:42 -0300

    add listRaw method to IAlunaSymbolModule

--- src/lib/modules/IAlunaSymbolModule.ts
+++ src/lib/modules/IAlunaSymbolModule.ts
@@ -1,3 +1,7 @@
+import {
+  IValrSymbolSchema,
+} from 'src/exchanges/valr/lib/schemas/IValrSymbolSchema'
+
 import { IAlunaModule } from '@lib/abstracts/IAlunaModule'
 
 import { IAlunaSymbolSchema } from '../schemas/IAlunaSymbolSchema'
@@ -7,6 +11,7 @@ import { IAlunaSymbolSchema } from '../schemas/IAlunaSymbolSchema'
 export interface IAlunaSymbolModule extends IAlunaModule {
 
   list (): Promise<IAlunaSymbolSchema[]>
+  listRaw (): Promise<IValrSymbolSchema[]>
   get? (params: { id: string | number }): Promise<IAlunaSymbolSchema>
   parse (params: { rawSymbol: any }): IAlunaSymbolSchema
   parseMany (params: { rawSymbols: any[] }): IAlunaSymbolSchema[]
