ea92c96afb0d7eae4b6f0a0d48a810da9efc30ae
Torres-ssf <sergio.uft@gmail.com>
2021-05-27 09:55:11 -0300

    fix canceled ValrOrderStatusEnum

--- src/exchanges/valr/lib/enums/ValrOrderStatusEnum.ts
+++ src/exchanges/valr/lib/enums/ValrOrderStatusEnum.ts
@@ -2,7 +2,7 @@ export enum ValrOrderStatusEnum {
   ACTIVE = 'Active',
   PLACED = 'Placed',
   FILLED = 'Filled',
-  CANCELED = 'Cancelled',
+  CANCELED = 'Canceled',
   FAILED = 'Failed',
   PARTIALLY_FILLED = 'Partially Filled'
 }
