478e37322b8bc394678bb192399d1e1f334aefc6
Anderson Arboleya <anderson@arboleya.me>
2021-05-26 22:50:47 -0300

    Adding empty stub class.

--- src/lib/abstracts/AAlunaRequest.ts
+++ src/lib/abstracts/AAlunaRequest.ts
@@ -1,25 +1,11 @@
-import { IAlunaKeySecretSchema } from '@lib/schemas/IAlunaKeySecretSchema'
-import { IAlunaSettingsSchema } from '@lib/schemas/IAlunaSettingsSchema'
-
-
 
 export abstract class AAlunaRequest {
 
-  public readonly keySecret?: IAlunaKeySecretSchema
-
-  public readonly settings?: IAlunaSettingsSchema
-
-
-
   constructor (
-    params?: {
-      keySecret?: IAlunaKeySecretSchema
-      settings?: IAlunaSettingsSchema
-    },
+    _params?: {},
   ) {
 
-    this.keySecret = params?.keySecret
-    this.settings = params?.settings
+    // ...
 
   }
 
