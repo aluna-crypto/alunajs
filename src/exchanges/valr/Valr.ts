3d1c788f542a0e9e95ead83e5f2eb402eec51495
Anderson Arboleya <anderson@arboleya.me>
2021-05-26 22:44:38 -0300

    Updating all broken refs.

--- src/exchanges/valr/Valr.ts
+++ src/exchanges/valr/Valr.ts
@@ -8,11 +8,11 @@ import { IAlunaSymbolModule } from '@lib/modules/IAlunaSymbolModule'
 import { IAlunaKeySecretSchema } from '@lib/schemas/IAlunaKeySecretSchema'
 import { IAlunaSettingsSchema } from '@lib/schemas/IAlunaSettingsSchema'
 
-import { ValrBalance } from './lib/modules/balance/ValrBalance'
-import { ValrKey } from './lib/modules/key/ValrKey'
-import { ValrMarket } from './lib/modules/market/ValrMarket'
-import { ValrOrder } from './lib/modules/order/ValrOrder'
-import { ValrSymbol } from './lib/modules/symbol/ValrSymbol'
+import { ValrBalanceModule } from './lib/modules/ValrBalanceModule'
+import { ValrKeyModule } from './lib/modules/ValrKeyModule'
+import { ValrMarketModule } from './lib/modules/ValrMarketModule'
+import { ValrOrderModule } from './lib/modules/ValrOrderModule'
+import { ValrSymbolModule } from './lib/modules/ValrSymbolModule'
 import { ValrSpecs } from './lib/ValrSpecs'
 
 
@@ -42,12 +42,12 @@ export class Valr extends AAlunaExchange implements IAlunaExchange {
     this.keySecret = params.keySecret
     this.settings = params.settings
 
-    this.Symbol = new ValrSymbol({ exchange: this })
-    this.Market = new ValrMarket({ exchange: this })
+    this.Symbol = new ValrSymbolModule({ exchange: this })
+    this.Market = new ValrMarketModule({ exchange: this })
 
-    this.Key = new ValrKey({ exchange: this })
-    this.Balance = new ValrBalance({ exchange: this })
-    this.Order = new ValrOrder({ exchange: this })
+    this.Key = new ValrKeyModule({ exchange: this })
+    this.Balance = new ValrBalanceModule({ exchange: this })
+    this.Order = new ValrOrderModule({ exchange: this })
   }
 
 }
