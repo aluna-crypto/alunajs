c55d50a423afa38b9d6b9fb56a833b7d7f81d759
Anderson Arboleya <anderson@arboleya.me>
2021-05-26 22:52:16 -0300

    Moving file around.

---
+++ src/exchanges/valr/lib/ValrError.ts
@@ -0,0 +1,6 @@
+import { AAlunaError } from '../../../lib/abstracts/AAlunaError'
+import { IAlunaError } from '../../../lib/abstracts/IAlunaError'
+
+
+
+export class ValrError extends AAlunaError implements IAlunaError {}
--- src/exchanges/valr/lib/errors/ValrError.ts
+++
@@ -1,6 +0,0 @@
-import { AAlunaError } from '../../../../lib/abstracts/AAlunaError'
-import { IAlunaError } from '../../../../lib/abstracts/IAlunaError'
-
-
-
-export class ValrError extends AAlunaError implements IAlunaError {}
