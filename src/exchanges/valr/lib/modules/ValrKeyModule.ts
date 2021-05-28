40b94cbeb46d618a39c58b247fa45914b9a1bfd6
Torres-ssf <sergio.uft@gmail.com>
2021-05-28 19:22:36 -0300

    made ValrKeyModule conform with ValrHttp

--- src/exchanges/valr/lib/modules/ValrKeyModule.ts
+++ src/exchanges/valr/lib/modules/ValrKeyModule.ts
@@ -1,4 +1,5 @@
 import { AAlunaModule } from '@lib/abstracts/AAlunaModule'
+import { HttpVerbEnum } from '@lib/enums/HtttpVerbEnum'
 import { IAlunaKeyModule } from '@lib/modules/IAlunaKeyModule'
 import {
   IAlunaKeyPermissionSchema,
@@ -8,8 +9,8 @@ import { ValrErrorEnum } from '../enums/ValrErrorEnum'
 import { ValrOrderTimeInForceEnum } from '../enums/ValrOrderTimeInForceEnum'
 import { ValrSideEnum } from '../enums/ValrSideEnum'
 import { IValrKeySchema } from '../schemas/IValrKeySchema'
-import { IValrOrderSchema } from '../schemas/IValrOrderSchema'
-import { ValrRequest } from '../ValrRequest'
+import { IValrOrderListSchema } from '../schemas/IValrOrderSchema'
+import { ValrHttp } from '../ValrHttp'
 
 
 
@@ -27,8 +28,6 @@ export class ValrKeyModule extends AAlunaModule implements IAlunaKeyModule {
 
   public async getPermissions (): Promise<IAlunaKeyPermissionSchema> {
 
-    const request = new ValrRequest()
-
     const permissions = {
       read: false,
       trade: false,
@@ -37,7 +36,8 @@ export class ValrKeyModule extends AAlunaModule implements IAlunaKeyModule {
 
     try {
 
-      await request.get<IValrKeySchema>({
+      await ValrHttp.privateRequest<IValrKeySchema>({
+        verb: HttpVerbEnum.GET,
         url: 'https://api.valr.com/v1/account/balances',
         keySecret: this.exchange.keySecret,
       })
@@ -51,17 +51,16 @@ export class ValrKeyModule extends AAlunaModule implements IAlunaKeyModule {
        * This trick allow us to findout if the API key has permission to
        * create orders, since Valr does not provide a request for it
        */
-
-      await request.get<IValrOrderSchema>({
+      await ValrHttp.privateRequest<IValrOrderListSchema>({
         url: 'https://api.valr.com/v1/orders/limit',
-        body: JSON.stringify({
+        body: {
           side: ValrSideEnum.SELL,
           quantity: '0',
           price: '0',
           pair: 'NotACurrencyXX99',
           postOnly: false,
           timeInForce: ValrOrderTimeInForceEnum.GOOD_TILL_CANCELLED,
-        }),
+        },
         keySecret: this.exchange.keySecret,
       })
 
