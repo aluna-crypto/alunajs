26f0c83b8500c32cc249f27d893ef82b00d1eca9
Torres-ssf <sergio.uft@gmail.com>
2021-05-28 07:56:05 -0300

    add listRaw method to IAlunaMarketModule

--- src/lib/modules/IAlunaMarketModule.ts
+++ src/lib/modules/IAlunaMarketModule.ts
@@ -7,6 +7,7 @@ import { IAlunaMarketSchema } from '../schemas/IAlunaMarketSchema'
 export interface IAlunaMarketModule extends IAlunaModule {
 
   list (): Promise<IAlunaMarketSchema[]>
+  listRaw (): Promise<any[]>
   get? (params: { id: string | number }): Promise<IAlunaMarketSchema>
   parse (params: { rawMarket: any }): IAlunaMarketSchema
   parseMany (parms: { rawMarkets: any[] }): IAlunaMarketSchema[]
