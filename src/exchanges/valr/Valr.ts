b4130f0ea7f55252f293e560d3205efb8b6065f3
Torres-ssf <sergio.uft@gmail.com>
2021-05-28 19:38:38 -0300

    made keySecret required at Valr class

--- src/exchanges/valr/Valr.ts
+++ src/exchanges/valr/Valr.ts
@@ -32,8 +32,8 @@ export class Valr extends AAlunaExchange implements IAlunaExchange {
 
 
   constructor (
-    params?: {
-      keySecret?: IAlunaKeySecretSchema,
+    params: {
+      keySecret: IAlunaKeySecretSchema,
       settings?: IAlunaSettingsSchema,
     },
   ) {
