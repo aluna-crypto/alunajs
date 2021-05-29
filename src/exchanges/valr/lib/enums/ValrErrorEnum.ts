cd785d0383e65476ea6f0e572a45871ae506b36d
Torres-ssf <sergio.uft@gmail.com>
2021-05-28 21:26:32 -0300

    add error to ValrErrorEnum

--- src/exchanges/valr/lib/enums/ValrErrorEnum.ts
+++ src/exchanges/valr/lib/enums/ValrErrorEnum.ts
@@ -1,5 +1,6 @@
 export enum ValrErrorEnum {
   UNAUTHORIZED = 'Unauthorized',
+  INVALID_KEY = 'API key or secret is invalid',
   INVALID_REQUEST = 'Invalid Request, please check your request and try again',
-  INVALID_SIGNATURE = 'Request has an invalid signature'
+  INVALID_SIGNATURE = 'Request has an invalid signature',
 }
