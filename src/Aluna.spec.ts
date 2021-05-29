44a6fb3a40d42f1590bad3ae9445a6b3ed053bd3
Torres-ssf <sergio.uft@gmail.com>
2021-05-29 15:23:15 -0300

    comment Aluna.spec file

--- src/Aluna.spec.ts
+++ src/Aluna.spec.ts
@@ -1,41 +1,41 @@
-import { Aluna } from './Aluna'
+// import { Aluna } from './Aluna'
 
 
 
-/*
-  Matrix of all implemented exchanges
-*/
-console.log(Aluna.exchanges)
+// /*
+//   Matrix of all implemented exchanges
+// */
+// console.log(Aluna.exchanges)
 
 
 
-/*
-  Instantiating (via static resolver)
-*/
-export const valr1 = Aluna.new({
-  exchangeId: 'valr',
-  keySecret: {
-    key: 'asdf',
-    secret: 'asdf',
-  },
-})
+// /*
+//   Instantiating (via static resolver)
+// */
+// export const valr1 = Aluna.new({
+//   exchangeId: 'valr',
+//   keySecret: {
+//     key: 'asdf',
+//     secret: 'asdf',
+//   },
+// })
 
-valr1.Balance.list().then(console.log).catch(console.error)
-valr1.Order.list().then(console.log).catch(console.error)
-// valr1.Order.get({ id: 123 }).then(console.log).catch(console.error)
+// valr1.Balance.list().then(console.log).catch(console.error)
+// valr1.OrderRead.list().then(console.log).catch(console.error)
+// // valr1.Order.get({ id: 123 }).then(console.log).catch(console.error)
 
 
 
-/*
-  Instantiating (via static shortcut)
-*/
-export const valr2 = new Aluna.Valr({
-  keySecret: {
-    key: 'asdf',
-    secret: 'asdf',
-  },
-})
+// /*
+//   Instantiating (via static shortcut)
+// */
+// export const valr2 = new Aluna.Valr({
+//   keySecret: {
+//     key: 'asdf',
+//     secret: 'asdf',
+//   },
+// })
 
-valr2.Balance.list().then(console.log).catch(console.error)
-valr2.Order.list().then(console.log).catch(console.error)
-// valr2.Order.get({ id: 123 }).then(console.log).catch(console.error)
+// valr2.Balance.list().then(console.log).catch(console.error)
+// valr2.OrderRead.list().then(console.log).catch(console.error)
+// // valr2.Order.get({ id: 123 }).then(console.log).catch(console.error)
