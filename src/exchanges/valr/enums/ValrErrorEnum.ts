5f8c0e744def8701352f99a650722bbb7249e364
Torres-ssf <sergio.uft@gmail.com>
2021-05-30 16:41:58 -0300

    add error message to ValrErrorEnum

--- src/exchanges/valr/enums/ValrErrorEnum.ts
+++ src/exchanges/valr/enums/ValrErrorEnum.ts
@@ -3,4 +3,5 @@ export enum ValrErrorEnum {
   INVALID_KEY = 'API key or secret is invalid',
   INVALID_REQUEST = 'Invalid Request, please check your request and try again',
   INVALID_SIGNATURE = 'Request has an invalid signature',
+  INVALID_ORDER = 'Invalid Order. ' // it comes with an extra empty char
 }
