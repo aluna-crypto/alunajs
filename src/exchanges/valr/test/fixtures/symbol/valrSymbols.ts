0350da1d591e988ba4d22f1149f6833ef27d8503
Torres-ssf <sergio.uft@gmail.com>
2021-06-01 12:47:59 -0300

    add fixtures for Varl symbols

---
+++ src/exchanges/valr/test/fixtures/symbol/valrSymbols.ts
@@ -0,0 +1,48 @@
+import { IAlunaSymbolSchema } from '../../../../../lib/schemas/IAlunaSymbolSchema'
+import { IValrSymbolSchema } from '../../../schemas/IValrSymbolSchema'
+
+
+
+export const VALR_FETCH_SYMBOLS_RESPONSE: IValrSymbolSchema[] = [
+  {
+    symbol: 'R',
+    isActive: true,
+    shortName: 'ZAR',
+    longName: 'Rand',
+    decimalPlaces: 2,
+    withdrawalDecimalPlaces: 2,
+  },
+  {
+    symbol: 'BTC',
+    isActive: true,
+    shortName: 'BTC',
+    longName: 'Bitcoin',
+    decimalPlaces: 8,
+    withdrawalDecimalPlaces: 8,
+  },
+  {
+    symbol: 'ETH',
+    isActive: true,
+    shortName: 'ETH',
+    longName: 'Ethereum',
+    decimalPlaces: 18,
+    withdrawalDecimalPlaces: 8,
+  },
+]
+
+
+
+export const VALR_PARSED_SYMBOLS: IAlunaSymbolSchema[] = [
+  {
+    acronym: 'ZAR',
+    name: 'Rand',
+  },
+  {
+    acronym: 'BTC',
+    name: 'Bitcoin',
+  },
+  {
+    acronym: 'ETH',
+    name: 'Ethereum',
+  },
+]
