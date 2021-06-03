5f87f1ecb35611df80edcb154c5d9b6a58a28fe2
Torres-ssf <sergio.uft@gmail.com>
2021-06-03 10:55:54 -0300

    implement object with all valr seeds

---
+++ src/exchanges/valr/test/fixtures/index.ts
@@ -0,0 +1,9 @@
+import { VALR_MARKET_SEEDS } from './market'
+import { VALR_SYMBOL_SEEDS } from './symbol'
+
+
+
+export const VALR_SEEDS = {
+  marketsSeeds: VALR_MARKET_SEEDS,
+  symbolsSeeds: VALR_SYMBOL_SEEDS,
+}
