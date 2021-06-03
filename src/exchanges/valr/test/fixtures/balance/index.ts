79f9d60c474f5020d4eafe0117627ab67c195fa9
Torres-ssf <sergio.uft@gmail.com>
2021-06-03 13:17:04 -0300

    add Valr balance seeds to Valr seeds object

---
+++ src/exchanges/valr/test/fixtures/balance/index.ts
@@ -0,0 +1,11 @@
+import {
+  VALR_RAW_BALANCES,
+  VALR_PARSED_BALANCES,
+} from './valrBalance'
+
+
+
+export const VALR_BALANCE_SEEDS = {
+  rawBalances: VALR_RAW_BALANCES,
+  parsedBalances: VALR_PARSED_BALANCES,
+}
--- src/exchanges/valr/test/fixtures/index.ts
+++ src/exchanges/valr/test/fixtures/index.ts
@@ -1,3 +1,4 @@
+import { VALR_BALANCE_SEEDS } from './balance'
 import { VALR_MARKET_SEEDS } from './market'
 import { VALR_SYMBOL_SEEDS } from './symbol'
 
@@ -6,4 +7,5 @@ import { VALR_SYMBOL_SEEDS } from './symbol'
 export const VALR_SEEDS = {
   marketsSeeds: VALR_MARKET_SEEDS,
   symbolsSeeds: VALR_SYMBOL_SEEDS,
+  balanceSeeds: VALR_BALANCE_SEEDS,
 }
