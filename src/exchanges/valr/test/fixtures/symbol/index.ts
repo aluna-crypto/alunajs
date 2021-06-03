12b117d74f3c9c2b5482b7ca8931aee1dbd65f0f
Torres-ssf <sergio.uft@gmail.com>
2021-06-03 10:55:20 -0300

    implement object with valr symbol seeds

---
+++ src/exchanges/valr/test/fixtures/symbol/index.ts
@@ -0,0 +1,11 @@
+import {
+  VALR_RAW_SYMBOLS,
+  VALR_PARSED_SYMBOLS,
+} from './valrSymbols'
+
+
+
+export const VALR_SYMBOL_SEEDS = {
+  rawSymbols: VALR_RAW_SYMBOLS,
+  parsedSymbols: VALR_PARSED_SYMBOLS,
+}
