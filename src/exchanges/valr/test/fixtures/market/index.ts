a2905f4509fb7f68175a7be81e3529ee87d40865
Torres-ssf <sergio.uft@gmail.com>
2021-06-03 10:55:35 -0300

    implement object with valr market seeds

---
+++ src/exchanges/valr/test/fixtures/market/index.ts
@@ -0,0 +1,13 @@
+import {
+  VALR_RAW_MARKETS,
+  VALR_RAW_MARKETS_WITH_SYMBOLS,
+  VALR_PARSED_MARKETS,
+} from './valrMarket'
+
+
+
+export const VALR_MARKET_SEEDS = {
+  rawMarkets: VALR_RAW_MARKETS,
+  rawMarketWithSymbols: VALR_RAW_MARKETS_WITH_SYMBOLS,
+  parsedMarkets: VALR_PARSED_MARKETS,
+}
