8d7932ffff5fd4f2f999301a7a078c54a1ea0328
Anderson Arboleya <anderson@arboleya.me>
2021-06-07 08:47:38 -0300

    Attempting to split spec into two (static x instance).

--- src/lib/abstracts/IAlunaExchange.ts
+++ src/lib/abstracts/IAlunaExchange.ts
@@ -2,7 +2,8 @@ import { IAlunaBalanceModule } from '../modules/IAlunaBalanceModule'
 import { IAlunaKeyModule } from '../modules/IAlunaKeyModule'
 import { IAlunaMarketModule } from '../modules/IAlunaMarketModule'
 import {
-  IAlunaOrderReadModule, IAlunaOrderWriteModule,
+  IAlunaOrderReadModule,
+  IAlunaOrderWriteModule,
 } from '../modules/IAlunaOrderModule'
 import { IAlunaPositionModule } from '../modules/IAlunaPositionModule'
 import { IAlunaSymbolModule } from '../modules/IAlunaSymbolModule'
@@ -14,22 +15,33 @@ import { IAlunaSettingsSchema } from '../schemas/IAlunaSettingsSchema'
 
 export interface IAlunaExchange {
 
-  // constants
-  ID: string
-  SPECS: IAlunaExchangeSpecsSchema
-
   // basics
   keySecret: IAlunaKeySecretSchema
   settings?: IAlunaSettingsSchema
-
-  // modules (public)
-  Symbol: IAlunaSymbolModule
-  Market: IAlunaMarketModule
 
   // modules (private/signed)
   Key: IAlunaKeyModule
   Order: IAlunaOrderReadModule | IAlunaOrderWriteModule
   Balance: IAlunaBalanceModule
   Position?: IAlunaPositionModule
+
+}
+
+
+
+export interface IAlunaExchangeStatic {
+
+  // constants
+  ID: string
+  SPECS: IAlunaExchangeSpecsSchema
+
+  // modules (public)
+  Symbol: IAlunaSymbolModule
+  Market: IAlunaMarketModule
+
+  new (params: {
+    keySecret: IAlunaKeySecretSchema,
+    settings?: IAlunaSettingsSchema,
+  }): IAlunaExchange
 
 }
