73784e5863c08a8f8f2b7cfb3b45669038edb1c1
Torres-ssf <sergio.uft@gmail.com>
2021-06-07 23:19:04 -0300

    add ordersSeeds to VALR_SEEDS object

--- src/exchanges/valr/test/fixtures/index.ts
+++ src/exchanges/valr/test/fixtures/index.ts
@@ -1,5 +1,6 @@
 import { VALR_BALANCE_SEEDS } from './balance'
 import { VALR_MARKET_SEEDS } from './market'
+import { VALR_ORDERS_SEEDS } from './order'
 import { VALR_SYMBOL_SEEDS } from './symbol'
 
 
@@ -8,4 +9,5 @@ export const VALR_SEEDS = {
   marketsSeeds: VALR_MARKET_SEEDS,
   symbolsSeeds: VALR_SYMBOL_SEEDS,
   balanceSeeds: VALR_BALANCE_SEEDS,
+  ordersSeeds: VALR_ORDERS_SEEDS,
 }
