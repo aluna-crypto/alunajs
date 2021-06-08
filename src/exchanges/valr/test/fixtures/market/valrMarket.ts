679cad174debc9ff853288407091f2509c725542
Torres-ssf <sergio.uft@gmail.com>
2021-06-08 13:20:18 -0300

    add mock object for valr currency pairs

--- src/exchanges/valr/test/fixtures/market/valrMarket.ts
+++ src/exchanges/valr/test/fixtures/market/valrMarket.ts
@@ -1,5 +1,6 @@
 import { IAlunaMarketSchema } from '../../../../../lib/schemas/IAlunaMarketSchema'
 import { IMarketWithCurrency } from '../../../modules/ValrMarketModule'
+import { IValrCurrencyPairs } from '../../../schemas/IValrCurrencyPairs'
 import { IValrMarketSchema } from '../../../schemas/IValrMarketSchema'
 
 
@@ -45,7 +46,83 @@ export const VALR_RAW_MARKETS: IValrMarketSchema[] = [
 
 
 
-export const VALR_RAW_MARKETS_WITH_SYMBOLS: IMarketWithCurrency[] = [
+export const VALR_RAW_CURRENCY_PAIRS: IValrCurrencyPairs[] = [
+  {
+    symbol: 'BTCZAR',
+    baseCurrency: 'BTC',
+    quoteCurrency: 'ZAR',
+    shortName: 'BTC/ZAR',
+    active: true,
+    minBaseAmount: '0.0001',
+    maxBaseAmount: '9',
+    minQuoteAmount: '10',
+    maxQuoteAmount: '5000000',
+    tickSize: '1',
+    baseDecimalPlaces: '8',
+  },
+  {
+    symbol: 'ETHZAR',
+    baseCurrency: 'ETH',
+    quoteCurrency: 'ZAR',
+    shortName: 'ETH/ZAR',
+    active: true,
+    minBaseAmount: '0.001',
+    maxBaseAmount: '240',
+    minQuoteAmount: '10',
+    maxQuoteAmount: '5000000',
+    tickSize: '1',
+    baseDecimalPlaces: '8',
+  },
+  {
+    symbol: 'USDCETH',
+    baseCurrency: 'USDC',
+    quoteCurrency: 'ETH',
+    shortName: 'USDC/ETH',
+    active: true,
+    minBaseAmount: '3',
+    maxBaseAmount: '11510',
+    minQuoteAmount: '0.0079',
+    maxQuoteAmount: '30',
+    tickSize: '0.00000001',
+    baseDecimalPlaces: '8',
+  },
+
+  /**
+   * The next 2 currency pairs have not market related to it. They will be
+   * ignored by the ValrCurrencyPairsParser
+   */
+
+  {
+    symbol: 'XMRETH',
+    baseCurrency: 'XMR',
+    quoteCurrency: 'ETH',
+    shortName: 'XMR/ETH',
+    active: false,
+    minBaseAmount: '0.0189',
+    maxBaseAmount: '64',
+    minQuoteAmount: '0.0075',
+    maxQuoteAmount: '25',
+    tickSize: '0.00000001',
+    baseDecimalPlaces: '8',
+  },
+  {
+    symbol: 'GRINBTC',
+    baseCurrency: 'GRIN',
+    quoteCurrency: 'BTC',
+    shortName: 'GRIN/BTC',
+    active: false,
+    minBaseAmount: '3.8',
+    maxBaseAmount: '7194',
+    minQuoteAmount: '0.00051',
+    maxQuoteAmount: '1',
+    tickSize: '0.00000001',
+    baseDecimalPlaces: '8',
+  },
+]
+
+
+
+export const VALR_RAW_MARKETS_WITH_CURRENCY: IMarketWithCurrency[] = [
   {
     currencyPair: 'USDCETH',
     askPrice: '0.00039334',
