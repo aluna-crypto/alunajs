35623ebb27043e5bfa13c672b73bade7e6e66ac5
Torres-ssf <sergio.uft@gmail.com>
2021-06-07 23:13:34 -0300

    add setupFilesAfterEnv to jest config file

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['<rootDir>/src/**/*.spec.ts'],
+  setupFilesAfterEnv: ['<rootDir>/test/hooks.ts'],
   collectCoverage: true,
   collectCoverageFrom: [
     "<rootDir>/src/**/*.ts",
