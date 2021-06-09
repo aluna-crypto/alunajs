8c56e69a37efbc75e45f9abce8b59fe11347ea72
Torres-ssf <sergio.uft@gmail.com>
2021-06-09 15:29:57 -0300

    implement test for place market orders

--- src/exchanges/valr/modules/ValrOrderWriteModule.spec.ts
+++ src/exchanges/valr/modules/ValrOrderWriteModule.spec.ts
@@ -16,18 +16,107 @@ describe('ValrOrderWriteModule', () => {
 
   const valrOrderWriteModule = ValrOrderWriteModule.prototype
 
+  const keySecret = {
+    key: '',
+    secret: '',
+  }
+
+  const placedOrderId = 'placed-order-id'
+
+  const placedOrder = 'placed-order'
+
 
 
   it('should place a new Valr limit order just fine', async () => {
 
-    const keySecret = {
-      key: '',
-      secret: '',
+    ImportMock.mockOther(
+      valrOrderWriteModule,
+      'exchange',
+      { keySecret } as IAlunaExchange,
+    )
+
+    const requestMock = ImportMock.mockFunction(
+      ValrHttp,
+      'privateRequest',
+      { id: placedOrderId },
+    )
+
+    const getMock = ImportMock.mockFunction(
+      valrOrderWriteModule,
+      'get',
+      placedOrder,
+    )
+
+    const placeOrderParams = {
+      amount: '0.001',
+      rate: '10000',
+      symbolPair: 'ETHZAR',
+      side: SideEnum.LONG,
+      type: OrderTypesEnum.LIMIT,
+      account: AccountEnum.EXCHANGE,
     }
 
-    const placedOrderId = 'placed-order-id'
+    const requestBody = {
+      side: ValrSideEnum.BUY,
+      pair: placeOrderParams.symbolPair,
+      quantity: placeOrderParams.amount,
+      price: placeOrderParams.rate,
+      postOnly: false,
+      timeInForce: ValrOrderTimeInForceEnum.GOOD_TILL_CANCELLED,
+    }
 
-    const placedOrder = 'placed-order'
+
+    // place long limit order
+    const placeResponse1 = await valrOrderWriteModule.place(placeOrderParams)
+
+
+    expect(requestMock.callCount).to.be.eq(1)
+    expect(requestMock.calledWith({
+      url: 'https://api.valr.com/v1/orders/limit',
+      body: requestBody,
+      keySecret,
+    })).to.be.true
+
+
+    expect(getMock.callCount).to.be.eq(1)
+    expect(getMock.calledWith({
+      id: placedOrderId,
+      symbolPair: placeOrderParams.symbolPair,
+    })).to.be.true
+
+    expect(placeResponse1).to.deep.eq(getMock.returnValues[0])
+
+
+    // place short limit order
+    const placeResponse2 = await valrOrderWriteModule.place({
+      ...placeOrderParams,
+      side: SideEnum.SHORT,
+    })
+
+
+    expect(requestMock.callCount).to.be.eq(2)
+    expect(requestMock.calledWith({
+      url: 'https://api.valr.com/v1/orders/limit',
+      body: {
+        ...requestBody,
+        side: ValrSideEnum.SELL,
+      },
+      keySecret,
+    })).to.be.true
+
+    expect(getMock.callCount).to.be.eq(2)
+    expect(getMock.calledWith({
+      id: placedOrderId,
+      symbolPair: placeOrderParams.symbolPair,
+    })).to.be.true
+
+    expect(placeResponse2).to.deep.eq(getMock.returnValues[1])
+
+  })
+
+
+
+  it('should place a new Valr market order just fine', async () => {
 
     ImportMock.mockOther(
       valrOrderWriteModule,
@@ -49,38 +138,64 @@ describe('ValrOrderWriteModule', () => {
 
     const params = {
       amount: '0.001',
-      rate: '10000',
+      rate: '0',
       symbolPair: 'ETHZAR',
       side: SideEnum.LONG,
-      type: OrderTypesEnum.LIMIT,
+      type: OrderTypesEnum.MARKET,
       account: AccountEnum.EXCHANGE,
     }
 
+    const requestBody = {
+      side: ValrSideEnum.BUY,
+      pair: params.symbolPair,
+      baseAmount: params.amount,
+    }
 
-    const placeResponse = await valrOrderWriteModule.place(params)
+
+    // place long market order
+    const placeResponse1 = await valrOrderWriteModule.place(params)
 
 
-    expect(requestMock.calledOnce).to.be.true
+    expect(requestMock.callCount).to.be.eq(1)
     expect(requestMock.calledWith({
-      url: 'https://api.valr.com/v1/orders/limit',
-      body: {
-        side: ValrSideEnum.BUY,
-        pair: params.symbolPair,
-        quantity: params.amount,
-        price: params.rate,
-        postOnly: false,
-        timeInForce: ValrOrderTimeInForceEnum.GOOD_TILL_CANCELLED,
-      },
+      url: 'https://api.valr.com/v1/orders/market',
+      body: requestBody,
       keySecret,
     })).to.be.true
 
-    expect(getMock.calledOnce).to.be.true
+    expect(getMock.callCount).to.be.eq(1)
     expect(getMock.calledWith({
       id: placedOrderId,
       symbolPair: params.symbolPair,
     })).to.be.true
 
-    expect(placeResponse).to.deep.eq(getMock.returnValues[0])
+    expect(placeResponse1).to.deep.eq(getMock.returnValues[0])
+
+
+    // place short market order
+    const placeResponse2 = await valrOrderWriteModule.place({
+      ...params,
+      side: SideEnum.SHORT,
+    })
+
+
+    expect(requestMock.callCount).to.be.eq(2)
+    expect(requestMock.calledWith({
+      url: 'https://api.valr.com/v1/orders/market',
+      body: {
+        ...requestBody,
+        side: ValrSideEnum.SELL,
+      },
+      keySecret,
+    })).to.be.true
+
+    expect(getMock.callCount).to.be.eq(2)
+    expect(getMock.calledWith({
+      id: placedOrderId,
+      symbolPair: params.symbolPair,
+    })).to.be.true
+
+    expect(placeResponse2).to.deep.eq(getMock.returnValues[1])
 
   })
 
