db2d462e9b89e823b8f051047c4a4db41bbab6ca
Torres-ssf <sergio.uft@gmail.com>
2021-06-08 22:03:52 -0300

    implement test for ValrOrderTypeAdapter

---
+++ src/exchanges/valr/enums/adapters/ValrOrderTypeAdapter.spec.ts
@@ -0,0 +1,95 @@
+import { expect } from 'chai'
+
+import { OrderTypesEnum } from '../../../../lib/enums/OrderTypeEnum'
+import { ValrError } from '../../ValrError'
+import { ValrOrderTypesEnum } from '../ValrOrderTypesEnum'
+import { ValrOrderTypeAdapter } from './ValrOrderTypeAdapter'
+
+
+
+describe('ValrOrderTypeAdapter', () => {
+
+  const notSupported = 'not-supported'
+
+
+
+  it('should properly translate Valr order types to Aluna order types', () => {
+
+    expect(ValrOrderTypeAdapter.translateToAluna({
+      type: ValrOrderTypesEnum.LIMIT,
+    })).to.be.eq(OrderTypesEnum.LIMIT)
+
+    expect(ValrOrderTypeAdapter.translateToAluna({
+      type: ValrOrderTypesEnum.LIMIT_POST_ONLY,
+    })).to.be.eq(OrderTypesEnum.LIMIT)
+
+    expect(ValrOrderTypeAdapter.translateToAluna({
+      type: ValrOrderTypesEnum.MARKET,
+    })).to.be.eq(OrderTypesEnum.MARKET)
+
+    expect(ValrOrderTypeAdapter.translateToAluna({
+      type: ValrOrderTypesEnum.SIMPLE,
+    })).to.be.eq(OrderTypesEnum.MARKET)
+
+    expect(ValrOrderTypeAdapter.translateToAluna({
+      type: ValrOrderTypesEnum.STOP_LOSS_LIMIT,
+    })).to.be.eq(OrderTypesEnum.STOP_LIMIT)
+
+    expect(ValrOrderTypeAdapter.translateToAluna({
+      type: ValrOrderTypesEnum.TAKE_PROFIT_LIMIT,
+    })).to.be.eq(OrderTypesEnum.TAKE_PROFIT_LIMIT)
+
+
+    try {
+
+      ValrOrderTypeAdapter.translateToAluna({
+        type: notSupported as ValrOrderTypesEnum,
+      })
+
+    } catch (err) {
+
+      expect(err instanceof ValrError).to.be.true
+      expect(err.message).to.be.eq(`Order type not supported: ${notSupported}`)
+
+    }
+
+
+  })
+
+
+
+  it('should properly translate Aluna order types to Valr order types', () => {
+
+    expect(ValrOrderTypeAdapter.translateToValr({
+      type: OrderTypesEnum.LIMIT,
+    })).to.be.eq(ValrOrderTypesEnum.LIMIT)
+
+    expect(ValrOrderTypeAdapter.translateToValr({
+      type: OrderTypesEnum.MARKET,
+    })).to.be.eq(ValrOrderTypesEnum.MARKET)
+
+    expect(ValrOrderTypeAdapter.translateToValr({
+      type: OrderTypesEnum.STOP_LIMIT,
+    })).to.be.eq(ValrOrderTypesEnum.STOP_LOSS_LIMIT)
+
+    expect(ValrOrderTypeAdapter.translateToValr({
+      type: OrderTypesEnum.TAKE_PROFIT_LIMIT,
+    })).to.be.eq(ValrOrderTypesEnum.TAKE_PROFIT_LIMIT)
+
+
+    try {
+
+      ValrOrderTypeAdapter.translateToValr({
+        type: notSupported as OrderTypesEnum,
+      })
+
+    } catch (err) {
+
+      expect(err instanceof ValrError).to.be.true
+      expect(err.message).to.be.eq(`Order type not supported: ${notSupported}`)
+
+    }
+
+  })
+
+})
