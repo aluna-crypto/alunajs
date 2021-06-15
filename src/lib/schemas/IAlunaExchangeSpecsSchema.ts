9a630bd70a6b66d2fff7b1802f73bbd7927d643a
Anderson Arboleya <anderson@arboleya.me>
2021-06-15 08:24:53 -0300

    Improving exchange readability, refactoring enums usage.

--- src/lib/schemas/IAlunaExchangeSpecsSchema.ts
+++ src/lib/schemas/IAlunaExchangeSpecsSchema.ts
@@ -4,26 +4,30 @@ import { AlunaOrderTypesEnum } from '../enums/AlunaOrderTypesEnum'
 
 
 
-export interface IAlunaExchangeOrderTypesSpecsSchema {
-  [AlunaOrderTypesEnum.LIMIT]?: IAlunaExchangeOrderSpecsSchema
-  [AlunaOrderTypesEnum.MARKET]?: IAlunaExchangeOrderSpecsSchema
-  [AlunaOrderTypesEnum.STOP_MARKET]?: IAlunaExchangeOrderSpecsSchema
-  [AlunaOrderTypesEnum.STOP_LIMIT]?: IAlunaExchangeOrderSpecsSchema
-
-  [AlunaOrderTypesEnum.TRAILING_STOP]?: IAlunaExchangeOrderSpecsSchema
-
-  [AlunaOrderTypesEnum.FILL_OF_KILL]?: IAlunaExchangeOrderSpecsSchema
-  [AlunaOrderTypesEnum.IMMEDIATE_OR_CANCEL]?: IAlunaExchangeOrderSpecsSchema
-  [AlunaOrderTypesEnum.LIMIT_ORDER_BOOK]?: IAlunaExchangeOrderSpecsSchema
-
-  [AlunaOrderTypesEnum.TAKE_PROFIT_LIMIT]?: IAlunaExchangeOrderSpecsSchema
-  [AlunaOrderTypesEnum.TAKE_PROFIT_MARKET]?: IAlunaExchangeOrderSpecsSchema
+export interface IAlunaExchangeSpecsSchema {
+  id: string
+  acceptFloatAmounts: boolean
+  features: {
+    balance: AlunaFeaturesModeEnum,
+    order: AlunaFeaturesModeEnum,
+    position?: AlunaFeaturesModeEnum,
+  }
+  accounts: IAlunaExchangeAccountSpecsSchema[]
 }
 
 export interface IAlunaExchangeAccountSpecsSchema {
+  type: AlunaAccountEnum
   supported: boolean // supported by the exchange
   implemented?: boolean // implemented by aluna
-  orderTypes?: IAlunaExchangeOrderTypesSpecsSchema
+  orderTypes?: IAlunaExchangeOrderSpecsSchema[]
+}
+
+export interface IAlunaExchangeOrderSpecsSchema {
+  type: AlunaOrderTypesEnum
+  supported: boolean // supported by the exchange
+  implemented: boolean // implemented by aluna
+  mode: AlunaFeaturesModeEnum
+  options: IAlunaExchangeOrderOptionsSchema
 }
 
 export interface IAlunaExchangeOrderOptionsSchema {
@@ -34,42 +38,20 @@ export interface IAlunaExchangeOrderOptionsSchema {
   leverage?: number
 
   /*
-    Options not yet implemented
+    Options not yet implemented:
+
+    trailingRate?: number
+    triggerRate?: number
+    distance?: number
+    trigger?: string | TriggerEnum | TriggerEnum[]
+    closeOnTrigger?: boolean
+    oco?: number
+    hidden?: boolean
+    postOnly?: boolean
+    reduceOnly?: boolean
+    tif?: boolean | string | TimeInForceEnum | TimeInForceEnum[]
+    understandConfirmation?: boolean
+
   */
 
-  // trailingRate?: number
-  // triggerRate?: number
-  // distance?: number
-  // trigger?: string | TriggerEnum | TriggerEnum[]
-  // closeOnTrigger?: boolean
-  // oco?: number
-  // hidden?: boolean
-  // postOnly?: boolean
-  // reduceOnly?: boolean
-  // tif?: boolean | string | TimeInForceEnum | TimeInForceEnum[]
-  // understandConfirmation?: boolean
-
-}
-
-export interface IAlunaExchangeOrderSpecsSchema {
-  supported: boolean // supported by the exchange
-  implemented: boolean // implemented by aluna
-  mode: AlunaFeaturesModeEnum
-  options: IAlunaExchangeOrderOptionsSchema
-}
-
-export interface IAlunaExchangeSpecsSchema {
-  id: string
-  acceptFloatAmounts: boolean
-  features: {
-    balance: AlunaFeaturesModeEnum,
-    order: AlunaFeaturesModeEnum,
-    position?: AlunaFeaturesModeEnum,
-  }
-  accounts: {
-    [AlunaAccountEnum.EXCHANGE]: IAlunaExchangeAccountSpecsSchema,
-    [AlunaAccountEnum.MARGIN]: IAlunaExchangeAccountSpecsSchema,
-    [AlunaAccountEnum.DERIVATIVES]: IAlunaExchangeAccountSpecsSchema,
-    [AlunaAccountEnum.LENDING]: IAlunaExchangeAccountSpecsSchema,
-  }
 }
