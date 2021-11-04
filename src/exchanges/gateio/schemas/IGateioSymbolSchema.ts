b0edb818f5d6d01b9558b74b7a94fe4a1803bd84
Izaias Neto <izaias@aluna.social>
2021-11-04 15:21:33 -0300

    Raw symbol schema.

--- src/exchanges/gateio/schemas/IGateioSymbolSchema.ts
+++ src/exchanges/gateio/schemas/IGateioSymbolSchema.ts
@@ -1,3 +1,8 @@
 export interface IGateioSymbolSchema {
-  // TODO implement me
+  currency: string
+  delisted: boolean
+  withdraw_disabled: boolean
+  withdraw_delayed: boolean
+  deposit_disabled: boolean
+  trade_disabled: boolean
 }
