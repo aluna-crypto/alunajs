8c6288d467fc34ce11ec9fb2b6c0425912518eab
Izaias Neto <izaias@aluna.social>
2021-11-08 17:24:31 -0300

    Created market and ticker schema.

--- src/exchanges/gateio/schemas/IGateioMarketSchema.ts
+++ src/exchanges/gateio/schemas/IGateioMarketSchema.ts
@@ -1,3 +1,23 @@
-export interface IGateioMarketSchema {
-  // TODO implement me
+import { IGateioCurrencyPairs } from './IGateioCurrencyPair'
+
+
+
+export interface IGateioMarketSchema extends IGateioCurrencyPairs {
+  ticker: IGateioTickerSchema
+}
+
+export interface IGateioTickerSchema {
+  currency_pair: string
+  last: string
+  lowest_ask: string
+  highest_bid: string
+  change_percentage: string
+  base_volume: string
+  quote_volume: string
+  high_24h: string
+  low_24h: string
+  etf_net_value: string
+  etf_pre_net_value: string
+  etf_pre_timestamp: number
+  etf_leverage: string
 }
