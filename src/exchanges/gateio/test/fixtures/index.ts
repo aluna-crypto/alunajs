c79e6ab93b5469263e12409b0fce3d043be0bd9c
Izaias Neto <izaias@aluna.social>
2021-11-09 08:18:24 -0300

    Exporting market seeds.

--- src/exchanges/gateio/test/fixtures/index.ts
+++ src/exchanges/gateio/test/fixtures/index.ts
@@ -1,7 +1,9 @@
+import { GATEIO_MARKET_SEEDS } from './market'
 import { GATEIO_SYMBOL_SEEDS } from './symbol'
 
 
 
 export const GATEIO_SEEDS = {
   symbolsSeeds: GATEIO_SYMBOL_SEEDS,
+  marketsSeeds: GATEIO_MARKET_SEEDS,
 }
---
+++ src/exchanges/gateio/test/fixtures/market/index.ts
@@ -0,0 +1,11 @@
+import {
+  GATEIO_PARSED_MARKETS,
+  GATEIO_RAW_MARKETS,
+} from './gateioMarket'
+
+
+
+export const GATEIO_MARKET_SEEDS = {
+  rawMarkets: GATEIO_RAW_MARKETS,
+  parsedMarkets: GATEIO_PARSED_MARKETS,
+}
