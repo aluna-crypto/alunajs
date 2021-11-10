64b15f69c80e5343a519ba800c94fbefc1dc4889
Izaias Neto <izaias@aluna.social>
2021-11-10 07:32:27 -0300

    Key schema.

--- src/exchanges/gate.io/schemas/IGateIOKeySchema.ts
+++ src/exchanges/gate.io/schemas/IGateIOKeySchema.ts
@@ -1,3 +1,21 @@
 export interface IGateIOKeySchema {
-  // TODO implement me
+  label: string
+  permissions: IGateIOKeyPermissions
+  addedAt: string
+}
+
+export interface IGateIOKeyPermissions {
+  spot: GateIOApiKeyPermissions
+  margin: GateIOApiKeyPermissions
+  wallet: GateIOApiKeyPermissions
+}
+
+export enum GateIOApiKeyPermissions {
+  READ_ONLY = 'Read-only',
+  READ_WRITE = 'Read-write',
+  WITHDRAW = 'Withdraw',
+}
+export interface IGateIOKeyAllowedWithdrawAddressList {
+  currency: string
+  address: string
 }
