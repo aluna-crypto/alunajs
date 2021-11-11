15a342affa6fdb1fa59e9f6a3f916a77a9aa692d
Izaias Neto <izaias@aluna.social>
2021-11-11 09:27:14 -0300

    Added missing line breaks.

--- src/exchanges/gate.io/schemas/IGateIOKeySchema.ts
+++ src/exchanges/gate.io/schemas/IGateIOKeySchema.ts
@@ -4,17 +4,24 @@ export interface IGateIOKeySchema {
   addedAt: string
 }
 
+
+
 export interface IGateIOKeyPermissions {
   spot: GateIOApiKeyPermissions
   margin: GateIOApiKeyPermissions
   wallet: GateIOApiKeyPermissions
 }
 
+
+
 export enum GateIOApiKeyPermissions {
   READ_ONLY = 'Read-only',
   READ_WRITE = 'Read-write',
   WITHDRAW = 'Withdraw',
 }
+
+
+
 export interface IGateIOKeyAllowedWithdrawAddressList {
   currency: string
   address: string
