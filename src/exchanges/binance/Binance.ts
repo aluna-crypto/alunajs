5a98574fc3ad3699e182655b634332acc32d9a49
LucianoPierdona <lucianompj1@gmail.com>
2021-11-24 11:34:40 -0300

    Start Binance Implementation

---
+++ src/exchanges/binance/Binance.ts
@@ -0,0 +1,11 @@
+// import {
+//   IAlunaExchange,
+//   IAlunaExchangeStatic,
+// } from '../../index'
+// import { AAlunaExchange } from '../../lib/core/abstracts/AAlunaExchange'
+
+
+
+// export const Binance: IAlunaExchangeStatic = class extends AAlunaExchange implements IAlunaExchange {
+
+// }
