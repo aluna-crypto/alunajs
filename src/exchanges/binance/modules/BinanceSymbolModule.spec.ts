e1df3eec81bca31f5775a4db97509a0047c779f4
LucianoPierdona <lucianompj1@gmail.com>
2021-11-25 10:13:12 -0300

    Added tests for BinanceSymbols and updated binanceSymbols fixtures

---
+++ src/exchanges/binance/modules/BinanceSymbolModule.spec.ts
@@ -0,0 +1,145 @@
+import { expect } from 'chai'
+import { ImportMock } from 'ts-mock-imports'
+
+import { Binance } from '../Binance'
+import { BinanceHttp } from '../BinanceHttp'
+import {
+  BINANCE_PARSED_SYMBOLS,
+  BINANCE_RAW_SYMBOLS,
+} from '../test/fixtures/binanceSymbols'
+import { BinanceSymbolModule } from './BinanceSymbolModule'
+
+
+
+describe('BinanceSymbolModule', () => {
+
+
+  it('should list Binance raw symbols just fine', async () => {
+
+    const requestMock = ImportMock.mockFunction(
+      BinanceHttp,
+      'publicRequest',
+      Promise.resolve(BINANCE_RAW_SYMBOLS),
+    )
+
+    const rawSymbols = await BinanceSymbolModule.listRaw()
+
+    expect(rawSymbols.length).to.eq(3)
+    expect(rawSymbols).to.deep.eq(BINANCE_RAW_SYMBOLS.symbols)
+
+    expect(rawSymbols[0].symbol).to.be.eq(BINANCE_RAW_SYMBOLS.symbols[0].symbol)
+    expect(rawSymbols[0].baseAsset).to.be.eq(BINANCE_RAW_SYMBOLS.symbols[0].baseAsset)
+    expect(rawSymbols[0].quoteAsset).to.be.eq(BINANCE_RAW_SYMBOLS.symbols[0].quoteAsset)
+
+    expect(rawSymbols[1].symbol).to.be.eq(BINANCE_RAW_SYMBOLS.symbols[1].symbol)
+    expect(rawSymbols[1].baseAsset).to.be.eq(BINANCE_RAW_SYMBOLS.symbols[1].baseAsset)
+    expect(rawSymbols[1].quoteAsset).to.be.eq(BINANCE_RAW_SYMBOLS.symbols[1].quoteAsset)
+
+    expect(rawSymbols[2].symbol).to.be.eq(BINANCE_RAW_SYMBOLS.symbols[2].symbol)
+    expect(rawSymbols[2].baseAsset).to.be.eq(BINANCE_RAW_SYMBOLS.symbols[2].baseAsset)
+    expect(rawSymbols[2].quoteAsset).to.be.eq(BINANCE_RAW_SYMBOLS.symbols[2].quoteAsset)
+
+    expect(requestMock.callCount).to.be.eq(1)
+
+  })
+
+
+
+  it('should list Binance parsed symbols just fine', async () => {
+
+    const mockedRawSymbols = 'raw-symbols'
+
+    const listRawMock = ImportMock.mockFunction(
+      BinanceSymbolModule,
+      'listRaw',
+      Promise.resolve(mockedRawSymbols),
+    )
+
+    const parseManyMock = ImportMock.mockFunction(
+      BinanceSymbolModule,
+      'parseMany',
+      BINANCE_PARSED_SYMBOLS,
+    )
+
+
+    const rawSymbols = await BinanceSymbolModule.list()
+
+    expect(rawSymbols.length).to.eq(3)
+    expect(rawSymbols).to.deep.eq(BINANCE_PARSED_SYMBOLS)
+
+    expect(rawSymbols[0].exchangeId).to.be.eq(Binance.ID)
+    expect(rawSymbols[0].id).to.be.eq(BINANCE_PARSED_SYMBOLS[0].id)
+
+    expect(rawSymbols[1].exchangeId).to.be.eq(Binance.ID)
+    expect(rawSymbols[1].id).to.be.eq(BINANCE_PARSED_SYMBOLS[1].id)
+    
+    expect(rawSymbols[2].exchangeId).to.be.eq(Binance.ID)
+    expect(rawSymbols[2].id).to.be.eq(BINANCE_PARSED_SYMBOLS[2].id)
+
+    expect(listRawMock.callCount).to.eq(1)
+
+    expect(parseManyMock.callCount).to.eq(1)
+    expect(parseManyMock.calledWith({
+      rawSymbols: mockedRawSymbols,
+    })).to.be.ok
+
+  })
+
+
+
+  it('should parse a Binance symbol just fine', async () => {
+
+    const parsedSymbol1 = BinanceSymbolModule.parse({
+      rawSymbol: BINANCE_RAW_SYMBOLS.symbols[1],
+    })
+
+    expect(parsedSymbol1.exchangeId).to.be.eq(Binance.ID)
+    expect(parsedSymbol1.id).to.be.eq(BINANCE_RAW_SYMBOLS.symbols[1].baseAsset)
+
+    const parsedSymbol2 = BinanceSymbolModule.parse({
+      rawSymbol: BINANCE_RAW_SYMBOLS.symbols[2],
+    })
+
+    expect(parsedSymbol2.exchangeId).to.be.eq(Binance.ID)
+    expect(parsedSymbol2.id).to.be.eq(BINANCE_RAW_SYMBOLS.symbols[2].baseAsset)
+
+  })
+
+
+
+  it('should parse many Binance symbols just fine', async () => {
+
+    const parseMock = ImportMock.mockFunction(
+      BinanceSymbolModule,
+      'parse',
+    )
+
+    const rawSymbol = {}
+
+    parseMock
+      .onFirstCall()
+      .returns(BINANCE_PARSED_SYMBOLS[0])
+      .onSecondCall()
+      .returns(BINANCE_PARSED_SYMBOLS[1])
+      .onThirdCall()
+      .returns(BINANCE_PARSED_SYMBOLS[2])
+
+    const parsedSymbols = BinanceSymbolModule.parseMany({
+      rawSymbols: [rawSymbol, rawSymbol, rawSymbol],
+    })
+
+    expect(parsedSymbols[0].exchangeId).to.be.eq(Binance.ID)
+    expect(parsedSymbols[0].id).to.be.eq(BINANCE_PARSED_SYMBOLS[0].id)
+
+    expect(parsedSymbols[1].exchangeId).to.be.eq(Binance.ID)
+    expect(parsedSymbols[1].id).to.be.eq(BINANCE_PARSED_SYMBOLS[1].id)
+
+    expect(parsedSymbols[2].exchangeId).to.be.eq(Binance.ID)
+    expect(parsedSymbols[2].id).to.be.eq(BINANCE_PARSED_SYMBOLS[2].id)
+
+    expect(parseMock.callCount).to.be.eq(3)
+    expect(parseMock.calledWith({ rawSymbol }))
+
+  })
+
+})
--- src/exchanges/binance/test/fixtures/binanceSymbols.ts
+++ src/exchanges/binance/test/fixtures/binanceSymbols.ts
@@ -2,7 +2,7 @@ import { IBinanceSymbolSchema } from '../../schemas/IBinanceSymbolSchema'
 
 
 
-export const BINANCE_RAW_SYMBOLS_INFO: IBinanceSymbolSchema = {
+export const BINANCE_RAW_SYMBOLS: IBinanceSymbolSchema = {
   timezone: "UTC",
   serverTime: 1637841089806,
   rateLimits: [
@@ -24,11 +24,11 @@ export const BINANCE_RAW_SYMBOLS_INFO: IBinanceSymbolSchema = {
   exchangeFilters: [],
   symbols: [
     {
-      symbol: "BNBUSDT",
+      symbol: "ETHBTC",
       status: "TRADING",
-      baseAsset: "BNB",
+      baseAsset: "ETH",
       baseAssetPrecision: 8,
-      quoteAsset: "USDT",
+      quoteAsset: "BTC",
       quotePrecision: 8,
       quoteAssetPrecision: 8,
       baseCommissionPrecision: 8,
@@ -48,9 +48,68 @@ export const BINANCE_RAW_SYMBOLS_INFO: IBinanceSymbolSchema = {
       filters: [
         {
           filterType: "PRICE_FILTER",
-          minPrice: "0.10000000",
+          minPrice: "0.00000100",
+          maxPrice: "922327.00000000",
+          tickSize: "0.00000100",
+        },
+        {
+          filterType: "PERCENT_PRICE",
+          multiplierUp: "5",
+          multiplierDown: "0.2",
+          avgPriceMins: 5,
+        },
+        {
+          filterType: "LOT_SIZE",
+          minQty: "0.00010000",
+          maxQty: "100000.00000000",
+          stepSize: "0.00010000",
+        },
+        {
+          filterType: "MIN_NOTIONAL",
+          minNotional: "0.00010000",
+          applyToMarket: true,
+          avgPriceMins: 5,
+        },
+        { filterType: "ICEBERG_PARTS", limit: 10 },
+        {
+          filterType: "MARKET_LOT_SIZE",
+          minQty: "0.00000000",
+          maxQty: "1010.54961473",
+          stepSize: "0.00000000",
+        },
+        { filterType: "MAX_NUM_ORDERS", maxNumOrders: 200 },
+        { filterType: "MAX_NUM_ALGO_ORDERS", maxNumAlgoOrders: 5 },
+      ],
+      permissions: ["SPOT", "MARGIN"],
+    },
+    {
+      symbol: "LTCBTC",
+      status: "TRADING",
+      baseAsset: "LTC",
+      baseAssetPrecision: 8,
+      quoteAsset: "BTC",
+      quotePrecision: 8,
+      quoteAssetPrecision: 8,
+      baseCommissionPrecision: 8,
+      quoteCommissionPrecision: 8,
+      orderTypes: [
+        "LIMIT",
+        "LIMIT_MAKER",
+        "MARKET",
+        "STOP_LOSS_LIMIT",
+        "TAKE_PROFIT_LIMIT",
+      ],
+      icebergAllowed: true,
+      ocoAllowed: true,
+      quoteOrderQtyMarketAllowed: true,
+      isSpotTradingAllowed: true,
+      isMarginTradingAllowed: true,
+      filters: [
+        {
+          filterType: "PRICE_FILTER",
+          minPrice: "0.00000100",
           maxPrice: "100000.00000000",
-          tickSize: "0.10000000",
+          tickSize: "0.00000100",
         },
         {
           filterType: "PERCENT_PRICE",
@@ -61,12 +120,12 @@ export const BINANCE_RAW_SYMBOLS_INFO: IBinanceSymbolSchema = {
         {
           filterType: "LOT_SIZE",
           minQty: "0.00100000",
-          maxQty: "900000.00000000",
+          maxQty: "100000.00000000",
           stepSize: "0.00100000",
         },
         {
           filterType: "MIN_NOTIONAL",
-          minNotional: "10.00000000",
+          minNotional: "0.00010000",
           applyToMarket: true,
           avgPriceMins: 5,
         },
@@ -74,7 +133,66 @@ export const BINANCE_RAW_SYMBOLS_INFO: IBinanceSymbolSchema = {
         {
           filterType: "MARKET_LOT_SIZE",
           minQty: "0.00000000",
-          maxQty: "24882.38319305",
+          maxQty: "7359.49099722",
+          stepSize: "0.00000000",
+        },
+        { filterType: "MAX_NUM_ORDERS", maxNumOrders: 200 },
+        { filterType: "MAX_NUM_ALGO_ORDERS", maxNumAlgoOrders: 5 },
+      ],
+      permissions: ["SPOT", "MARGIN"],
+    },
+    {
+      symbol: "BNBBTC",
+      status: "TRADING",
+      baseAsset: "BNB",
+      baseAssetPrecision: 8,
+      quoteAsset: "BTC",
+      quotePrecision: 8,
+      quoteAssetPrecision: 8,
+      baseCommissionPrecision: 8,
+      quoteCommissionPrecision: 8,
+      orderTypes: [
+        "LIMIT",
+        "LIMIT_MAKER",
+        "MARKET",
+        "STOP_LOSS_LIMIT",
+        "TAKE_PROFIT_LIMIT",
+      ],
+      icebergAllowed: true,
+      ocoAllowed: true,
+      quoteOrderQtyMarketAllowed: true,
+      isSpotTradingAllowed: true,
+      isMarginTradingAllowed: true,
+      filters: [
+        {
+          filterType: "PRICE_FILTER",
+          minPrice: "0.00000100",
+          maxPrice: "100000.00000000",
+          tickSize: "0.00000100",
+        },
+        {
+          filterType: "PERCENT_PRICE",
+          multiplierUp: "5",
+          multiplierDown: "0.2",
+          avgPriceMins: 5,
+        },
+        {
+          filterType: "LOT_SIZE",
+          minQty: "0.00100000",
+          maxQty: "100000.00000000",
+          stepSize: "0.00100000",
+        },
+        {
+          filterType: "MIN_NOTIONAL",
+          minNotional: "0.00010000",
+          applyToMarket: true,
+          avgPriceMins: 5,
+        },
+        { filterType: "ICEBERG_PARTS", limit: 10 },
+        {
+          filterType: "MARKET_LOT_SIZE",
+          minQty: "0.00000000",
+          maxQty: "13755.68382070",
           stepSize: "0.00000000",
         },
         { filterType: "MAX_NUM_ORDERS", maxNumOrders: 200 },
@@ -83,4 +201,22 @@ export const BINANCE_RAW_SYMBOLS_INFO: IBinanceSymbolSchema = {
       permissions: ["SPOT", "MARGIN"],
     },
   ],
-};
+}
+
+export const BINANCE_PARSED_SYMBOLS = [
+  {
+    id: "ETH",
+    exchangeId: "binance",
+    meta: {},
+  },
+  {
+    id: "LTC",
+    exchangeId: "binance",
+    meta: {},
+  },
+  {
+    id: "BNB",
+    exchangeId: "binance",
+    meta: {},
+  },
+]
