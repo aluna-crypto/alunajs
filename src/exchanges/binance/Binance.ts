b2bd5af426ba5def6beb6ce7974b1f979051d779
LucianoPierdona <lucianompj1@gmail.com>
2021-11-25 06:24:02 -0300

    Created BinanceSpecs file

--- src/exchanges/binance/Binance.ts
+++ src/exchanges/binance/Binance.ts
@@ -3,9 +3,21 @@
 //   IAlunaExchangeStatic,
 // } from '../../index'
 // import { AAlunaExchange } from '../../lib/core/abstracts/AAlunaExchange'
+// import { BinanceSpecs } from './BinanceSpecs'
 
 
 
 // export const Binance: IAlunaExchangeStatic = class extends AAlunaExchange implements IAlunaExchange {
-
+//     // static definitions
+//     static readonly ID = BinanceSpecs.id
+//     static readonly SPECS = BinanceSpecs
+  
+//     static Symbol = ValrSymbolModule
+//     static Market = ValrMarketModule
+  
+  
+//     // local definitions
+//     key: IAlunaKeyModule
+//     order: IAlunaOrderWriteModule
+//     balance: IAlunaBalanceModule
 // }
---
+++ src/exchanges/binance/BinanceSpecs.ts
@@ -0,0 +1,40 @@
+import { AlunaAccountEnum } from '../../lib/enums/AlunaAccountEnum'
+import { AlunaFeaturesModeEnum } from '../../lib/enums/AlunaFeaturesModeEnum'
+import { IAlunaExchangeSpecsSchema } from '../../lib/schemas/IAlunaExchangeSpecsSchema'
+
+
+
+export const BinanceSpecs: IAlunaExchangeSpecsSchema = {
+  id: 'binance',
+  acceptFloatAmounts: true,
+  features: {
+    balance: AlunaFeaturesModeEnum.READ,
+    order: AlunaFeaturesModeEnum.WRITE,
+  },
+  accounts: [
+    {
+      type: AlunaAccountEnum.EXCHANGE,
+      supported: true,
+      implemented: false,
+    },
+    {
+      type: AlunaAccountEnum.SPOT,
+      supported: true,
+      implemented: true,
+    },
+    {
+      type: AlunaAccountEnum.MARGIN,
+      supported: true,
+      implemented: false,
+    },
+    {
+      type: AlunaAccountEnum.DERIVATIVES,
+      supported: true,
+      implemented: false,
+    },
+    {
+      type: AlunaAccountEnum.LENDING,
+      supported: false,
+    },
+  ],
+}
