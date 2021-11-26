9e03d04d972244dcb6dec3118b217fab0747c56d
LucianoPierdona <lucianompj1@gmail.com>
2021-11-26 07:08:44 -0300

    Finished BinanceMarketModule logic

--- src/exchanges/binance/modules/BinanceMarketModule.ts
+++ src/exchanges/binance/modules/BinanceMarketModule.ts
@@ -1,6 +1,5 @@
 import { IAlunaMarketModule } from '../../../lib/modules/IAlunaMarketModule'
 import { IAlunaMarketSchema } from '../../../lib/schemas/IAlunaMarketSchema'
-import { IMarketWithCurrency } from '../../valr/modules/ValrMarketModule'
 import { PROD_BINANCE_URL } from '../Binance'
 import { BinanceHttp } from '../BinanceHttp'
 import { BinanceLog } from '../BinanceLog'
@@ -9,6 +8,7 @@ import {
   IBinanceMarketWithCurrency,
 } from '../schemas/IBinanceMarketSchema'
 import { BinanceCurrencyMarketParser } from '../schemas/parses/BinanceCurrencyMarketParser'
+import { BinanceMarketParser } from '../schemas/parses/BinanceMarketParser'
 import { BinanceSymbolModule } from './BinanceSymbolModule'
 
 
@@ -22,7 +22,7 @@ export const BinanceMarketModule: IAlunaMarketModule = class {
     BinanceLog.info('fetching Binance markets')
 
     const rawMarkets = await publicRequest<IBinanceMarketSchema[]>({
-      url: PROD_BINANCE_URL + '/api/v3/ticker/24hr?symbol=ETHBTC',
+      url: PROD_BINANCE_URL + '/api/v3/ticker/24hr',
     })
 
     BinanceLog.info('fetching Binance symbols')
@@ -49,34 +49,34 @@ export const BinanceMarketModule: IAlunaMarketModule = class {
   }
 
   public static parse (params: {
-    rawMarket: IMarketWithCurrency,
-  }): any { // @TODO -> Update any
+    rawMarket: IBinanceMarketWithCurrency,
+  }): IAlunaMarketSchema {
 
     const { rawMarket } = params
 
-    // const parsedMarket = BinanceCurrencyMarketParser.parse({ rawMarket })
+    const parsedMarket = BinanceMarketParser.parse({ rawMarket })
 
-    return null // @TODO -> Update
+    return parsedMarket
 
   }
 
   public static parseMany (params: {
-    rawMarkets: IMarketWithCurrency[],
+    rawMarkets: IBinanceMarketWithCurrency[],
   }): IAlunaMarketSchema[] {
 
     const { rawMarkets } = params
 
     const parsedMarkets = rawMarkets.map((rawMarket) => {
 
-      // const parsedMarket = BinanceCurrencyMarketParser.parse({ rawMarket })
+      const parsedMarket = BinanceMarketParser.parse({ rawMarket })
 
-      // return parsedMarket
+      return parsedMarket
 
     })
 
     BinanceLog.info(`parsed ${parsedMarkets.length} markets for Binance`)
 
-    return [] // @TODO -> Update
+    return parsedMarkets
 
   }
 
