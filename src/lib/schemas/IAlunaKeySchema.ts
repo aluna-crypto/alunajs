7c551a2f4b3f02a70c84262aba7fdc24e91f5be0
Anderson Arboleya <anderson@arboleya.me>
2021-12-08 09:26:32 -0300

    Renaming file, refactoring and segmenting iterface.

---
+++ src/lib/schemas/IAlunaKeySchema.ts
@@ -0,0 +1,18 @@
+export interface IAlunaKeySchema {
+
+  accountId?: string
+  permissions: IAlunaKeyPermissionSchema
+
+  meta: any
+
+}
+
+
+
+export interface IAlunaKeyPermissionSchema {
+
+  read: boolean
+  trade?: boolean
+  withdraw?: boolean
+
+}
