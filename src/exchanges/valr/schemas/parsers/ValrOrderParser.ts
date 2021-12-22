2a85d5dca56b8847e59080e879d4b7c0331784aa
Sergio Torres <sergio.uft@gmail.com>
2021-12-22 09:34:21 -0300

    made 'ValrOrderParser' deal with different types of orders

--- src/exchanges/valr/schemas/parsers/ValrOrderParser.ts
+++ src/exchanges/valr/schemas/parsers/ValrOrderParser.ts
@@ -1,4 +1,7 @@
-import { AlunaOrderStatusEnum } from '../../../..'
+import {
+  AlunaOrderStatusEnum,
+  AlunaOrderTypesEnum,
+} from '../../../..'
 import { AlunaAccountEnum } from '../../../../lib/enums/AlunaAccountEnum'
 import { IAlunaOrderSchema } from '../../../../lib/schemas/IAlunaOrderSchema'
 import { ValrOrderTypeAdapter } from '../../enums/adapters/ValrOrderTypeAdapter'
@@ -30,6 +33,7 @@ export class ValrOrderParser {
 
     const {
       orderId,
+      stopPrice,
       originalQuantity,
     } = rawOrder
 
@@ -79,10 +83,31 @@ export class ValrOrderParser {
 
     }
 
-    const amount = parseFloat(originalQuantity)
-    const rate = parseFloat(price)
-
     const exchangeId = Valr.ID
+
+    const alnOrderType = ValrOrderTypeAdapter.translateToAluna({ from: type })
+
+    let rate: number | undefined
+    let stopRate: number | undefined
+    let limitRate: number | undefined
+
+    switch (alnOrderType) {
+
+      case AlunaOrderTypesEnum.STOP_LIMIT:
+      case AlunaOrderTypesEnum.TAKE_PROFIT_LIMIT:
+
+        stopRate = Number(stopPrice)
+        limitRate = Number(price)
+
+        break
+
+      default:
+
+        rate = Number(price)
+
+    }
+
+    const amount = Number(originalQuantity)
 
     const alnOrderStatus = ValrStatusAdapter.translateToAluna({ from: status })
 
@@ -105,14 +130,16 @@ export class ValrOrderParser {
       exchangeId,
       baseSymbolId: baseCurrency,
       quoteSymbolId: quoteCurrency,
-      total: amount * rate,
+      total: amount * Number(price),
       amount,
       isAmountInContracts: false,
       rate,
+      stopRate,
+      limitRate,
       account: AlunaAccountEnum.EXCHANGE,
       side: ValrSideAdapter.translateToAluna({ from: side }),
       status: alnOrderStatus,
-      type: ValrOrderTypeAdapter.translateToAluna({ from: type }),
+      type: alnOrderType,
       placedAt: new Date(createdAt),
       filledAt,
       canceledAt,
