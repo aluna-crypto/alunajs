4bef55fd0f60fde320af7f093fe45768c818a422
Anderson Arboleya <anderson@arboleya.me>
2022-01-12 16:42:23 -0300

    Deleting unused enum items.

--- src/exchanges/valr/enums/ValrErrorEnum.ts
+++ src/exchanges/valr/enums/ValrErrorEnum.ts
@@ -1,7 +1,3 @@
 export enum ValrErrorEnum {
-  UNAUTHORIZED = 'Unauthorized',
   INVALID_KEY = 'API key or secret is invalid',
-  INVALID_REQUEST = 'Invalid Request, please check your request and try again',
-  INVALID_SIGNATURE = 'Request has an invalid signature',
-  INVALID_ORDER = 'Invalid Order. ' // it comes with an extra empty char
 }
