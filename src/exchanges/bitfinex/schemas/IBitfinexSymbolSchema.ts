ce58898ca29de592c585fd445aa12d953b6595c2
Sergio Torres <sergio.uft@gmail.com>
2022-01-24 15:19:50 -0300

    improve bitfinex symbols schemas

--- src/exchanges/bitfinex/schemas/IBitfinexSymbolSchema.ts
+++ src/exchanges/bitfinex/schemas/IBitfinexSymbolSchema.ts
@@ -4,8 +4,11 @@ type TSymbolsTuple = [
   IBitfinexSymbolsIds, IBitfinexSymbolsLabels, IProperBitfinexSymbolsIds
 ]
 
+
 export interface IBitfinexSymbolsIds extends Array<string> {}
 
-export interface IBitfinexSymbolsLabels extends Array<[bitfinexSymbolId: string, symbolName: string]> {}
+export interface IBitfinexSymbolsLabels extends Array<TBitfinexLabel> {}
+export type TBitfinexLabel = [bitfinexSymbolId: string, symbolName: string]
 
-export interface IProperBitfinexSymbolsIds extends Array<[bitfinexSymbolId: string, symbolId: string]> {}
+export interface IProperBitfinexSymbolsIds extends Array<TBitfinexProperSymbol> {}
+export type TBitfinexProperSymbol = [bitfinexSymbolId: string, symbolId: string]
