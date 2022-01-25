d8927f1e6881a7582de0d77aac7407813edcd489
Sergio Torres <sergio.uft@gmail.com>
2022-01-24 23:42:56 -0300

    refact method 'parseMany' on 'BitfinexSymbolModule'

--- src/exchanges/bitfinex/modules/BitfinexSymbolModule.ts
+++ src/exchanges/bitfinex/modules/BitfinexSymbolModule.ts
@@ -1,20 +1,20 @@
 import { IAlunaSymbolModule } from '../../../lib/modules/IAlunaSymbolModule'
 import { IAlunaSymbolSchema } from '../../../lib/schemas/IAlunaSymbolSchema'
-import { Bitfinex } from '../Bitfinex'
 import { BitfinexHttp } from '../BitfinexHttp'
 import { BitfinexLog } from '../BitfinexLog'
 import {
   IBitfinexSymbols,
-  TBitfinexLabel,
-  TBitfinexProperSymbol,
+  TBitfinexCurrencyLabel,
+  TBitfinexCurrencySym,
 } from '../schemas/IBitfinexSymbolSchema'
+import { BitfinexSymbolParser } from '../schemas/parsers/BitfinexSymbolParser'
 
 
 
 interface IBitfinexParseSymbolParams {
-  bitfinexSymbolId: string
-  symbolsLabelsDict: Record<string, TBitfinexLabel>
-  properSymbolsIdsDict: Record<string, TBitfinexProperSymbol>
+  bitfinexCurrency: string
+  bitfinexCurrencyLabel: TBitfinexCurrencyLabel | undefined
+  bitfinexSym: TBitfinexCurrencySym | undefined
 }
 
 
@@ -58,51 +58,11 @@ export const BitfinexSymbolModule: IAlunaSymbolModule = class {
     rawSymbol: IBitfinexParseSymbolParams,
   }): IAlunaSymbolSchema {
 
-    // Consider implementing separeted parser
     const { rawSymbol } = params
 
-    const {
-      bitfinexSymbolId,
-      properSymbolsIdsDict,
-      symbolsLabelsDict,
-    } = rawSymbol
+    const parsedSymbol = BitfinexSymbolParser.parse(rawSymbol)
 
-    let id = bitfinexSymbolId
-
-    let name: string | undefined
-    let alias: string | undefined
-
-    const symbolLabel = symbolsLabelsDict[bitfinexSymbolId]
-
-    if (symbolLabel) {
-
-      [, name] = symbolLabel
-
-    }
-
-    const properSymbol = properSymbolsIdsDict[bitfinexSymbolId]
-
-    if (properSymbol) {
-
-      [, id] = properSymbol
-
-      alias = bitfinexSymbolId
-
-    }
-
-    const symbol: IAlunaSymbolSchema = {
-      id: id.toUpperCase(), // some symbols ids are like: 'USDt'
-      name,
-      exchangeId: Bitfinex.ID,
-      alias,
-      meta: {
-        currency: bitfinexSymbolId,
-        currencyLabel: symbolLabel,
-        currencySym: properSymbol,
-      },
-    }
-
-    return symbol
+    return parsedSymbol
 
   }
 
@@ -118,38 +78,38 @@ export const BitfinexSymbolModule: IAlunaSymbolModule = class {
       properSymbolsIds,
     ] = rawSymbols
 
-    const symbolsLabelsDict: Record<string, TBitfinexLabel> = {}
-    const properSymbolsIdsDict: Record<string, TBitfinexProperSymbol> = {}
+    const currencyLabelsDict: Record<string, TBitfinexCurrencyLabel> = {}
+    const currencySymsDict: Record<string, TBitfinexCurrencySym> = {}
 
-    symbolsLabels.forEach((symbolLabel) => {
+    symbolsLabels.forEach((currencyLabel) => {
 
-      const [bitfinexSymbolId] = symbolLabel
+      const [bitfinexSymbolId] = currencyLabel
 
-      symbolsLabelsDict[bitfinexSymbolId] = symbolLabel
-
-    })
-
-    properSymbolsIds.forEach((properSymbol) => {
-
-      const [bitfinexSymbolId] = properSymbol
-
-      properSymbolsIdsDict[bitfinexSymbolId] = properSymbol
+      currencyLabelsDict[bitfinexSymbolId] = currencyLabel
 
     })
 
-    const parsedSymbols = symbolsIds.reduce((acc, bitfinexSymbolId) => {
+    properSymbolsIds.forEach((currencySym) => {
+
+      const [bitfinexSymbolId] = currencySym
+
+      currencySymsDict[bitfinexSymbolId] = currencySym
+
+    })
+
+    const parsedSymbols = symbolsIds.reduce((acc, bitfinexCurrency) => {
 
       // skipping derivatives symbols for now
-      if (/F0/.test(bitfinexSymbolId)) {
+      if (/F0/.test(bitfinexCurrency)) {
 
         return acc
 
       }
 
       const rawSymbol: IBitfinexParseSymbolParams = {
-        bitfinexSymbolId,
-        properSymbolsIdsDict,
-        symbolsLabelsDict,
+        bitfinexCurrency,
+        bitfinexCurrencyLabel: currencyLabelsDict[bitfinexCurrency],
+        bitfinexSym: currencySymsDict[bitfinexCurrency],
       }
 
       const parsedSymbol = BitfinexSymbolModule.parse({ rawSymbol })
