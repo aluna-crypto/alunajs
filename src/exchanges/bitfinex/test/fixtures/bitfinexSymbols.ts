45aa17a56b4b9a165f64720373a4be692d130c6b
Sergio Torres <sergio.uft@gmail.com>
2022-01-24 23:41:26 -0300

    refact bitfinex symbols fixtures

--- src/exchanges/bitfinex/test/fixtures/bitfinexSymbols.ts
+++ src/exchanges/bitfinex/test/fixtures/bitfinexSymbols.ts
@@ -1,55 +1,130 @@
 import { IAlunaSymbolSchema } from '../../../../lib/schemas/IAlunaSymbolSchema'
 import { Bitfinex } from '../../Bitfinex'
-import { IBitfinexSymbols } from '../../schemas/IBitfinexMarketSchema'
+import {
+  IBitfinexCurrencies,
+  TBitfinexCurrencyLabel,
+  TBitfinexCurrencySym,
+} from '../../schemas/IBitfinexSymbolSchema'
 
 
 
-export const BITFINEX_RAW_SYMBOLS: IBitfinexSymbols[] = [
+export const BITFINEX_CURRENCIES: IBitfinexCurrencies[] = [
   [
-    ['AAVE', 'AAVE'],
-    ['ADA', 'Cardano ADA'],
-    ['BTC', 'Bitcoin'],
-    ['ETH', 'Ethereum'],
-    ['EUR', 'Euro'],
-    ['USD', 'US Dollar'],
+    'AAVE',
+    'ADA',
+    'BTC',
+    'ETH',
+    'EUR',
+    'USD',
+    'UST',
+    'UDC',
+    'USTF0',
   ],
 ]
+
+
+
+export const BITFINEX_CURRENCIES_LABELS: TBitfinexCurrencyLabel[] = [
+  ['AAVE', 'AAVE'],
+  ['ADA', 'Cardano ADA'],
+  ['BTC', 'Bitcoin'],
+  ['ETH', 'Ethereum'],
+  ['EUR', 'Euro'],
+  ['USD', 'US Dollar'],
+  ['UDC', 'USDC'],
+  ['UST', 'Tether USDt'],
+]
+
+
+
+export const BITFINEX_CURRENCIES_SYMS: TBitfinexCurrencySym[] = [
+  ['UDC', 'USDC'],
+  ['UST', 'USDT'],
+]
+
+
+
+export const BITFINEX_RAW_SYMBOLS = [
+  BITFINEX_CURRENCIES,
+  BITFINEX_CURRENCIES_LABELS,
+  BITFINEX_CURRENCIES_SYMS,
+]
+
+
 
 export const BITFINEX_PARSED_SYMBOLS: IAlunaSymbolSchema[] = [
   {
     exchangeId: Bitfinex.ID,
     id: 'AAVE',
     name: 'AAVE',
-    meta: ['AAVE', 'AAVE'],
+    meta: {
+      currency: 'AAVE',
+      currencyLabel: ['AAVE', 'AAVE'],
+    },
   },
   {
     exchangeId: Bitfinex.ID,
     id: 'ADA',
     name: 'Cardano ADA',
-    meta: ['ADA', 'Cardano ADA'],
+    meta: {
+      currency: 'ADA',
+      currencyLabel: ['ADA', 'Cardano ADA'],
+    },
   },
   {
     exchangeId: Bitfinex.ID,
     id: 'BTC',
     name: 'Bitcoin',
-    meta: ['BTC', 'Bitcoin'],
+    meta: {
+      currency: 'BTC',
+      currencyLabel: ['BTC', 'Bitcoin'],
+    },
   },
   {
     exchangeId: Bitfinex.ID,
     id: 'ETH',
     name: 'Ethereum',
-    meta: ['ETH', 'Ethereum'],
+    meta: {
+      currency: 'ETH',
+      currencyLabel: ['ETH', 'Ethereum'],
+    },
   },
   {
     exchangeId: Bitfinex.ID,
     id: 'EUR',
     name: 'Euro',
-    meta: ['EUR', 'Euro'],
+    meta: {
+      currency: 'EUR',
+      currencyLabel: ['EUR', 'Euro'],
+    },
   },
   {
     id: 'USD',
     name: 'US Dollar',
     exchangeId: 'bitfinex',
-    meta: ['USD', 'US Dollar'],
+    meta: {
+      currency: 'USD',
+      currencyLabel: ['USD', 'US Dollar'],
+    },
+  },
+  {
+    id: 'USDT',
+    name: 'Tether USDt',
+    exchangeId: 'bitfinex',
+    meta: {
+      currency: 'UST',
+      currencyLabel: ['UST', 'Tether USDt'],
+      currencySym: ['UST', 'USDt'],
+    },
+  },
+  {
+    id: 'UDC',
+    name: 'USDC',
+    exchangeId: 'bitfinex',
+    meta: {
+      currency: 'UDC',
+      currencyLabel: ['UDC', 'USDc'],
+      currencySym: ['UDC', 'USDC'],
+    },
   },
 ]
