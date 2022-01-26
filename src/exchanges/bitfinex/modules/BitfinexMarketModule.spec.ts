729b3d40af01045132c9eeabaf576a969d0197d8
Sergio Torres <sergio.uft@gmail.com>
2022-01-26 08:48:23 -0300

    fix test suite for 'BitfinexMarketModule' 'listRaw' method

--- src/exchanges/bitfinex/modules/BitfinexMarketModule.spec.ts
+++ src/exchanges/bitfinex/modules/BitfinexMarketModule.spec.ts
@@ -1,29 +1,55 @@
-// import { expect } from 'chai'
-// import { ImportMock } from 'ts-mock-imports'
+import { expect } from 'chai'
+import { ImportMock } from 'ts-mock-imports'
 
-// import { BitfinexHttp } from '../BitfinexHttp'
-// import { BITFINEX_RAW_TICKERS } from '../test/fixtures/bitfinexMarkets'
-// import { BitfinexMarketModule } from './BitfinexMarketModule'
+import { BitfinexHttp } from '../BitfinexHttp'
+import {
+  BITFINEX_MARGIN_ENABLED_CURRENCIES,
+  BITFINEX_RAW_TICKERS,
+} from '../test/fixtures/bitfinexMarkets'
+import { BITFINEX_CURRENCIES_SYMS } from '../test/fixtures/bitfinexSymbols'
+import { BitfinexMarketModule } from './BitfinexMarketModule'
 
 
 
-// describe('BitfinexMarketModule', () => {
+describe('BitfinexMarketModule', () => {
 
-//   it('should list Bitfinex raw markets just fine', async () => {
+  it('should list Bitfinex raw markets just fine', async () => {
 
-//     const requestMock = ImportMock.mockFunction(
-//       BitfinexHttp,
-//       'publicRequest',
-//       Promise.resolve(BITFINEX_RAW_TICKERS),
-//     )
+    const requestMock = ImportMock.mockFunction(
+      BitfinexHttp,
+      'publicRequest',
+    )
 
-//     const rawMarkets = await BitfinexMarketModule.listRaw()
+    requestMock.onFirstCall().returns(Promise.resolve(BITFINEX_RAW_TICKERS))
+    requestMock.onSecondCall().returns(Promise.resolve([
+      BITFINEX_MARGIN_ENABLED_CURRENCIES,
+      BITFINEX_CURRENCIES_SYMS,
+    ]))
 
-//     expect(rawMarkets.length).to.eq(BITFINEX_RAW_TICKERS.length)
-//     expect(rawMarkets).to.deep.eq(BITFINEX_RAW_TICKERS)
+    const rawMarkets = await BitfinexMarketModule.listRaw()
 
-//     expect(requestMock.callCount).to.be.eq(1)
+    expect(rawMarkets.length).to.eq(3)
 
-//   })
+    expect(rawMarkets).to.deep.eq([
+      BITFINEX_RAW_TICKERS,
+      BITFINEX_MARGIN_ENABLED_CURRENCIES,
+      BITFINEX_CURRENCIES_SYMS,
+    ])
 
-// })
+    expect(requestMock.calledWithExactly({
+      url: 'https://api-pub.bitfinex.com/v2/tickers?symbols=ALL',
+    }))
+
+    const secondCallUrl = 'https://api-pub.bitfinex.com/v2/conf/'
+      .concat('pub:list:pair:margin')
+      .concat(',pub:map:currency:sym')
+
+    expect(requestMock.calledWithExactly({
+      url: secondCallUrl,
+    }))
+
+    expect(requestMock.callCount).to.be.eq(2)
+
+  })
+
+})
