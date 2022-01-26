a80b9b1de546c52318489392fc43a9de54590aeb
Sergio Torres <sergio.uft@gmail.com>
2022-01-26 09:00:00 -0300

    implement test suite for 'BitfinexMarketModule' 'parse' method

--- src/exchanges/bitfinex/modules/BitfinexMarketModule.spec.ts
+++ src/exchanges/bitfinex/modules/BitfinexMarketModule.spec.ts
@@ -2,8 +2,14 @@ import { expect } from 'chai'
 import { ImportMock } from 'ts-mock-imports'
 
 import { BitfinexHttp } from '../BitfinexHttp'
+import { TBitfinexCurrencySym } from '../schemas/IBitfinexSymbolSchema'
+import {
+  BitfinexMarketParser,
+  IBitfinexMarketParseParams,
+} from '../schemas/parsers/BitfinexMarketParser'
 import {
   BITFINEX_MARGIN_ENABLED_CURRENCIES,
+  BITFINEX_PARSED_MARKETS,
   BITFINEX_RAW_TICKERS,
 } from '../test/fixtures/bitfinexMarkets'
 import { BITFINEX_CURRENCIES_SYMS } from '../test/fixtures/bitfinexSymbols'
@@ -49,6 +55,70 @@ describe('BitfinexMarketModule', () => {
     }))
 
     expect(requestMock.callCount).to.be.eq(2)
+
+  })
+
+  it('should parse Bitfinex raw markets just fine', async () => {
+
+    const BitfinexMarketParserMock = ImportMock.mockFunction(
+      BitfinexMarketParser,
+      'parse',
+      BITFINEX_PARSED_MARKETS[0],
+    )
+
+    // creating dicitonaries to call 'parse' method
+    const currencySymsDict: Record<string, TBitfinexCurrencySym> = {}
+    const enabledMarginMarketsDict: Record<string, string> = {}
+
+    BITFINEX_CURRENCIES_SYMS.forEach((s) => {
+
+      currencySymsDict[s[0]] = s
+
+    })
+
+    BITFINEX_MARGIN_ENABLED_CURRENCIES.forEach((c) => {
+
+      enabledMarginMarketsDict[c[0]] = c
+
+    })
+
+    const rawMarket: IBitfinexMarketParseParams = {
+      currencySymsDict,
+      enabledMarginMarketsDict,
+      rawTicker: BITFINEX_RAW_TICKERS[0],
+    }
+
+    const parsedMarket1 = BitfinexMarketModule.parse({ rawMarket })
+
+    expect(BitfinexMarketParserMock.callCount).to.be.eq(1)
+
+    expect(BitfinexMarketParserMock.calledWithExactly({
+      rawTicker: BITFINEX_RAW_TICKERS[0],
+      currencySymsDict,
+      enabledMarginMarketsDict,
+    })).to.be.ok
+
+    expect(BitfinexMarketParserMock.returned(parsedMarket1)).to.be.ok
+
+    // new mock
+    BitfinexMarketParserMock.returns(BITFINEX_PARSED_MARKETS[1])
+
+    const parsedMarket2 = BitfinexMarketModule.parse({
+      rawMarket: {
+        ...rawMarket,
+        rawTicker: BITFINEX_RAW_TICKERS[1],
+      },
+    })
+
+    expect(BitfinexMarketParserMock.callCount).to.be.eq(2)
+
+    expect(BitfinexMarketParserMock.calledWithExactly({
+      rawTicker: BITFINEX_RAW_TICKERS[1],
+      currencySymsDict,
+      enabledMarginMarketsDict,
+    })).to.be.ok
+
+    expect(BitfinexMarketParserMock.returned(parsedMarket2)).to.be.ok
 
   })
 
