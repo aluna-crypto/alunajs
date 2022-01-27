e933add78c91b02d9b062f07c313d556283637ff
Sergio Torres <sergio.uft@gmail.com>
2022-01-26 21:02:00 -0300

    implement 'generateAuthHeader' for 'BitifinexHttp'

--- src/exchanges/bitfinex/BitfinexHttp.ts
+++ src/exchanges/bitfinex/BitfinexHttp.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import crypto from 'crypto'
 
 import {
   IAlunaHttp,
@@ -6,12 +7,62 @@ import {
   IAlunaHttpPublicParams,
 } from '../../lib/core/IAlunaHttp'
 import { AlunaHttpVerbEnum } from '../../lib/enums/AlunaHtttpVerbEnum'
+import { IAlunaKeySecretSchema } from '../../lib/schemas/IAlunaKeySecretSchema'
+
+
+
+interface IBitfinexSignedHashParams {
+  body?: Record<string, any>
+  path: string
+  keySecret: IAlunaKeySecretSchema
+}
+
+
+
+interface IBitfinexSignedHeaders {
+  'Content-Type': string
+  'bfx-nonce': string
+  'bfx-apikey': string
+  'bfx-signature': string
+}
+
+
+
+export const generateAuthHeader = (
+  params: IBitfinexSignedHashParams,
+): IBitfinexSignedHeaders => {
+
+  const {
+    path,
+    keySecret,
+    body,
+  } = params
+
+  const {
+    key,
+    secret,
+  } = keySecret
+
+  const nonce = (Date.now() * 1000).toString()
+
+  const signature = `/api${path}${nonce}${JSON.stringify(body)}`
+
+  const sig = crypto.createHmac('sha384', secret)
+    .update(signature)
+    .digest('hex')
+
+  return {
+    'Content-Type': 'application/json',
+    'bfx-nonce': nonce,
+    'bfx-apikey': key,
+    'bfx-signature': sig,
+  }
+
+}
 
 
 
 export const BitfinexHttp: IAlunaHttp = class {
-
-
 
   static async publicRequest<T> (params: IAlunaHttpPublicParams): Promise<T> {
 
@@ -45,7 +96,6 @@ export const BitfinexHttp: IAlunaHttp = class {
   static async privateRequest<T> (params: IAlunaHttpPrivateParams): Promise<T> {
 
     // TODO: Implement private request
-
     throw Error(`Not implemented ${JSON.stringify(params)}`)
 
   }
