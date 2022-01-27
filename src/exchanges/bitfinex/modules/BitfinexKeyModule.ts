2aef78bb9aee70bd620b06deecba8215933ee903
Sergio Torres <sergio.uft@gmail.com>
2022-01-26 21:04:05 -0300

    implement 'BitfinexKeyModule'

---
+++ src/exchanges/bitfinex/modules/BitfinexKeyModule.ts
@@ -0,0 +1,93 @@
+import { IAlunaKeySchema } from '../../..'
+import { AAlunaModule } from '../../../lib/core/abstracts/AAlunaModule'
+import { IAlunaKeyModule } from '../../../lib/modules/IAlunaKeyModule'
+import { IAlunaKeyPermissionSchema } from '../../../lib/schemas/IAlunaKeySchema'
+import { BitfinexHttp } from '../BitfinexHttp'
+import { BitfinexLog } from '../BitfinexLog'
+import { IBitfinexKey } from '../schemas/IBitfinexKeySchema'
+
+
+
+export class BitfinexKeyModule extends AAlunaModule implements IAlunaKeyModule {
+
+  public details: IAlunaKeySchema
+
+  public async fetchDetails (): Promise<IAlunaKeySchema> {
+
+    BitfinexLog.info('fetching Bitfinex key details')
+
+    const { keySecret } = this.exchange
+
+    let rawKey: IBitfinexKey
+
+    try {
+
+      rawKey = await BitfinexHttp.privateRequest<IBitfinexKey>({
+        url: 'https://api.bitfinex.com/v2/auth/r/permissions',
+        keySecret,
+      })
+
+    } catch (error) {
+
+      throw new Error()
+
+    }
+
+    const details = this.parseDetails({ rawKey })
+
+    return details
+
+  }
+
+  public parseDetails (params: {
+    rawKey: IBitfinexKey,
+  }): IAlunaKeySchema {
+
+    BitfinexLog.info('parsing Bitfinex key details')
+
+    const {
+      rawKey,
+    } = params
+
+    this.details = {
+      meta: rawKey,
+      accountId: undefined,
+      permissions: this.parsePermissions({ rawKey }),
+    }
+
+    return this.details
+
+  }
+
+  public parsePermissions (params: {
+    rawKey: IBitfinexKey,
+  }): IAlunaKeyPermissionSchema {
+
+    BitfinexLog.info('parsing Bitfinex key permissions')
+
+    const { rawKey } = params
+
+    const [,
+      ordersScope,,,
+      walletsScope,
+      withdrawScope,
+    ] = rawKey
+
+    const canReadBalances = walletsScope[1] === 1
+
+    const canReadOrder = ordersScope[1] === 1
+    const canCreateOrders = ordersScope[2] === 1
+
+    const canWithdraw = withdrawScope[2] === 1
+
+    const alunaPermissions: IAlunaKeyPermissionSchema = {
+      read: canReadBalances && canReadOrder,
+      trade: canCreateOrders,
+      withdraw: canWithdraw,
+    }
+
+    return alunaPermissions
+
+  }
+
+}
