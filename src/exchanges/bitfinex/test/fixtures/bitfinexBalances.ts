11d9ee5c152223b2988703f8477045d92fb892b9
Sergio Torres <sergio.uft@gmail.com>
2022-01-31 12:38:12 -0300

    add fixture to bitfinex balances fixtures

--- src/exchanges/bitfinex/test/fixtures/bitfinexBalances.ts
+++ src/exchanges/bitfinex/test/fixtures/bitfinexBalances.ts
@@ -1,3 +1,7 @@
+import {
+  AlunaAccountEnum,
+  IAlunaBalanceSchema,
+} from '../../../..'
 import { BitfinexAccountsEnum } from '../../enums/BitfinexAccountsEnum'
 import { IBitfinexBalanceSchema } from '../../schemas/IBitfinexBalanceSchema'
 
@@ -100,4 +104,91 @@ export const BITFINEX_RAW_BALANCES: IBitfinexBalanceSchema[] = [
     null,
     null,
   ],
+]
+
+
+
+export const BITFINEX_PARSED_BALANCES: IAlunaBalanceSchema[] = [
+  {
+    account: AlunaAccountEnum.EXCHANGE,
+    symbolId: 'BTC',
+    available: 7.35296e-7,
+    total: 7.35296e-7,
+    meta: [
+      'exchange',
+      'BTC',
+      7.35296e-7,
+      0,
+      7.35296e-7,
+      'Exchange 0.0008 ETH for BTC @ 0.065457',
+      [Object],
+    ],
+  },
+  {
+    account: AlunaAccountEnum.EXCHANGE,
+    symbolId: 'ATO',
+    available: 0.1992,
+    total: 0.1992,
+    meta: [
+      'exchange',
+      'ATO',
+      0.1992,
+      0,
+      0.1992,
+      'Exchange 0.2 ATO for USD @ 37.271',
+      [Object],
+    ],
+  },
+  {
+    account: AlunaAccountEnum.EXCHANGE,
+    symbolId: 'USD',
+    available: 13.89214731,
+    total: 13.89214731,
+    meta: [
+      'exchange',
+      'USD',
+      13.89214731,
+      0,
+      13.89214731,
+      'Exchange 0.002 ETH for USD @ 3296.6',
+      [Object],
+    ],
+  },
+  {
+    account: AlunaAccountEnum.MARGIN,
+    symbolId: 'USD',
+    available: 1.13204658,
+    total: 1.13204658,
+    meta: [
+      'margin',
+      'USD',
+      1.13204658,
+      0,
+      1.13204658,
+      'Trading fees for 0.0008 ETH (ETHBTC) @ 0.0657 on BFX (0.2%)',
+      null,
+    ],
+  },
+  {
+    account: AlunaAccountEnum.MARGIN,
+    symbolId: 'BTC',
+    available: 0.00000211,
+    total: 0.00000211,
+    meta: [
+      'margin',
+      'BTC',
+      0.00000211,
+      0,
+      0.00000211,
+      'Position closed @ 0.0657 (TRADE)',
+      [Object],
+    ],
+  },
+  {
+    account: AlunaAccountEnum.MARGIN,
+    symbolId: 'ETH',
+    available: 0.0071264,
+    total: 0.0071264,
+    meta: ['margin', 'ETH', 0.0071264, 0, 0.0071264, null, null],
+  },
 ]
