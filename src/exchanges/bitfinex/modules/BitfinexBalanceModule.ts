27800783505acdd09acf69068b2c790fcdb25142
Sergio Torres <sergio.uft@gmail.com>
2022-01-31 11:21:05 -0300

    implemented class 'BitfinexBalanceModule'

---
+++ src/exchanges/bitfinex/modules/BitfinexBalanceModule.ts
@@ -0,0 +1,82 @@
+import { AAlunaModule } from '../../../lib/core/abstracts/AAlunaModule'
+import { IAlunaBalanceModule } from '../../../lib/modules/IAlunaBalanceModule'
+import { IAlunaBalanceSchema } from '../../../lib/schemas/IAlunaBalanceSchema'
+import { BitfinexHttp } from '../BitfinexHttp'
+import { BitfinexLog } from '../BitfinexLog'
+import { BitfinexAccountsEnum } from '../enums/BitfinexAccountsEnum'
+import { IBitfinexBalanceSchema } from '../schemas/IBitfinexBalanceSchema'
+import { BitfinexBalanceParser } from '../schemas/parsers/BitifnexBalanceParser'
+
+
+
+export class BitfinexBalanceModule extends AAlunaModule implements IAlunaBalanceModule {
+
+  public async listRaw (): Promise<IBitfinexBalanceSchema[]> {
+
+    BitfinexLog.info('fetching Bitfinex balances')
+
+    const { privateRequest } = BitfinexHttp
+
+    const rawBalances = await privateRequest<IBitfinexBalanceSchema[]>({
+      url: 'https://api.bitfinex.com/v2/auth/r/wallets',
+      keySecret: this.exchange.keySecret,
+    })
+
+    return rawBalances
+
+  }
+
+  public async list (): Promise<IAlunaBalanceSchema[]> {
+
+    const rawBalances = await this.listRaw()
+
+    const parsedBalances = this.parseMany({ rawBalances })
+
+    BitfinexLog.info(`parsed ${parsedBalances.length} balances for Bitfinex`)
+
+    return parsedBalances
+
+  }
+
+  public parse (params: {
+    rawBalance: IBitfinexBalanceSchema,
+  }): IAlunaBalanceSchema {
+
+    const { rawBalance } = params
+
+    const parsedBalance = BitfinexBalanceParser.parser({ rawBalance })
+
+    return parsedBalance
+
+  }
+
+  public parseMany (params: {
+    rawBalances: IBitfinexBalanceSchema[],
+  }): IAlunaBalanceSchema[] {
+
+    const { rawBalances } = params
+
+    const parsedBalances = rawBalances.reduce((accumulator, rawBalance) => {
+
+      const [walletType] = rawBalance
+
+      // skipping 'lending' balances types for now
+      if (walletType === BitfinexAccountsEnum.FUNDING) {
+
+        return accumulator
+
+      }
+
+      const parsedBalance = this.parse({ rawBalance })
+
+      accumulator.push(parsedBalance)
+
+      return accumulator
+
+    }, [] as IAlunaBalanceSchema[])
+
+    return parsedBalances
+
+  }
+
+}
