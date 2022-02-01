1fc0d01f64daee56ba993dd4b73333d6095b7aae
Sergio Torres <sergio.uft@gmail.com>
2022-02-01 17:03:27 -0300

    standardize some order error codes values

--- src/lib/errors/AlunaOrderErrorCodes.ts
+++ src/lib/errors/AlunaOrderErrorCodes.ts
@@ -2,7 +2,7 @@ export const AlunaOrderErrorCodes = {
   TYPE_NOT_SUPPORTED: 'order-type-not-supported',
   TYPE_IS_READ_ONLY: 'order-type-is-read-only',
   IS_NOT_OPEN: 'order-isnt-open',
-  PLACE_FAILED: 'order-place-fail',
-  CANCEL_FAILED: 'order-cancel-fail',
+  PLACE_FAILED: 'order-place-failed',
+  CANCEL_FAILED: 'order-cancel-failed',
   NOT_FOUND: 'order-not-found',
 }
