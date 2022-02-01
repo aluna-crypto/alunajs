45ff447004a0147153dfad199fd17d032235cb09
Sergio Torres <sergio.uft@gmail.com>
2022-02-01 17:25:46 -0300

    add test case for 'getRaw' on BitfinexOrderReadModule

--- src/exchanges/bitfinex/modules/BitfinexOrderReadModule.spec.ts
+++ src/exchanges/bitfinex/modules/BitfinexOrderReadModule.spec.ts
@@ -39,7 +39,7 @@ describe.only('BitfinexOrderReadModule', () => {
     const requestMock = ImportMock.mockFunction(
       BitfinexHttp,
       'privateRequest',
-      BITFINEX_RAW_ORDERS,
+      Promise.resolve(BITFINEX_RAW_ORDERS),
     )
 
     const rawOrders = await bitfinexOrderReadModule.listRaw()
@@ -49,6 +49,71 @@ describe.only('BitfinexOrderReadModule', () => {
     expect(requestMock.calledWithExactly({
       url: 'https://api.bitfinex.com/v2/auth/r/orders',
       keySecret: exchangeMock.getValue().keySecret,
+    })).to.be.ok
+
+  })
+
+  it('should get a raw open Bitfinex order just fine', async () => {
+
+    const { exchangeMock } = mockKeySecret()
+
+    const returnedRawOrder = BITFINEX_RAW_ORDERS[0][0]
+
+    const requestMock = ImportMock.mockFunction(
+      BitfinexHttp,
+      'privateRequest',
+      Promise.resolve([returnedRawOrder]),
+    )
+
+    const symbolPair = 'symbol'
+    const id = 666
+
+    const rawOrder = await bitfinexOrderReadModule.getRaw({
+      id,
+      symbolPair,
+    })
+
+    expect(requestMock.callCount).to.be.eq(1)
+    expect(rawOrder).to.deep.eq(returnedRawOrder)
+    expect(requestMock.calledWithExactly({
+      url: `https://api.bitfinex.com/v2/auth/r/orders/${symbolPair}`,
+      keySecret: exchangeMock.getValue().keySecret,
+      body: { id: [id] },
+    })).to.be.ok
+
+  })
+
+  it('should get a raw not open Bitfinex order just fine', async () => {
+
+    const { exchangeMock } = mockKeySecret()
+
+    const returnedRawOrder = BITFINEX_RAW_ORDERS[0][0]
+
+    const requestMock = ImportMock.mockFunction(
+      BitfinexHttp,
+      'privateRequest',
+    )
+
+    requestMock
+      .onFirstCall()
+      .returns(Promise.resolve([]))
+      .onSecondCall()
+      .returns(Promise.resolve([returnedRawOrder]))
+
+    const symbolPair = 'symbol'
+    const id = 666
+
+    const rawOrder = await bitfinexOrderReadModule.getRaw({
+      id,
+      symbolPair,
+    })
+
+    expect(requestMock.callCount).to.be.eq(2)
+    expect(rawOrder).to.deep.eq(returnedRawOrder)
+    expect(requestMock.calledWithExactly({
+      url: `https://api.bitfinex.com/v2/auth/r/orders/${symbolPair}/hist`,
+      keySecret: exchangeMock.getValue().keySecret,
+      body: { id: [id] },
     })).to.be.ok
 
   })
