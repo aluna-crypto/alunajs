9089644742912e92ed5eb7be1bb9349483f54c58
LucianoPierdona <lucianompj1@gmail.com>
2022-02-03 09:58:43 -0300

    initiated Bittrex implementation and added BittrexSpecs file

---
+++ src/exchanges/bittrex/BittrexSpecs.ts
@@ -0,0 +1,91 @@
+import {
+  AlunaAccountEnum,
+  AlunaFeaturesModeEnum,
+  AlunaOrderTypesEnum,
+} from '../../index'
+import {
+  IAlunaExchangeOrderSpecsSchema,
+  IAlunaExchangeSchema,
+} from '../../lib/schemas/IAlunaExchangeSchema'
+
+
+
+const exchangeOrderTypes: IAlunaExchangeOrderSpecsSchema[] = [
+  {
+    type: AlunaOrderTypesEnum.LIMIT,
+    supported: true,
+    implemented: true,
+    mode: AlunaFeaturesModeEnum.WRITE,
+    options: {
+      rate: 1,
+      amount: 1,
+    },
+  },
+  {
+    type: AlunaOrderTypesEnum.MARKET,
+    supported: true,
+    implemented: true,
+    mode: AlunaFeaturesModeEnum.WRITE,
+    options: {
+      rate: 1,
+      amount: 1,
+    },
+  },
+  {
+    type: AlunaOrderTypesEnum.STOP_LIMIT,
+    supported: true,
+    implemented: true,
+    mode: AlunaFeaturesModeEnum.READ,
+    options: {
+      rate: 1,
+      amount: 1,
+      limitRate: 1,
+    },
+  },
+  {
+    type: AlunaOrderTypesEnum.TRAILING_STOP,
+    supported: true,
+    implemented: true,
+    mode: AlunaFeaturesModeEnum.READ,
+    options: {
+      rate: 1,
+      amount: 1,
+      limitRate: 1,
+    },
+  },
+]
+
+export const BittrexSpecs: IAlunaExchangeSchema = {
+  id: 'bittrex',
+  name: 'Bittrex',
+  signupUrl: 'https://global.bittrex.com/account/register',
+  connectApiUrl: 'https://global.bittrex.com/Manage?view=api',
+  rateLimitingPerMinute: {
+    perApiKey: 60,
+    perIp: 60,
+  },
+  modes: {
+    balance: AlunaFeaturesModeEnum.READ,
+    order: AlunaFeaturesModeEnum.WRITE,
+  },
+  accounts: [
+    {
+      type: AlunaAccountEnum.EXCHANGE,
+      supported: true,
+      implemented: true,
+      orderTypes: exchangeOrderTypes,
+    },
+    {
+      type: AlunaAccountEnum.MARGIN,
+      supported: false
+    },
+    {
+      type: AlunaAccountEnum.DERIVATIVES,
+      supported: false
+    },
+    {
+      type: AlunaAccountEnum.LENDING,
+      supported: false
+    },
+  ],
+}
