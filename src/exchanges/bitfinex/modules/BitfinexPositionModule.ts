035d634ad40f13265dea3a4297debe5161f6af54
Sergio Torres <sergio.uft@gmail.com>
2022-02-08 09:49:15 -0300

    implement 'parse' and 'parseMany' on 'BitfinexPositionModule'

--- src/exchanges/bitfinex/modules/BitfinexPositionModule.ts
+++ src/exchanges/bitfinex/modules/BitfinexPositionModule.ts
@@ -8,14 +8,21 @@ import {
 import { IAlunaPositionSchema } from '../../../lib/schemas/IAlunaPositionSchema'
 import { BitfinexHttp } from '../BitfinexHttp'
 import { IBitfinexPositionSchema } from '../schemas/IBitfinexPositionSchema'
+import { BitfinexPositionParser } from '../schemas/parsers/BitfinexPositionParser'
 
 
 
 export class BitfinexPositionModule extends AAlunaModule implements IAlunaPositionModule {
 
-  list (params?: IAlunaPositionListParams): Promise<IAlunaPositionSchema[]> {
+  async list (
+    params?: IAlunaPositionListParams,
+  ): Promise<IAlunaPositionSchema[]> {
 
-    throw new Error('Method not implemented.')
+    const rawPositions = await this.listRaw(params)
+
+    const parsedPositions = this.parseMany({ rawPositions })
+
+    return parsedPositions
 
   }
 
@@ -24,18 +31,25 @@ export class BitfinexPositionModule extends AAlunaModule implements IAlunaPositi
   ): Promise<IBitfinexPositionSchema[]> {
 
     let url = 'https://api.bitfinex.com/v2/auth/r/positions'
+
     const body: Record<string, any> = {}
 
     if (params) {
 
-      const { end, start, openPositionsOnly } = params
+      const {
+        end,
+        start,
+        openPositionsOnly,
+        limit,
+      } = params
 
-      if (!openPositionsOnly && end && start) {
+      if (!openPositionsOnly) {
 
         body.start = start
         body.end = end
+        body.status = limit
 
-        url = 'https://api.bitfinex.com/v2/auth/r/positions/snap'
+        url = 'https://api.bitfinex.com/v2/auth/r/positions/hist'
 
       }
 
@@ -58,24 +72,56 @@ export class BitfinexPositionModule extends AAlunaModule implements IAlunaPositi
     throw new Error('Method not implemented.')
 
   }
+
   getRaw (params?: IAlunaPositionGetParams): Promise<any> {
 
     throw new Error('Method not implemented.')
 
   }
+
   close (params: IAlunaPositionCloseParams): Promise<IAlunaPositionSchema> {
 
     throw new Error('Method not implemented.')
 
   }
-  parse (params: { rawPosition: any }): IAlunaPositionSchema {
 
-    throw new Error('Method not implemented.')
+  parse (params: {
+    rawPosition: IBitfinexPositionSchema,
+  }): IAlunaPositionSchema {
+
+    const { rawPosition } = params
+
+    const parsedPosition = BitfinexPositionParser.parse({
+      rawPosition,
+    })
+
+    return parsedPosition
 
   }
-  parseMany (parms: { rawPositions: any[] }): IAlunaPositionSchema[] {
 
-    throw new Error('Method not implemented.')
+  parseMany (params: {
+    rawPositions: IBitfinexPositionSchema[],
+  }): IAlunaPositionSchema[] {
+
+    const { rawPositions } = params
+
+    const parsedPositions = rawPositions.reduce((acc, rawPosition) => {
+
+      if (rawPosition[14] === 1) {
+
+        return acc
+
+      }
+
+      const parsedPosition = this.parse({ rawPosition })
+
+      acc.push(parsedPosition)
+
+      return acc
+
+    }, [] as IAlunaPositionSchema[])
+
+    return parsedPositions
 
   }
 
