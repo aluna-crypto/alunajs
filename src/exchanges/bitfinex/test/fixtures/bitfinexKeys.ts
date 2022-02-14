ef44a3b2e02cbcfe22e35e4f8ac83e141fe6f00e
Sergio Torres <sergio.uft@gmail.com>
2022-02-14 08:50:10 -0300

    refact keys fixtures to conform with last changes

--- src/exchanges/bitfinex/test/fixtures/bitfinexKeys.ts
+++ src/exchanges/bitfinex/test/fixtures/bitfinexKeys.ts
@@ -1,9 +1,14 @@
 import { IAlunaKeySchema } from '../../../../lib/schemas/IAlunaKeySchema'
-import { IBitfinexKey } from '../../schemas/IBitfinexKeySchema'
+import {
+  IBitfinexKeySchema,
+  IBitfinexPermissionsScope,
+} from '../../schemas/IBitfinexKeySchema'
 
 
 
-export const BITFINEX_RAW_KEY: IBitfinexKey = [
+export const BITFINEX_USER_ACCOUNT_ID = 'userId'
+
+export const BITFINEX_PERMISSIONS_SCOPE: IBitfinexPermissionsScope = [
   ['account', 1, 0],
   ['orders', 1, 1],
   ['funding', 1, 0],
@@ -12,7 +17,10 @@ export const BITFINEX_RAW_KEY: IBitfinexKey = [
   ['withdraw', 0, 0],
 ]
 
-
+export const BITFINEX_RAW_KEY: IBitfinexKeySchema = {
+  permissionsScope: BITFINEX_PERMISSIONS_SCOPE,
+  accountId: BITFINEX_USER_ACCOUNT_ID,
+}
 
 export const BITFINEX_PARSED_KEY: IAlunaKeySchema = {
   permissions: {
@@ -20,13 +28,9 @@ export const BITFINEX_PARSED_KEY: IAlunaKeySchema = {
     trade: true,
     withdraw: false,
   },
-  accountId: undefined,
-  meta: [
-    ['account', 1, 0],
-    ['orders', 1, 1],
-    ['funding', 1, 0],
-    ['settings', 0, 0],
-    ['wallets', 1, 0],
-    ['withdraw', 0, 0],
-  ],
+  accountId: BITFINEX_USER_ACCOUNT_ID,
+  meta: {
+    permissionsScope: BITFINEX_PERMISSIONS_SCOPE,
+    accountId: BITFINEX_USER_ACCOUNT_ID,
+  },
 }
