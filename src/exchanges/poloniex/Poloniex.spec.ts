e029e25fbcc8d75dcb92049230a8cf243d1d14a9
LucianoPierdona <lucianompj1@gmail.com>
2022-02-17 07:50:28 -0300

    created Poloniex file and tests

---
+++ src/exchanges/poloniex/Poloniex.spec.ts
@@ -0,0 +1,48 @@
+import { expect } from 'chai'
+
+import { Poloniex } from './Poloniex'
+
+
+
+describe('Poloniex', () => {
+
+  it('should have all static properties and methods', async () => {
+
+    expect(Poloniex.ID).to.eq('poloniex')
+    expect(Poloniex.SPECS).to.be.ok
+    // @TODO
+    // expect(Poloniex.Symbol).to.be.ok
+    // expect(Poloniex.Market).to.be.ok
+
+  })
+
+  it('should have all instance properties and methods', async () => {
+
+    const key = 'asdf'
+    const secret = 'qwer'
+    const referralCode = 'xyz'
+
+    const poloniex = new Poloniex({
+      keySecret: {
+        key,
+        secret,
+      },
+      settings: {
+        referralCode,
+      },
+    })
+
+    expect(poloniex.keySecret.key).to.eq(key)
+    expect(poloniex.keySecret.secret).to.eq(secret)
+
+    expect(poloniex.settings).to.be.ok
+    expect(poloniex.settings?.referralCode).to.eq(referralCode)
+
+    // @TODO
+    // expect(poloniex.key).to.be.ok
+    // expect(poloniex.balance).to.be.ok
+    // expect(poloniex.order).to.be.ok
+
+  })
+
+})
---
+++ src/exchanges/poloniex/Poloniex.ts
@@ -0,0 +1,48 @@
+import { AAlunaExchange } from '../../lib/core/abstracts/AAlunaExchange'
+import {
+  IAlunaExchange,
+  IAlunaExchangeStatic,
+} from '../../lib/core/IAlunaExchange'
+import { IAlunaBalanceModule } from '../../lib/modules/IAlunaBalanceModule'
+import { IAlunaKeyModule } from '../../lib/modules/IAlunaKeyModule'
+import { IAlunaOrderWriteModule } from '../../lib/modules/IAlunaOrderModule'
+import { IAlunaKeySecretSchema } from '../../lib/schemas/IAlunaKeySecretSchema'
+import { IAlunaSettingsSchema } from '../../lib/schemas/IAlunaSettingsSchema'
+import { PoloniexLog } from './PoloniexLog'
+import { PoloniexSpecs } from './PoloniexSpecs'
+
+
+
+export const Poloniex: IAlunaExchangeStatic = class extends AAlunaExchange implements IAlunaExchange {
+
+  // static definitions
+  static readonly ID = PoloniexSpecs.id
+  static readonly SPECS = PoloniexSpecs
+
+  // @TODO
+  static Symbol = null as any
+  static Market = null as any
+
+  // local definitions
+  key: IAlunaKeyModule
+  order: IAlunaOrderWriteModule
+  balance: IAlunaBalanceModule
+
+  constructor (
+    params: {
+      keySecret: IAlunaKeySecretSchema,
+      settings?: IAlunaSettingsSchema,
+    },
+  ) {
+
+    super(params)
+
+    // @TODO
+    PoloniexLog.info('poloniex')
+    // this.key = new PoloniexKeyModule({ exchange: this })
+    // this.balance = new PoloniexBalanceModule({ exchange: this })
+    // this.order = new PoloniexOrderWriteModule({ exchange: this })
+
+  }
+
+}
