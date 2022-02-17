f129ccd8be08de7daa90a0cfe5cec596e1bbab06
Sergio Torres <sergio.uft@gmail.com>
2022-02-17 19:49:32 -0300

    implement 'BitfinexSymbolMapping'

---
+++ src/exchanges/bitfinex/mappings/BitfinexSymbolMapping.ts
@@ -0,0 +1,49 @@
+interface ISymbolMappingResponse {
+  baseSymbolId: string
+  quoteSymbolId: string
+}
+
+
+
+export class BitfinexSymbolMapping {
+
+  static translateToAluna (params: {
+    symbolPair: string,
+    mappings?: Record<string, string>,
+  }): ISymbolMappingResponse {
+
+    const {
+      symbolPair,
+      mappings,
+    } = params
+
+    let baseSymbolId: string
+    let quoteSymbolId: string
+
+    const spliter = symbolPair.indexOf(':')
+
+    if (spliter >= 0) {
+
+      baseSymbolId = symbolPair.slice(1, spliter)
+      quoteSymbolId = symbolPair.slice(spliter + 1)
+
+    } else {
+
+      baseSymbolId = symbolPair.slice(1, 4)
+      quoteSymbolId = symbolPair.slice(4)
+
+    }
+
+    baseSymbolId = mappings?.[baseSymbolId] || baseSymbolId
+    quoteSymbolId = mappings?.[baseSymbolId] || quoteSymbolId
+
+    const translated: ISymbolMappingResponse = {
+      baseSymbolId,
+      quoteSymbolId,
+    }
+
+    return translated
+
+  }
+
+}
