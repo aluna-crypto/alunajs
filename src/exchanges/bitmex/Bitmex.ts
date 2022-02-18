758d439bb102f5b72c86a934fb811f31d5a7ae7d
Sergio Torres <sergio.uft@gmail.com>
2022-02-18 10:14:14 -0300

    implement Bitmex class

---
+++ src/exchanges/bitmex/Bitmex.ts
@@ -0,0 +1,46 @@
+import { AAlunaExchange } from '../../lib/core/abstracts/AAlunaExchange'
+import {
+  IAlunaExchange,
+  IAlunaExchangeStatic,
+} from '../../lib/core/IAlunaExchange'
+import { IAlunaBalanceModule } from '../../lib/modules/IAlunaBalanceModule'
+import { IAlunaKeyModule } from '../../lib/modules/IAlunaKeyModule'
+import { IAlunaOrderWriteModule } from '../../lib/modules/IAlunaOrderModule'
+import { IAlunaPositionModule } from '../../lib/modules/IAlunaPositionModule'
+import { BitmexSpecs } from './BitmexSpecs'
+
+
+
+export const Bitmex: IAlunaExchangeStatic = class extends AAlunaExchange implements IAlunaExchange {
+
+  // static definitions
+  static readonly ID = BitmexSpecs.id
+  static readonly SPECS = BitmexSpecs
+
+  static Symbol = {} as any
+  static Market = {} as any
+
+  // local definitions
+  key: IAlunaKeyModule
+  // order: IAlunaOrderWriteModule
+  order: IAlunaOrderWriteModule
+  balance: IAlunaBalanceModule
+  position: IAlunaPositionModule
+
+  // constructor (
+  //   params: {
+  //     keySecret: IAlunaKeySecretSchema,
+  //     settings?: IAlunaSettingsSchema,
+  //   },
+  // ) {
+
+  //   super(params)
+
+  //   this.key = new BitmexKeyModule({ exchange: this })
+  //   this.balance = new BitmexBalanceModule({ exchange: this })
+  //   this.order = new BitmexOrderWriteModule({ exchange: this })
+  //   this.position = new BitmexPositionModule({ exchange: this })
+
+  // }
+
+}
