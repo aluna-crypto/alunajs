8d98051f9d5ecbca41429c46cd556e33ce606656
Sergio Torres <sergio.uft@gmail.com>
2022-02-18 12:53:46 -0300

    add test case for 'parseMany' on 'BitmexSymbolModule' test suite

--- src/exchanges/bitmex/modules/BitmexSymbolModule.spec.ts
+++ src/exchanges/bitmex/modules/BitmexSymbolModule.spec.ts
@@ -1,9 +1,13 @@
 import { expect } from 'chai'
+import { each } from 'lodash'
 import { ImportMock } from 'ts-mock-imports'
 
 import { BitmexHttp } from '../BitmexHttp'
 import { BitmexSpecs } from '../BitmexSpecs'
-import { BITMEX_RAW_SYMBOLS } from '../test/bitmexSymbols'
+import {
+  BITMEX_PARSED_SYMBOLS,
+  BITMEX_RAW_SYMBOLS,
+} from '../test/bitmexSymbols'
 import { BitmexSymbolModule } from './BitmexSymbolModule'
 
 
@@ -26,6 +30,29 @@ describe.only('BitmexSymbolModule', () => {
     })
 
     expect(rawSymbols).to.be.eq(BITMEX_RAW_SYMBOLS)
+
+  })
+
+  it('should parse many Bitmex raw symbols just fine', () => {
+
+    const parseMock = ImportMock.mockFunction(
+      BitmexSymbolModule,
+      'parse',
+    )
+
+    each(BITMEX_PARSED_SYMBOLS, (rawSymbol, i) => {
+
+      parseMock.onCall(i).returns(rawSymbol)
+
+    })
+
+
+    const rawSymbols = BitmexSymbolModule.parseMany({
+      rawSymbols: BITMEX_RAW_SYMBOLS,
+    })
+
+    expect(parseMock.callCount).to.deep.eq(BITMEX_PARSED_SYMBOLS.length)
+    expect(rawSymbols).to.deep.eq(BITMEX_PARSED_SYMBOLS)
 
   })
 
