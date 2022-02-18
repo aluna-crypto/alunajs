b6510494eeaa4899ef6f578005f4773b76cd8220
Sergio Torres <sergio.uft@gmail.com>
2022-02-18 16:21:47 -0300

    add test suite for 'BitmexMarketParser'

---
+++ src/exchanges/bitmex/schemas/parsers/BitmexMarketParser.spec.ts
@@ -0,0 +1,94 @@
+import { BigNumber } from 'bignumber.js'
+import { expect } from 'chai'
+import { each } from 'lodash'
+import { ImportMock } from 'ts-mock-imports'
+
+import { IAlunaInstrumentSchema } from '../../../../lib/schemas/IAlunaInstrumentSchema'
+import { IAlunaTickerSchema } from '../../../../lib/schemas/IAlunaTickerSchema'
+import { BitmexSpecs } from '../../BitmexSpecs'
+import { BITMEX_RAW_SYMBOLS } from '../../test/bitmexSymbols'
+import { BitmexInstrumentParser } from './BitmexInstrumentParser'
+import { BitmexMarketParser } from './BitmexMarketParser'
+
+
+
+describe('BitmexMarketParser', () => {
+
+
+  it('should properly parse Bitmex markets', () => {
+
+    const mockedInstrument = {} as IAlunaInstrumentSchema
+
+    const bitmexInstrumentParserMock = ImportMock.mockFunction(
+      BitmexInstrumentParser,
+      'parse',
+      mockedInstrument,
+    )
+
+    each(BITMEX_RAW_SYMBOLS, (rawMarket) => {
+
+      const {
+        symbol,
+        rootSymbol,
+        quoteCurrency,
+        highPrice,
+        lowPrice,
+        bidPrice,
+        lastPrice,
+        prevClosePrice,
+        volume24h,
+        askPrice,
+        initMargin,
+      } = rawMarket
+
+      const change = (1 - (lastPrice / prevClosePrice))
+      const quoteVolume = volume24h
+      const baseVolume = new BigNumber(volume24h)
+        .div(bidPrice)
+        .toNumber()
+
+      const ticker: IAlunaTickerSchema = {
+        high: highPrice,
+        low: lowPrice,
+        bid: bidPrice,
+        ask: askPrice,
+        last: lastPrice,
+        change,
+        date: new Date(),
+        baseVolume,
+        quoteVolume,
+      }
+
+      const maxLeverage = (1 / initMargin)
+
+      const parsedMarket = BitmexMarketParser.parse({
+        rawMarket,
+      })
+
+      expect(parsedMarket.exchangeId).to.deep.eq(BitmexSpecs.id)
+
+      expect(parsedMarket.symbolPair).to.deep.eq(symbol)
+      expect(parsedMarket.baseSymbolId).to.deep.eq(rootSymbol)
+      expect(parsedMarket.quoteSymbolId).to.deep.eq(quoteCurrency)
+
+      expect(parsedMarket.instrument).to.deep.eq(mockedInstrument)
+
+      expect(parsedMarket.ticker).to.deep.eq(ticker)
+
+      expect(parsedMarket.maxLeverage).to.be.eq(maxLeverage)
+      expect(parsedMarket.leverageEnabled).to.be.ok
+
+      expect(parsedMarket.spotEnabled).not.to.be.ok
+      expect(parsedMarket.marginEnabled).not.to.be.ok
+
+      expect(parsedMarket.meta).to.deep.eq(rawMarket)
+
+    })
+
+    const { length } = BITMEX_RAW_SYMBOLS
+
+    expect(bitmexInstrumentParserMock.callCount).to.be.eq(length)
+
+  })
+
+})
