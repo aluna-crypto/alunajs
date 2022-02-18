8326260c827a75dde736d32dccb3070092893f77
Sergio Torres <sergio.uft@gmail.com>
2022-02-18 08:56:46 -0300

    refact 'BitfinexMarketParser'

--- src/exchanges/bitfinex/schemas/parsers/BitfinexMarketParser.ts
+++ src/exchanges/bitfinex/schemas/parsers/BitfinexMarketParser.ts
@@ -1,15 +1,14 @@
 import { IAlunaMarketSchema } from '../../../../lib/schemas/IAlunaMarketSchema'
 import { IAlunaTickerSchema } from '../../../../lib/schemas/IAlunaTickerSchema'
 import { Bitfinex } from '../../Bitfinex'
+import { BitfinexSymbolMapping } from '../../mappings/BitfinexSymbolMapping'
 import { IBitfinexTicker } from '../IBitfinexMarketSchema'
-import { TBitfinexCurrencySym } from '../IBitfinexSymbolSchema'
 
 
 
 export interface IBitfinexMarketParseParams {
   rawTicker: IBitfinexTicker
   enabledMarginMarketsDict: Record<string, string>
-  currencySymsDict: Record<string, TBitfinexCurrencySym>
 }
 
 
@@ -20,7 +19,6 @@ export class BitfinexMarketParser {
 
     const {
       rawTicker,
-      currencySymsDict,
       enabledMarginMarketsDict,
     } = params
 
@@ -35,30 +33,13 @@ export class BitfinexMarketParser {
       low,
     ] = rawTicker
 
-    let baseSymbolId: string
-    let quoteSymbolId: string
-
-    const spliter = symbol.indexOf(':')
-
-    if (spliter >= 0) {
-
-      baseSymbolId = symbol.slice(1, spliter)
-      quoteSymbolId = symbol.slice(spliter + 1)
-
-    } else {
-
-      baseSymbolId = symbol.slice(1, 4)
-      quoteSymbolId = symbol.slice(4)
-
-    }
-
-    baseSymbolId = currencySymsDict[baseSymbolId]
-      ? currencySymsDict[baseSymbolId][1].toUpperCase()
-      : baseSymbolId
-
-    quoteSymbolId = currencySymsDict[quoteSymbolId]
-      ? currencySymsDict[quoteSymbolId][1].toUpperCase()
-      : quoteSymbolId
+    const {
+      baseSymbolId,
+      quoteSymbolId,
+    } = BitfinexSymbolMapping.translateToAluna({
+      symbolPair: symbol,
+      mappings: Bitfinex.mappings,
+    })
 
     const ticker: IAlunaTickerSchema = {
       bid,
