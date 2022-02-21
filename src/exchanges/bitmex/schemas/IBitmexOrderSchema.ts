f4dfdb80aed1243def2715b066056d96350b4064
Sergio Torres <sergio.uft@gmail.com>
2022-02-21 13:21:34 -0300

    implement 'IBitmexOrderSchema'

---
+++ src/exchanges/bitmex/schemas/IBitmexOrderSchema.ts
@@ -0,0 +1,35 @@
+export interface IBitmexOrderSchema {
+  orderID: string
+  clOrdID: string
+  clOrdLinkID: string
+  account: number
+  symbol: string
+  side: string
+  simpleOrderQty: number
+  orderQty: number
+  price: number
+  displayQty: number
+  stopPx: number
+  pegOffsetValue: number
+  pegPriceType: string
+  currency: string
+  settlCurrency: string
+  ordType: string
+  timeInForce: string
+  execInst: string
+  contingencyType: string
+  exDestination: string
+  ordStatus: string
+  triggered: string
+  workingIndicator: true
+  ordRejReason: string
+  simpleLeavesQty: number
+  leavesQty: number
+  simpleCumQty: number
+  cumQty: number
+  avgPx: number
+  multiLegReportingType: string
+  text: string
+  transactTime: string
+  timestamp: string
+}
