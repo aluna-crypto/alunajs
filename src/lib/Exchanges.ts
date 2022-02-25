a1cc2149b50434811c4d59314fd9bc8ac0160677
Sergio Torres <sergio.uft@gmail.com>
2022-02-25 10:54:23 -0300

    add 'Bitmex' to 'Exchanges' class

--- src/lib/Exchanges.ts
+++ src/lib/Exchanges.ts
@@ -1,5 +1,6 @@
 import { Binance } from '../exchanges/binance/Binance'
 import { Bitfinex } from '../exchanges/bitfinex/Bitfinex'
+import { Bitmex } from '../exchanges/bitmex/Bitmex'
 import { Bittrex } from '../exchanges/bittrex/Bittrex'
 import { Valr } from '../exchanges/valr/Valr'
 
@@ -9,6 +10,7 @@ export class Exchanges {
 
   public static readonly Binance = Binance
   public static readonly Bitfinex = Bitfinex
+  public static readonly Bitmex = Bitmex
   public static readonly Bittrex = Bittrex
   public static readonly Valr = Valr
 
