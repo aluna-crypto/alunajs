72069cc62775c16114f8352d43ae23b49be29f2c
LucianoPierdona <lucianompj1@gmail.com>
2022-03-02 09:40:57 -0300

    Implemented Ftx main file

---
+++ src/exchanges/ftx/Ftx.ts
@@ -0,0 +1,48 @@
+import { AAlunaExchange } from '../../lib/core/abstracts/AAlunaExchange'
+import {
+  IAlunaExchange,
+  IAlunaExchangeStatic,
+} from '../../lib/core/IAlunaExchange'
+import { IAlunaBalanceModule } from '../../lib/modules/IAlunaBalanceModule'
+import { IAlunaKeyModule } from '../../lib/modules/IAlunaKeyModule'
+import { IAlunaOrderWriteModule } from '../../lib/modules/IAlunaOrderModule'
+import { IAlunaKeySecretSchema } from '../../lib/schemas/IAlunaKeySecretSchema'
+import { IAlunaSettingsSchema } from '../../lib/schemas/IAlunaSettingsSchema'
+import { FtxLog } from './FtxLog'
+import { FtxSpecs } from './FtxSpecs'
+
+
+
+export const Ftx: IAlunaExchangeStatic = class extends AAlunaExchange implements IAlunaExchange {
+
+  // static definitions
+  static readonly ID = FtxSpecs.id
+  static readonly SPECS = FtxSpecs
+
+  // @TODO -> Update
+  static Symbol = null as any
+  static Market = null as any
+  // static Symbol = FtxSymbolModule
+  // static Market = FtxMarketModule
+
+  // local definitions
+  key: IAlunaKeyModule
+  order: IAlunaOrderWriteModule
+  balance: IAlunaBalanceModule
+
+  constructor (params: {
+    keySecret: IAlunaKeySecretSchema,
+    settings?: IAlunaSettingsSchema,
+  }) {
+
+    super(params)
+
+    FtxLog.info('Fodase')
+    // @TODO -> Update
+    // this.key = new FtxKeyModule({ exchange: this })
+    // this.balance = new FtxBalanceModule({ exchange: this })
+    // this.order = new FtxOrderWriteModule({ exchange: this })
+
+  }
+
+}
