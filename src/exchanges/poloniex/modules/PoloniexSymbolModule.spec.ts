542e298dd355d18b65a0d7579242c9ba71205be7
Sergio Torres <sergio.uft@gmail.com>
2022-03-03 08:50:18 -0300

    made 'PoloniexSymbolModule' use symbol mapping

--- src/exchanges/poloniex/modules/PoloniexSymbolModule.spec.ts
+++ src/exchanges/poloniex/modules/PoloniexSymbolModule.spec.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai'
 import { ImportMock } from 'ts-mock-imports'
 
+import { mockAlunaSymbolMapping } from '../../../utils/mappings/AlunaSymbolMapping.mock'
 import { Poloniex } from '../Poloniex'
 import { PoloniexHttp } from '../PoloniexHttp'
 import { PoloniexCurrencyParser } from '../schemas/parsers/PoloniexCurrencyParser'
@@ -84,25 +85,46 @@ describe('PoloniexSymbolModule', () => {
 
   it('should parse a Poloniex symbol just fine', async () => {
 
+    const translateSymbolId = 'ETH'
+
+    const { alunaSymbolMappingMock } = mockAlunaSymbolMapping({
+      returnSymbol: translateSymbolId,
+    })
+
+    const rawSymbol1 = POLONIEX_RAW_SYMBOLS_WITH_CURRENCY[1]
+    const rawSymbol2 = POLONIEX_RAW_SYMBOLS_WITH_CURRENCY[2]
+
     const parsedSymbol1 = PoloniexSymbolModule.parse({
-      rawSymbol: POLONIEX_RAW_SYMBOLS_WITH_CURRENCY[1],
+      rawSymbol: rawSymbol1,
     })
 
     expect(parsedSymbol1.exchangeId).to.be.eq(Poloniex.ID)
-    expect(parsedSymbol1.id)
-      .to.be.eq(POLONIEX_RAW_SYMBOLS_WITH_CURRENCY[1].currency)
-    expect(parsedSymbol1.name)
-      .to.be.eq(POLONIEX_RAW_SYMBOLS_WITH_CURRENCY[1].name)
+    expect(parsedSymbol1.id).to.be.eq(translateSymbolId)
+    expect(parsedSymbol1.name).to.be.eq(rawSymbol1.name)
+    expect(parsedSymbol1.alias).to.be.eq(rawSymbol1.currency)
+
+    expect(alunaSymbolMappingMock.callCount).to.be.eq(1)
+    expect(alunaSymbolMappingMock.args[0][0]).to.deep.eq({
+      exchangeSymbolId: rawSymbol1.currency,
+      symbolMappings: Poloniex.settings.mappings,
+    })
+
+    alunaSymbolMappingMock.returns(rawSymbol2.currency)
 
     const parsedSymbol2 = PoloniexSymbolModule.parse({
-      rawSymbol: POLONIEX_RAW_SYMBOLS_WITH_CURRENCY[2],
+      rawSymbol: rawSymbol2,
     })
 
     expect(parsedSymbol2.exchangeId).to.be.eq(Poloniex.ID)
-    expect(parsedSymbol2.id)
-      .to.be.eq(POLONIEX_RAW_SYMBOLS_WITH_CURRENCY[2].currency)
-    expect(parsedSymbol2.name)
-      .to.be.eq(POLONIEX_RAW_SYMBOLS_WITH_CURRENCY[2].name)
+    expect(parsedSymbol2.id).to.be.eq(rawSymbol2.currency)
+    expect(parsedSymbol2.name).to.be.eq(rawSymbol2.name)
+    expect(parsedSymbol2.alias).not.to.be.ok
+
+    expect(alunaSymbolMappingMock.callCount).to.be.eq(2)
+    expect(alunaSymbolMappingMock.args[1][0]).to.deep.eq({
+      exchangeSymbolId: rawSymbol2.currency,
+      symbolMappings: Poloniex.settings.mappings,
+    })
 
   })
 
