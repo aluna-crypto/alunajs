bf8bd3c6b86965d6880c07d47dcef18d9f14e270
Sergio Torres <sergio.uft@gmail.com>
2022-03-03 15:22:40 -0300

    add mock file for 'AlunaCache'

---
+++ src/utils/cache/AlunaCache.mock.ts
@@ -0,0 +1,39 @@
+import { spy } from 'sinon'
+import { ImportMock } from 'ts-mock-imports'
+
+import { AlunaCache } from './AlunaCache'
+
+
+
+export const mockAlunaCache = (returns?: {
+  get?: any,
+  set?: boolean,
+  has?: boolean,
+}) => {
+
+  const {
+    get = {},
+    set = true,
+    has = false,
+  } = returns || {}
+
+  const cache = {
+    get: spy(() => get),
+    set: spy(() => set),
+    has: spy(() => has),
+  }
+
+  ImportMock.mockOther(AlunaCache, 'cache', cache)
+
+  const hashCacheKey = ImportMock.mockFunction(
+    AlunaCache,
+    'hashCacheKey',
+    'abc',
+  )
+
+  return {
+    cache,
+    hashCacheKey,
+  }
+
+}
