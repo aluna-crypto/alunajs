c0e6b3e818ffec05781ccac5e8abc9c7408f8e78
Sergio Torres <sergio.uft@gmail.com>
2022-03-11 10:10:30 -0300

    revert change on some Aluna modules interfaces

--- src/lib/modules/IAlunaMarketModule.ts
+++ src/lib/modules/IAlunaMarketModule.ts
@@ -5,12 +5,12 @@ import { IAlunaMarketSchema } from '../schemas/IAlunaMarketSchema'
 export interface IAlunaMarketModule {
 
   list (): Promise<IAlunaMarketSchema[]>
-  listRaw (): Promise<any>
+  listRaw (): Promise<any[]>
 
   get? (params: { symbolPair: string }): Promise<IAlunaMarketSchema>
   getRaw? (params: { symbolPair: string }): Promise<any>
 
   parse (params: { rawMarket: any }): IAlunaMarketSchema
-  parseMany (parms: { rawMarkets: any }): IAlunaMarketSchema[]
+  parseMany (parms: { rawMarkets: any[] }): IAlunaMarketSchema[]
 
 }
--- src/lib/modules/IAlunaSymbolModule.ts
+++ src/lib/modules/IAlunaSymbolModule.ts
@@ -5,12 +5,12 @@ import { IAlunaSymbolSchema } from '../schemas/IAlunaSymbolSchema'
 export interface IAlunaSymbolModule {
 
   list (): Promise<IAlunaSymbolSchema[]>
-  listRaw (): Promise<any>
+  listRaw (): Promise<any[]>
 
   get? (params: { symbolPair: string }): Promise<IAlunaSymbolSchema>
   getRaw? (params: { symbolPair: string }): Promise<any>
 
   parse (params: { rawSymbol: any }): IAlunaSymbolSchema
-  parseMany (params: { rawSymbols: any }): IAlunaSymbolSchema[]
+  parseMany (params: { rawSymbols: any[] }): IAlunaSymbolSchema[]
 
 }
