e341ea52eab4b697c5b04cf5f6d6d8c26d16f311
Sergio Torres <sergio.uft@gmail.com>
2022-03-30 17:31:51 -0300

    made 'ValrHttp' use 'assembleAxiosRequestConfig'

--- src/exchanges/valr/ValrHttp.ts
+++ src/exchanges/valr/ValrHttp.ts
@@ -1,9 +1,5 @@
-import axios, {
-  AxiosError,
-  AxiosRequestConfig,
-} from 'axios'
+import axios, { AxiosError } from 'axios'
 import crypto from 'crypto'
-import { assign } from 'lodash'
 import { URL } from 'url'
 
 import { AlunaError } from '../../lib/core/AlunaError'
@@ -15,6 +11,7 @@ import {
 import { AlunaHttpVerbEnum } from '../../lib/enums/AlunaHtttpVerbEnum'
 import { AlunaHttpErrorCodes } from '../../lib/errors/AlunaHttpErrorCodes'
 import { IAlunaKeySecretSchema } from '../../lib/schemas/IAlunaKeySecretSchema'
+import { assembleAxiosRequestConfig } from '../../utils/axios/assembleAxiosRequestConfig'
 import { AlunaCache } from '../../utils/cache/AlunaCache'
 import { Valr } from './Valr'
 import { ValrLog } from './ValrLog'
@@ -117,23 +114,12 @@ export const ValrHttp: IAlunaHttp = class {
 
     }
 
-    let requestConfig: AxiosRequestConfig = {
+    const { requestConfig } = assembleAxiosRequestConfig({
       url,
       method: verb,
       data: body,
-    }
-
-    const { proxySettings } = Valr.settings
-
-    if (proxySettings) {
-
-      const { agent, ...proxy } = proxySettings
-
-      requestConfig = proxy.protocol === 'https'
-        ? assign(requestConfig, { proxy, httpsAgent: agent })
-        : assign(requestConfig, { proxy, httpAgent: agent })
-
-    }
+      proxySettings: Valr.settings.proxySettings,
+    })
 
     try {
 
@@ -167,24 +153,13 @@ export const ValrHttp: IAlunaHttp = class {
       body,
     })
 
-    let requestConfig: AxiosRequestConfig = {
+    const { requestConfig } = assembleAxiosRequestConfig({
       url,
       method: verb,
       data: body,
       headers: signedHash,
-    }
-
-    const { proxySettings } = Valr.settings
-
-    if (proxySettings) {
-
-      const { agent, ...proxy } = proxySettings
-
-      requestConfig = proxy.protocol === 'https'
-        ? assign(requestConfig, { proxy, httpsAgent: agent })
-        : assign(requestConfig, { proxy, httpAgent: agent })
-
-    }
+      proxySettings: Valr.settings.proxySettings,
+    })
 
     try {
 
