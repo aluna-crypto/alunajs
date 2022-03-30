0cd2fad5bb7be702e389ce02fbcefd52a36a13b1
Sergio Torres <sergio.uft@gmail.com>
2022-03-30 17:28:29 -0300

    refact 'BitfinexHttp' test suite

--- src/exchanges/bitfinex/BitfinexHttp.spec.ts
+++ src/exchanges/bitfinex/BitfinexHttp.spec.ts
@@ -1,8 +1,6 @@
 import { AxiosError } from 'axios'
 import { expect } from 'chai'
 import crypto from 'crypto'
-import { Agent as HttpAgent } from 'http'
-import { Agent as HttpsAgent } from 'https'
 import Sinon from 'sinon'
 import { ImportMock } from 'ts-mock-imports'
 
@@ -12,10 +10,8 @@ import { IAlunaHttpPublicParams } from '../../lib/core/IAlunaHttp'
 import { AlunaHttpVerbEnum } from '../../lib/enums/AlunaHtttpVerbEnum'
 import { AlunaGenericErrorCodes } from '../../lib/errors/AlunaGenericErrorCodes'
 import { IAlunaKeySecretSchema } from '../../lib/schemas/IAlunaKeySecretSchema'
-import {
-  IAlunaProxySchema,
-  IAlunaSettingsSchema,
-} from '../../lib/schemas/IAlunaSettingsSchema'
+import { IAlunaSettingsSchema } from '../../lib/schemas/IAlunaSettingsSchema'
+import { mockAssembleRequestConfig } from '../../utils/axios/assembleAxiosRequestConfig.mock'
 import {
   mockAlunaCache,
   validateCache,
@@ -49,23 +45,6 @@ describe('BitfinexHttp', () => {
     secret: 'secret',
   }
 
-  const host = 'localhost'
-  const port = 3000
-
-  const httpProxySettings: IAlunaProxySchema = {
-    host,
-    port,
-    protocol: 'http',
-    agent: new HttpAgent({ keepAlive: true }),
-  }
-
-  const httpsProxySettings: IAlunaProxySchema = {
-    host,
-    port,
-    protocol: 'https',
-    agent: new HttpsAgent({ keepAlive: true }),
-  }
-
   const mockDeps = (
     params: {
       requestResponse?: any,
@@ -87,6 +66,8 @@ describe('BitfinexHttp', () => {
       errorMsgRes = 'error',
       mockedExchangeSettings = {},
     } = params
+
+    const { assembleAxiosRequestMock } = mockAssembleRequestConfig()
 
     const throwedError = new AlunaError({
       code: AlunaGenericErrorCodes.UNKNOWN,
@@ -134,6 +115,7 @@ describe('BitfinexHttp', () => {
       exchangeMock,
       axiosCreateMock,
       formatRequestErrorSpy,
+      assembleAxiosRequestMock,
       generateAuthHeaderMock,
     }
 
@@ -488,116 +470,6 @@ describe('BitfinexHttp', () => {
       error.message,
     ).to.be.eq('Error while trying to execute Axios request')
     expect(error.httpStatusCode).to.be.eq(400)
-
-  })
-
-  it("should use http proxy when it's available", async () => {
-
-    const { requestSpy } = mockDeps({
-      requestResponse: Promise.resolve(dummyResponse),
-      mockedExchangeSettings: {
-        proxySettings: httpProxySettings,
-      },
-    })
-
-    const publicRes = await publicRequest({
-      url: dummyUrl,
-      body: dummyBody.body as any,
-    })
-
-    expect(publicRes).to.be.eq(dummyResponse.data)
-
-    expect(requestSpy.args[0]).to.deep.eq([
-      {
-        url: dummyUrl,
-        method: AlunaHttpVerbEnum.GET,
-        proxy: {
-          host: httpProxySettings.host,
-          port: httpProxySettings.port,
-          protocol: httpProxySettings.protocol,
-        },
-        httpAgent: httpProxySettings.agent,
-        data: dummyBody.body,
-      },
-    ])
-
-    const privateRes = await privateRequest({
-      url: dummyUrl,
-      body: dummyBody.body as any,
-      keySecret: dummyKeysecret,
-    })
-
-    expect(privateRes).to.be.eq(dummyResponse.data)
-
-    expect(requestSpy.args[1]).to.deep.eq([
-      {
-        url: dummyUrl,
-        method: AlunaHttpVerbEnum.POST,
-        headers: dummySignedHeaders,
-        data: dummyBody,
-        proxy: {
-          host: httpProxySettings.host,
-          port: httpProxySettings.port,
-          protocol: httpProxySettings.protocol,
-        },
-        httpAgent: httpProxySettings.agent,
-      },
-    ])
-
-  })
-
-  it("should use https proxy when it's available", async () => {
-
-    const { requestSpy } = mockDeps({
-      requestResponse: Promise.resolve(dummyResponse),
-      mockedExchangeSettings: {
-        proxySettings: httpsProxySettings,
-      },
-    })
-
-    const publicRes = await publicRequest({
-      url: dummyUrl,
-      body: dummyBody.body as any,
-    })
-
-    expect(publicRes).to.be.eq(dummyResponse.data)
-
-    expect(requestSpy.args[0]).to.deep.eq([
-      {
-        url: dummyUrl,
-        method: AlunaHttpVerbEnum.GET,
-        proxy: {
-          host: httpsProxySettings.host,
-          port: httpsProxySettings.port,
-          protocol: httpsProxySettings.protocol,
-        },
-        httpsAgent: httpsProxySettings.agent,
-        data: dummyBody.body,
-      },
-    ])
-
-    const privateRes = await privateRequest({
-      url: dummyUrl,
-      body: dummyBody.body as any,
-      keySecret: dummyKeysecret,
-    })
-
-    expect(privateRes).to.be.eq(dummyResponse.data)
-
-    expect(requestSpy.args[1]).to.deep.eq([
-      {
-        url: dummyUrl,
-        data: dummyBody,
-        method: AlunaHttpVerbEnum.POST,
-        headers: dummySignedHeaders,
-        proxy: {
-          host: httpsProxySettings.host,
-          port: httpsProxySettings.port,
-          protocol: httpsProxySettings.protocol,
-        },
-        httpsAgent: httpsProxySettings.agent,
-      },
-    ])
 
   })
 
