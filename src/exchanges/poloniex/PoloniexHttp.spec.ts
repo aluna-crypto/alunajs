a61f9b6c7048edd74406279b12552cc1d3425393
Sergio Torres <sergio.uft@gmail.com>
2022-03-30 17:31:43 -0300

    refact 'PoloniexHttp' test suite

--- src/exchanges/poloniex/PoloniexHttp.spec.ts
+++ src/exchanges/poloniex/PoloniexHttp.spec.ts
@@ -1,8 +1,6 @@
 import { AxiosError } from 'axios'
 import { expect } from 'chai'
 import crypto from 'crypto'
-import { Agent as HttpAgent } from 'http'
-import { Agent as HttpsAgent } from 'https'
 import Sinon from 'sinon'
 import { ImportMock } from 'ts-mock-imports'
 
@@ -12,10 +10,8 @@ import { IAlunaHttpPublicParams } from '../../lib/core/IAlunaHttp'
 import { AlunaHttpVerbEnum } from '../../lib/enums/AlunaHtttpVerbEnum'
 import { AlunaGenericErrorCodes } from '../../lib/errors/AlunaGenericErrorCodes'
 import { IAlunaKeySecretSchema } from '../../lib/schemas/IAlunaKeySecretSchema'
-import {
-  IAlunaProxySchema,
-  IAlunaSettingsSchema,
-} from '../../lib/schemas/IAlunaSettingsSchema'
+import { IAlunaSettingsSchema } from '../../lib/schemas/IAlunaSettingsSchema'
+import { mockAssembleRequestConfig } from '../../utils/axios/assembleAxiosRequestConfig.mock'
 import {
   mockAlunaCache,
   validateCache,
@@ -29,37 +25,12 @@ describe('PoloniexHttp', () => {
 
   const { PoloniexHttp } = PoloniexHttpMod
 
-  const {
-    publicRequest,
-    privateRequest,
-  } = PoloniexHttp
-
   const dummyUrl = 'http://dummy.com/path/XXXDUMMY/dummy'
   const dummyBody = new URLSearchParams('dummy=dummy-body')
   const dummyResponse = { data: 'dummy-data' }
-  const dummyKeysecret: IAlunaKeySecretSchema = {
-    key: 'key',
-    secret: 'secret',
-  }
+
   const dummySignedHeaders = {
     'Content-Type': 'application/x-www-form-urlencoded',
-  }
-
-  const host = 'localhost'
-  const port = 3000
-
-  const httpProxySettings: IAlunaProxySchema = {
-    host,
-    port,
-    protocol: 'http',
-    agent: new HttpAgent({ keepAlive: true }),
-  }
-
-  const httpsProxySettings: IAlunaProxySchema = {
-    host,
-    port,
-    protocol: 'https',
-    agent: new HttpsAgent({ keepAlive: true }),
   }
 
   const mockDeps = (
@@ -89,6 +60,8 @@ describe('PoloniexHttp', () => {
       message: errorMsgRes,
       httpStatusCode: 400,
     })
+
+    const { assembleAxiosRequestMock } = mockAssembleRequestConfig()
 
     const {
       requestSpy,
@@ -132,6 +105,7 @@ describe('PoloniexHttp', () => {
       axiosCreateMock,
       formatRequestErrorSpy,
       generateAuthHeaderMock,
+      assembleAxiosRequestMock,
     }
 
   }
@@ -497,116 +471,6 @@ describe('PoloniexHttp', () => {
     expect(digestSpy.callCount).to.be.eq(2)
 
     expect(signedHash2.Sign).to.deep.eq(digestSpy.returnValues[1])
-
-  })
-
-  it("should use http proxy when it's available", async () => {
-
-    const { requestSpy } = mockDeps({
-      requestResponse: Promise.resolve(dummyResponse),
-      mockedExchangeSettings: {
-        proxySettings: httpProxySettings,
-      },
-    })
-
-    const publicRes = await publicRequest({
-      url: dummyUrl,
-      body: dummyBody as any,
-    })
-
-    expect(publicRes).to.be.eq(dummyResponse.data)
-
-    expect(requestSpy.args[0]).to.deep.eq([
-      {
-        url: dummyUrl,
-        method: AlunaHttpVerbEnum.GET,
-        proxy: {
-          host: httpProxySettings.host,
-          port: httpProxySettings.port,
-          protocol: httpProxySettings.protocol,
-        },
-        httpAgent: httpProxySettings.agent,
-        data: dummyBody,
-      },
-    ])
-
-    const privateRes = await privateRequest({
-      url: dummyUrl,
-      body: dummyBody as any,
-      keySecret: dummyKeysecret,
-    })
-
-    expect(privateRes).to.be.eq(dummyResponse.data)
-
-    expect(requestSpy.args[1]).to.deep.eq([
-      {
-        url: dummyUrl,
-        method: AlunaHttpVerbEnum.POST,
-        headers: dummySignedHeaders,
-        data: dummyBody,
-        proxy: {
-          host: httpProxySettings.host,
-          port: httpProxySettings.port,
-          protocol: httpProxySettings.protocol,
-        },
-        httpAgent: httpProxySettings.agent,
-      },
-    ])
-
-  })
-
-  it("should use https proxy when it's available", async () => {
-
-    const { requestSpy } = mockDeps({
-      requestResponse: Promise.resolve(dummyResponse),
-      mockedExchangeSettings: {
-        proxySettings: httpsProxySettings,
-      },
-    })
-
-    const publicRes = await publicRequest({
-      url: dummyUrl,
-      body: dummyBody as any,
-    })
-
-    expect(publicRes).to.be.eq(dummyResponse.data)
-
-    expect(requestSpy.args[0]).to.deep.eq([
-      {
-        url: dummyUrl,
-        method: AlunaHttpVerbEnum.GET,
-        proxy: {
-          host: httpsProxySettings.host,
-          port: httpsProxySettings.port,
-          protocol: httpsProxySettings.protocol,
-        },
-        httpsAgent: httpsProxySettings.agent,
-        data: dummyBody,
-      },
-    ])
-
-    const privateRes = await privateRequest({
-      url: dummyUrl,
-      body: dummyBody as any,
-      keySecret: dummyKeysecret,
-    })
-
-    expect(privateRes).to.be.eq(dummyResponse.data)
-
-    expect(requestSpy.args[1]).to.deep.eq([
-      {
-        url: dummyUrl,
-        method: AlunaHttpVerbEnum.POST,
-        headers: dummySignedHeaders,
-        data: dummyBody,
-        proxy: {
-          host: httpsProxySettings.host,
-          port: httpsProxySettings.port,
-          protocol: httpsProxySettings.protocol,
-        },
-        httpsAgent: httpsProxySettings.agent,
-      },
-    ])
 
   })
 
