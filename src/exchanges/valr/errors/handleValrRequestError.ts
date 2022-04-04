dc64ca25a46ca2152b044dd19c33f0cc40d7b871
Sergio Torres <sergio.uft@gmail.com>
2022-04-04 17:18:18 -0300

    add invalid key error pattern to Valr patters

--- src/exchanges/valr/errors/handleValrRequestError.ts
+++ src/exchanges/valr/errors/handleValrRequestError.ts
@@ -7,6 +7,7 @@ import { handleExchangeRequestError } from '../../../utils/errors/handleExchange
 
 export const valrInvalidApiKeyErrorPatterns: Array<RegExp | string> = [
   'API-key is invalid',
+  'API key or secret is invalid',
 ]
 
 
