dfe634db2ee909c233c832326064f331c52bac2f
Sergio Torres <sergio.uft@gmail.com>
2022-04-05 21:49:35 -0300

    refact 'handlePoloniexRequestError' test suite

--- src/exchanges/poloniex/errors/handlePoloniexRequestError.spec.ts
+++ src/exchanges/poloniex/errors/handlePoloniexRequestError.spec.ts
@@ -5,24 +5,79 @@ import {
   filter,
   random,
 } from 'lodash'
+import { ImportMock } from 'ts-mock-imports'
 
-import { AlunaError } from '../../../lib/core/AlunaError'
+import { AlunaHttpErrorCodes } from '../../../lib/errors/AlunaHttpErrorCodes'
 import { AlunaKeyErrorCodes } from '../../../lib/errors/AlunaKeyErrorCodes'
-import { EXCHANGE_INVALID_KEY_ERROR_MESSAGE } from '../../../utils/errors/handleExchangeRequestError'
-import { mockHandleExchangeRequestError } from '../../../utils/errors/handleExchangeRequestError.mock'
-import {
-  handlePoloniexRequestError,
-  poloniexDownErrorPatterns,
-  poloniexInvalidApiKeyErrorPatterns,
-} from './handlePoloniexRequestError'
+import * as handlePoloniexMod from './handlePoloniexRequestError'
 
 
 
 describe('handlePoloniexRequestError', () => {
 
-  it('should ensure request error is being handle', async () => {
+  const {
+    handlePoloniexRequestError,
+    isPoloniexKeyInvalid,
+    poloniexInvalidKeyPatterns,
+  } = handlePoloniexMod
 
-    const { handleExchangeRequestErrorMock } = mockHandleExchangeRequestError()
+  const requestErrorMsg = 'Error while executing request.'
+
+  const mockDeps = (
+    params: {
+      isInvalid: boolean,
+    } = {
+      isInvalid: false,
+    },
+  ) => {
+
+    const {
+      isInvalid,
+    } = params
+
+    const isPoloniexKeyInvalidMock = ImportMock.mockFunction(
+      handlePoloniexMod,
+      'isPoloniexKeyInvalid',
+      isInvalid,
+    )
+
+    return {
+      isPoloniexKeyInvalidMock,
+    }
+
+  }
+
+  it('should return Poloniex key invalid error when applicable', async () => {
+
+    const { isPoloniexKeyInvalidMock } = mockDeps({ isInvalid: true })
+
+    const dummyError = 'Key is invalid'
+
+    const axiosError1 = {
+      isAxiosError: true,
+      response: {
+        status: 400,
+        data: {
+          error: dummyError,
+        },
+      },
+    } as AxiosError
+
+    const alunaError = handlePoloniexRequestError({ error: axiosError1 })
+
+    expect(alunaError).to.deep.eq({
+      code: AlunaKeyErrorCodes.INVALID,
+      message: dummyError,
+      httpStatusCode: axiosError1.response!.status,
+      metadata: axiosError1.response!.data,
+    })
+
+    expect(isPoloniexKeyInvalidMock.callCount).to.be.eq(1)
+    expect(isPoloniexKeyInvalidMock.args[0][0]).to.be.eq(dummyError)
+
+  })
+
+  it('should ensure Poloniex request error is being handle', async () => {
 
     const dummyError = 'dummy-error'
 
@@ -38,15 +93,13 @@ describe('handlePoloniexRequestError', () => {
 
     let alunaError = handlePoloniexRequestError({ error: axiosError1 })
 
-    expect(handleExchangeRequestErrorMock.args[0][0]).to.deep.eq({
+    expect(alunaError).to.deep.eq({
+      code: AlunaHttpErrorCodes.REQUEST_ERROR,
       metadata: axiosError1.response!.data,
-      errorMessage: axiosError1.response!.data!.error,
+      message: axiosError1.response!.data!.error,
       httpStatusCode: axiosError1.response!.status,
-      exchangeDownErrorPatterns: poloniexDownErrorPatterns,
-      exchangeKeyInvalidErrorPatterns: poloniexInvalidApiKeyErrorPatterns,
     })
 
-    expect(alunaError instanceof AlunaError).to.be.ok
 
     const axiosError2 = {
       isAxiosError: true,
@@ -62,15 +115,12 @@ describe('handlePoloniexRequestError', () => {
 
     alunaError = handlePoloniexRequestError({ error: axiosError2 })
 
-    expect(handleExchangeRequestErrorMock.args[1][0]).to.deep.eq({
+    expect(alunaError).to.deep.eq({
+      code: AlunaHttpErrorCodes.REQUEST_ERROR,
       metadata: axiosError2.response!.data,
-      errorMessage: axiosError2!.response!.data!.result!.error,
+      message: axiosError2!.response!.data!.result!.error,
       httpStatusCode: axiosError2.response!.status,
-      exchangeDownErrorPatterns: poloniexDownErrorPatterns,
-      exchangeKeyInvalidErrorPatterns: poloniexInvalidApiKeyErrorPatterns,
     })
-
-    expect(alunaError instanceof AlunaError).to.be.ok
 
 
     const axiosError3 = {
@@ -84,15 +134,12 @@ describe('handlePoloniexRequestError', () => {
 
     alunaError = handlePoloniexRequestError({ error: axiosError3 })
 
-    expect(handleExchangeRequestErrorMock.args[2][0]).to.deep.eq({
+    expect(alunaError).to.deep.eq({
+      code: AlunaHttpErrorCodes.REQUEST_ERROR,
       metadata: axiosError3.response!.data,
-      errorMessage: undefined,
+      message: requestErrorMsg,
       httpStatusCode: axiosError3.response!.status,
-      exchangeDownErrorPatterns: poloniexDownErrorPatterns,
-      exchangeKeyInvalidErrorPatterns: poloniexInvalidApiKeyErrorPatterns,
     })
-
-    expect(alunaError instanceof AlunaError).to.be.ok
 
 
     const axiosError4 = {
@@ -101,15 +148,12 @@ describe('handlePoloniexRequestError', () => {
 
     alunaError = handlePoloniexRequestError({ error: axiosError4 })
 
-    expect(handleExchangeRequestErrorMock.args[3][0]).to.deep.eq({
+    expect(alunaError).to.deep.eq({
+      code: AlunaHttpErrorCodes.REQUEST_ERROR,
       metadata: axiosError4,
-      errorMessage: undefined,
-      httpStatusCode: undefined,
-      exchangeDownErrorPatterns: poloniexDownErrorPatterns,
-      exchangeKeyInvalidErrorPatterns: poloniexInvalidApiKeyErrorPatterns,
+      message: requestErrorMsg,
+      httpStatusCode: 500,
     })
-
-    expect(alunaError instanceof AlunaError).to.be.ok
 
 
     const error = {
@@ -118,30 +162,24 @@ describe('handlePoloniexRequestError', () => {
 
     alunaError = handlePoloniexRequestError({ error })
 
-    expect(handleExchangeRequestErrorMock.args[4][0]).to.deep.eq({
+    expect(alunaError).to.deep.eq({
+      code: AlunaHttpErrorCodes.REQUEST_ERROR,
       metadata: error,
-      errorMessage: undefined,
-      httpStatusCode: undefined,
-      exchangeDownErrorPatterns: poloniexDownErrorPatterns,
-      exchangeKeyInvalidErrorPatterns: poloniexInvalidApiKeyErrorPatterns,
+      message: dummyError,
+      httpStatusCode: 500,
     })
-
-    expect(alunaError instanceof AlunaError).to.be.ok
 
 
     const unknown = {} as any
 
     alunaError = handlePoloniexRequestError({ error: unknown })
 
-    expect(handleExchangeRequestErrorMock.args[5][0]).to.deep.eq({
+    expect(alunaError).to.deep.eq({
+      code: AlunaHttpErrorCodes.REQUEST_ERROR,
       metadata: unknown,
-      errorMessage: undefined,
-      httpStatusCode: undefined,
-      exchangeDownErrorPatterns: poloniexDownErrorPatterns,
-      exchangeKeyInvalidErrorPatterns: poloniexInvalidApiKeyErrorPatterns,
+      message: requestErrorMsg,
+      httpStatusCode: 500,
     })
-
-    expect(alunaError instanceof AlunaError).to.be.ok
 
   })
 
@@ -149,23 +187,10 @@ describe('handlePoloniexRequestError', () => {
     'should ensure Poloniex invalid api patterns work as expected',
     async () => {
 
-
-      let alunaError: AlunaError
-      const error = {
-        isAxiosError: true,
-        response: {
-          status: 401,
-          data: {
-            error: '',
-          },
-        },
-      } as AxiosError
-
-
       // Testing all string patterns
-      const message = 'Lorem Ipsum is simply dummy text of the printing'
+      let message = 'Lorem Ipsum is simply dummy text of the printing'
 
-      const strPatters = filter(poloniexInvalidApiKeyErrorPatterns, (p) => {
+      const strPatters = filter(poloniexInvalidKeyPatterns, (p) => {
 
         return !(p instanceof RegExp)
 
@@ -179,37 +204,19 @@ describe('handlePoloniexRequestError', () => {
           .concat(pattern as string)
           .concat(message.slice(insertPos))
 
-        error.response!.data!.error = msgWithInjectedPattern
+        message = msgWithInjectedPattern
 
-        alunaError = handlePoloniexRequestError({ error })
-
-        expect(alunaError.code).to.be.eq(AlunaKeyErrorCodes.INVALID)
-        expect(alunaError.message).to.be.eq(EXCHANGE_INVALID_KEY_ERROR_MESSAGE)
-        expect(alunaError.httpStatusCode).to.be.eq(error.response!.status)
-        expect(alunaError.metadata).to.be.eq(error.response!.data)
+        expect(isPoloniexKeyInvalid(message)).to.be.ok
 
       })
 
 
       // Manualy testing saved regex patterns
-      error.response!.data!.error = 'Lorem Invalid API key/secret pair Ipsum'
+      message = 'Lorem Invalid API key/secret pair Ipsum'
+      expect(isPoloniexKeyInvalid(message)).to.be.ok
 
-      alunaError = handlePoloniexRequestError({ error })
-
-      expect(alunaError.code).to.be.eq(AlunaKeyErrorCodes.INVALID)
-      expect(alunaError.message).to.be.eq(EXCHANGE_INVALID_KEY_ERROR_MESSAGE)
-      expect(alunaError.httpStatusCode).to.be.eq(error.response!.status)
-      expect(alunaError.metadata).to.be.eq(error.response!.data)
-
-
-      error.response!.data!.error = 'Lorem Ipsum is simply Permission denied'
-
-      alunaError = handlePoloniexRequestError({ error })
-
-      expect(alunaError.code).to.be.eq(AlunaKeyErrorCodes.INVALID)
-      expect(alunaError.message).to.be.eq(EXCHANGE_INVALID_KEY_ERROR_MESSAGE)
-      expect(alunaError.httpStatusCode).to.be.eq(error.response!.status)
-      expect(alunaError.metadata).to.be.eq(error.response!.data)
+      message = 'Lorem Ipsum is simply Permission denied'
+      expect(isPoloniexKeyInvalid(message)).to.be.ok
 
     },
   )
