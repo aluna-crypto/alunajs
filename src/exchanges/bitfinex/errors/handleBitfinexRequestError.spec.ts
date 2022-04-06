cffb4d8da0a332550206485b7082930d788b6b78
Sergio Torres <sergio.uft@gmail.com>
2022-04-05 21:48:02 -0300

    refact 'handleBitfinexRequestError' test suite

--- src/exchanges/bitfinex/errors/handleBitfinexRequestError.spec.ts
+++ src/exchanges/bitfinex/errors/handleBitfinexRequestError.spec.ts
@@ -5,51 +5,37 @@ import {
   filter,
   random,
 } from 'lodash'
+import { ImportMock } from 'ts-mock-imports'
 
 import { AlunaError } from '../../../lib/core/AlunaError'
+import { AlunaHttpErrorCodes } from '../../../lib/errors/AlunaHttpErrorCodes'
 import { AlunaKeyErrorCodes } from '../../../lib/errors/AlunaKeyErrorCodes'
-import { EXCHANGE_INVALID_KEY_ERROR_MESSAGE } from '../../../utils/errors/handleExchangeRequestError'
-import { mockHandleExchangeRequestError } from '../../../utils/errors/handleExchangeRequestError.mock'
-import {
-  bitfinexDownErrorPatterns,
-  bitfinexInvalidApiKeyErrorPatterns,
-  handleBitfinexRequestError,
-} from './handleBitfinexRequestError'
+import * as handleBitfinexMod from './handleBitfinexRequestError'
 
 
 
 describe('handleBitfinexRequestError', () => {
 
-  it('should properly handle Bitfinex request error', () => {
 
-    const { handleExchangeRequestErrorMock } = mockHandleExchangeRequestError()
+  const {
+    bitfinexInvalidKeyPatterns,
+    isBitfinexKeyInvalid,
+    handleBitfinexRequestError,
+  } = handleBitfinexMod
+
+  const requestErrorMsg = 'Error while executing request.'
+
+  it('should return Bitfinex key invalid error when applicable', async () => {
+
+    const isBitfinexKeyInvalidMock = ImportMock.mockFunction(
+      handleBitfinexMod,
+      'isBitfinexKeyInvalid',
+      true,
+    )
 
     const dummyError = 'dummy-error'
 
-    const axiosError1 = {
-      isAxiosError: true,
-      response: {
-        request: {
-          path: 'v1/getPositions',
-        },
-        data: { message: dummyError },
-      },
-    } as AxiosError
-
-    handleBitfinexRequestError({ error: axiosError1 })
-
-    expect(handleExchangeRequestErrorMock.callCount).to.be.eq(1)
-
-    expect(handleExchangeRequestErrorMock.args[0][0]).to.deep.eq({
-      metadata: axiosError1.response!.data,
-      errorMessage: axiosError1.response!.data!.message,
-      httpStatusCode: undefined,
-      exchangeDownErrorPatterns: bitfinexDownErrorPatterns,
-      exchangeKeyInvalidErrorPatterns: bitfinexInvalidApiKeyErrorPatterns,
-    })
-
-
-    const axiosError2 = {
+    const axiosError = {
       isAxiosError: true,
       response: {
         status: 401,
@@ -57,20 +43,49 @@ describe('handleBitfinexRequestError', () => {
       },
     } as AxiosError
 
-    handleBitfinexRequestError({ error: axiosError2 })
 
-    expect(handleExchangeRequestErrorMock.callCount).to.be.eq(2)
+    const alunaError = handleBitfinexRequestError({ error: axiosError })
 
-    expect(handleExchangeRequestErrorMock.args[1][0]).to.deep.eq({
-      metadata: axiosError2.response!.data,
-      errorMessage: axiosError2.response!.data![2],
-      httpStatusCode: axiosError2.response!.status,
-      exchangeDownErrorPatterns: bitfinexDownErrorPatterns,
-      exchangeKeyInvalidErrorPatterns: bitfinexInvalidApiKeyErrorPatterns,
-    })
+    expect(alunaError).to.deep.eq(new AlunaError({
+      code: AlunaKeyErrorCodes.INVALID,
+      message: dummyError,
+      httpStatusCode: axiosError.response!.status,
+      metadata: axiosError.response!.data,
+    }))
+
+    expect(isBitfinexKeyInvalidMock.callCount).to.be.eq(1)
+
+  })
+
+  it('should properly handle Bitfinex request errors', () => {
+
+    ImportMock.mockFunction(
+      handleBitfinexMod,
+      'isBitfinexKeyInvalid',
+      false,
+    )
+
+    const dummyError = 'dummy-error'
+
+    const axiosError1 = {
+      isAxiosError: true,
+      response: {
+        status: 401,
+        data: ['error', 10010, dummyError],
+      },
+    } as AxiosError
+
+    let alunaError = handleBitfinexRequestError({ error: axiosError1 })
+
+    expect(alunaError).to.deep.eq(new AlunaError({
+      code: AlunaHttpErrorCodes.REQUEST_ERROR,
+      message: axiosError1.response!.data![2],
+      httpStatusCode: axiosError1.response!.status,
+      metadata: axiosError1.response!.data,
+    }))
 
 
-    const axiosError3 = {
+    const axiosError2 = {
       isAxiosError: true,
       response: {
         status: 500,
@@ -78,65 +93,57 @@ describe('handleBitfinexRequestError', () => {
       },
     } as AxiosError
 
-    handleBitfinexRequestError({ error: axiosError3 })
-
-    expect(handleExchangeRequestErrorMock.callCount).to.be.eq(3)
-
-    expect(handleExchangeRequestErrorMock.args[2][0]).to.deep.eq({
-      metadata: axiosError3.response!.data,
-      errorMessage: undefined,
-      httpStatusCode: axiosError3.response!.status,
-      exchangeDownErrorPatterns: bitfinexDownErrorPatterns,
-      exchangeKeyInvalidErrorPatterns: bitfinexInvalidApiKeyErrorPatterns,
-    })
+    alunaError = handleBitfinexRequestError({ error: axiosError2 })
 
 
-    const axiosError4 = {
+    expect(alunaError).to.deep.eq(new AlunaError({
+      code: AlunaHttpErrorCodes.REQUEST_ERROR,
+      metadata: axiosError2.response!.data,
+      message: requestErrorMsg,
+      httpStatusCode: axiosError2.response!.status,
+    }))
+
+
+    const axiosError3 = {
       isAxiosError: true,
     } as AxiosError
 
-    handleBitfinexRequestError({ error: axiosError4 })
+    alunaError = handleBitfinexRequestError({ error: axiosError3 })
 
-    expect(handleExchangeRequestErrorMock.callCount).to.be.eq(4)
 
-    expect(handleExchangeRequestErrorMock.args[3][0]).to.deep.eq({
-      metadata: axiosError4,
-      errorMessage: undefined,
-      httpStatusCode: undefined,
-      exchangeDownErrorPatterns: bitfinexDownErrorPatterns,
-      exchangeKeyInvalidErrorPatterns: bitfinexInvalidApiKeyErrorPatterns,
-    })
+    expect(alunaError).to.deep.eq(new AlunaError({
+      code: AlunaHttpErrorCodes.REQUEST_ERROR,
+      metadata: axiosError3,
+      message: requestErrorMsg,
+      httpStatusCode: 500,
+    }))
 
 
     const error = {
       message: dummyError,
     } as Error
 
-    handleBitfinexRequestError({ error })
+    alunaError = handleBitfinexRequestError({ error })
 
-    expect(handleExchangeRequestErrorMock.callCount).to.be.eq(5)
 
-    expect(handleExchangeRequestErrorMock.args[4][0]).to.deep.eq({
+    expect(alunaError).to.deep.eq(new AlunaError({
+      code: AlunaHttpErrorCodes.REQUEST_ERROR,
       metadata: error,
-      errorMessage: undefined,
-      httpStatusCode: undefined,
-      exchangeDownErrorPatterns: bitfinexDownErrorPatterns,
-      exchangeKeyInvalidErrorPatterns: bitfinexInvalidApiKeyErrorPatterns,
-    })
+      message: dummyError,
+      httpStatusCode: 500,
+    }))
 
     const unknown = {} as any
 
-    handleBitfinexRequestError({ error: unknown })
+    alunaError = handleBitfinexRequestError({ error: unknown })
 
-    expect(handleExchangeRequestErrorMock.callCount).to.be.eq(6)
 
-    expect(handleExchangeRequestErrorMock.args[5][0]).to.deep.eq({
+    expect(alunaError).to.deep.eq(new AlunaError({
+      code: AlunaHttpErrorCodes.REQUEST_ERROR,
       metadata: unknown,
-      errorMessage: undefined,
-      httpStatusCode: undefined,
-      exchangeDownErrorPatterns: bitfinexDownErrorPatterns,
-      exchangeKeyInvalidErrorPatterns: bitfinexInvalidApiKeyErrorPatterns,
-    })
+      message: requestErrorMsg,
+      httpStatusCode: 500,
+    }))
 
   })
 
@@ -144,20 +151,10 @@ describe('handleBitfinexRequestError', () => {
     'should ensure Bitfinex invalid api patterns work as expected',
     async () => {
 
-      let alunaError: AlunaError
-      const error = {
-        isAxiosError: true,
-        response: {
-          status: 401,
-          data: [-12, 'error', 'reason'],
-        },
-      } as AxiosError
-
-
       // Testing all string patterns
       const message = 'Lorem Ipsum is simply dummy text of the printing'
 
-      const strPatters = filter(bitfinexInvalidApiKeyErrorPatterns, (p) => {
+      const strPatters = filter(bitfinexInvalidKeyPatterns, (p) => {
 
         return !(p instanceof RegExp)
 
@@ -171,57 +168,23 @@ describe('handleBitfinexRequestError', () => {
           .concat(pattern as string)
           .concat(message.slice(insertPos))
 
-        error.response!.data![2] = msgWithInjectedPattern
-
-        alunaError = handleBitfinexRequestError({ error })
-
-        expect(alunaError.code).to.be.eq(AlunaKeyErrorCodes.INVALID)
-        expect(alunaError.message).to.be.eq(EXCHANGE_INVALID_KEY_ERROR_MESSAGE)
-        expect(alunaError.httpStatusCode).to.be.eq(error.response!.status)
-        expect(alunaError.metadata).to.be.eq(error.response!.data)
+        expect(isBitfinexKeyInvalid(msgWithInjectedPattern)).to.be.ok
 
       })
 
 
       // Manualy testing saved regex patterns
-      error.response!.data![2] = 'Lorem Ipsum is simply Invalid X-BFX-SIGNATURE'
+      let pattern = 'Lorem Ipsum is simply Invalid X-BFX-SIGNATURE'
+      expect(isBitfinexKeyInvalid(pattern)).to.be.ok
 
-      alunaError = handleBitfinexRequestError({ error })
+      pattern = 'Lorem Ipsum is simply X-BFX-APIKEY'
+      expect(isBitfinexKeyInvalid(pattern)).to.be.ok
 
-      expect(alunaError.code).to.be.eq(AlunaKeyErrorCodes.INVALID)
-      expect(alunaError.message).to.be.eq(EXCHANGE_INVALID_KEY_ERROR_MESSAGE)
-      expect(alunaError.httpStatusCode).to.be.eq(error.response!.status)
-      expect(alunaError.metadata).to.be.eq(error.response!.data)
+      pattern = 'Lorem Ipsum is simply apikey: invalid'
+      expect(isBitfinexKeyInvalid(pattern)).to.be.ok
 
-
-      error.response!.data![2] = 'Lorem Ipsum is simply X-BFX-APIKEY'
-
-      alunaError = handleBitfinexRequestError({ error })
-
-      expect(alunaError.code).to.be.eq(AlunaKeyErrorCodes.INVALID)
-      expect(alunaError.message).to.be.eq(EXCHANGE_INVALID_KEY_ERROR_MESSAGE)
-      expect(alunaError.httpStatusCode).to.be.eq(error.response!.status)
-      expect(alunaError.metadata).to.be.eq(error.response!.data)
-
-
-      error.response!.data![2] = 'Lorem Ipsum is simply apikey: invalid'
-
-      alunaError = handleBitfinexRequestError({ error })
-
-      expect(alunaError.code).to.be.eq(AlunaKeyErrorCodes.INVALID)
-      expect(alunaError.message).to.be.eq(EXCHANGE_INVALID_KEY_ERROR_MESSAGE)
-      expect(alunaError.httpStatusCode).to.be.eq(error.response!.status)
-      expect(alunaError.metadata).to.be.eq(error.response!.data)
-
-
-      error.response!.data![2] = 'Lorem Ipsum is simply apikey: digest invalid'
-
-      alunaError = handleBitfinexRequestError({ error })
-
-      expect(alunaError.code).to.be.eq(AlunaKeyErrorCodes.INVALID)
-      expect(alunaError.message).to.be.eq(EXCHANGE_INVALID_KEY_ERROR_MESSAGE)
-      expect(alunaError.httpStatusCode).to.be.eq(error.response!.status)
-      expect(alunaError.metadata).to.be.eq(error.response!.data)
+      pattern = 'Lorem Ipsum is simply apikey: digest invalid'
+      expect(isBitfinexKeyInvalid(pattern)).to.be.ok
 
     },
   )
