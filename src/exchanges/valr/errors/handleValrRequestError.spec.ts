6f8b46c15f774edbcc83dd1452186ca5813eff95
Sergio Torres <sergio.uft@gmail.com>
2022-04-05 21:49:55 -0300

    refact 'handleValrRequestError' test suite

--- src/exchanges/valr/errors/handleValrRequestError.spec.ts
+++ src/exchanges/valr/errors/handleValrRequestError.spec.ts
@@ -2,27 +2,81 @@ import { AxiosError } from 'axios'
 import { expect } from 'chai'
 import {
   each,
-  filter,
   random,
 } from 'lodash'
+import { ImportMock } from 'ts-mock-imports'
 
-import { AlunaError } from '../../../lib/core/AlunaError'
+import { AlunaHttpErrorCodes } from '../../../lib/errors/AlunaHttpErrorCodes'
 import { AlunaKeyErrorCodes } from '../../../lib/errors/AlunaKeyErrorCodes'
-import { EXCHANGE_INVALID_KEY_ERROR_MESSAGE } from '../../../utils/errors/handleExchangeRequestError'
-import { mockHandleExchangeRequestError } from '../../../utils/errors/handleExchangeRequestError.mock'
-import {
-  handleValrRequestError,
-  valrDownErrorPatterns,
-  valrInvalidApiKeyErrorPatterns,
-} from './handleValrRequestError'
+import * as handleValrMod from './handleValrRequestError'
 
 
 
 describe('handleValrRequestError', () => {
 
-  it('should ensure request error is being handle', async () => {
+  const {
+    handleValrRequestError,
+    isValrKeyInvalid,
+    valrInvalidKeyPatterns,
+  } = handleValrMod
 
-    const { handleExchangeRequestErrorMock } = mockHandleExchangeRequestError()
+  const requestErrorMsg = 'Error while executing request.'
+
+  const mockDeps = (
+    params: {
+      isInvalid: boolean,
+    } = {
+      isInvalid: false,
+    },
+  ) => {
+
+    const {
+      isInvalid,
+    } = params
+
+    const isValrKeyInvalidMock = ImportMock.mockFunction(
+      handleValrMod,
+      'isValrKeyInvalid',
+      isInvalid,
+    )
+
+    return {
+      isValrKeyInvalidMock,
+    }
+
+  }
+
+  it('should return Valr key invalid error when applicable', async () => {
+
+    const { isValrKeyInvalidMock } = mockDeps({ isInvalid: true })
+
+    const dummyError = 'Key is invalid'
+
+    const axiosError = {
+      isAxiosError: true,
+      response: {
+        status: 400,
+        data: {
+          message: dummyError,
+        },
+      },
+    } as AxiosError
+
+    const alunaError = handleValrRequestError({ error: axiosError })
+
+    expect(alunaError).to.deep.eq({
+      code: AlunaKeyErrorCodes.INVALID,
+      message: dummyError,
+      httpStatusCode: axiosError.response!.status,
+      metadata: axiosError.response!.data,
+    })
+
+    expect(isValrKeyInvalidMock.callCount).to.be.eq(1)
+    expect(isValrKeyInvalidMock.args[0][0]).to.be.eq(dummyError)
+
+  })
+
+  it('should ensure Valr request error is being handle', async () => {
 
     const dummyError = 'dummy-error'
 
@@ -38,15 +92,13 @@ describe('handleValrRequestError', () => {
 
     let alunaError = handleValrRequestError({ error: axiosError1 })
 
-    expect(handleExchangeRequestErrorMock.args[0][0]).to.deep.eq({
+    expect(alunaError).to.deep.eq({
+      code: AlunaHttpErrorCodes.REQUEST_ERROR,
       metadata: axiosError1.response!.data,
-      errorMessage: axiosError1.response!.data!.message,
+      message: axiosError1.response!.data!.message,
       httpStatusCode: axiosError1.response!.status,
-      exchangeDownErrorPatterns: valrDownErrorPatterns,
-      exchangeKeyInvalidErrorPatterns: valrInvalidApiKeyErrorPatterns,
     })
 
-    expect(alunaError instanceof AlunaError).to.be.ok
 
     const axiosError2 = {
       isAxiosError: true,
@@ -59,15 +111,12 @@ describe('handleValrRequestError', () => {
 
     alunaError = handleValrRequestError({ error: axiosError2 })
 
-    expect(handleExchangeRequestErrorMock.args[1][0]).to.deep.eq({
+    expect(alunaError).to.deep.eq({
+      code: AlunaHttpErrorCodes.REQUEST_ERROR,
       metadata: axiosError2.response!.data,
-      errorMessage: undefined,
+      message: requestErrorMsg,
       httpStatusCode: axiosError2.response!.status,
-      exchangeDownErrorPatterns: valrDownErrorPatterns,
-      exchangeKeyInvalidErrorPatterns: valrInvalidApiKeyErrorPatterns,
     })
-
-    expect(alunaError instanceof AlunaError).to.be.ok
 
 
     const axiosError3 = {
@@ -76,15 +125,12 @@ describe('handleValrRequestError', () => {
 
     alunaError = handleValrRequestError({ error: axiosError3 })
 
-    expect(handleExchangeRequestErrorMock.args[2][0]).to.deep.eq({
+    expect(alunaError).to.deep.eq({
+      code: AlunaHttpErrorCodes.REQUEST_ERROR,
       metadata: axiosError3,
-      errorMessage: undefined,
-      httpStatusCode: undefined,
-      exchangeDownErrorPatterns: valrDownErrorPatterns,
-      exchangeKeyInvalidErrorPatterns: valrInvalidApiKeyErrorPatterns,
+      message: requestErrorMsg,
+      httpStatusCode: 500,
     })
-
-    expect(alunaError instanceof AlunaError).to.be.ok
 
 
     const error = {
@@ -93,30 +139,24 @@ describe('handleValrRequestError', () => {
 
     alunaError = handleValrRequestError({ error })
 
-    expect(handleExchangeRequestErrorMock.args[3][0]).to.deep.eq({
+    expect(alunaError).to.deep.eq({
+      code: AlunaHttpErrorCodes.REQUEST_ERROR,
       metadata: error,
-      errorMessage: undefined,
-      httpStatusCode: undefined,
-      exchangeDownErrorPatterns: valrDownErrorPatterns,
-      exchangeKeyInvalidErrorPatterns: valrInvalidApiKeyErrorPatterns,
+      message: dummyError,
+      httpStatusCode: 500,
     })
-
-    expect(alunaError instanceof AlunaError).to.be.ok
 
 
     const unknown = {} as any
 
     alunaError = handleValrRequestError({ error: unknown })
 
-    expect(handleExchangeRequestErrorMock.args[4][0]).to.deep.eq({
+    expect(alunaError).to.deep.eq({
+      code: AlunaHttpErrorCodes.REQUEST_ERROR,
       metadata: unknown,
-      errorMessage: undefined,
-      httpStatusCode: undefined,
-      exchangeDownErrorPatterns: valrDownErrorPatterns,
-      exchangeKeyInvalidErrorPatterns: valrInvalidApiKeyErrorPatterns,
+      message: requestErrorMsg,
+      httpStatusCode: 500,
     })
-
-    expect(alunaError instanceof AlunaError).to.be.ok
 
   })
 
@@ -124,29 +164,10 @@ describe('handleValrRequestError', () => {
     'should ensure Valr invalid api patterns work as expected',
     async () => {
 
-      let alunaError: AlunaError
-
-      const error = {
-        isAxiosError: true,
-        response: {
-          status: 400,
-          data: {
-            message: '',
-          },
-        },
-      } as AxiosError
-
-
       // Testing all string patterns
       const message = 'Lorem Ipsum is simply dummy text of the printing'
 
-      const strPatters = filter(valrInvalidApiKeyErrorPatterns, (p) => {
-
-        return !(p instanceof RegExp)
-
-      })
-
-      each(strPatters, (pattern) => {
+      each(valrInvalidKeyPatterns, (pattern) => {
 
         const insertPos = random(0, message.length - 1)
 
@@ -154,14 +175,7 @@ describe('handleValrRequestError', () => {
           .concat(pattern as string)
           .concat(message.slice(insertPos))
 
-        error.response!.data!.message = msgWithInjectedPattern
-
-        alunaError = handleValrRequestError({ error })
-
-        expect(alunaError.code).to.be.eq(AlunaKeyErrorCodes.INVALID)
-        expect(alunaError.message).to.be.eq(EXCHANGE_INVALID_KEY_ERROR_MESSAGE)
-        expect(alunaError.httpStatusCode).to.be.eq(error.response!.status)
-        expect(alunaError.metadata).to.be.eq(error.response!.data)
+        expect(isValrKeyInvalid(msgWithInjectedPattern)).to.be.ok
 
       })
 
