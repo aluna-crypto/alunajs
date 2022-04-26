077810be75e65ad80b8b7c8e07934e034c3538ba
Anderson Arboleya <anderson@arboleya.me>
2022-04-26 16:26:30 -0300

    Improving example usage for Bittrex `market/get`.

--- src/exchanges/bittrex/modules/public/market/get.ts
+++ src/exchanges/bittrex/modules/public/market/get.ts
@@ -1,13 +1,29 @@
+import { debug } from 'debug'
+
 import {
   IAlunaMarketGetParams,
   IAlunaMarketGetReturns,
 } from '../../../../../lib/modules/IAlunaMarketModule'
+import { BittrexHttp } from '../../../BittrexHttp'
+
+
+
+const log = debug('@aluna.js:bittrex/market/get')
 
 
 
 export async function get (
   params: IAlunaMarketGetParams,
 ): Promise<IAlunaMarketGetReturns> {
+
+  const {
+    id,
+    http = new BittrexHttp(),
+  } = params
+
+  const { requestCount } = http
+
+  log({ id, requestCount })
 
   return params as any
 
