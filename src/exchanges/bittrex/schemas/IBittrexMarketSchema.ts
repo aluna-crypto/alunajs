b0a68a4606cf5b2e70134cd85a7f09d2f520cfc7
Torres-ssf <sergio.uft@gmail.com>
2022-04-26 20:33:27 -0300

    add 'IBittrexMarketSummarySchema'

---
+++ src/exchanges/bittrex/schemas/IBittrexMarketSchema.ts
@@ -0,0 +1,26 @@
+export interface IBittrexMarketSummarySchema {
+  symbol: string
+  high: string
+  low: string
+  volume: string
+  quoteVolume: string
+  percentChange: string
+  updatedAt: string
+}
+
+export interface IBittrexMarketTickerSchema {
+  symbol: string
+  lastTradeRate: string
+  bidRate: string
+  askRate: string
+}
+
+export interface IBittrexMarketWithTicker extends IBittrexMarketTickerSchema {
+  baseCurrencySymbol: string
+  quoteCurrencySymbol: string
+  high: string
+  low: string
+  volume: string
+  quoteVolume: string
+  percentChange: string
+}
