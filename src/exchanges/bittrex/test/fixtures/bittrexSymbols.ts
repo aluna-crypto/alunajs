0b65361635491f033d828063f4c838058d7679e3
Torres-ssf <sergio.uft@gmail.com>
2022-04-27 16:14:40 -0300

    add fixtures for Bittrex symbols

---
+++ src/exchanges/bittrex/test/fixtures/bittrexSymbols.ts
@@ -0,0 +1,70 @@
+import { IAlunaSymbolSchema } from '../../../../lib/schemas/IAlunaSymbolSchema'
+import { IBittrexSymbolSchema } from '../../schemas/IBittrexSymbolSchema'
+
+
+
+export const BITTREX_RAW_SYMBOLS: IBittrexSymbolSchema[] = [
+  {
+    symbol: 'BTC',
+    name: 'Bitcoin',
+    coinType: 'BITCOIN',
+    status: 'ONLINE',
+    minConfirmations: 2,
+    notice: '',
+    txFee: '0.00030000',
+    logoUrl: '',
+    prohibitedIn: [],
+    baseAddress: '1N52wHoVR79PMDishab2XmRHsbekCdGquK',
+    associatedTermsOfService: [],
+    tags: [],
+  },
+  {
+    symbol: 'LTC',
+    name: 'Litecoin',
+    coinType: 'BITCOIN16',
+    status: 'ONLINE',
+    minConfirmations: 6,
+    notice: '',
+    txFee: '0.01000000',
+    logoUrl: '',
+    prohibitedIn: [],
+    baseAddress: 'LhyLNfBkoKshT7R8Pce6vkB9T2cP2o84hx',
+    associatedTermsOfService: [],
+    tags: [],
+  },
+  {
+    symbol: 'ETH',
+    name: 'Ethereum',
+    coinType: 'ETH',
+    status: 'ONLINE',
+    minConfirmations: 36,
+    notice: '',
+    txFee: '0.00460000',
+    logoUrl: '',
+    prohibitedIn: [],
+    baseAddress: '0xfbb1b73c4f0bda4f67dca266ce6ef42f520fbb98',
+    associatedTermsOfService: [],
+    tags: [],
+  },
+]
+
+export const BITTREX_PARSED_SYMBOLS: IAlunaSymbolSchema[] = [
+  {
+    id: 'BTC',
+    name: 'Bitcoin',
+    exchangeId: 'bittrex',
+    meta: {},
+  },
+  {
+    id: 'LTC',
+    name: 'Litecoin',
+    exchangeId: 'bittrex',
+    meta: {},
+  },
+  {
+    id: 'ETH',
+    name: 'Ethereum',
+    exchangeId: 'bittrex',
+    meta: {},
+  },
+]
