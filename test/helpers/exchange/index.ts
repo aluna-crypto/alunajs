f60bdf998497bba2a42aa2f40d5542b8bbe80fb2
Anderson Arboleya <anderson@arboleya.me>
2022-04-27 08:08:41 -0300

    Temporarily commenting test utility.

--- test/helpers/exchange/index.ts
+++ test/helpers/exchange/index.ts
@@ -1,44 +1,47 @@
-import {
-  ImportMock,
-  OtherManager,
-} from 'ts-mock-imports'
+/* eslint-disable max-len */
+// TODO: Review utility
 
-import { IAlunaExchange } from '../../../src/lib/core/IAlunaExchange'
-import { IAlunaModule } from '../../../src/lib/core/IAlunaModule'
-import { IAlunaKeySecretSchema } from '../../../src/lib/schemas/IAlunaKeySecretSchema'
+// import {
+//   ImportMock,
+//   OtherManager,
+// } from 'ts-mock-imports'
 
-
-
-export interface IMockExchangeModuleResponse {
-  exchangeMock: OtherManager<IAlunaExchange>
-}
+// import { IAlunaExchange } from '../../../src/lib/core/IAlunaExchange'
+// import { IAlunaModule } from '../../../src/lib/core/IAlunaModule'
+// import { IAlunaKeySecretSchema } from '../../../src/lib/schemas/IAlunaKeySecretSchema'
 
 
 
-export const mockExchangeModule = (params: {
-  module: IAlunaModule,
-  overrides?: Partial<IAlunaExchange>,
-}): IMockExchangeModuleResponse => {
+// export interface IMockExchangeModuleResponse {
+//   exchangeMock: OtherManager<IAlunaExchange>
+// }
 
-  const {
-    module,
-    overrides = {},
-  } = params
 
-  const keySecret: IAlunaKeySecretSchema = {
-    key: '',
-    secret: '',
-  }
 
-  const exchangeMock = ImportMock.mockOther(
-    module,
-    'exchange',
-    {
-      keySecret,
-      ...overrides,
-    },
-  )
+// export const mockExchangeModule = (params: {
+//   module: IAlunaModule,
+//   overrides?: Partial<IAlunaExchange>,
+// }): IMockExchangeModuleResponse => {
 
-  return { exchangeMock }
+//   const {
+//     module,
+//     overrides = {},
+//   } = params
 
-}
+//   const keySecret: IAlunaKeySecretSchema = {
+//     key: '',
+//     secret: '',
+//   }
+
+//   const exchangeMock = ImportMock.mockOther(
+//     module,
+//     'exchange',
+//     {
+//       keySecret,
+//       ...overrides,
+//     },
+//   )
+
+//   return { exchangeMock }
+
+// }
