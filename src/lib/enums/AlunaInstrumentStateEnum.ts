37cae8ee1dce8dacc4b4de83e9279d571823a141
Anderson Arboleya <anderson@arboleya.me>
2022-04-27 08:08:07 -0300

    Purging jest related stuff.

--- src/lib/enums/AlunaInstrumentStateEnum.ts
+++ src/lib/enums/AlunaInstrumentStateEnum.ts
@@ -1,4 +1,3 @@
-// TODO: Review coverage for file and adjust configs in jest.config.js
 export enum AlunaInstrumentStateEnum {
   OPEN = 'open',
   CLOSED = 'closed',
--- src/lib/enums/AlunaPositionStatusEnum.ts
+++ src/lib/enums/AlunaPositionStatusEnum.ts
@@ -1,4 +1,3 @@
-// TODO: Review coverage for file and adjust configs in jest.config.js
 export enum AlunaPositionStatusEnum {
   OPEN = 'open',
   CLOSED = 'closed',
