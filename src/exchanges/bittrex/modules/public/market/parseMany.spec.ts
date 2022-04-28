b4865b64afd129f12c1dde36978bbc88393be228
Sergio <sergio.uft@gmail.com>
2022-04-28 13:14:27 -0300

    fix test suite for Bittrex market 'parseMany'

--- src/exchanges/bittrex/modules/public/market/parseMany.spec.ts
+++ src/exchanges/bittrex/modules/public/market/parseMany.spec.ts
@@ -1,13 +1,14 @@
 import { expect } from 'chai'
 
+import { mockMarketParse } from '../../../../../../test/helpers/exchange/modules/market/parse'
+import { Bittrex } from '../../../Bittrex'
 import {
   BITTREX_PARSED_MARKETS,
   BITTREX_RAW_MARKET_SUMMARIES,
   BITTREX_RAW_MARKET_TICKERS,
   BITTREX_RAW_MARKETS_INFO,
 } from '../../../test/fixtures/bittrexMarket'
-import { mockBittrexParse } from './parse.mock'
-import { parseMany } from './parseMany'
+import * as parseMod from './parse'
 
 
 
@@ -15,10 +16,15 @@ describe(__filename, () => {
 
   it('should parse many Bittrex raw markets just fine', async () => {
 
-    const { parse } = mockBittrexParse()
-
     const parsed1 = BITTREX_PARSED_MARKETS[0]
     const parsed2 = BITTREX_PARSED_MARKETS[1]
+
+    const { parse } = mockMarketParse({
+      module: parseMod,
+      returns: {
+        market: parsed1,
+      },
+    })
 
     parse.onCall(0).returns({ market: parsed1 })
     parse.onCall(1).returns({ market: parsed2 })
@@ -27,7 +33,9 @@ describe(__filename, () => {
     const summaries = BITTREX_RAW_MARKET_SUMMARIES
     const tickers = BITTREX_RAW_MARKET_TICKERS
 
-    const { markets } = parseMany({
+    const exchange = new Bittrex({ settings: {} })
+
+    const { markets } = exchange.market.parseMany({
       rawMarkets: {
         marketsInfo,
         summaries,
