27d2d4776d72ea9e2a8ca0c6a5b0b92563946924
Sergio <sergio.uft@gmail.com>
2022-04-28 13:13:56 -0300

    fix test suite for Bittrex market 'list'

--- src/exchanges/bittrex/modules/public/market/list.spec.ts
+++ src/exchanges/bittrex/modules/public/market/list.spec.ts
@@ -1,10 +1,16 @@
 import { expect } from 'chai'
 
+import { mockMarketListRaw } from '../../../../../../test/helpers/exchange/modules/market/listRaw'
+import { mockMarketParseMany } from '../../../../../../test/helpers/exchange/modules/market/parseMany'
+import { Bittrex } from '../../../Bittrex'
 import { mockBittrexHttp } from '../../../BittrexHttp.mock'
-import { BITTREX_PARSED_MARKETS } from '../../../test/fixtures/bittrexMarket'
-import { list } from './list'
-import { mockBittrexListRaw } from './listRaw.mock'
-import { mockBittrexParseMany } from './parseMany.mock'
+import { IBittrexMarketsSchema } from '../../../schemas/IBittrexMarketSchema'
+import {
+  BITTREX_PARSED_MARKETS,
+  BITTREX_RAW_MARKETS,
+} from '../../../test/fixtures/bittrexMarket'
+import * as listRawMod from './listRaw'
+import * as parseManyMod from './parseMany'
 
 
 
@@ -12,16 +18,35 @@ describe(__filename, () => {
 
   it('should list Bittrex parsed markets just fine', async () => {
 
+
+    const { listRaw } = mockMarketListRaw<IBittrexMarketsSchema>({
+      module: listRawMod,
+      returns: {
+        rawMarkets: BITTREX_RAW_MARKETS,
+        requestCount: {
+          authed: 0,
+          public: 0,
+        },
+      },
+    })
+
+    const { parseMany } = mockMarketParseMany({
+      module: parseManyMod,
+      returns: {
+        markets: BITTREX_PARSED_MARKETS,
+      },
+    })
+
+    const exchange = new Bittrex({ settings: {} })
+
     const {
       authedRequest,
       publicRequest,
       requestCount,
     } = mockBittrexHttp()
 
-    const { listRaw } = mockBittrexListRaw()
-    const { parseMany } = mockBittrexParseMany()
 
-    const { markets } = await list()
+    const { markets } = await exchange.market.list()
 
     expect(markets).to.deep.eq(BITTREX_PARSED_MARKETS)
 
