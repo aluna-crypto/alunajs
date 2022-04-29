cfaeb48295a0e3370b94cd18e2849164fa279158
Sergio <sergio.uft@gmail.com>
2022-04-29 14:30:08 -0300

    implement class 'BittrexHttp'

--- src/exchanges/bittrex/BittrexHttp.ts
+++ src/exchanges/bittrex/BittrexHttp.ts
@@ -1,9 +1,81 @@
+import axios from 'axios'
+import crypto from 'crypto'
+
 import {
   IAlunaHttp,
   IAlunaHttpAuthedParams,
   IAlunaHttpPublicParams,
   IAlunaHttpRequestCount,
 } from '../../lib/core/IAlunaHttp'
+import { AlunaHttpVerbEnum } from '../../lib/enums/AlunaHtttpVerbEnum'
+import { IAlunaCredentialsSchema } from '../../lib/schemas/IAlunaCredentialsSchema'
+import { assembleRequestConfig } from '../../utils/axios/assembleRequestConfig'
+import { AlunaCache } from '../../utils/cache/AlunaCache'
+import { handleBittrexRequestError } from './errors/handleBittrexRequestError'
+
+
+
+export const BITTREX_HTTP_CACHE_KEY_PREFIX = 'BittrexHttp.publicRequest'
+
+
+
+interface ISignedHashParams {
+  verb: AlunaHttpVerbEnum
+  path: string
+  credentials: IAlunaCredentialsSchema
+  url: string
+  body?: any
+}
+
+
+
+export interface IBittrexSignedHeaders {
+    'Api-Key': string
+    'Api-Timestamp': number
+    'Api-Content-Hash': string
+    'Api-Signature': string
+}
+
+
+
+export const generateAuthHeader = (
+  params: ISignedHashParams,
+): IBittrexSignedHeaders => {
+
+  const {
+    credentials,
+    verb,
+    body,
+    url,
+  } = params
+
+  const timestamp = new Date().getTime()
+
+  const {
+    key,
+    secret,
+  } = credentials
+
+  const contentHash = crypto
+    .createHash('sha512')
+    .update(body ? JSON.stringify(body) : '')
+    .digest('hex')
+
+  const preSigned = [timestamp, url, verb.toUpperCase(), contentHash].join('')
+
+  const signedHeader = crypto
+    .createHmac('sha512', secret)
+    .update(preSigned)
+    .digest('hex')
+
+  return {
+    'Api-Content-Hash': contentHash,
+    'Api-Key': key,
+    'Api-Signature': signedHeader,
+    'Api-Timestamp': timestamp,
+  }
+
+}
 
 
 
@@ -29,16 +101,46 @@ export class BittrexHttp implements IAlunaHttp {
   ): Promise<T> {
 
     const {
+      url,
+      body,
+      verb = AlunaHttpVerbEnum.GET,
       weight = 1,
+      settings,
     } = params
+
+    const cacheKey = AlunaCache.hashCacheKey({
+      args: params,
+      prefix: BITTREX_HTTP_CACHE_KEY_PREFIX,
+    })
+
+    if (AlunaCache.cache.has(cacheKey)) {
+
+      return AlunaCache.cache.get<T>(cacheKey) as T
+
+    }
+
+    const { requestConfig } = assembleRequestConfig({
+      url,
+      method: verb,
+      data: body,
+      proxySettings: settings?.proxySettings,
+    })
 
     this.requestCount.public += weight
 
-    const data: T = {
-      ...params,
-    } as any
+    try {
 
-    return data
+      const { data } = await axios.create().request<T>(requestConfig)
+
+      AlunaCache.cache.set<T>(cacheKey, data)
+
+      return data
+
+    } catch (error) {
+
+      throw handleBittrexRequestError({ error })
+
+    }
 
   }
 
@@ -49,16 +151,43 @@ export class BittrexHttp implements IAlunaHttp {
   ): Promise<T> {
 
     const {
+      url,
+      body,
+      verb = AlunaHttpVerbEnum.POST,
+      credentials,
+      settings,
       weight = 1,
     } = params
 
+    const signedHash = generateAuthHeader({
+      verb,
+      path: new URL(url).pathname,
+      credentials,
+      body,
+      url,
+    })
+
+    const { requestConfig } = assembleRequestConfig({
+      url,
+      method: verb,
+      data: body,
+      headers: signedHash,
+      proxySettings: settings?.proxySettings,
+    })
+
     this.requestCount.authed += weight
 
-    const data: T = {
-      ...params,
-    } as any
+    try {
 
-    return data
+      const { data } = await axios.create().request<T>(requestConfig)
+
+      return data
+
+    } catch (error) {
+
+      throw handleBittrexRequestError({ error })
+
+    }
 
   }
 
