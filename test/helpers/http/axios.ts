621189102dde5fb6d2be42cf004031db971d1292
Sergio <sergio.uft@gmail.com>
2022-04-29 14:30:20 -0300

    refact 'mockAxiosRequest'

--- test/helpers/http/axios.ts
+++ test/helpers/http/axios.ts
@@ -2,93 +2,13 @@ import axios from 'axios'
 import Sinon from 'sinon'
 import { ImportMock } from 'ts-mock-imports'
 
-import { IAlunaHttp } from '../../../src/lib/core/IAlunaHttp'
 
 
+export const mockAxiosRequest = () => {
 
-export const mockPublicHttpRequest = (params: {
-  exchangeHttp: IAlunaHttp
-  requestResponse?: any
-  isReject?: boolean
-}) => {
+  const request = Sinon.stub()
 
-  const {
-    exchangeHttp,
-    requestResponse = Promise.resolve(true),
-    isReject = false,
-  } = params
-
-  const publicRequest = ImportMock.mockFunction(
-    exchangeHttp,
-    'publicRequest',
-    isReject
-      ? requestResponse
-      : Promise.resolve({
-        data: requestResponse,
-        requestCount: 1,
-      }),
-  )
-
-  return { publicRequest }
-
-}
-
-
-
-export const mockPrivateHttpRequest = (params: {
-  exchangeHttp: IAlunaHttp
-  requestResponse?: any
-  isReject?: boolean
-}) => {
-
-  const {
-    exchangeHttp,
-    requestResponse = true,
-    isReject = false,
-  } = params
-
-  const authedRequest = ImportMock.mockFunction(
-    exchangeHttp,
-    'authedRequest',
-    isReject
-      ? requestResponse
-      : Promise.resolve({
-        data: requestResponse,
-        requestCount: 1,
-      }),
-  )
-
-  return { authedRequest }
-
-}
-
-
-
-export const mockAxiosRequest = (
-  params: {
-    responseData?: any
-    error?: any
-  } = { responseData: {} },
-) => {
-
-  const {
-    error,
-    responseData,
-  } = params
-
-  let request: any
-
-  if (error) {
-
-    request = Sinon.spy(async () => Promise.reject(error))
-
-  } else {
-
-    request = Sinon.spy(async () => Promise.resolve({ data: responseData }))
-
-  }
-
-  const create = ImportMock.mockFunction(
+  const axiosCreate = ImportMock.mockFunction(
     axios,
     'create',
     { request },
@@ -96,7 +16,7 @@ export const mockAxiosRequest = (
 
   return {
     request,
-    create,
+    axiosCreate,
   }
 
 }
