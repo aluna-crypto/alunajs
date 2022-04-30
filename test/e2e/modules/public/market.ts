c820f9c53f11a84e78b3fd833225b24050254b7d
Anderson Arboleya <anderson@arboleya.me>
2022-04-30 14:31:57 -0300

    Implementing tests for `market` and `symbol` modules.

--- test/e2e/modules/public/market.ts
+++ test/e2e/modules/public/market.ts
@@ -7,11 +7,31 @@ import { IAlunaExchangePublic } from '../../../../src/lib/core/IAlunaExchange'
 export function market(exchange: IAlunaExchangePublic) {
 
   it('listRaw', async () => {
-    expect(true).to.be.ok
+
+    const {
+      rawMarkets,
+      requestCount,
+    } = await exchange.market.listRaw()
+
+    expect(rawMarkets).to.exist // not always an array
+
+    expect(requestCount.public).to.be.greaterThan(0) // at lesat one request
+    expect(requestCount.authed).to.be.eq(0)
+
   })
 
   it('list', async () => {
-    expect(true).to.be.ok
+
+    const {
+      markets,
+      requestCount,
+    } = await exchange.market.list()
+
+    expect(markets.length).to.be.greaterThan(0)
+
+    expect(requestCount.public).to.be.eq(0) // must come from cache now
+    expect(requestCount.authed).to.be.eq(0)
+
   })
 
 }
--- test/e2e/modules/public/symbol.ts
+++ test/e2e/modules/public/symbol.ts
@@ -7,11 +7,31 @@ import { IAlunaExchangePublic } from '../../../../src/lib/core/IAlunaExchange'
 export function symbol(exchange: IAlunaExchangePublic) {
 
   it('listRaw', async () => {
-    expect(true).to.be.ok
+
+    const {
+      rawSymbols,
+      requestCount,
+    } = await exchange.symbol.listRaw()
+
+    expect(rawSymbols.length).to.be.greaterThan(0)
+
+    expect(requestCount.public).to.be.greaterThan(0) // at lesat one request
+    expect(requestCount.authed).to.be.eq(0)
+
   })
 
   it('list', async () => {
-    expect(true).to.be.ok
+
+    const {
+      symbols,
+      requestCount,
+    } = await exchange.symbol.list()
+
+    expect(symbols.length).to.be.greaterThan(0)
+
+    expect(requestCount.public).to.be.eq(0) // must come from cache now
+    expect(requestCount.authed).to.be.eq(0)
+
   })
 
 }
