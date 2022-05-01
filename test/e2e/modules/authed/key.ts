bda09440c34fac049b9541b4849e0e70c0c8650b
Anderson Arboleya <anderson@arboleya.me>
2022-04-30 21:06:05 -0300

    Add new test case for `key.fetchDetails`.

--- test/e2e/modules/authed/key.ts
+++ test/e2e/modules/authed/key.ts
@@ -1,13 +1,31 @@
 import { expect } from 'chai'
 
-import { IAlunaExchangeAuthed } from '../../../../src/lib/core/IAlunaExchange'
+import { IAlunaExchangeAuthed, IAlunaExchangePublic } from '../../../../src/lib/core/IAlunaExchange'
 
 
 
 export function key(exchange: IAlunaExchangeAuthed) {
 
   it('fetchDetails', async () => {
-    expect(true).to.be.ok
+
+    const {
+      key,
+      requestCount,
+    } = await exchange.key.fetchDetails()
+
+    expect(key).to.exist
+    // expect(key.accountId).to.exist // <— not always present
+
+    expect(key.permissions).to.exist
+    expect(key.permissions.read).to.exist
+    expect(key.permissions.withdraw).to.exist
+    expect(key.permissions.trade).to.exist
+
+    expect(key.meta).to.exist
+
+    expect(requestCount.authed).to.be.greaterThan(1) // at least one
+    expect(requestCount.public).to.be.eq(0) // never called
+
   })
 
 }
