cb517d8fb76e7dc2815234b4c3c0f45995690626
Anderson Arboleya <anderson@arboleya.me>
2022-05-02 18:46:04 -0300

    Start implementing `Web3Http.publicRequest`.

--- src/modules/web3/Web3Http.ts
+++ src/modules/web3/Web3Http.ts
@@ -1,9 +1,25 @@
+import axios, { AxiosError } from 'axios'
+import { debug } from 'debug'
+
+import { AlunaError } from '../../lib/core/AlunaError'
 import {
   IAlunaHttp,
   IAlunaHttpAuthedParams,
   IAlunaHttpPublicParams,
   IAlunaHttpRequestCount,
 } from '../../lib/core/IAlunaHttp'
+import { AlunaHttpVerbEnum } from '../../lib/enums/AlunaHtttpVerbEnum'
+import { AlunaHttpErrorCodes } from '../../lib/errors/AlunaHttpErrorCodes'
+import { assembleRequestConfig } from '../../utils/axios/assembleRequestConfig'
+import { AlunaCache } from '../../utils/cache/AlunaCache'
+
+
+
+const log = debug('@alunajs:web3/http')
+
+
+
+export const WEB3_CACHE_KEY_PREFIX = 'Web3.publicRequest'
 
 
 
@@ -29,16 +45,53 @@ export class Web3Http implements IAlunaHttp {
   ): Promise<T> {
 
     const {
+      url,
+      body,
+      verb = AlunaHttpVerbEnum.GET,
       weight = 1,
+      settings,
     } = params
 
     this.requestCount.public += weight
 
-    const data: T = {
-      ...params,
-    } as any
+    const { requestConfig } = assembleRequestConfig({
+      url,
+      method: verb,
+      data: body,
+      proxySettings: settings?.proxySettings,
+    })
 
-    return data
+    const cacheKey = AlunaCache.hashCacheKey({
+      args: params,
+      prefix: WEB3_CACHE_KEY_PREFIX,
+    })
+
+    if (AlunaCache.cache.has(cacheKey)) {
+
+      return AlunaCache.cache.get<T>(cacheKey) as T
+
+    }
+
+    try {
+
+      const { data } = await axios.create().request<T>(requestConfig)
+
+      AlunaCache.cache.set<T>(cacheKey, data)
+
+      return data
+
+    } catch (error) {
+
+      const { response } = error as AxiosError
+      const defaultMessage = 'Error request Web3 data.'
+
+      throw new AlunaError({
+        code: AlunaHttpErrorCodes.REQUEST_ERROR,
+        message: response?.data?.code || defaultMessage,
+        metadata: error,
+      })
+
+    }
 
   }
 
@@ -52,11 +105,11 @@ export class Web3Http implements IAlunaHttp {
       weight = 1,
     } = params
 
+    log('TODO: Implement stub method: Web3.authedRequest')
+
     this.requestCount.authed += weight
 
-    const data: T = {
-      ...params,
-    } as any
+    const data: T = params as any
 
     return data
 
