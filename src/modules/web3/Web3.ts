07e643d2fcccc1e6422228b7c1033cf6aac6c1ea
Anderson Arboleya <anderson@arboleya.me>
2022-05-02 18:48:40 -0300

    Initialing Web3 `token` module.

--- src/modules/web3/Web3.ts
+++ src/modules/web3/Web3.ts
@@ -2,17 +2,23 @@ import {
   balance,
   IWeb3BalanceModule,
 } from './modules/balance'
+import {
+  IWeb3TokenModule,
+  token,
+} from './modules/token'
 
 
 
 export class Web3 {
 
+  public token: IWeb3TokenModule
   public balance: IWeb3BalanceModule
 
 
 
   constructor() {
 
+    this.token = token(this)
     this.balance = balance(this)
 
     return this
