088efc296cc885dd48b7ddf744876a63248fcea9
Sergio <sergio.uft@gmail.com>
2022-05-02 16:40:54 -0300

    implement Bittrex Order 'cancel'

--- src/exchanges/bittrex/modules/authed/order/cancel.ts
+++ src/exchanges/bittrex/modules/authed/order/cancel.ts
@@ -1,13 +1,16 @@
 import { debug } from 'debug'
 
+import { AlunaError } from '../../../../../lib/core/AlunaError'
 import { IAlunaExchangeAuthed } from '../../../../../lib/core/IAlunaExchange'
+import { AlunaHttpVerbEnum } from '../../../../../lib/enums/AlunaHtttpVerbEnum'
+import { AlunaOrderErrorCodes } from '../../../../../lib/errors/AlunaOrderErrorCodes'
 import {
   IAlunaOrderCancelParams,
   IAlunaOrderCancelReturns,
 } from '../../../../../lib/modules/authed/IAlunaOrderModule'
-import { IAlunaOrderSchema } from '../../../../../lib/schemas/IAlunaOrderSchema'
 import { BittrexHttp } from '../../../BittrexHttp'
 import { BITTREX_PRODUCTION_URL } from '../../../bittrexSpecs'
+import { IBittrexOrderSchema } from '../../../schemas/IBittrexOrderSchema'
 
 
 
@@ -23,18 +26,46 @@ export const cancel = (exchange: IAlunaExchangeAuthed) => async (
 
   const { credentials } = exchange
 
-  const { http = new BittrexHttp() } = params
+  const {
+    id,
+    http = new BittrexHttp(),
+  } = params
 
-  const order = await http.authedRequest<IAlunaOrderSchema>({
-    url: BITTREX_PRODUCTION_URL,
-    credentials,
-  })
+  try {
 
-  const { requestCount } = http
+    const rawOrder = await http.authedRequest<IBittrexOrderSchema>({
+      verb: AlunaHttpVerbEnum.DELETE,
+      url: `${BITTREX_PRODUCTION_URL}/orders/${id}`,
+      credentials,
+    })
 
-  return {
-    order,
-    requestCount,
+    const { order } = exchange.order.parse({ rawOrder })
+
+    const { requestCount } = http
+
+    return {
+      order,
+      requestCount,
+    }
+
+  } catch (err) {
+
+    const {
+      metadata,
+      httpStatusCode,
+    } = err
+
+    const error = new AlunaError({
+      message: 'Something went wrong, order not canceled',
+      httpStatusCode,
+      code: AlunaOrderErrorCodes.CANCEL_FAILED,
+      metadata,
+    })
+
+    log(error)
+
+    throw error
+
   }
 
 }
