2f5d4c34a9a8067b580bdbe8172b470f5eeac408
Anderson Arboleya <anderson@arboleya.me>
2022-05-02 19:04:03 -0300

    Adding test stub files for Web3 `balance.list`.

--- src/modules/web3/modules/balance/getTotalBalance.spec.ts
+++ src/modules/web3/modules/balance/getTotalBalance.spec.ts
@@ -4,7 +4,7 @@ import { expect } from 'chai'
 
 describe(__filename, () => {
 
-  it('should list Web3 balances just fine', async () => {
+  it('should get Web3 totalBalance just fine', async () => {
     expect(true).to.be.ok
   })
 
