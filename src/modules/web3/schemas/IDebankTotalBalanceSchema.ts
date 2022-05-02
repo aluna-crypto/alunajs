88101e9f352e93ebfa95faadddc57da8204f66c8
Anderson Arboleya <anderson@arboleya.me>
2022-05-02 18:45:25 -0300

    Adding schemas for Debank API.

---
+++ src/modules/web3/schemas/IDebankTotalBalanceSchema.ts
@@ -0,0 +1,12 @@
+import { IDebankChainSchema } from './IDebankChainSchema'
+
+
+
+export interface IDebankTotalBalanceSchema {
+
+  /* eslint-disable camelcase */
+  total_usd_value: number
+  chain_list: IDebankChainSchema[]
+  /* eslint-disable camelcase */
+
+}
