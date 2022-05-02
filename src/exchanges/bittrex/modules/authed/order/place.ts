1221274468860064892956a0eccec15f40600afd
Sergio <sergio.uft@gmail.com>
2022-05-02 17:26:37 -0300

    implement Bittrex Order 'place'

--- src/exchanges/bittrex/modules/authed/order/place.ts
+++ src/exchanges/bittrex/modules/authed/order/place.ts
@@ -1,13 +1,29 @@
 import { debug } from 'debug'
+import { assign } from 'lodash'
 
+import { AlunaError } from '../../../../../lib/core/AlunaError'
 import { IAlunaExchangeAuthed } from '../../../../../lib/core/IAlunaExchange'
+import { AlunaFeaturesModeEnum } from '../../../../../lib/enums/AlunaFeaturesModeEnum'
+import { AlunaAccountsErrorCodes } from '../../../../../lib/errors/AlunaAccountsErrorCodes'
+import { AlunaBalanceErrorCodes } from '../../../../../lib/errors/AlunaBalanceErrorCodes'
+import { AlunaGenericErrorCodes } from '../../../../../lib/errors/AlunaGenericErrorCodes'
+import { AlunaOrderErrorCodes } from '../../../../../lib/errors/AlunaOrderErrorCodes'
 import {
   IAlunaOrderPlaceParams,
   IAlunaOrderPlaceReturns,
 } from '../../../../../lib/modules/authed/IAlunaOrderModule'
-import { IAlunaOrderSchema } from '../../../../../lib/schemas/IAlunaOrderSchema'
+import { placeOrderParamsSchema } from '../../../../../utils/validation/schemas/placeOrderParamsSchema'
+import { validateParams } from '../../../../../utils/validation/validateParams'
 import { BittrexHttp } from '../../../BittrexHttp'
-import { BITTREX_PRODUCTION_URL } from '../../../bittrexSpecs'
+import {
+  BITTREX_PRODUCTION_URL,
+  bittrexBaseSpecs,
+} from '../../../bittrexSpecs'
+import { translateOrderSideToBittrex } from '../../../enums/adapters/bittrexOrderSideAdapter'
+import { translateOrderTypeToBittrex } from '../../../enums/adapters/bittrexOrderTypeAdapter'
+import { BittrexOrderTimeInForceEnum } from '../../../enums/BittrexOrderTimeInForceEnum'
+import { BittrexOrderTypeEnum } from '../../../enums/BittrexOrderTypeEnum'
+import { IBittrexOrderSchema } from '../../../schemas/IBittrexOrderSchema'
 
 
 
@@ -23,12 +39,160 @@ export const place = (exchange: IAlunaExchangeAuthed) => async (
 
   const { credentials } = exchange
 
-  const { http = new BittrexHttp() } = params
-
-  const order = await http.authedRequest<IAlunaOrderSchema>({
-    url: BITTREX_PRODUCTION_URL,
-    credentials,
+  validateParams({
+    params,
+    schema: placeOrderParamsSchema,
   })
+
+  const {
+    amount,
+    rate,
+    symbolPair,
+    side,
+    type,
+    account,
+    http = new BittrexHttp(),
+  } = params
+
+  try {
+
+    const accountSpecs = bittrexBaseSpecs.accounts.find((a) => {
+      return a.type === account
+    })
+
+    if (!accountSpecs) {
+
+      throw new AlunaError({
+        message: `Account type '${account}' not found`,
+        code: AlunaAccountsErrorCodes.TYPE_NOT_FOUND,
+      })
+
+    }
+
+    const {
+      supported,
+      implemented,
+      orderTypes,
+    } = accountSpecs
+
+    if (!supported || !implemented) {
+
+      throw new AlunaError({
+        message:
+            `Account type '${account}' not supported/implemented for Bittrex`,
+        code: AlunaAccountsErrorCodes.TYPE_NOT_SUPPORTED,
+      })
+
+    }
+
+    const orderType = orderTypes.find((o) => o.type === type)
+
+    if (!orderType || !orderType.implemented || !orderType.supported) {
+
+      throw new AlunaError({
+        message: `Order type '${type}' not supported/implemented for Bittrex`,
+        code: AlunaOrderErrorCodes.TYPE_NOT_SUPPORTED,
+      })
+
+    }
+
+    if (orderType.mode === AlunaFeaturesModeEnum.READ) {
+
+      throw new AlunaError({
+        message: `Order type '${type}' is in read mode`,
+        code: AlunaOrderErrorCodes.TYPE_IS_READ_ONLY,
+      })
+
+    }
+
+  } catch (error) {
+
+    log(error)
+
+    throw error
+
+  }
+
+  const translatedOrderType = translateOrderTypeToBittrex({
+    from: type,
+  })
+
+  const body = {
+    direction: translateOrderSideToBittrex({ from: side }),
+    marketSymbol: symbolPair,
+    type: translatedOrderType,
+    quantity: Number(amount),
+  }
+
+  if (translatedOrderType === BittrexOrderTypeEnum.LIMIT) {
+
+    assign(body, {
+      limit: Number(rate),
+      timeInForce: BittrexOrderTimeInForceEnum.GOOD_TIL_CANCELLED,
+    })
+
+  } else {
+
+    assign(body, {
+      timeInForce: BittrexOrderTimeInForceEnum.FILL_OR_KILL,
+    })
+
+  }
+
+  log('placing new order for Bittrex')
+
+  let placedOrder: IBittrexOrderSchema
+
+  try {
+
+    const orderResponse = await http.authedRequest<IBittrexOrderSchema>({
+      url: `${BITTREX_PRODUCTION_URL}/orders`,
+      body,
+      credentials,
+    })
+
+    placedOrder = orderResponse
+
+  } catch (err) {
+
+    let {
+      code,
+      message,
+    } = err
+
+    const { metadata } = err
+
+    if (metadata.code === 'INSUFFICIENT_FUNDS') {
+
+      code = AlunaBalanceErrorCodes.INSUFFICIENT_BALANCE
+
+      message = 'Account has insufficient balance for requested action.'
+
+    } else if (metadata.code === 'DUST_TRADE_DISALLOWED_MIN_VALUE') {
+
+      code = AlunaGenericErrorCodes.UNKNOWN
+
+      message = 'The amount of quote currency involved in a transaction '
+        .concat('would be less than the minimum limit of 10K satoshis')
+
+    } else if (metadata.code === 'MIN_TRADE_REQUIREMENT_NOT_MET') {
+
+      code = AlunaOrderErrorCodes.PLACE_FAILED
+
+      message = 'The trade was smaller than the min trade size quantity for '
+        .concat('the market')
+
+    }
+
+    throw new AlunaError({
+      ...err,
+      code,
+      message,
+    })
+
+  }
+
+  const { order } = exchange.order.parse({ rawOrder: placedOrder })
 
   const { requestCount } = http
 
