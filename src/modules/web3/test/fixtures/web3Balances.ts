5719feab63da5842e7faeb12dc533722f62e3377
Anderson Arboleya <anderson@arboleya.me>
2022-05-02 18:46:56 -0300

    Adding fixture stub file.

--- src/modules/web3/test/fixtures/web3Balances.ts
+++ src/modules/web3/test/fixtures/web3Balances.ts
@@ -1,10 +1,1 @@
-import { IWeb3BalanceSchema } from '../../schemas/IWeb3BalanceSchema'
-
-
-
-export const WEB3_BALANCES: IWeb3BalanceSchema[] = [{
-  symbolId: 'BTC',
-  symbolName: 'Bitcoin',
-  available: '1.2',
-  total: '1.3',
-}]
+export const WEB3_BALANCES: any[] = [{}]
