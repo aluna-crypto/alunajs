ca21379a59880022008a05b9b2a1c3546201a3cf
Sergio <sergio.uft@gmail.com>
2022-05-02 16:22:49 -0300

    implement Bittrex Order 'parse'

--- src/exchanges/bittrex/modules/authed/order/parse.ts
+++ src/exchanges/bittrex/modules/authed/order/parse.ts
@@ -1,13 +1,19 @@
 import { debug } from 'debug'
 
 import { IAlunaExchangeAuthed } from '../../../../../lib/core/IAlunaExchange'
+import { AlunaAccountEnum } from '../../../../../lib/enums/AlunaAccountEnum'
+import { AlunaOrderStatusEnum } from '../../../../../lib/enums/AlunaOrderStatusEnum'
 import {
   IAlunaOrderParseParams,
   IAlunaOrderParseReturns,
 } from '../../../../../lib/modules/authed/IAlunaOrderModule'
 import { IAlunaOrderSchema } from '../../../../../lib/schemas/IAlunaOrderSchema'
-import { BittrexHttp } from '../../../BittrexHttp'
-import { BITTREX_PRODUCTION_URL } from '../../../bittrexSpecs'
+import { AlunaSymbolMapping } from '../../../../../utils/mappings/AlunaSymbolMapping'
+import { bittrexBaseSpecs } from '../../../bittrexSpecs'
+import { translateOrderSideToAluna } from '../../../enums/adapters/bittrexOrderSideAdapter'
+import { translateOrderStatusToAluna } from '../../../enums/adapters/bittrexOrderStatusAdapter'
+import { translateOrderTypeToAluna } from '../../../enums/adapters/bittrexOrderTypeAdapter'
+import { BittrexOrderStatusEnum } from '../../../enums/BittrexOrderStatusEnum'
 
 
 
@@ -15,26 +21,101 @@ const log = debug('@aluna.js:bittrex/order/parse')
 
 
 
-export const parse = (exchange: IAlunaExchangeAuthed) => async (
+export const parse = (exchange: IAlunaExchangeAuthed) => (
   params: IAlunaOrderParseParams,
-): Promise<IAlunaOrderParseReturns> => {
+): IAlunaOrderParseReturns => {
 
   log('params', params)
 
-  const { credentials } = exchange
+  const { rawOrder } = params
 
-  const { http = new BittrexHttp() } = params
+  const exchangeId = bittrexBaseSpecs.id
 
-  const order = await http.authedRequest<IAlunaOrderSchema>({
-    url: BITTREX_PRODUCTION_URL,
-    credentials,
+  const {
+    createdAt,
+    direction,
+    id,
+    fillQuantity,
+    marketSymbol,
+    quantity,
+    type: orderType,
+    status,
+    limit,
+    closedAt,
+    proceeds,
+  } = rawOrder
+
+  const [baseCurrency, quoteCurrency] = rawOrder.marketSymbol.split('-')
+
+  const baseSymbolId = AlunaSymbolMapping.translateSymbolId({
+    exchangeSymbolId: baseCurrency,
+    symbolMappings: exchange.settings.mappings,
   })
 
-  const { requestCount } = http
+  const quoteSymbolId = AlunaSymbolMapping.translateSymbolId({
+    exchangeSymbolId: quoteCurrency,
+    symbolMappings: exchange.settings.mappings,
+  })
 
-  return {
-    order,
-    requestCount,
+  let rate: number | undefined
+
+  if (limit) {
+
+    rate = parseFloat(limit)
+
+  } else if (proceeds) {
+
+    rate = parseFloat(proceeds)
+
   }
+
+  const amount = parseFloat(quantity)
+  const total = rate ? amount * rate : amount
+
+  const orderStatus = translateOrderStatusToAluna({
+    fillQuantity,
+    quantity,
+    from: status,
+  })
+
+  let filledAt: Date | undefined
+  let canceledAt: Date | undefined
+
+  if (orderStatus === AlunaOrderStatusEnum.FILLED) {
+
+    filledAt = new Date(closedAt)
+
+  }
+
+  const isClosed = status === BittrexOrderStatusEnum.CLOSED
+  const isCanceled = orderStatus === AlunaOrderStatusEnum.CANCELED
+  const isPartFilled = orderStatus === AlunaOrderStatusEnum.PARTIALLY_FILLED
+
+  if (isCanceled || (isPartFilled && isClosed)) {
+
+    canceledAt = new Date(closedAt)
+
+  }
+
+  const parsedOrder: IAlunaOrderSchema = {
+    id,
+    symbolPair: marketSymbol,
+    total,
+    amount,
+    rate,
+    exchangeId,
+    baseSymbolId,
+    quoteSymbolId,
+    account: AlunaAccountEnum.EXCHANGE,
+    side: translateOrderSideToAluna({ from: direction }),
+    status: orderStatus,
+    type: translateOrderTypeToAluna({ from: orderType }),
+    placedAt: new Date(createdAt),
+    filledAt,
+    canceledAt,
+    meta: rawOrder,
+  }
+
+  return { order: parsedOrder }
 
 }
