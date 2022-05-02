2907b8cce821890d14a6a8642d01b36bbe11f20a
LucianoPierdona <lucianompj1@gmail.com>
2022-05-02 20:34:56 -0300

    implement tests for Bittrex order parseMany

---
+++ src/exchanges/bittrex/modules/authed/order/parseMany.spec.ts
@@ -0,0 +1,50 @@
+import { expect } from 'chai'
+import { each } from 'lodash'
+
+import { mockOrderParse } from '../../../../../../test/mocks/exchange/modules/order/mockOrderParse'
+import { IAlunaCredentialsSchema } from '../../../../../lib/schemas/IAlunaCredentialsSchema'
+import { BittrexAuthed } from '../../../BittrexAuthed'
+import {
+  BITTREX_PARSED_ORDERS,
+  BITTREX_RAW_ORDERS,
+} from '../../../test/fixtures/bittrexOrders'
+import * as parseMod from './parse'
+
+
+
+describe(__filename, () => {
+
+  const credentials: IAlunaCredentialsSchema = {
+    key: 'key',
+    secret: 'secret',
+  }
+
+  it('should parse many Bittrex raw orders just fine', async () => {
+
+    // preparing data
+    const exchange = new BittrexAuthed({ credentials })
+
+    const parsedOrders = BITTREX_PARSED_ORDERS
+    const rawOrders = BITTREX_RAW_ORDERS
+
+
+    // mocking
+    const { parse } = mockOrderParse({ module: parseMod })
+
+    each(parsedOrders, (order, index) => {
+      parse.onCall(index).returns({ order })
+    })
+
+
+    // executing
+    const { orders } = exchange.order.parseMany({ rawOrders })
+
+
+    // validating
+    expect(orders).to.deep.eq(parsedOrders)
+
+    expect(parse.callCount).to.be.eq(parsedOrders.length)
+
+  })
+
+})
