7dc10231dcd602f5db3abf921ca4643d32e5bfaa
LucianoPierdona <lucianompj1@gmail.com>
2022-05-02 19:07:58 -0300

    Implement order listRaw tests

---
+++ src/exchanges/bittrex/modules/authed/order/listRaw.spec.ts
@@ -0,0 +1,61 @@
+import { expect } from 'chai'
+
+import { mockHttp } from '../../../../../../test/mocks/exchange/Http'
+import { AlunaHttpVerbEnum } from '../../../../../lib/enums/AlunaHtttpVerbEnum'
+import { IAlunaCredentialsSchema } from '../../../../../lib/schemas/IAlunaCredentialsSchema'
+import { BittrexAuthed } from '../../../BittrexAuthed'
+import { BittrexHttp } from '../../../BittrexHttp'
+import { BITTREX_PRODUCTION_URL } from '../../../bittrexSpecs'
+import { BITTREX_RAW_ORDERS } from '../../../test/fixtures/bittrexOrders'
+
+
+
+describe(__filename, () => {
+
+  const credentials: IAlunaCredentialsSchema = {
+    key: 'key',
+    secret: 'secret',
+  }
+
+  it('should list Bittrex raw orders just fine', async () => {
+
+    // preparing data
+    const http = new BittrexHttp()
+
+    const mockedRawOrders = BITTREX_RAW_ORDERS
+
+    // mocking
+    const {
+      publicRequest,
+      authedRequest,
+    } = mockHttp({ classPrototype: BittrexHttp.prototype })
+
+    authedRequest.returns(Promise.resolve(mockedRawOrders))
+
+    // executing
+    const exchange = new BittrexAuthed({ credentials })
+
+    const {
+      rawOrders,
+      requestCount,
+    } = await exchange.order.listRaw({})
+
+
+    // validating
+    expect(rawOrders).to.deep.eq(mockedRawOrders)
+
+    expect(requestCount).to.deep.eq(http.requestCount)
+
+    expect(authedRequest.callCount).to.be.eq(1)
+
+    expect(authedRequest.firstCall.args[0]).to.deep.eq({
+      verb: AlunaHttpVerbEnum.GET,
+      credentials,
+      url: `${BITTREX_PRODUCTION_URL}/orders/open`,
+    })
+
+    expect(publicRequest.callCount).to.be.eq(0)
+
+  })
+
+})
