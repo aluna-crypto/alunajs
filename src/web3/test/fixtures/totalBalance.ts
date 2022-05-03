64e459ba14bf78370b9a1e9419bd9e42e98f83b4
Anderson Arboleya <anderson@arboleya.me>
2022-05-03 11:48:46 -0300

    Adding proper Web3 fixtures.

---
+++ src/web3/test/fixtures/totalBalance.ts
@@ -0,0 +1,13 @@
+import { IDebankTotalBalanceSchema } from '../../schemas/IDebankTotalBalanceSchema'
+import { WEB3_DEBANK_CHAIN_LIST } from './chains'
+
+
+
+/* eslint-disable max-len */
+
+
+
+export const WEB3_DEBANK_TOTAL_BALANCE: IDebankTotalBalanceSchema = {
+  total_usd_value: 1624.2446599144257,
+  chain_list: WEB3_DEBANK_CHAIN_LIST,
+}
