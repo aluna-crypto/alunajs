5923b41f1be780694ffc340ef1dbaa58a52c5d93
Sergio <sergio.uft@gmail.com>
2022-05-03 11:33:51 -0300

    implement e2e helper 'placeLimitOrder'

--- test/e2e/modules/authed/helpers/order/placeLimitOrder.ts
+++ test/e2e/modules/authed/helpers/order/placeLimitOrder.ts
@@ -1,16 +1,33 @@
-import { IAlunaExchangeAuthed } from '../../../../../../src/lib/core/IAlunaExchange'
+import { AlunaAccountEnum } from '../../../../../../src/lib/enums/AlunaAccountEnum'
+import { AlunaOrderSideEnum } from '../../../../../../src/lib/enums/AlunaOrderSideEnum'
+import { AlunaOrderTypesEnum } from '../../../../../../src/lib/enums/AlunaOrderTypesEnum'
+import { IAuthedParams } from '../../../IAuthedParams'
 
 
 
-export const placeLimitOrder = async (params: {
-  exchangeAuthed: IAlunaExchangeAuthed
-}) => {
+export const placeLimitOrder = async (
+  params: IAuthedParams,
+) => {
 
-  const { exchangeAuthed } = params
+  const {
+    exchangeAuthed,
+    exchangeConfigs,
+  } = params
+
+  const {
+    symbolPair,
+    orderAccount,
+    orderAmount,
+    orderRate,
+  } = exchangeConfigs
 
   return exchangeAuthed.order.place({
-    ...params,
-    // ...
-  } as any)
+    symbolPair,
+    account: orderAccount || AlunaAccountEnum.EXCHANGE,
+    rate: orderRate,
+    amount: orderAmount,
+    side: AlunaOrderSideEnum.BUY,
+    type: AlunaOrderTypesEnum.LIMIT,
+  })
 
 }
