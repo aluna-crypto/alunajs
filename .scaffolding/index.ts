8f0fc54070659e280fb066841907917734a7ffbb
Sergio <sergio.uft@gmail.com>
2022-05-03 15:15:53 -0300

    moving validations to external file

--- .scaffolding/index.ts
+++ .scaffolding/index.ts
@@ -3,23 +3,7 @@ import inquirer from 'inquirer'
 import { AlunaAccountEnum } from '../src/lib/enums/AlunaAccountEnum'
 import { AlunaApiFeaturesEnum } from '../src/lib/enums/AlunaApiFeaturesEnum'
 import { generate } from './generate'
-
-
-
-const validations = {
-  notNull (answer: string) {
-    if (!answer) {
-      return 'You must inform exchange name.';
-    }
-    return true
-  },
-  atLeastOne (answer: string[]) {
-    if (answer.length < 1) {
-      return 'You must select at least one.';
-    }
-    return true
-  },
-}
+import { validations } from './validations'
 
 
 
---
+++ .scaffolding/validations.ts
@@ -0,0 +1,14 @@
+export const validations = {
+  notNull(answer: string) {
+    if (!answer) {
+      return 'You must inform exchange name.';
+    }
+    return true;
+  },
+  atLeastOne(answer: string[]) {
+    if (answer.length < 1) {
+      return 'You must select at least one.';
+    }
+    return true;
+  },
+};
