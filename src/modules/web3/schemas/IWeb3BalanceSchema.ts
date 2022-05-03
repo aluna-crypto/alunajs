34e1989e2ca7db7897cbb25bdd48f89e7c7fd98f
Anderson Arboleya <anderson@arboleya.me>
2022-05-03 07:56:29 -0300

    Simplifying schema.

--- src/modules/web3/schemas/IWeb3BalanceSchema.ts
+++ src/modules/web3/schemas/IWeb3BalanceSchema.ts
@@ -1,9 +1,4 @@
-import { IWeb3ChainSchema } from './IWeb3ChainSchema'
-import { IWeb3TokenSchema } from './IWeb3TokenSchema'
-
-
-
 export interface IWeb3BalanceSchema {
-  chain: Omit<IWeb3ChainSchema, 'meta'>
-  tokens: IWeb3TokenSchema[]
+  tokenId: string
+  tokenSymbol: string
 }
