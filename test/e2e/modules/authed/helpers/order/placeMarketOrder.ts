aa13a2cc37dc48ba08849d5367b67704972e269a
Sergio <sergio.uft@gmail.com>
2022-05-03 11:47:06 -0300

    implement e2e helper 'placeMarketOrder'

---
+++ test/e2e/modules/authed/helpers/order/placeMarketOrder.ts
@@ -0,0 +1,31 @@
+import { AlunaAccountEnum } from '../../../../../../src/lib/enums/AlunaAccountEnum'
+import { AlunaOrderSideEnum } from '../../../../../../src/lib/enums/AlunaOrderSideEnum'
+import { AlunaOrderTypesEnum } from '../../../../../../src/lib/enums/AlunaOrderTypesEnum'
+import { IAuthedParams } from '../../../IAuthedParams'
+
+
+
+export const placeMarketOrder = async (
+  params: IAuthedParams,
+) => {
+
+  const {
+    exchangeAuthed,
+    exchangeConfigs,
+  } = params
+
+  const {
+    symbolPair,
+    orderAccount,
+    orderAmount,
+  } = exchangeConfigs
+
+  return exchangeAuthed.order.place({
+    symbolPair,
+    account: orderAccount || AlunaAccountEnum.EXCHANGE,
+    amount: orderAmount,
+    side: AlunaOrderSideEnum.SELL,
+    type: AlunaOrderTypesEnum.MARKET,
+  })
+
+}
