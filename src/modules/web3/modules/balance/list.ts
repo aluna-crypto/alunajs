bb077183122ad2d9f56b66d7a1e0573583c9351c
Anderson Arboleya <anderson@arboleya.me>
2022-05-03 07:58:06 -0300

    Refactoring `balance.list` method for Web3.

--- src/modules/web3/modules/balance/list.ts
+++ src/modules/web3/modules/balance/list.ts
@@ -1,15 +1,16 @@
+import { debug } from 'debug'
 import {
   filter,
   flatten,
   map,
-  omit,
 } from 'lodash'
 
 import {
   IAlunaHttp,
   IAlunaHttpRequestCount,
 } from '../../../../lib/core/IAlunaHttp'
-import { IWeb3BalanceSchema } from '../../schemas/IWeb3BalanceSchema'
+import { AlunaWalletEnum } from '../../../../lib/enums/AlunaWalletEnum'
+import { IAlunaBalanceSchema } from '../../../../lib/schemas/IAlunaBalanceSchema'
 import { IWeb3TokenSchema } from '../../schemas/IWeb3TokenSchema'
 import { IWeb3TotalBalanceSchema } from '../../schemas/IWeb3TotalBalanceSchema'
 import { Web3 } from '../../Web3'
@@ -17,42 +18,45 @@ import { Web3Http } from '../../Web3Http'
 
 
 
-// const log = debug('@aluna.js:web3/balance/listRaw')
+const log = debug('@aluna.js:web3/balance/listRaw')
 
 
 
 export interface IWeb3BalanceListParams {
   http?: IAlunaHttp
   address: string
-  chainId: string
 }
 
 export interface IWeb3BalanceListReturns {
-  balances: IWeb3BalanceSchema[]
+  balances: IAlunaBalanceSchema[]
   requestCount: IAlunaHttpRequestCount
 }
 
 
 
-export const list = (module: Web3) => async (
+export const list = (web3: Web3) => async (
   params: IWeb3BalanceListParams,
 ): Promise<IWeb3BalanceListReturns> => {
+
+  log('listing web3 raw balances')
 
   const {
     address,
     http = new Web3Http(),
   } = params
 
-  const { totalBalance } = await module.balance.getTotalBalance({
+  const { totalBalance } = await web3.balance.getTotalBalance({
     address,
     http,
   })
 
   const { chains } = totalBalance
 
-  const promises = map(chains, ({ id: chainId }) => {
-    return module.token.list({ address, chainId })
-  })
+  const promises = map(chains, ({ id: chainId }) => web3.token.list({
+    address,
+    chainId,
+    http,
+  }))
 
   const tokenList = flatten(map(await Promise.all(promises), 'tokens'))
 
@@ -75,7 +79,7 @@ export const list = (module: Web3) => async (
 export const parseBalances = async (params: {
   totalBalance: IWeb3TotalBalanceSchema
   tokenList: IWeb3TokenSchema[]
-}): Promise<{ parsedBalances: IWeb3BalanceSchema[] }> => {
+}): Promise<{ parsedBalances: IAlunaBalanceSchema[] }> => {
 
   const {
     totalBalance,
@@ -84,17 +88,31 @@ export const parseBalances = async (params: {
 
   const { chains } = totalBalance
 
-  const parsedBalances: IWeb3BalanceSchema[] = map(chains, (chain) => {
+  const parsedBalances: IAlunaBalanceSchema[] = flatten(map(chains, (chain) => {
 
+    const tokens = filter(tokenList, { chain: chain.id })
 
-    const balance: IWeb3BalanceSchema = {
-      chain: omit(chain, 'meta'),
-      tokens: filter(tokenList, { chain: chain.id }),
-    }
+    return map(tokens, (token) => {
 
-    return balance
+      const { amount } = token
 
-  })
+      const balance: IAlunaBalanceSchema = {
+        symbolId: token.symbol,
+        chainId: chain.id,
+        wallet: AlunaWalletEnum.WEB3,
+        total: amount,
+        available: amount,
+        meta: {
+          totalBalance,
+          tokenList,
+        },
+      }
+
+      return balance
+
+    })
+
+  }))
 
   return { parsedBalances }
 
