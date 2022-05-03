782cc04d1c7b2813876b6741387ffbc1c5c10159
Anderson Arboleya <anderson@arboleya.me>
2022-05-03 07:55:48 -0300

    Updating `IAlunaBalanceSchema` definitions.

--- src/lib/schemas/IAlunaBalanceSchema.ts
+++ src/lib/schemas/IAlunaBalanceSchema.ts
@@ -5,7 +5,11 @@ import { AlunaWalletEnum } from '../enums/AlunaWalletEnum'
 export interface IAlunaBalanceSchema {
 
   symbolId: string
-  account: AlunaWalletEnum
+
+  exchangeId?: string // if it belongs to exchange
+  chainId?: string // if it belongs to web3
+
+  wallet: AlunaWalletEnum
 
   total: number
   available: number
