e26c9e8dad33b9c0c6e5321bf3bb916c2cf8ba3c
Anderson Arboleya <anderson@arboleya.me>
2022-05-04 12:38:10 -0300

    Improving helper, adding new condition param.

--- .scaffolding/bootstrapExchange.ts
+++ .scaffolding/bootstrapExchange.ts
@@ -258,15 +258,22 @@ export const replaceSync = (params: {
   search: string | RegExp,
   replace: string,
   filepath: string,
+  condition?: Function,
 }) => {
 
   const {
     filepath,
     replace,
     search,
+    condition,
   } = params
 
   const contents = readFileSync(filepath, 'utf8')
+
+  if (condition && !condition(contents)) {
+    return
+  }
+
   const newContents = contents.replace(search, replace)
 
   writeFileSync(filepath, newContents)
