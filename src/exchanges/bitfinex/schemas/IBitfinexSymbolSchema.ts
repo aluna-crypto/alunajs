471e1942b162b3a8dfefc9043aaff738a83b9cd5
Sergio <sergio.uft@gmail.com>
2022-05-05 09:33:30 -0300

    add Bitfinex symbols schemas

--- src/exchanges/bitfinex/schemas/IBitfinexSymbolSchema.ts
+++ src/exchanges/bitfinex/schemas/IBitfinexSymbolSchema.ts
@@ -1,6 +1,15 @@
-// TODO: Describe symbol interface for Bitfinex exchange
-export interface IBitfinexSymbolSchema {
-  symbol: string
-  name: string
-  // ...
+export interface IBitfinexSymbolsSchema {
+  currencies: string[]
+  currenciesNames: TBitfinexCurrenciesPairs
 }
+
+
+export interface IBitfinexSymbolSchema {
+  currency: string
+  currencyName: TBitfinexCurrencyPair | undefined
+}
+
+
+
+export type TBitfinexCurrencyPair = [currency: string, name: string]
+export type TBitfinexCurrenciesPairs = Array<[currency: string, name: string]>
