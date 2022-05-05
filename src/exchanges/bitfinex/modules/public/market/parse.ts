50d0407774278bab1a370356f937ebb12692d046
Sergio <sergio.uft@gmail.com>
2022-05-05 12:46:03 -0300

    implement Bitfinex market 'parse'

--- src/exchanges/bitfinex/modules/public/market/parse.ts
+++ src/exchanges/bitfinex/modules/public/market/parse.ts
@@ -4,6 +4,9 @@ import {
   IAlunaMarketParseReturns,
 } from '../../../../../lib/modules/public/IAlunaMarketModule'
 import { IAlunaMarketSchema } from '../../../../../lib/schemas/IAlunaMarketSchema'
+import { IAlunaTickerSchema } from '../../../../../lib/schemas/IAlunaTickerSchema'
+import { translateSymbolId } from '../../../../../utils/mappings/translateSymbolId'
+import { bitfinexBaseSpecs } from '../../../bitfinexSpecs'
 import { IBitfinexMarketSchema } from '../../../schemas/IBitfinexMarketSchema'
 
 
@@ -12,10 +15,83 @@ export const parse = (exchange: IAlunaExchangePublic) => (
   params: IAlunaMarketParseParams<IBitfinexMarketSchema>,
 ): IAlunaMarketParseReturns => {
 
+
   const { rawMarket } = params
 
-  // TODO: Implement proper parser
-  const market: IAlunaMarketSchema = rawMarket as any
+  const {
+    ticker: rawTicker,
+    enabledMarginCurrency,
+  } = rawMarket
+
+  const [
+    symbol,
+    bid,
+    _bid_size,
+    ask,
+    _ask_size,
+    _daily_change,
+    dailyChangeRelative,
+    lastPrice,
+    volume,
+    high,
+    low,
+  ] = rawTicker
+
+
+  let baseSymbolId: string
+  let quoteSymbolId: string
+
+  const spliter = symbol.indexOf(':')
+
+  if (spliter >= 0) {
+
+    baseSymbolId = symbol.slice(1, spliter)
+    quoteSymbolId = symbol.slice(spliter + 1)
+
+  } else {
+
+    baseSymbolId = symbol.slice(1, 4)
+    quoteSymbolId = symbol.slice(4)
+
+  }
+
+  const symbolMappings = bitfinexBaseSpecs.settings.mappings
+
+  baseSymbolId = translateSymbolId({
+    exchangeSymbolId: baseSymbolId,
+    symbolMappings,
+  })
+
+  quoteSymbolId = translateSymbolId({
+    exchangeSymbolId: quoteSymbolId,
+    symbolMappings,
+  })
+  const ticker: IAlunaTickerSchema = {
+    bid,
+    ask,
+    high,
+    low,
+    last: lastPrice,
+    date: new Date(),
+    baseVolume: volume,
+    quoteVolume: volume * bid, // estimate
+    change: dailyChangeRelative,
+  }
+
+  const marginEnabled = !!enabledMarginCurrency
+
+  const market: IAlunaMarketSchema = {
+    exchangeId: bitfinexBaseSpecs.id,
+    symbolPair: symbol,
+    baseSymbolId,
+    quoteSymbolId,
+    ticker,
+    spotEnabled: true,
+    marginEnabled,
+    derivativesEnabled: false,
+    leverageEnabled: false,
+    meta: rawTicker,
+  }
 
   return { market }
 
