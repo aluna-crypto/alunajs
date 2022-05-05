de1d0721497b5345e440edc99fd4ef9cbc3e10b4
Sergio <sergio.uft@gmail.com>
2022-05-05 12:44:48 -0300

    add Bitfinex market fixtures

--- src/exchanges/bitfinex/test/fixtures/bitfinexMarket.ts
+++ src/exchanges/bitfinex/test/fixtures/bitfinexMarket.ts
@@ -1,27 +1,78 @@
-import { BitfinexMarketStatusEnum } from '../../enums/BitfinexMarketStatusEnum'
-import { IBitfinexMarketSchema } from '../../schemas/IBitfinexMarketSchema'
+import {
+  IBitfinexMarketsSchema,
+  IBitfinexTicker,
+} from '../../schemas/IBitfinexMarketSchema'
 
 
 
-// TODO: Review fixtures
-export const BITFINEX_RAW_MARKETS: IBitfinexMarketSchema[] = [
-  {
-    symbol: 'BTC-EUR',
-    volume: '9.64127008',
-    quoteVolume: '311825.04145095',
-    status: BitfinexMarketStatusEnum.ONLINE,
-  },
-  {
-    symbol: 'BTC-USD',
-    volume: '126.19108264',
-    quoteVolume: '4696409.21649740',
-    status: BitfinexMarketStatusEnum.ONLINE,
-  },
-  {
-    symbol: 'BTC-USDT',
-    volume: '86.32449650',
-    quoteVolume: '3191538.22788546',
-    status: BitfinexMarketStatusEnum.OFFLINE,
-  },
+export const BITFINEX_RAW_TICKERS: IBitfinexTicker[] = [
+  [
+    'tBTCUSD',
+    36682,
+    20.4328296,
+    36689,
+    13.02177299,
+    2260,
+    0.0656,
+    36687,
+    14675.38709555,
+    37596,
+    33828,
+  ],
+  [
+    'tUDC:BTC',
+    0.0017555,
+    5821.594260479999,
+    0.0017588,
+    8178.96547349,
+    -0.0001771,
+    -0.0916,
+    0.0017559,
+    476196.82839484,
+    0.001933,
+    0.001735,
+  ],
+  [
+    'tETHBTC',
+    0.066311,
+    92.98021137,
+    0.066343,
+    44.94425328,
+    -0.000179,
+    -0.0027,
+    0.066335,
+    6272.44117045,
+    0.066943,
+    0.064728,
+  ],
+  [
+    'tBTCF0:USTF0', // derivative ticker, should be skipped for now
+    37945,
+    11.257977640000002,
+    37949,
+    14.656851710000003,
+    1467,
+    0.0402,
+    37944,
+    2253.11236956,
+    38291,
+    36003,
+  ],
 ]
+
+
+
+export const BITFINEX_MARGIN_ENABLED_CURRENCIES: string[][] = [
+  [
+    'tBTCUSD',
+    'tUDC:BTC',
+  ],
+]
+
+
+
+export const BITFINEX_RAW_MARKETS: IBitfinexMarketsSchema = {
+  enabledMarginCurrencies: BITFINEX_MARGIN_ENABLED_CURRENCIES,
+  tickers: BITFINEX_RAW_TICKERS,
+}
 
