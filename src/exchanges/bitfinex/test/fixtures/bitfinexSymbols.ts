d9b34817ec9f096bf67eb2b38f6c3a7c67681ba2
Sergio <sergio.uft@gmail.com>
2022-05-05 09:33:42 -0300

    ajust Bitfinex symbol fixture

--- src/exchanges/bitfinex/test/fixtures/bitfinexSymbols.ts
+++ src/exchanges/bitfinex/test/fixtures/bitfinexSymbols.ts
@@ -1,20 +1,46 @@
-import { IBitfinexSymbolSchema } from '../../schemas/IBitfinexSymbolSchema'
+import {
+  IBitfinexSymbolSchema,
+  IBitfinexSymbolsSchema,
+  TBitfinexCurrenciesPairs,
+} from '../../schemas/IBitfinexSymbolSchema'
 
 
 
-// TODO: Review fixtures
-export const BITFINEX_RAW_SYMBOLS: IBitfinexSymbolSchema[] = [
-  {
-    symbol: 'BTC',
-    name: 'Bitcoin',
-  },
-  {
-    symbol: 'LTC',
-    name: 'Litecoin',
-  },
-  {
-    symbol: 'ETH',
-    name: 'Ethereum',
-  },
+export const BITFINEX_CURRENCIES: string[] = [
+  'AAVE',
+  'ADA',
+  'BTC',
+  'ETH',
+  'EUR',
+  'USD',
+  'UST',
+  'UDC',
+  'USTF0',
 ]
 
+
+
+export const BITFINEX_CURRENCIES_LABELS: TBitfinexCurrenciesPairs = [
+  ['AAVE', 'AAVE'],
+  ['ADA', 'Cardano ADA'],
+  ['BTC', 'Bitcoin'],
+  ['ETH', 'Ethereum'],
+  ['EUR', 'Euro'],
+  ['USD', 'US Dollar'],
+  ['UDC', 'USDC'],
+  ['UST', 'Tether USDt'],
+]
+
+
+
+export const BITFINEX_RAW_SYMBOLS: IBitfinexSymbolsSchema = {
+  currencies: BITFINEX_CURRENCIES,
+  currenciesNames: BITFINEX_CURRENCIES_LABELS,
+}
+
+
+
+export const BITFINEX_RAW_SYMBOL: IBitfinexSymbolSchema = {
+  currency: BITFINEX_CURRENCIES[0],
+  currencyName: BITFINEX_CURRENCIES_LABELS[0],
+}
