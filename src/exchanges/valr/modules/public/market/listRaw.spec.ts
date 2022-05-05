c43080bf438f99d89a4f4ded5bb85d5c9e0bde75
LucianoPierdona <lucianompj1@gmail.com>
2022-05-05 12:08:26 -0300

    Implement Valr ListRaw and tests

--- src/exchanges/valr/modules/public/market/listRaw.spec.ts
+++ src/exchanges/valr/modules/public/market/listRaw.spec.ts
@@ -4,7 +4,7 @@ import { mockHttp } from '../../../../../../test/mocks/exchange/Http'
 import { Valr } from '../../../Valr'
 import { ValrHttp } from '../../../ValrHttp'
 import { valrEndpoints } from '../../../valrSpecs'
-import { VALR_RAW_MARKETS } from '../../../test/fixtures/valrMarket'
+import { VALR_RAW_CURRENCY_PAIRS, VALR_RAW_MARKETS } from '../../../test/fixtures/valrMarket'
 
 
 
@@ -18,7 +18,8 @@ describe(__filename, () => {
       authedRequest,
     } = mockHttp({ classPrototype: ValrHttp.prototype })
 
-    publicRequest.returns(Promise.resolve(VALR_RAW_MARKETS))
+    publicRequest.onFirstCall().returns(Promise.resolve(VALR_RAW_MARKETS))
+    publicRequest.onSecondCall().returns(Promise.resolve(VALR_RAW_CURRENCY_PAIRS))
 
 
     // executing
@@ -31,14 +32,21 @@ describe(__filename, () => {
 
 
     // validating
-    expect(rawMarkets).to.deep.eq(VALR_RAW_MARKETS)
+    expect(rawMarkets).to.deep.eq({
+      summaries: VALR_RAW_MARKETS,
+      pairs: VALR_RAW_CURRENCY_PAIRS,
+    })
 
     expect(requestCount).to.be.ok
 
-    expect(publicRequest.callCount).to.be.eq(1)
+    expect(publicRequest.callCount).to.be.eq(2)
 
     expect(publicRequest.firstCall.args[0]).to.deep.eq({
-      url: valrEndpoints.market.list,
+      url: valrEndpoints.market.summaries,
+    })
+
+    expect(publicRequest.secondCall.args[0]).to.deep.eq({
+      url: valrEndpoints.market.pairs,
     })
 
     expect(authedRequest.callCount).to.be.eq(0)
