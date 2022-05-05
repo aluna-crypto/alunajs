535ef4f23bd033509d2ebd529f4d1734bd9d90fb
Anderson Arboleya <anderson@arboleya.me>
2022-05-05 19:00:26 -0300

    Adding non-interactive options.

--- .scaffolding/addExchange.ts
+++ .scaffolding/addExchange.ts
@@ -1,8 +1,12 @@
 import inquirer from 'inquirer'
+import yargs from 'yargs'
 
 import { AlunaAccountEnum } from '../src/lib/enums/AlunaAccountEnum'
 import { AlunaApiFeaturesEnum } from '../src/lib/enums/AlunaApiFeaturesEnum'
-import { bootstrapExchange } from './bootstrapExchange'
+import {
+  bootstrapExchange,
+  IPromptAnswers,
+} from './bootstrapExchange'
 import { inquirerValidations } from './utils/inquirerValidations'
 
 
@@ -39,13 +43,54 @@ const questions = [
 
 
 
+export function getArgV (): any {
+
+  const argv = yargs
+    .option('exchangeName', {
+      alias: 'e',
+      description: 'Exchange Name',
+      type: 'string'
+    })
+    .option('tradingFeatures', {
+      alias: 't',
+      description: 'Exchange Features',
+      type: 'string'
+    })
+    .option('apiFeatures', {
+      alias: 'a',
+      description: 'API Featurs',
+      type: 'string'
+    })
+    .help()
+    .alias('help', 'h').argv
+
+  return argv
+
+}
+
+
+
 export async function addExchange () {
 
-  const answers = await inquirer.prompt(questions)
+  const argv: IPromptAnswers = getArgV()
+  let answers
+
+  const {
+    exchangeName,
+    tradingFeatures,
+    apiFeatures,
+  } = argv
+
+  if (exchangeName && tradingFeatures && apiFeatures) {
+    answers = argv
+  } else {
+    answers = await inquirer.prompt(questions)
+  }
 
   await bootstrapExchange(answers)
 
 }
+
 
 
 addExchange()
