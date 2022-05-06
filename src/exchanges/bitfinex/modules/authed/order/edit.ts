959693ff13664000ec6550419a4ca906bc34f63f
Sergio <sergio.uft@gmail.com>
2022-05-06 12:05:04 -0300

    implement Bitfinex order 'edit'

--- src/exchanges/bitfinex/modules/authed/order/edit.ts
+++ src/exchanges/bitfinex/modules/authed/order/edit.ts
@@ -1,6 +1,11 @@
 import { debug } from 'debug'
 
+import { AlunaError } from '../../../../../lib/core/AlunaError'
 import { IAlunaExchangeAuthed } from '../../../../../lib/core/IAlunaExchange'
+import { AlunaOrderTypesEnum } from '../../../../../lib/enums/AlunaOrderTypesEnum'
+import { AlunaBalanceErrorCodes } from '../../../../../lib/errors/AlunaBalanceErrorCodes'
+import { AlunaHttpErrorCodes } from '../../../../../lib/errors/AlunaHttpErrorCodes'
+import { AlunaOrderErrorCodes } from '../../../../../lib/errors/AlunaOrderErrorCodes'
 import {
   IAlunaOrderEditParams,
   IAlunaOrderEditReturns,
@@ -8,6 +13,12 @@ import {
 import { editOrderParamsSchema } from '../../../../../utils/validation/schemas/editOrderParamsSchema'
 import { validateParams } from '../../../../../utils/validation/validateParams'
 import { BitfinexHttp } from '../../../BitfinexHttp'
+import { bitfinexEndpoints } from '../../../bitfinexSpecs'
+import { translateOrderSideToBitfinex } from '../../../enums/adapters/bitfinexOrderSideAdapter'
+import {
+  IBitfinexOrderSchema,
+  TBitfinexEditCancelOrderResponse,
+} from '../../../schemas/IBitfinexOrderSchema'
 
 
 
@@ -20,6 +31,8 @@ export const edit = (exchange: IAlunaExchangeAuthed) => async (
 ): Promise<IAlunaOrderEditReturns> => {
 
   log('params', params)
+
+  const { credentials } = exchange
 
   validateParams({
     params,
@@ -34,32 +47,120 @@ export const edit = (exchange: IAlunaExchangeAuthed) => async (
     side,
     type,
     amount,
-    account,
-    symbolPair,
+    stopRate,
+    limitRate,
     http = new BitfinexHttp(),
   } = params
 
-  await exchange.order.cancel({
-    id,
-    symbolPair,
-    http,
-  })
-
-  const { order: newOrder } = await exchange.order.place({
-    rate,
+  const translatedAmount = translateOrderSideToBitfinex({
+    amount: Number(amount),
     side,
-    type,
-    amount,
-    account,
-    symbolPair,
-    http,
   })
 
-  const { requestCount } = http
+  let price: undefined | string
+  let priceAuxLimit: undefined | string
 
-  return {
-    order: newOrder,
-    requestCount,
+  switch (type) {
+
+    case AlunaOrderTypesEnum.LIMIT:
+      price = rate!.toString()
+      break
+
+    case AlunaOrderTypesEnum.STOP_MARKET:
+      price = stopRate!.toString()
+      break
+
+    case AlunaOrderTypesEnum.STOP_LIMIT:
+      price = stopRate!.toString()
+      priceAuxLimit = limitRate!.toString()
+      break
+
+    default:
+
   }
 
+  const body = {
+    amount: translatedAmount,
+    id: Number(id),
+    ...(price ? { price } : {}),
+    ...(priceAuxLimit ? { price_aux_limit: priceAuxLimit } : {}),
+  }
+
+  log('editing order for Bitfinex')
+
+  let rawOrder: IBitfinexOrderSchema
+
+  try {
+
+    const response = await http.authedRequest<TBitfinexEditCancelOrderResponse>({
+      url: bitfinexEndpoints.order.edit,
+      body,
+      credentials,
+    })
+
+    const [
+      _mts,
+      _type,
+      _messageId,
+      _placeHolder,
+      editedOrder,
+      _code,
+      status,
+      text,
+    ] = response
+
+    if (status !== 'SUCCESS') {
+
+      throw new AlunaError({
+        code: AlunaHttpErrorCodes.REQUEST_ERROR,
+        message: text,
+        metadata: response,
+      })
+
+    }
+
+    // Bitfinex already returns the order edited/updated
+    rawOrder = editedOrder
+
+    const { order } = exchange.order.parse({ rawOrder })
+
+    const { requestCount } = http
+
+    return {
+      order,
+      requestCount,
+    }
+
+  } catch (err) {
+
+    const { httpStatusCode, metadata } = err
+
+    let {
+      code,
+      message,
+    } = err
+
+    if (/order: invalid/.test(err.message)) {
+
+      code = AlunaOrderErrorCodes.NOT_FOUND
+      message = 'order was not found or may not be open'
+
+    } else if (/not enough.+balance/i.test(err.message)) {
+
+      code = AlunaBalanceErrorCodes.INSUFFICIENT_BALANCE
+
+    }
+
+    const error = new AlunaError({
+      message,
+      code,
+      metadata,
+      httpStatusCode,
+    })
+
+    log(error)
+
+    throw error
+
+  }
 }
