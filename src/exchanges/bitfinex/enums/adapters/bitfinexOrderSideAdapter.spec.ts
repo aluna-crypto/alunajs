3cd8d34078656472992a5e8dffacbad4fde46e12
Sergio <sergio.uft@gmail.com>
2022-05-06 12:00:23 -0300

    fix Bitfinex order side adapter

--- src/exchanges/bitfinex/enums/adapters/bitfinexOrderSideAdapter.spec.ts
+++ src/exchanges/bitfinex/enums/adapters/bitfinexOrderSideAdapter.spec.ts
@@ -1,8 +1,6 @@
 import { expect } from 'chai'
 
-import { AlunaError } from '../../../../lib/core/AlunaError'
 import { AlunaOrderSideEnum } from '../../../../lib/enums/AlunaOrderSideEnum'
-import { BitfinexOrderSideEnum } from '../BitfinexOderSideEnum'
 import {
   translateOrderSideToAluna,
   translateOrderSideToBitfinex,
@@ -12,81 +10,30 @@ import {
 
 describe(__filename, () => {
 
-  const notSupported = 'not-supported'
+  it('should properly translate Bitfinex order side to Aluna side', () => {
 
+    expect(translateOrderSideToAluna({
+      amount: 100,
+    })).to.be.eq(AlunaOrderSideEnum.BUY)
 
+    expect(translateOrderSideToAluna({
+      amount: -6,
+    })).to.be.eq(AlunaOrderSideEnum.SELL)
 
-  it(
-    'should properly translate Bitfinex order sides to Aluna order sides',
-    () => {
+  })
 
-      expect(translateOrderSideToAluna({
-        from: BitfinexOrderSideEnum.BUY,
-      })).to.be.eq(AlunaOrderSideEnum.BUY)
+  it('should properly translate Aluna order amount to Bitfinex amount', () => {
 
-      expect(translateOrderSideToAluna({
-        from: BitfinexOrderSideEnum.SELL,
-      })).to.be.eq(AlunaOrderSideEnum.SELL)
+    expect(translateOrderSideToBitfinex({
+      amount: 80,
+      side: AlunaOrderSideEnum.BUY,
+    })).to.be.eq('80')
 
-      let result
-      let error
+    expect(translateOrderSideToBitfinex({
+      amount: 60,
+      side: AlunaOrderSideEnum.SELL,
+    })).to.be.eq('-60')
 
-      try {
-
-        result = translateOrderSideToAluna({
-          from: notSupported as BitfinexOrderSideEnum,
-        })
-
-      } catch (err) {
-
-        error = err
-
-      }
-
-      expect(result).not.to.be.ok
-
-      expect(error instanceof AlunaError).to.be.ok
-      expect(error.message)
-        .to.be.eq(`Order side not supported: ${notSupported}`)
-
-    },
-  )
-
-
-
-  it(
-    'should properly translate Aluna order sides to Bitfinex order sides',
-    () => {
-
-      expect(translateOrderSideToBitfinex({
-        from: AlunaOrderSideEnum.BUY,
-      })).to.be.eq(BitfinexOrderSideEnum.BUY)
-
-      expect(translateOrderSideToBitfinex({
-        from: AlunaOrderSideEnum.SELL,
-      })).to.be.eq(BitfinexOrderSideEnum.SELL)
-
-      let result
-      let error
-
-      try {
-
-        result = translateOrderSideToBitfinex({
-          from: notSupported as AlunaOrderSideEnum,
-        })
-
-      } catch (err) {
-
-        error = err
-
-      }
-
-      expect(result).not.to.be.ok
-      expect(error instanceof AlunaError).to.be.ok
-      expect(error.message)
-        .to.be.eq(`Order side not supported: ${notSupported}`)
-
-    },
-  )
+  })
 
 })
