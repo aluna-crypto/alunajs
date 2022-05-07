e1a7178f41dc4a54fff4227b3a50c8f855a32298
Anderson Arboleya <anderson@arboleya.me>
2022-05-07 10:04:10 -0300

    Removing lost imports.

--- src/exchanges/bitfinex/modules/authed/position.ts
+++ src/exchanges/bitfinex/modules/authed/position.ts
@@ -2,13 +2,11 @@ import { IAlunaExchangeAuthed } from '../../../../lib/core/IAlunaExchange'
 import { IAlunaPositionModule } from '../../../../lib/modules/authed/IAlunaPositionModule'
 import { close } from './position/close'
 import { get } from './position/get'
-import { getLeverage } from './position/getLeverage'
 import { getRaw } from './position/getRaw'
 import { list } from './position/list'
 import { listRaw } from './position/listRaw'
 import { parse } from './position/parse'
 import { parseMany } from './position/parseMany'
-import { setLeverage } from './position/setLeverage'
 
 
 
@@ -22,8 +20,6 @@ export function position(exchange: IAlunaExchangeAuthed): IAlunaPositionModule {
     listRaw: listRaw(exchange),
     parse: parse(exchange),
     parseMany: parseMany(exchange),
-    getLeverage: getLeverage(exchange),
-    setLeverage: setLeverage(exchange),
   }
 
 }
