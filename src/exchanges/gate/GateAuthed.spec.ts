3f6ef571044edd8ef40ea90301ef58bfbede461a
Anderson Arboleya <anderson@arboleya.me>
2022-05-07 11:48:01 -0300

    Standardizing tests.

--- src/exchanges/gate/GateAuthed.spec.ts
+++ src/exchanges/gate/GateAuthed.spec.ts
@@ -1,23 +1,35 @@
 import { expect } from 'chai'
 
+import { IAlunaCredentialsSchema } from '../../lib/schemas/IAlunaCredentialsSchema'
 import { IAlunaSettingsSchema } from '../../lib/schemas/IAlunaSettingsSchema'
-import { Gate } from './Gate'
+import { GateAuthed } from './GateAuthed'
 
 
 
 describe(__filename, () => {
 
-  it('should ensure exchangehave all properties and methods', async () => {
+  it('should contain public & authed modules', async () => {
 
-    const settings: IAlunaSettingsSchema = {
-      referralCode: '666',
+    const credentials: IAlunaCredentialsSchema = {
+      key: 'some-key',
+      secret: 'some-secret',
     }
 
-    const gate = new Gate({ settings })
+    const settings: IAlunaSettingsSchema = {
+      referralCode: '123',
+    }
+
+    const gate = new GateAuthed({ settings, credentials })
 
     expect(gate.id).to.eq('gate')
+
     expect(gate.symbol).to.be.ok
     expect(gate.market).to.be.ok
+
+    expect(gate.key).to.be.ok
+    expect(gate.balance).to.be.ok
+    expect(gate.order).to.be.ok
+
     expect(gate.specs).to.be.ok
     expect(gate.settings).to.deep.eq(settings)
 
