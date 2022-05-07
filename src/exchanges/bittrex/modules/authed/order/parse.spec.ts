3255f09de6142d2528170e1fb7d5d8f1da9ad71a
Anderson Arboleya <anderson@arboleya.me>
2022-05-07 17:55:57 -0300

    Fixing broken tests.

--- src/exchanges/bittrex/modules/authed/order/parse.spec.ts
+++ src/exchanges/bittrex/modules/authed/order/parse.spec.ts
@@ -76,11 +76,11 @@ describe(__filename, () => {
     expect(translateSymbolId.callCount).to.be.eq(2)
     expect(translateSymbolId.firstCall.args[0]).to.deep.eq({
       exchangeSymbolId: baseCurrency,
-      symbolMappings: ({}).mappings,
+      symbolMappings: exchange.settings.mappings,
     })
     expect(translateSymbolId.secondCall.args[0]).to.deep.eq({
       exchangeSymbolId: quoteCurrency,
-      symbolMappings: ({}).mappings,
+      symbolMappings: exchange.settings.mappings,
     })
 
   })
