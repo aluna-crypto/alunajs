7f128a02fc20fec01f13be8da7bab387f654b37e
Anderson Arboleya <anderson@arboleya.me>
2022-05-07 12:29:48 -0300

    Improving sample demo code.

--- src/exchanges/sample/modules/authed/order/parse.ts
+++ src/exchanges/sample/modules/authed/order/parse.ts
@@ -4,6 +4,7 @@ import {
   IAlunaOrderParseReturns,
 } from '../../../../../lib/modules/authed/IAlunaOrderModule'
 import { IAlunaOrderSchema } from '../../../../../lib/schemas/IAlunaOrderSchema'
+import { translateSymbolId } from '../../../../../utils/mappings/translateSymbolId'
 import { ISampleOrderSchema } from '../../../schemas/ISampleOrderSchema'
 
 
@@ -20,9 +21,38 @@ export const parse = (exchange: IAlunaExchangeAuthed) => (
 
   const { rawOrder } = params
 
-  // TODO: Implement proper parser
-  const parsedOrder: IAlunaOrderSchema = rawOrder as any
+  const { symbol } = rawOrder
 
-  return { order: parsedOrder }
+  let [
+    baseSymbolId,
+    quoteSymbolId,
+  ] = symbol.split('/')
+
+  baseSymbolId = translateSymbolId({
+    exchangeSymbolId: baseSymbolId,
+    symbolMappings: exchange.settings.mappings,
+  })
+
+  quoteSymbolId = translateSymbolId({
+    exchangeSymbolId: quoteSymbolId,
+    symbolMappings: exchange.settings.mappings,
+  })
+
+  // TODO: Implement proper parser
+  const order: IAlunaOrderSchema = {
+    id: rawOrder.id,
+    symbolPair: rawOrder.id,
+    exchangeId: exchange.specs.id,
+    baseSymbolId,
+    quoteSymbolId,
+    // total: rawOrder.total,
+    // amount: rawOrder.amount,
+    // account: AlunaAccountEnum.MARGIN,
+    // status: rawOrder.status,
+    // side: rawOrder.side,
+    meta: rawOrder,
+  } as any // TODO: Remove casting to any
+
+  return { order }
 
 }
