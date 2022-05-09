6849bcec6fc0f8cbeec125b53c749517985ea357
LucianoPierdona <lucianompj1@gmail.com>
2022-05-09 11:17:24 -0300

    Add Gate Order Schema and fixtures

--- src/exchanges/gate/schemas/IGateOrderSchema.ts
+++ src/exchanges/gate/schemas/IGateOrderSchema.ts
@@ -1,11 +1,48 @@
+import { GateOrderSideEnum } from '../enums/GateOrderSideEnum'
+import { GateOrderStatusEnum } from '../enums/GateOrderStatusEnum'
+import { GateOrderTimeInForceEnum } from '../enums/GateOrderTimeInForceEnum'
 import { GateOrderTypeEnum } from '../enums/GateOrderTypeEnum'
 
 
 
-// TODO: Describe order interface for Gate exchange
 export interface IGateOrderSchema {
+
+  /* eslint-disable camelcase */
   id: string
+  text: string
+  create_time: string
+  update_time: string
+  create_time_ms: number
+  update_time_ms: number
+  currency_pair: string
+  status: GateOrderStatusEnum
   type: GateOrderTypeEnum
-  quantity: string
-  // ...
+  account: string
+  side: GateOrderSideEnum
+  iceberg: string
+  amount: string
+  price: string
+  time_in_force: GateOrderTimeInForceEnum
+  left: string
+  filled_total: string
+  fee: string
+  fee_currency: string
+  point_fee: string
+  gt_fee: string
+  gt_discount: boolean
+  rebated_fee: string
+  rebated_fee_currency: string
+  fill_price: string
+  /* eslint-disable camelcase */
+
+}
+
+export interface IGateOrderListResponseSchema {
+
+  /* eslint-disable camelcase */
+  currency_pair: string
+  total: number
+  orders: IGateOrderSchema[]
+  /* eslint-disable camelcase */
+
 }
--- src/exchanges/gate/test/fixtures/gateOrders.ts
+++ src/exchanges/gate/test/fixtures/gateOrders.ts
@@ -1,3 +1,6 @@
+import { GateOrderSideEnum } from '../../enums/GateOrderSideEnum'
+import { GateOrderStatusEnum } from '../../enums/GateOrderStatusEnum'
+import { GateOrderTimeInForceEnum } from '../../enums/GateOrderTimeInForceEnum'
 import { GateOrderTypeEnum } from '../../enums/GateOrderTypeEnum'
 import { IGateOrderSchema } from '../../schemas/IGateOrderSchema'
 
@@ -6,14 +9,58 @@ import { IGateOrderSchema } from '../../schemas/IGateOrderSchema'
 // TODO: Review fixtures
 export const GATE_RAW_ORDERS: IGateOrderSchema[] = [
   {
-    id: '8bc1e59c-77fa-4554-bd11-966e360e4eb7',
+    id: '124084530741',
+    text: '3',
+    create_time: '1644845927',
+    update_time: '1644845927',
+    create_time_ms: 1644845927564,
+    update_time_ms: 1644845927564,
+    status: GateOrderStatusEnum.OPEN,
+    currency_pair: 'BNB_USDT',
     type: GateOrderTypeEnum.LIMIT,
-    quantity: '9.94497801',
+    account: 'spot',
+    side: GateOrderSideEnum.SELL,
+    amount: '0.02',
+    price: '600',
+    time_in_force: GateOrderTimeInForceEnum.GOOD_TILL_CANCELLED,
+    iceberg: '0',
+    left: '0.02',
+    fill_price: '0',
+    filled_total: '0',
+    fee: '0',
+    fee_currency: 'USDT',
+    point_fee: '0',
+    gt_fee: '0',
+    gt_discount: false,
+    rebated_fee: '0',
+    rebated_fee_currency: 'BNB',
   },
   {
-    id: '8bc1e59c-77fa-4554-bd11-966e360e4eb8',
-    type: GateOrderTypeEnum.MARKET,
-    quantity: '9.94497801',
+    id: '124084530743',
+    text: '3',
+    create_time: '1644845927',
+    update_time: '1644845927',
+    create_time_ms: 1644845927564,
+    update_time_ms: 1644845927564,
+    status: GateOrderStatusEnum.OPEN,
+    currency_pair: 'BNB_USDT',
+    type: GateOrderTypeEnum.LIMIT,
+    account: 'spot',
+    side: GateOrderSideEnum.SELL,
+    amount: '0.02',
+    price: '600',
+    time_in_force: GateOrderTimeInForceEnum.GOOD_TILL_CANCELLED,
+    iceberg: '0',
+    left: '0.02',
+    fill_price: '0',
+    filled_total: '0',
+    fee: '0',
+    fee_currency: 'USDT',
+    point_fee: '0',
+    gt_fee: '0',
+    gt_discount: false,
+    rebated_fee: '0',
+    rebated_fee_currency: 'BNB',
   },
 ]
 
