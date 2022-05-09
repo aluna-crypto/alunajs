1778072c68734226977252f437c6e5e7a16c5569
LucianoPierdona <lucianompj1@gmail.com>
2022-05-09 11:19:02 -0300

    Add Gate Order Parse

--- src/exchanges/gate/modules/authed/order/parse.ts
+++ src/exchanges/gate/modules/authed/order/parse.ts
@@ -1,9 +1,16 @@
 import { IAlunaExchangeAuthed } from '../../../../../lib/core/IAlunaExchange'
+import { AlunaAccountEnum } from '../../../../../lib/enums/AlunaAccountEnum'
+import { AlunaOrderStatusEnum } from '../../../../../lib/enums/AlunaOrderStatusEnum'
 import {
   IAlunaOrderParseParams,
   IAlunaOrderParseReturns,
 } from '../../../../../lib/modules/authed/IAlunaOrderModule'
 import { IAlunaOrderSchema } from '../../../../../lib/schemas/IAlunaOrderSchema'
+import { translateSymbolId } from '../../../../../utils/mappings/translateSymbolId'
+import { translateOrderSideToAluna } from '../../../enums/adapters/gateOrderSideAdapter'
+import { translateOrderStatusToAluna } from '../../../enums/adapters/gateOrderStatusAdapter'
+import { translateOrderTypeToAluna } from '../../../enums/adapters/gateOrderTypeAdapter'
+import { gateBaseSpecs } from '../../../gateSpecs'
 import { IGateOrderSchema } from '../../../schemas/IGateOrderSchema'
 
 
@@ -20,8 +27,79 @@ export const parse = (exchange: IAlunaExchangeAuthed) => (
 
   const { rawOrder } = params
 
-  // TODO: Implement proper parser
-  const parsedOrder: IAlunaOrderSchema = rawOrder as any
+  const {
+    id,
+    type: orderType,
+    status,
+    amount: quantity,
+    currency_pair: currencyPair,
+    side,
+    price,
+    create_time_ms: createdTime,
+    update_time_ms: updatedTime,
+    left,
+  } = rawOrder
+
+  const exchangeId = gateBaseSpecs.id
+
+  const amount = Number(quantity)
+  const leftToFill = Number(left)
+  const rate = Number(price)
+  const total = amount * rate
+  const createdAt = new Date(createdTime)
+  const [baseCurrency, quoteCurrency] = currencyPair.split('_')
+
+  const baseSymbolId = translateSymbolId({
+    exchangeSymbolId: baseCurrency,
+    symbolMappings: exchange.settings.symbolMappings,
+  })
+
+  const quoteSymbolId = translateSymbolId({
+    exchangeSymbolId: quoteCurrency,
+    symbolMappings: exchange.settings.symbolMappings,
+  })
+
+  const orderStatus = translateOrderStatusToAluna({
+    from: status, leftToFill, amount,
+  })
+
+  const orderSide = translateOrderSideToAluna({ from: side })
+
+  const type = translateOrderTypeToAluna({ from: orderType })
+
+  let filledAt: Date | undefined
+  let canceledAt: Date | undefined
+
+  if (orderStatus === AlunaOrderStatusEnum.FILLED) {
+
+    filledAt = new Date(updatedTime)
+
+  }
+
+  if (orderStatus === AlunaOrderStatusEnum.CANCELED) {
+
+    canceledAt = new Date(updatedTime)
+
+  }
+
+  const parsedOrder: IAlunaOrderSchema = {
+    id,
+    symbolPair: currencyPair,
+    total,
+    amount,
+    rate,
+    exchangeId,
+    baseSymbolId,
+    quoteSymbolId,
+    account: AlunaAccountEnum.EXCHANGE,
+    side: orderSide,
+    status: orderStatus,
+    type,
+    placedAt: new Date(createdAt),
+    filledAt,
+    canceledAt,
+    meta: rawOrder,
+  }
 
   return { order: parsedOrder }
 
