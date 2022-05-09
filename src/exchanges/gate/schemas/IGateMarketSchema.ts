9671ec280e363ffcbf7bd79380674bd585acd164
LucianoPierdona <lucianompj1@gmail.com>
2022-05-09 07:53:50 -0300

    Add Valr Market schema and fixtures

--- src/exchanges/gate/schemas/IGateMarketSchema.ts
+++ src/exchanges/gate/schemas/IGateMarketSchema.ts
@@ -1,12 +1,19 @@
-import { GateMarketStatusEnum } from '../enums/GateMarketStatusEnum'
-
-
-
-// TODO: Describe market interface for Gate exchange
 export interface IGateMarketSchema {
-  symbol: string
-  volume: string
-  quoteVolume: string
-  status: GateMarketStatusEnum
-  // ...
+
+  /* eslint-disable camelcase */
+  currency_pair: string
+  last: string
+  lowest_ask: string
+  highest_bid: string
+  change_percentage: string
+  base_volume: string
+  quote_volume: string
+  high_24h: string
+  low_24h: string
+  etf_net_value: string
+  etf_pre_net_value: string
+  etf_pre_timestamp: string
+  etf_leverage: string
+  /* eslint-disable camelcase */
+
 }
--- src/exchanges/gate/test/fixtures/gateMarket.ts
+++ src/exchanges/gate/test/fixtures/gateMarket.ts
@@ -1,27 +1,52 @@
-import { GateMarketStatusEnum } from '../../enums/GateMarketStatusEnum'
 import { IGateMarketSchema } from '../../schemas/IGateMarketSchema'
 
 
 
-// TODO: Review fixtures
 export const GATE_RAW_MARKETS: IGateMarketSchema[] = [
   {
-    symbol: 'BTC-EUR',
-    volume: '9.64127008',
-    quoteVolume: '311825.04145095',
-    status: GateMarketStatusEnum.ONLINE,
+    currency_pair: 'ETH_USD',
+    last: '43516.18',
+    lowest_ask: '43507.5',
+    highest_bid: '43453.51',
+    change_percentage: '-2.46',
+    base_volume: '251.96328174',
+    quote_volume: '11098460.538387',
+    high_24h: '43752.73',
+    low_24h: '42653.53',
+    etf_leverage: '1',
+    etf_net_value: '1',
+    etf_pre_net_value: '1',
+    etf_pre_timestamp: '1',
   },
   {
-    symbol: 'BTC-USD',
-    volume: '126.19108264',
-    quoteVolume: '4696409.21649740',
-    status: GateMarketStatusEnum.ONLINE,
+    currency_pair: 'BTC_USDT',
+    last: '43455.04',
+    lowest_ask: '43455.04',
+    highest_bid: '43455.03',
+    change_percentage: '-2.54',
+    base_volume: '3396.1923521757',
+    quote_volume: '149732837.04482',
+    high_24h: '43742.56',
+    low_24h: '42605',
+    etf_leverage: '1',
+    etf_net_value: '1',
+    etf_pre_net_value: '1',
+    etf_pre_timestamp: '1',
   },
   {
-    symbol: 'BTC-USDT',
-    volume: '86.32449650',
-    quoteVolume: '3191538.22788546',
-    status: GateMarketStatusEnum.OFFLINE,
+    currency_pair: 'BNB_BTC',
+    last: '0.009582',
+    lowest_ask: '0.009609',
+    highest_bid: '0.009572',
+    change_percentage: '0.11',
+    base_volume: '191.17347527',
+    quote_volume: '1.82665856569312',
+    high_24h: '0.009645',
+    low_24h: '0.009499',
+    etf_leverage: '1',
+    etf_net_value: '1',
+    etf_pre_net_value: '1',
+    etf_pre_timestamp: '1',
   },
 ]
 
