e9d50683550858d1f0d5962bc066c9aacfc08cd6
Sergio <sergio.uft@gmail.com>
2022-05-09 14:30:25 -0300

    moving bitfinex test helper

--- src/exchanges/bitfinex/modules/authed/order/test/testBitfinexOrderPlace.ts
+++ src/exchanges/bitfinex/modules/authed/order/test/testBitfinexOrderPlace.ts
@@ -3,7 +3,6 @@ import { expect } from 'chai'
 import { PARSED_ORDERS } from '../../../../../../../test/fixtures/parsedOrders'
 import { mockHttp } from '../../../../../../../test/mocks/exchange/Http'
 import { mockParse } from '../../../../../../../test/mocks/exchange/modules/mockParse'
-import { AlunaOrderTypesEnum } from '../../../../../../lib/enums/AlunaOrderTypesEnum'
 import { IAlunaOrderPlaceParams } from '../../../../../../lib/modules/authed/IAlunaOrderModule'
 import { IAlunaCredentialsSchema } from '../../../../../../lib/schemas/IAlunaCredentialsSchema'
 import { mockEnsureOrderIsSupported } from '../../../../../../utils/orders/ensureOrderIsSupported.mock'
@@ -11,14 +10,10 @@ import { placeOrderParamsSchema } from '../../../../../../utils/validation/schem
 import { mockValidateParams } from '../../../../../../utils/validation/validateParams.mock'
 import { BitfinexAuthed } from '../../../../BitfinexAuthed'
 import { BitfinexHttp } from '../../../../BitfinexHttp'
-import {
-  bitfinexBaseSpecs,
-  getBitfinexEndpoints,
-} from '../../../../bitfinexSpecs'
-import { translateOrderSideToBitfinex } from '../../../../enums/adapters/bitfinexOrderSideAdapter'
-import { translateOrderTypeToBitfinex } from '../../../../enums/adapters/bitfinexOrderTypeAdapter'
+import { getBitfinexEndpoints } from '../../../../bitfinexSpecs'
 import { BITFINEX_PLACE_ORDER_RESPONSE } from '../../../../test/fixtures/bitfinexOrders'
 import * as parseMod from '../parse'
+import { getExpectedRequestBody } from './helpers/getExpectedRequestBody'
 
 
 
@@ -99,69 +94,5 @@ export const testBitfinexOrderPlace = (params: IAlunaOrderPlaceParams) => {
     })
 
   })
-
-}
-
-
-
-const getExpectedRequestBody = (
-  params: IAlunaOrderPlaceParams,
-): Record<any, any> => {
-
-  const {
-    account,
-    type,
-    side,
-    amount,
-    rate,
-    symbolPair,
-    limitRate,
-    stopRate,
-  } = params
-
-  const translatedOrderType = translateOrderTypeToBitfinex({
-    from: type,
-    account,
-  })
-
-  const translatedAmount = translateOrderSideToBitfinex({
-    amount: Number(amount),
-    side,
-  })
-
-  let price: undefined | string
-  let priceAuxLimit: undefined | string
-
-  switch (type) {
-
-    case AlunaOrderTypesEnum.LIMIT:
-      price = rate!.toString()
-      break
-
-    case AlunaOrderTypesEnum.STOP_MARKET:
-      price = stopRate!.toString()
-      break
-
-    case AlunaOrderTypesEnum.STOP_LIMIT:
-      price = stopRate!.toString()
-      priceAuxLimit = limitRate!.toString()
-      break
-
-    default:
-
-  }
-
-  const { affiliateCode } = bitfinexBaseSpecs.settings
-
-  const body = {
-    amount: translatedAmount,
-    symbol: symbolPair,
-    type: translatedOrderType,
-    ...(price ? { price } : {}),
-    ...(priceAuxLimit ? { price_aux_limit: priceAuxLimit } : {}),
-    ...(affiliateCode ? { meta: { aff_code: affiliateCode } } : {}),
-  }
-
-  return body
 
 }
