85202d47779d92c88879ab2d4f06b49df1f9ca68
LucianoPierdona <lucianompj1@gmail.com>
2022-05-09 07:55:48 -0300

    Add Gate Market Parse

--- src/exchanges/gate/modules/public/market/parse.spec.ts
+++ src/exchanges/gate/modules/public/market/parse.spec.ts
@@ -1,25 +1,65 @@
 import { expect } from 'chai'
+import { IAlunaMarketSchema } from '../../../../../lib/schemas/IAlunaMarketSchema'
 
 import { mockTranslateSymbolId } from '../../../../../utils/mappings/translateSymbolId.mock'
 import { Gate } from '../../../Gate'
+import { gateBaseSpecs } from '../../../gateSpecs'
 import { GATE_RAW_MARKETS } from '../../../test/fixtures/gateMarket'
 
 
 
-describe.skip(__filename, () => {
+describe(__filename, () => {
 
   it('should parse a Gate raw market just fine', async () => {
 
     // preparing data
-    const translatedSymbolId = 'BTC'
     const rawMarket = GATE_RAW_MARKETS[0]
 
+    const {
+      base_volume: baseVolume,
+      change_percentage: changePercentage,
+      currency_pair: currencyPair,
+      high_24h: high24h,
+      highest_bid: highestBid,
+      last,
+      low_24h: low24h,
+      lowest_ask: lowestAsk,
+      quote_volume: quoteVolume,
+    } = rawMarket
+
+    const [baseCurrency, quoteCurrency] = currencyPair.split('_')
+
+    const ticker = {
+      high: Number(high24h),
+      low: Number(low24h),
+      bid: Number(highestBid),
+      ask: Number(lowestAsk),
+      last: Number(last),
+      date: new Date(),
+      change: Number(changePercentage),
+      baseVolume: Number(baseVolume),
+      quoteVolume: Number(quoteVolume),
+    }
+
+    const parsedMarket: IAlunaMarketSchema = {
+      exchangeId: gateBaseSpecs.id,
+      symbolPair: currencyPair,
+      baseSymbolId: baseCurrency,
+      quoteSymbolId: quoteCurrency,
+      ticker,
+      spotEnabled: true,
+      marginEnabled: false,
+      derivativesEnabled: false,
+      leverageEnabled: false,
+      meta: rawMarket,
+    }
 
     // mocking
     const { translateSymbolId } = mockTranslateSymbolId()
 
-    translateSymbolId.returns(translatedSymbolId)
+    translateSymbolId.onFirstCall().returns(baseCurrency)
 
+    translateSymbolId.onSecondCall().returns(quoteCurrency)
 
     // executing
     const exchange = new Gate({})
@@ -28,10 +68,29 @@ describe.skip(__filename, () => {
       rawMarket,
     })
 
-
     // validating
-    // TODO: add proper validations
     expect(market).to.exist
+
+    expect(market.baseSymbolId).to.be.eq(baseCurrency)
+    expect(market.quoteSymbolId).to.be.eq(quoteCurrency)
+
+    expect(market.ticker.ask).to.be.eq(ticker.ask)
+    expect(market.ticker.baseVolume).to.be.eq(ticker.baseVolume)
+    expect(market.ticker.bid).to.be.eq(ticker.bid)
+    expect(market.ticker.change).to.be.eq(ticker.change)
+    expect(market.ticker.high).to.be.eq(ticker.high)
+    expect(market.ticker.last).to.be.eq(ticker.last)
+    expect(market.ticker.low).to.be.eq(ticker.low)
+    expect(market.ticker.quoteVolume).to.be.eq(ticker.quoteVolume)
+
+    expect(market.exchangeId).to.be.eq(gateBaseSpecs.id)
+    expect(market.symbolPair).to.be.eq(currencyPair)
+    expect(market.meta).to.deep.eq(rawMarket)
+
+    expect(market.derivativesEnabled).not.to.be.ok
+    expect(market.marginEnabled).not.to.be.ok
+    expect(market.leverageEnabled).not.to.be.ok
+    expect(market.spotEnabled).to.be.ok
 
   })
 
--- src/exchanges/gate/modules/public/market/parse.ts
+++ src/exchanges/gate/modules/public/market/parse.ts
@@ -4,6 +4,8 @@ import {
   IAlunaMarketParseReturns,
 } from '../../../../../lib/modules/public/IAlunaMarketModule'
 import { IAlunaMarketSchema } from '../../../../../lib/schemas/IAlunaMarketSchema'
+import { translateSymbolId } from '../../../../../utils/mappings/translateSymbolId'
+import { gateBaseSpecs } from '../../../gateSpecs'
 import { IGateMarketSchema } from '../../../schemas/IGateMarketSchema'
 
 
@@ -14,8 +16,54 @@ export const parse = (exchange: IAlunaExchangePublic) => (
 
   const { rawMarket } = params
 
-  // TODO: Implement proper parser
-  const market: IAlunaMarketSchema = rawMarket as any
+  const {
+    base_volume: baseVolume,
+    change_percentage: changePercentage,
+    currency_pair: currencyPair,
+    high_24h: high24h,
+    highest_bid: highestBid,
+    last,
+    low_24h: low24h,
+    lowest_ask: lowestAsk,
+    quote_volume: quoteVolume,
+  } = rawMarket
+
+  const [baseCurrency, quoteCurrency] = currencyPair.split('_')
+
+  const baseSymbolId = translateSymbolId({
+    exchangeSymbolId: baseCurrency,
+    symbolMappings: exchange.settings.symbolMappings,
+  })
+
+  const quoteSymbolId = translateSymbolId({
+    exchangeSymbolId: quoteCurrency,
+    symbolMappings: exchange.settings.symbolMappings,
+  })
+
+  const ticker = {
+    high: Number(high24h),
+    low: Number(low24h),
+    bid: Number(highestBid),
+    ask: Number(lowestAsk),
+    last: Number(last),
+    date: new Date(),
+    change: Number(changePercentage),
+    baseVolume: Number(baseVolume),
+    quoteVolume: Number(quoteVolume),
+  }
+
+  const market: IAlunaMarketSchema = {
+    exchangeId: gateBaseSpecs.id,
+    symbolPair: currencyPair,
+    baseSymbolId,
+    quoteSymbolId,
+    ticker,
+    spotEnabled: true,
+    marginEnabled: false,
+    derivativesEnabled: false,
+    leverageEnabled: false,
+    meta: rawMarket,
+  }
 
   return { market }
 
