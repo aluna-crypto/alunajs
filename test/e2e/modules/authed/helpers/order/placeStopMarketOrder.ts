01d25792d8cdb54e1fb8c7c7fc78d155f0a3e5c9
Sergio <sergio.uft@gmail.com>
2022-05-10 10:01:25 -0300

    implement e2e helper 'placeStopMarketOrder'

---
+++ test/e2e/modules/authed/helpers/order/placeStopMarketOrder.ts
@@ -0,0 +1,32 @@
+import { AlunaAccountEnum } from '../../../../../../src/lib/enums/AlunaAccountEnum'
+import { AlunaOrderSideEnum } from '../../../../../../src/lib/enums/AlunaOrderSideEnum'
+import { AlunaOrderTypesEnum } from '../../../../../../src/lib/enums/AlunaOrderTypesEnum'
+import { IAuthedParams } from '../../../IAuthedParams'
+
+
+
+export const placeStopMarketOrder = async (
+  params: IAuthedParams,
+) => {
+
+
+  const {
+    exchangeAuthed,
+    exchangeConfigs: {
+      symbolPair,
+      orderAccount,
+      orderAmount,
+      orderStopRate,
+    },
+  } = params
+
+  return exchangeAuthed.order.place({
+    symbolPair,
+    account: orderAccount || AlunaAccountEnum.EXCHANGE,
+    amount: orderAmount,
+    side: AlunaOrderSideEnum.BUY,
+    stopRate: orderStopRate,
+    type: AlunaOrderTypesEnum.STOP_MARKET,
+  })
+
+}
