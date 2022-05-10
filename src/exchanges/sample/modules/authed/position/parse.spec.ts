3c4ac834a710b3c5885ef603746e13ce79ff86ae
Sergio <sergio.uft@gmail.com>
2022-05-09 22:51:13 -0300

    adding tests suites for Sample position module

--- src/exchanges/sample/modules/authed/position/parse.spec.ts
+++ src/exchanges/sample/modules/authed/position/parse.spec.ts
@@ -1,13 +1,33 @@
 import { expect } from 'chai'
 
-import { parse } from './parse'
+import { IAlunaCredentialsSchema } from '../../../../../lib/schemas/IAlunaCredentialsSchema'
+import { SampleAuthed } from '../../../SampleAuthed'
+import { SAMPLE_RAW_POSITIONS } from '../../../test/fixtures/samplePositions'
 
 
 
 describe(__filename, () => {
 
-  it('should parse position just fine', async () => {
-    expect(parse).to.exist
-  })
+  const credentials: IAlunaCredentialsSchema = {
+    key: 'key',
+    secret: 'secret',
+  }
 
+  it('should parse a Sample raw position just fine', async () => {
+
+    // preparing data
+    const rawPosition = SAMPLE_RAW_POSITIONS[0]
+
+    const exchange = new SampleAuthed({ credentials })
+
+    const { position } = exchange.position!.parse({ rawPosition })
+
+
+    // validating
+    expect(position).to.exist
+
+    // TODO: add expectations for everything
+    // expect(order).to.deep.eq(...)
+
+  })
 })
--- src/exchanges/sample/modules/authed/position/setLeverage.spec.ts
+++ src/exchanges/sample/modules/authed/position/setLeverage.spec.ts
@@ -1,13 +1,64 @@
 import { expect } from 'chai'
 
-import { setLeverage } from './setLeverage'
+import { mockHttp } from '../../../../../../test/mocks/exchange/Http'
+import { IAlunaPositionSetLeverageParams } from '../../../../../lib/modules/authed/IAlunaPositionModule'
+import { IAlunaCredentialsSchema } from '../../../../../lib/schemas/IAlunaCredentialsSchema'
+import { SampleAuthed } from '../../../SampleAuthed'
+import { SampleHttp } from '../../../SampleHttp'
+import { getSampleEndpoints } from '../../../sampleSpecs'
+import { getLeverage } from './getLeverage'
 
 
 
 describe(__filename, () => {
 
+  const credentials: IAlunaCredentialsSchema = {
+    key: 'key',
+    secret: 'secret',
+  }
+
   it('should set leverage just fine', async () => {
-    expect(setLeverage).to.exist
+
+    // preparing data
+    const mockedLeverage = 10
+
+
+    // mocking
+    const {
+      publicRequest,
+      authedRequest,
+    } = mockHttp({ classPrototype: SampleHttp.prototype })
+
+    authedRequest.returns(Promise.resolve(mockedLeverage))
+    expect(getLeverage).to.exist
+
+
+    // executing
+    const exchange = new SampleAuthed({
+      credentials,
+    })
+
+    const params: IAlunaPositionSetLeverageParams = {
+      id: 'id',
+      symbolPair: 'symbolPair',
+      leverage: 10,
+    }
+
+    const { leverage } = await exchange.position!.setLeverage!(params)
+
+
+    // validating
+    expect(leverage).to.deep.eq(mockedLeverage)
+
+    expect(authedRequest.callCount).to.be.eq(1)
+    expect(authedRequest.firstCall.args[0]).to.deep.eq({
+      credentials,
+      url: getSampleEndpoints({}).position.setLeverage,
+      body: params,
+    })
+
+    expect(publicRequest.callCount).to.be.eq(0)
+
   })
 
 })
