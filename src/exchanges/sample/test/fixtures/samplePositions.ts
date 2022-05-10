e04d148a28f4d735e5f7d1d2747d2c06186942ff
Sergio <sergio.uft@gmail.com>
2022-05-09 22:50:49 -0300

    add fixture for Sample positions

---
+++ src/exchanges/sample/test/fixtures/samplePositions.ts
@@ -0,0 +1,15 @@
+import { ISamplePositionSchema } from '../../schemas/ISamplePositionSchema'
+
+
+
+// TODO: Review fixtures
+export const SAMPLE_RAW_POSITIONS: ISamplePositionSchema[] = [
+  {
+    id: '8bc1e59c-77fa-4554-bd11-966e360e4eb7',
+    symbolPair: 'BTC/USD',
+  },
+  {
+    id: '8bc1e59c-77fa-4554-bd11-966e360e4eb8',
+    symbolPair: 'ETH/USD',
+  },
+]
