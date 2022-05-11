2b086919c3adcc951d2deedbd21f2472e654577f
Sergio <sergio.uft@gmail.com>
2022-05-11 10:30:26 -0300

    implement Bitmex market 'get'

---
+++ src/exchanges/bitmex/modules/public/market/get.ts
@@ -0,0 +1,41 @@
+import debug from 'debug'
+
+import { IAlunaExchangePublic } from '../../../../../lib/core/IAlunaExchange'
+import {
+  IAlunaMarketGetParams,
+  IAlunaMarketGetReturns,
+} from '../../../../../lib/modules/public/IAlunaMarketModule'
+import { BitmexHttp } from '../../../BitmexHttp'
+
+
+
+const log = debug('@alunajs:bitmex/symbol/list')
+
+
+
+export const get = (exchange: IAlunaExchangePublic) => async (
+  params: IAlunaMarketGetParams,
+): Promise<IAlunaMarketGetReturns> => {
+
+  log('listing Bitmex symbols')
+
+  const { symbolPair } = params
+
+  const { http = new BitmexHttp(exchange.settings) } = params
+
+  const { requestWeight } = http
+
+  const { rawMarket } = await exchange.market.getRaw!({
+    http,
+    symbolPair,
+  })
+
+  const { market } = exchange.market.parse({ rawMarket })
+
+  return {
+    market,
+    requestWeight,
+  }
+
+}
+
