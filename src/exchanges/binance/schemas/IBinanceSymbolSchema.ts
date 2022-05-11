9520ede593e8b4c576c7f1739c24ed37415dce08
LucianoPierdona <lucianompj1@gmail.com>
2022-05-11 17:55:22 -0300

    Add Binance Symbol Schema and Fixtures

--- src/exchanges/binance/schemas/IBinanceSymbolSchema.ts
+++ src/exchanges/binance/schemas/IBinanceSymbolSchema.ts
@@ -1,6 +1,38 @@
-// TODO: Describe symbol interface for Binance exchange
 export interface IBinanceSymbolSchema {
   symbol: string
-  name: string
-  // ...
+  status: string
+  baseAsset: string
+  baseAssetPrecision: number
+  quoteAsset: string
+  quotePrecision: number
+  quoteAssetPrecision: number
+  baseCommissionPrecision: number
+  quoteCommissionPrecision: number
+  orderTypes: string[]
+  icebergAllowed: boolean
+  ocoAllowed: boolean
+  quoteOrderQtyMarketAllowed: boolean
+  isSpotTradingAllowed: boolean
+  isMarginTradingAllowed: boolean
+  filters: IBinanceSymbolFilterSchema[]
+  permissions: string[]
 }
+
+export interface IBinanceSymbolFilterSchema {
+  filterType: string
+  minPrice?: string
+  maxPrice?: string
+  tickSize?: string
+  minQty?: string
+  maxQty?: string
+  stepSize?: string
+  limit?: number
+  minNotional?: string
+  applyToMarket?: boolean
+  avgPriceMins?: number
+  multiplierUp?: string
+  multiplierDown?: string
+  maxNumOrders?: number
+  maxNumAlgoOrders?: number
+}
+
--- src/exchanges/binance/test/fixtures/binanceSymbols.ts
+++ src/exchanges/binance/test/fixtures/binanceSymbols.ts
@@ -2,19 +2,142 @@ import { IBinanceSymbolSchema } from '../../schemas/IBinanceSymbolSchema'
 
 
 
-// TODO: Review fixtures
 export const BINANCE_RAW_SYMBOLS: IBinanceSymbolSchema[] = [
   {
-    symbol: 'BTC',
-    name: 'Bitcoin',
+    symbol: 'ETHBTC',
+    status: 'TRADING',
+    baseAsset: 'ETH',
+    baseAssetPrecision: 8,
+    quoteAsset: 'BTC',
+    quotePrecision: 8,
+    quoteAssetPrecision: 8,
+    baseCommissionPrecision: 8,
+    quoteCommissionPrecision: 8,
+    orderTypes: [
+      'LIMIT',
+      'LIMIT_MAKER',
+      'MARKET',
+      'STOP_LOSS_LIMIT',
+      'TAKE_PROFIT_LIMIT',
+    ],
+    icebergAllowed: true,
+    ocoAllowed: true,
+    quoteOrderQtyMarketAllowed: true,
+    isSpotTradingAllowed: true,
+    isMarginTradingAllowed: true,
+    filters: [
+      {
+        filterType: 'PRICE_FILTER',
+        minPrice: '0.00000100',
+        maxPrice: '922327.00000000',
+        tickSize: '0.00000100',
+      },
+      {
+        filterType: 'PERCENT_PRICE',
+        multiplierUp: '5',
+        multiplierDown: '0.2',
+        avgPriceMins: 5,
+      },
+      {
+        filterType: 'LOT_SIZE',
+        minQty: '0.00010000',
+        maxQty: '100000.00000000',
+        stepSize: '0.00010000',
+      },
+      {
+        filterType: 'MIN_NOTIONAL',
+        minNotional: '0.00010000',
+        applyToMarket: true,
+        avgPriceMins: 5,
+      },
+      {
+        filterType: 'ICEBERG_PARTS',
+        limit: 10,
+      },
+      {
+        filterType: 'MARKET_LOT_SIZE',
+        minQty: '0.00000000',
+        maxQty: '1010.54961473',
+        stepSize: '0.00000000',
+      },
+      {
+        filterType: 'MAX_NUM_ORDERS',
+        maxNumOrders: 200,
+      },
+      {
+        filterType: 'MAX_NUM_ALGO_ORDERS',
+        maxNumAlgoOrders: 5,
+      },
+    ],
+    permissions: ['SPOT', 'MARGIN'],
   },
   {
-    symbol: 'LTC',
-    name: 'Litecoin',
-  },
-  {
-    symbol: 'ETH',
-    name: 'Ethereum',
+    symbol: 'BTCBRL',
+    status: 'TRADING',
+    baseAsset: 'BTC',
+    baseAssetPrecision: 8,
+    quoteAsset: 'BRL',
+    quotePrecision: 8,
+    quoteAssetPrecision: 8,
+    baseCommissionPrecision: 8,
+    quoteCommissionPrecision: 8,
+    orderTypes: [
+      'LIMIT',
+      'LIMIT_MAKER',
+      'MARKET',
+      'STOP_LOSS_LIMIT',
+      'TAKE_PROFIT_LIMIT',
+    ],
+    icebergAllowed: true,
+    ocoAllowed: true,
+    quoteOrderQtyMarketAllowed: true,
+    isSpotTradingAllowed: true,
+    isMarginTradingAllowed: true,
+    filters: [
+      {
+        filterType: 'PRICE_FILTER',
+        minPrice: '0.00000100',
+        maxPrice: '100000.00000000',
+        tickSize: '0.00000100',
+      },
+      {
+        filterType: 'PERCENT_PRICE',
+        multiplierUp: '5',
+        multiplierDown: '0.2',
+        avgPriceMins: 5,
+      },
+      {
+        filterType: 'LOT_SIZE',
+        minQty: '0.00100000',
+        maxQty: '100000.00000000',
+        stepSize: '0.00100000',
+      },
+      {
+        filterType: 'MIN_NOTIONAL',
+        minNotional: '0.00010000',
+        applyToMarket: true,
+        avgPriceMins: 5,
+      },
+      {
+        filterType: 'ICEBERG_PARTS',
+        limit: 10,
+      },
+      {
+        filterType: 'MARKET_LOT_SIZE',
+        minQty: '0.00000000',
+        maxQty: '13755.68382070',
+        stepSize: '0.00000000',
+      },
+      {
+        filterType: 'MAX_NUM_ORDERS',
+        maxNumOrders: 200,
+      },
+      {
+        filterType: 'MAX_NUM_ALGO_ORDERS',
+        maxNumAlgoOrders: 5,
+      },
+    ],
+    permissions: ['SPOT', 'MARGIN'],
   },
 ]
 
