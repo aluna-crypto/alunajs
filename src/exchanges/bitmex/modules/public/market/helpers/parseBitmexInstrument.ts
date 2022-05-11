0c894efe08fdb3aef956ce9b31733f6d5d548bfa
Sergio <sergio.uft@gmail.com>
2022-05-11 08:54:42 -0300

    add Bitmex market helper 'parseBitmexInstrument'

---
+++ src/exchanges/bitmex/modules/public/market/helpers/parseBitmexInstrument.ts
@@ -0,0 +1,111 @@
+import { AlunaInstrumentStateEnum } from '../../../../../../lib/enums/AlunaInstrumentStateEnum'
+import { IAlunaInstrumentSchema } from '../../../../../../lib/schemas/IAlunaInstrumentSchema'
+import { BitmexInstrumentStateEnum } from '../../../../enums/BitmexInstrumentStateEnum'
+import { IBitmexMarketSchema } from '../../../../schemas/IBitmexMarketSchema'
+import { computeContractCurrency } from './computeContractCurrency'
+import { computeContractValue } from './computeContractValue'
+import { computeMinTradeAmount } from './computeMinTradeAmount'
+import { computeOrderValueMultiplier } from './computeOrderValueMultiplier'
+import { computeUsdPricePerUnit } from './computeUsdPricePerUnit'
+import { resolveSymbolsIds } from './resolveSymbolsIds'
+
+
+
+export const parseBitmexInstrument = (params: {
+    rawMarket: IBitmexMarketSchema
+  }): IAlunaInstrumentSchema => {
+
+  const { rawMarket } = params
+
+  const {
+    state,
+    expiry,
+    front,
+    listing,
+    settle,
+    symbol,
+    isQuanto,
+    isInverse,
+    lastPrice,
+    rootSymbol,
+    sessionInterval,
+    openingTimestamp,
+    closingTimestamp,
+  } = rawMarket
+
+  const {
+    rateSymbolId,
+    totalSymbolId,
+    amountSymbolId,
+  } = resolveSymbolsIds({ rawMarket })
+
+  const contractValue = computeContractValue({
+    rawMarket,
+  })
+
+  const contractCurrency = computeContractCurrency({
+    rawMarket,
+  })
+
+  const minTradeAmount = computeMinTradeAmount({
+    rawMarket,
+  })
+
+  let usdPricePerUnit: number | undefined
+
+  if (isQuanto) {
+
+    usdPricePerUnit = computeUsdPricePerUnit({
+      rawMarket,
+    })
+
+  }
+
+  const orderValueMultiplier = computeOrderValueMultiplier({
+    rawMarket,
+  })
+
+  const splittedName = symbol.replace(
+    new RegExp(`${rootSymbol}`),
+    `${rootSymbol} `,
+  )
+
+  const computedExpireDate = expiry
+    ? new Date(expiry)
+    : null
+
+  const computedSettleDate = settle
+    ? new Date(settle)
+    : null
+
+  const computedState = state === BitmexInstrumentStateEnum.OPEN
+    ? AlunaInstrumentStateEnum.OPEN
+    : AlunaInstrumentStateEnum.CLOSED
+
+  const instrument: IAlunaInstrumentSchema = {
+    name: symbol,
+    splittedName,
+    state: computedState,
+    openDate: new Date(openingTimestamp),
+    closeDate: new Date(closingTimestamp),
+    sessionIntervalDate: new Date(sessionInterval),
+    listingDate: new Date(listing),
+    frontDate: new Date(front),
+    expireDate: computedExpireDate,
+    settleDate: computedSettleDate,
+    price: lastPrice,
+    isTradedByUnitsOfContract: isQuanto,
+    isInverse,
+    rateSymbolId,
+    totalSymbolId,
+    amountSymbolId,
+    minTradeAmount,
+    contractValue,
+    contractCurrency,
+    orderValueMultiplier,
+    usdPricePerUnit,
+  }
+
+  return instrument
+
+}
