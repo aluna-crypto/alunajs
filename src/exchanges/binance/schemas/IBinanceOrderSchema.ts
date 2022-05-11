3c74eb16e10c3c7fa5fc1cc4241db91f9ae0991b
LucianoPierdona <lucianompj1@gmail.com>
2022-05-11 17:57:21 -0300

    Add Binance Order Schema and Fixtures

--- src/exchanges/binance/schemas/IBinanceOrderSchema.ts
+++ src/exchanges/binance/schemas/IBinanceOrderSchema.ts
@@ -1,11 +1,47 @@
+import { BinanceOrderSideEnum } from '../enums/BinanceOrderSideEnum'
+import { BinanceOrderStatusEnum } from '../enums/BinanceOrderStatusEnum'
 import { BinanceOrderTypeEnum } from '../enums/BinanceOrderTypeEnum'
+import { IBinanceSymbolSchema } from './IBinanceSymbolSchema'
 
 
 
-// TODO: Describe order interface for Binance exchange
 export interface IBinanceOrderSchema {
-  id: string
   symbol: string
+  orderId: number
+  orderListId: number // Unless OCO, the value will always be -1
+  clientOrderId: string
+  price: string
+  origQty: string
+  executedQty: string
+  cummulativeQuoteQty: string
+  status: BinanceOrderStatusEnum
+  timeInForce: string
   type: BinanceOrderTypeEnum
-  // ...
+  side: BinanceOrderSideEnum
+  stopPrice?: string
+  icebergQty?: string
+  time?: number
+  updateTime?: number
+  transactTime?: number
+  isWorking?: boolean
+  origQuoteOrderQty?: string
+  fills?: IBinanceOrderFills[]
+}
+
+export interface IBinanceOrderFills {
+  price: string
+  qty: string
+  commission: string
+  commissionAsset: string
+  tradeId: number
+}
+
+export interface IBinanceOrdersResponseSchema {
+  rawOrders: IBinanceOrderSchema[]
+  rawSymbols: IBinanceSymbolSchema[]
+}
+
+export interface IBinanceOrderResponseSchema {
+  rawOrder: IBinanceOrderSchema
+  rawSymbol: IBinanceSymbolSchema
 }
