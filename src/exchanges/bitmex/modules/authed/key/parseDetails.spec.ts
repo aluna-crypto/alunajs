3bf4675c640a2b6e441e07c988bd6d4b26705d8b
Sergio <sergio.uft@gmail.com>
2022-05-11 13:34:36 -0300

    add test suite for Bitmex 'parseDetails'

--- src/exchanges/bitmex/modules/authed/key/parseDetails.spec.ts
+++ src/exchanges/bitmex/modules/authed/key/parseDetails.spec.ts
@@ -1,11 +1,17 @@
 import { expect } from 'chai'
-import { omit } from 'lodash'
+import {
+  cloneDeep,
+  omit,
+} from 'lodash'
 
+import { PARSED_PERMISSIONS } from '../../../../../../test/fixtures/parsedKey'
 import { mockParsePermissions } from '../../../../../../test/mocks/exchange/modules/key/mockParsePermissions'
 import { IAlunaCredentialsSchema } from '../../../../../lib/schemas/IAlunaCredentialsSchema'
-import { IAlunaKeyPermissionSchema } from '../../../../../lib/schemas/IAlunaKeySchema'
 import { BitmexAuthed } from '../../../BitmexAuthed'
-import { IBitmexKeySchema } from '../../../schemas/IBitmexKeySchema'
+import {
+  BITMEX_RAW_KEY,
+  BITMEX_RAW_PERMISSIONS,
+} from '../../../test/fixtures/bitmexKey'
 import * as mockParsePermissionsMod from './parsePermissions'
 
 
@@ -15,34 +21,25 @@ describe(__filename, () => {
   it('should parse Bitmex key details just fine', async () => {
 
     // preparing data
-    const accountId = 'accountId'
-
-    const rawKey: IBitmexKeySchema = {
-      read: false,
-      trade: false,
-      withdraw: false,
-      accountId,
-    }
-
     const credentials: IAlunaCredentialsSchema = {
       key: 'key',
       secret: 'secret',
       passphrase: 'passphrase',
     }
 
-    const rawKeyWithoutAccId = omit(rawKey, 'accountId')
+    const mockedRawPermissions = BITMEX_RAW_PERMISSIONS[0]
+    const mockedPermissions = PARSED_PERMISSIONS
 
-    const permissions: IAlunaKeyPermissionSchema = {
-      ...rawKeyWithoutAccId,
-    }
+    const rawKey = cloneDeep([BITMEX_RAW_KEY])
+    rawKey[0].id = credentials.key
+    rawKey[0].permissions = mockedRawPermissions
 
 
     // mocking
     const { parsePermissions } = mockParsePermissions({
       module: mockParsePermissionsMod,
     })
-
-    parsePermissions.returns({ permissions })
+    parsePermissions.returns({ permissions: mockedPermissions })
 
 
     // executing
@@ -53,14 +50,14 @@ describe(__filename, () => {
 
     // validating
     expect(key).to.deep.eq({
-      accountId,
-      permissions,
-      meta: rawKey,
+      accountId: rawKey[0].userId.toString(),
+      permissions: mockedPermissions,
+      meta: omit(rawKey[0], 'secret', 'id'),
     })
 
     expect(parsePermissions.callCount).to.be.eq(1)
     expect(parsePermissions.firstCall.args[0]).to.deep.eq({
-      rawKey,
+      rawKey: mockedRawPermissions,
     })
 
   })
