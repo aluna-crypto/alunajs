4ab8ec2b0981968c77aac880f229c63bf2b439c0
Sergio <sergio.uft@gmail.com>
2022-05-11 18:19:45 -0300

    add schemas for Bitmex balance

--- src/exchanges/bitmex/schemas/IBitmexBalanceSchema.ts
+++ src/exchanges/bitmex/schemas/IBitmexBalanceSchema.ts
@@ -1,7 +1,88 @@
-// TODO: Describe balance interface for Bitmex exchange
+export interface IBitmexBalancesSchema {
+  assets: IBitmexAsset[]
+  assetsDetails: IBitmexAssetDetails[]
+}
+
+
+
 export interface IBitmexBalanceSchema {
-  total: string
-  available: string
-  currencySymbol: string
-  // ...
+  asset: IBitmexAsset
+  assetDetails: IBitmexAssetDetails
+}
+
+
+
+export interface IBitmexAsset {
+  account: number
+  currency: string
+  riskLimit: number | null
+  prevState: string
+  state: string
+  action: string
+  amount: number
+  pendingCredit: number
+  pendingDebit: number
+  confirmedDebit: number
+  prevRealisedPnl: number
+  prevUnrealisedPnl: number
+  grossComm: number
+  grossOpenCost: number
+  grossOpenPremium: number
+  grossExecCost: number
+  grossMarkValue: number
+  riskValue: number
+  taxableMargin: number
+  initMargin: number
+  maintMargin: number
+  sessionMargin: number
+  targetExcessMargin: number
+  varMargin: number
+  realisedPnl: number
+  unrealisedPnl: number
+  indicativeTax: number
+  unrealisedProfit: number
+  syntheticMargin: number | null
+  walletBalance: number
+  marginBalance: number
+  marginBalancePcnt: number
+  marginLeverage: number
+  marginUsedPcnt: number
+  excessMargin: number
+  excessMarginPcnt: number
+  availableMargin: number
+  withdrawableMargin: number
+  grossLastValue: number
+  commission: number | null
+  makerFeeDiscount: number | null
+  takerFeeDiscount: number | null
+  timestamp: string
+}
+
+
+
+export interface IBitmexAssetDetails {
+  asset: string
+  currency: string
+  majorCurrency: string
+  name: string
+  currencyType: string
+  scale: number
+  enabled: boolean
+  isMarginCurrency: boolean
+  minDepositAmount: number
+  minWithdrawalAmount: number
+  maxWithdrawalAmount: number
+  networks: IBitmexAssetDetailsNetwork[]
+}
+
+
+
+export interface IBitmexAssetDetailsNetwork {
+  asset: string
+  tokenAddress: string
+  depositEnabled: boolean
+  withdrawalEnabled: boolean
+  withdrawalFee: number
+  minFee: number
+  maxFee: number
 }
