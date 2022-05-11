6013164af0597eabe949b8d8e6a4c3b20643c746
LucianoPierdona <lucianompj1@gmail.com>
2022-05-11 18:07:38 -0300

    Add Binance Order Parse and test suite

--- src/exchanges/binance/modules/authed/order/parse.spec.ts
+++ src/exchanges/binance/modules/authed/order/parse.spec.ts
@@ -1,12 +1,18 @@
 import { expect } from 'chai'
 
+import { AlunaAccountEnum } from '../../../../../lib/enums/AlunaAccountEnum'
 import { IAlunaCredentialsSchema } from '../../../../../lib/schemas/IAlunaCredentialsSchema'
+import { mockTranslateSymbolId } from '../../../../../utils/mappings/translateSymbolId.mock'
 import { BinanceAuthed } from '../../../BinanceAuthed'
+import { translateOrderSideToAluna } from '../../../enums/adapters/binanceOrderSideAdapter'
+import { translateOrderStatusToAluna } from '../../../enums/adapters/binanceOrderStatusAdapter'
+import { translateOrderTypeToAluna } from '../../../enums/adapters/binanceOrderTypeAdapter'
 import { BINANCE_RAW_ORDERS } from '../../../test/fixtures/binanceOrders'
+import { BINANCE_RAW_SYMBOLS } from '../../../test/fixtures/binanceSymbols'
 
 
 
-describe.skip(__filename, () => {
+describe(__filename, () => {
 
   const credentials: IAlunaCredentialsSchema = {
     key: 'key',
@@ -17,17 +23,55 @@ describe.skip(__filename, () => {
 
     // preparing data
     const rawOrder = BINANCE_RAW_ORDERS[0]
+    const rawSymbol = BINANCE_RAW_SYMBOLS[0]
+
+    const rawOrderRequest = {
+      rawOrder,
+      rawSymbol,
+    }
+
+    const {
+      baseAsset,
+      quoteAsset,
+    } = rawSymbol
+
+    const {
+      side,
+      orderId,
+      symbol,
+      type,
+      status,
+    } = rawOrder
 
     const exchange = new BinanceAuthed({ credentials })
 
-    const { order } = exchange.order.parse({ rawOrder })
+    // mocking
 
+    const { translateSymbolId } = mockTranslateSymbolId()
+
+    translateSymbolId.onFirstCall().returns(baseAsset)
+    translateSymbolId.onSecondCall().returns(quoteAsset)
+
+    const orderStatus = translateOrderStatusToAluna({ from: status })
+    const orderSide = translateOrderSideToAluna({ from: side })
+    const orderType = translateOrderTypeToAluna({ from: type })
+
+    // executing
+    const { order } = exchange.order.parse({ rawOrder: rawOrderRequest })
 
     // validating
     expect(order).to.exist
 
-    // TODO: add expectations for everything
-    // expect(order).to.deep.eq(...)
+    // TODO -> Need to validate other properties
+    expect(order.id).to.be.eq(orderId.toString())
+    expect(order.symbolPair).to.be.eq(symbol.toString())
+    expect(order.exchangeId).to.be.eq(exchange.id)
+    expect(order.baseSymbolId).to.be.eq(baseAsset)
+    expect(order.quoteSymbolId).to.be.eq(quoteAsset)
+    expect(order.account).to.be.eq(AlunaAccountEnum.EXCHANGE)
+    expect(order.status).to.be.eq(orderStatus)
+    expect(order.side).to.be.eq(orderSide)
+    expect(order.type).to.be.eq(orderType)
 
   })
 
--- src/exchanges/binance/modules/authed/order/parse.ts
+++ src/exchanges/binance/modules/authed/order/parse.ts
@@ -1,11 +1,16 @@
 import { IAlunaExchangeAuthed } from '../../../../../lib/core/IAlunaExchange'
+import { AlunaAccountEnum } from '../../../../../lib/enums/AlunaAccountEnum'
+import { AlunaOrderStatusEnum } from '../../../../../lib/enums/AlunaOrderStatusEnum'
 import {
   IAlunaOrderParseParams,
   IAlunaOrderParseReturns,
 } from '../../../../../lib/modules/authed/IAlunaOrderModule'
 import { IAlunaOrderSchema } from '../../../../../lib/schemas/IAlunaOrderSchema'
 import { translateSymbolId } from '../../../../../utils/mappings/translateSymbolId'
-import { IBinanceOrderSchema } from '../../../schemas/IBinanceOrderSchema'
+import { translateOrderSideToAluna } from '../../../enums/adapters/binanceOrderSideAdapter'
+import { translateOrderStatusToAluna } from '../../../enums/adapters/binanceOrderStatusAdapter'
+import { translateOrderTypeToAluna } from '../../../enums/adapters/binanceOrderTypeAdapter'
+import { IBinanceOrderResponseSchema } from '../../../schemas/IBinanceOrderSchema'
 
 
 
@@ -14,44 +19,127 @@ import { IBinanceOrderSchema } from '../../../schemas/IBinanceOrderSchema'
 
 
 export const parse = (exchange: IAlunaExchangeAuthed) => (
-  params: IAlunaOrderParseParams<IBinanceOrderSchema>,
+  params: IAlunaOrderParseParams<IBinanceOrderResponseSchema>,
 ): IAlunaOrderParseReturns => {
 
   // log('parse order', params)
 
-  const { rawOrder } = params
+  const {
+    rawOrder: rawOrderRequest,
+  } = params
 
-  const { symbol } = rawOrder
+  const {
+    rawOrder,
+    rawSymbol,
+  } = rawOrderRequest
 
-  let [
-    baseSymbolId,
-    quoteSymbolId,
-  ] = symbol.split('/')
+  const {
+    baseAsset,
+    quoteAsset,
+  } = rawSymbol
 
-  baseSymbolId = translateSymbolId({
-    exchangeSymbolId: baseSymbolId,
-    symbolMappings: exchange.settings.symbolMappings,
+  const {
+    orderId,
+    time,
+    origQty,
+    symbol,
+    side,
+    price,
+    type,
+    status,
+    updateTime,
+    transactTime,
+    fills,
+  } = rawOrder
+
+  const { symbolMappings } = exchange.settings
+
+  const baseSymbolId = translateSymbolId({
+    exchangeSymbolId: baseAsset,
+    symbolMappings,
   })
 
-  quoteSymbolId = translateSymbolId({
-    exchangeSymbolId: quoteSymbolId,
-    symbolMappings: exchange.settings.symbolMappings,
+  const quoteSymbolId = translateSymbolId({
+    exchangeSymbolId: quoteAsset,
+    symbolMappings,
   })
 
-  // TODO: Implement proper parser
+  const updatedAt = updateTime ? new Date(updateTime) : undefined
+  const amount = parseFloat(origQty)
+
+  /**
+     * It seems Binance does not return the price for filled orders, but it does
+     * return a 'fills' prop when the placed order is filled immediately.
+     */
+  let rate = parseFloat(price)
+
+  if (fills?.length) {
+
+    rate = parseFloat(fills[0].price)
+
+
+  }
+
+  const orderStatus = translateOrderStatusToAluna({ from: status })
+  const orderSide = translateOrderSideToAluna({ from: side })
+  const orderType = translateOrderTypeToAluna({ from: type })
+
+  let createdAt: Date
+  let filledAt: Date | undefined
+  let canceledAt: Date | undefined
+
+  if (time) {
+
+    createdAt = new Date(time)
+
+  } else if (transactTime) {
+
+    createdAt = new Date(transactTime)
+
+  } else {
+
+    createdAt = new Date()
+
+  }
+
+  if (orderStatus === AlunaOrderStatusEnum.CANCELED) {
+
+    canceledAt = updatedAt
+
+  }
+
+  if (orderStatus === AlunaOrderStatusEnum.FILLED) {
+
+    if (fills?.length) {
+
+      filledAt = new Date()
+
+    } else {
+
+      filledAt = updatedAt
+
+    }
+
+  }
+
   const order: IAlunaOrderSchema = {
-    id: rawOrder.id,
-    symbolPair: rawOrder.id,
+    id: orderId.toString(),
+    symbolPair: symbol,
     exchangeId: exchange.specs.id,
     baseSymbolId,
     quoteSymbolId,
-    // total: rawOrder.total,
-    // amount: rawOrder.amount,
-    // account: AlunaAccountEnum.MARGIN,
-    // status: rawOrder.status,
-    // side: rawOrder.side,
+    rate,
+    total: amount * rate,
+    amount,
+    account: AlunaAccountEnum.EXCHANGE,
+    status: orderStatus,
+    side: orderSide,
+    type: orderType,
+    placedAt: new Date(createdAt),
+    filledAt,
+    canceledAt,
     meta: rawOrder,
-  } as any // TODO: Remove casting to any
+  }
 
   return { order }
 
