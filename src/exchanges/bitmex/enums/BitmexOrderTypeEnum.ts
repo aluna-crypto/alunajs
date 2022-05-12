703c7913f850d79a0ce3a6e158b42a631febdda8
Sergio <sergio.uft@gmail.com>
2022-05-12 13:31:35 -0300

    add 'BitmexOrderTypeEnum'

--- src/exchanges/bitmex/enums/BitmexOrderTypeEnum.ts
+++ src/exchanges/bitmex/enums/BitmexOrderTypeEnum.ts
@@ -1,6 +1,6 @@
 export enum BitmexOrderTypeEnum {
-  LIMIT = 'LIMIT',
-  MARKET = 'MARKET',
-  CEILING_LIMIT = 'CEILING_LIMIT',
-  CEILING_MARKET = 'CEILING_MARKET'
+  MARKET = 'Market',
+  LIMIT = 'Limit',
+  STOP_MARKET = 'Stop',
+  STOP_LIMIT = 'StopLimit',
 }
