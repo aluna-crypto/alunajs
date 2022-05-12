e30d82a334896f60fad5ffc6882db9e29a0bc47c
Sergio <sergio.uft@gmail.com>
2022-05-12 13:31:28 -0300

    add 'BitmexOrderStatusEnum'

--- src/exchanges/bitmex/enums/BitmexOrderStatusEnum.ts
+++ src/exchanges/bitmex/enums/BitmexOrderStatusEnum.ts
@@ -1,4 +1,12 @@
 export enum BitmexOrderStatusEnum {
-  OPEN = 'OPEN',
-  CLOSED = 'CLOSED'
+  NEW = 'New',
+  PARTIALLY_FILLED = 'PartiallyFilled',
+  FILLED = 'Filled',
+  DONE_FOR_DAY = 'DoneForDay',
+  CANCELED = 'Canceled',
+  PENDING_CANCEL = 'PendingCancel',
+  STOPPED = 'Stopped',
+  REJECTED = 'Rejected',
+  PENDING_NEW = 'PendingNew',
+  EXPIRED = 'Expired',
 }
