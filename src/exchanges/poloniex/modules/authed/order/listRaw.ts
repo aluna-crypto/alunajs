5bbbb1368e775b3e51e719ed46097d62b8eac839
LucianoPierdona <lucianompj1@gmail.com>
2022-05-12 22:19:52 -0300

    Add Poloniex Order List Raw

--- src/exchanges/poloniex/modules/authed/order/listRaw.ts
+++ src/exchanges/poloniex/modules/authed/order/listRaw.ts
@@ -1,14 +1,13 @@
 import { debug } from 'debug'
 
 import { IAlunaExchangeAuthed } from '../../../../../lib/core/IAlunaExchange'
-import { AlunaHttpVerbEnum } from '../../../../../lib/enums/AlunaHtttpVerbEnum'
 import {
   IAlunaOrderListParams,
   IAlunaOrderListRawReturns,
 } from '../../../../../lib/modules/authed/IAlunaOrderModule'
 import { PoloniexHttp } from '../../../PoloniexHttp'
 import { getPoloniexEndpoints } from '../../../poloniexSpecs'
-import { IPoloniexOrderSchema } from '../../../schemas/IPoloniexOrderSchema'
+import { IPoloniexOrderResponseSchema } from '../../../schemas/IPoloniexOrderSchema'
 
 
 
@@ -18,7 +17,7 @@ const log = debug('@alunajs:poloniex/order/listRaw')
 
 export const listRaw = (exchange: IAlunaExchangeAuthed) => async (
   params: IAlunaOrderListParams = {},
-): Promise<IAlunaOrderListRawReturns<IPoloniexOrderSchema[]>> => {
+): Promise<IAlunaOrderListRawReturns<IPoloniexOrderResponseSchema>> => {
 
   log('fetching Poloniex open orders', params)
 
@@ -29,11 +28,17 @@ export const listRaw = (exchange: IAlunaExchangeAuthed) => async (
 
   const { http = new PoloniexHttp(settings) } = params
 
-  // TODO: Implement proper request
-  const rawOrders = await http.authedRequest<IPoloniexOrderSchema[]>({
-    verb: AlunaHttpVerbEnum.GET,
+  const timestamp = new Date().getTime()
+  const searchParams = new URLSearchParams()
+
+  searchParams.append('command', 'returnOpenOrders')
+  searchParams.append('currencyPair', 'all')
+  searchParams.append('nonce', timestamp.toString())
+
+  const rawOrders = await http.authedRequest<IPoloniexOrderResponseSchema>({
     url: getPoloniexEndpoints(settings).order.list,
     credentials,
+    body: searchParams,
   })
 
   const { requestWeight } = http
