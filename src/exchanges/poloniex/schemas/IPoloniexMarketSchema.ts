59a4c4ca4ba2660f5a6f52e79b2c2fdce5108d7f
LucianoPierdona <lucianompj1@gmail.com>
2022-05-12 22:12:03 -0300

    Add Poloniex Market Schema and Fixtures

--- src/exchanges/poloniex/schemas/IPoloniexMarketSchema.ts
+++ src/exchanges/poloniex/schemas/IPoloniexMarketSchema.ts
@@ -1,12 +1,24 @@
-import { PoloniexMarketStatusEnum } from '../enums/PoloniexMarketStatusEnum'
-
-
-
-// TODO: Describe market interface for Poloniex exchange
-export interface IPoloniexMarketSchema {
-  symbol: string
-  volume: string
+export interface IPoloniexTickerSchema {
+  id: number
+  last: string
+  lowestAsk: string
+  highestBid: string
+  percentChange: string
+  baseVolume: string
   quoteVolume: string
-  status: PoloniexMarketStatusEnum
-  // ...
+  isFrozen: string
+  postOnly: string
+  marginTradingEnabled: string
+  high24hr: string
+  low24hr: string
+}
+
+export interface IPoloniexMarketSchema extends IPoloniexTickerSchema {
+  baseCurrency: string
+  quoteCurrency: string
+  currencyPair: string
+}
+
+export interface IPoloniexMarketResponseSchema {
+  [key: string]: IPoloniexTickerSchema
 }
--- src/exchanges/poloniex/test/fixtures/poloniexMarket.ts
+++ src/exchanges/poloniex/test/fixtures/poloniexMarket.ts
@@ -1,27 +1,81 @@
-import { PoloniexMarketStatusEnum } from '../../enums/PoloniexMarketStatusEnum'
-import { IPoloniexMarketSchema } from '../../schemas/IPoloniexMarketSchema'
+import {
+  IPoloniexMarketResponseSchema,
+  IPoloniexMarketSchema,
+  IPoloniexTickerSchema,
+} from '../../schemas/IPoloniexMarketSchema'
 
 
 
-// TODO: Review fixtures
+export const POLONIEX_RAW_BALANCES_RESPONSE: IPoloniexTickerSchema[] = [
+  {
+    id: 50,
+    last: '0.00289979',
+    lowestAsk: '0.00290072',
+    highestBid: '0.00289886',
+    percentChange: '-0.01731347',
+    baseVolume: '34.57662119',
+    quoteVolume: '11905.74320223',
+    isFrozen: '0',
+    postOnly: '0',
+    marginTradingEnabled: '1',
+    high24hr: '0.00295473',
+    low24hr: '0.00287440',
+  },
+  {
+    id: 148,
+    last: '0.07092277',
+    lowestAsk: '0.07094162',
+    highestBid: '0.07093593',
+    percentChange: '-0.00100107',
+    baseVolume: '181.41410372',
+    quoteVolume: '2549.32931801',
+    isFrozen: '0',
+    postOnly: '0',
+    marginTradingEnabled: '1',
+    high24hr: '0.07190323',
+    low24hr: '0.06998660',
+  },
+  {
+    id: 733,
+    last: '0.00002461',
+    lowestAsk: '0.00002478',
+    highestBid: '0.00002471',
+    percentChange: '-0.01243980',
+    baseVolume: '0.08452872',
+    quoteVolume: '3418.95083220',
+    isFrozen: '0',
+    postOnly: '0',
+    marginTradingEnabled: '0',
+    high24hr: '0.00002482',
+    low24hr: '0.00002457',
+  },
+]
+
+export const POLONIEX_RAW_MARKETS_RESPONSE: IPoloniexMarketResponseSchema = {
+  BTC_LTC: POLONIEX_RAW_BALANCES_RESPONSE[0],
+  BTC_ETH: POLONIEX_RAW_BALANCES_RESPONSE[1],
+  BTC_ADA: POLONIEX_RAW_BALANCES_RESPONSE[2],
+}
+
+
 export const POLONIEX_RAW_MARKETS: IPoloniexMarketSchema[] = [
   {
-    symbol: 'BTC-EUR',
-    volume: '9.64127008',
-    quoteVolume: '311825.04145095',
-    status: PoloniexMarketStatusEnum.ONLINE,
+    baseCurrency: 'LTC',
+    quoteCurrency: 'BTC',
+    currencyPair: 'BTC_LTC',
+    ...POLONIEX_RAW_BALANCES_RESPONSE[0],
   },
   {
-    symbol: 'BTC-USD',
-    volume: '126.19108264',
-    quoteVolume: '4696409.21649740',
-    status: PoloniexMarketStatusEnum.ONLINE,
+    baseCurrency: 'ETH',
+    quoteCurrency: 'BTC',
+    currencyPair: 'BTC_ETH',
+    ...POLONIEX_RAW_BALANCES_RESPONSE[1],
   },
   {
-    symbol: 'BTC-USDT',
-    volume: '86.32449650',
-    quoteVolume: '3191538.22788546',
-    status: PoloniexMarketStatusEnum.OFFLINE,
+    baseCurrency: 'ADA',
+    quoteCurrency: 'BTC',
+    currencyPair: 'BTC_ADA',
+    ...POLONIEX_RAW_BALANCES_RESPONSE[2],
   },
 ]
 
