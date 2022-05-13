2943985b44607a31eedea8f28388679e4e1ecfc2
LucianoPierdona <lucianompj1@gmail.com>
2022-05-12 22:11:38 -0300

    Add Poloniex Symbol Schema and Fixtures

--- src/exchanges/poloniex/schemas/IPoloniexSymbolSchema.ts
+++ src/exchanges/poloniex/schemas/IPoloniexSymbolSchema.ts
@@ -1,6 +1,24 @@
-// TODO: Describe symbol interface for Poloniex exchange
-export interface IPoloniexSymbolSchema {
-  symbol: string
+export interface IPoloniexSymbolInfoSchema {
+  id: number
   name: string
-  // ...
+  humanType: string
+  currencyType: string
+  txFee: string
+  minConf: number
+  depositAddress: string
+  depositDisabled: number
+  disabled: number
+  frozen: number
+  hexColor: string
+  blockchain: string
+  delisted: number
+  isGeofenced: number
+}
+
+export interface IPoloniexSymbolSchema extends IPoloniexSymbolInfoSchema {
+  currency: string
+}
+
+export interface IPoloniexSymbolResponseSchema {
+  [key: string]: IPoloniexSymbolInfoSchema
 }
--- src/exchanges/poloniex/test/fixtures/poloniexSymbols.ts
+++ src/exchanges/poloniex/test/fixtures/poloniexSymbols.ts
@@ -1,20 +1,81 @@
-import { IPoloniexSymbolSchema } from '../../schemas/IPoloniexSymbolSchema'
+import {
+  IPoloniexSymbolInfoSchema,
+  IPoloniexSymbolResponseSchema,
+  IPoloniexSymbolSchema,
+} from '../../schemas/IPoloniexSymbolSchema'
 
 
 
-// TODO: Review fixtures
+export const POLONIEX_RAW_SYMBOL_INFO: IPoloniexSymbolInfoSchema[] = [
+  {
+    id: 125,
+    name: 'Litecoin',
+    humanType: 'BTC Clone',
+    currencyType: 'address',
+    txFee: '0.00100000',
+    minConf: 4,
+    depositAddress: '',
+    disabled: 0,
+    frozen: 0,
+    depositDisabled: 0,
+    hexColor: '509BCA',
+    blockchain: 'LTC',
+    delisted: 0,
+    isGeofenced: 0,
+  },
+  {
+    id: 28,
+    name: 'Bitcoin',
+    humanType: 'BTC Clone',
+    currencyType: 'address',
+    txFee: '0.00050000',
+    minConf: 2,
+    depositAddress: '',
+    disabled: 0,
+    frozen: 0,
+    depositDisabled: 0,
+    hexColor: 'F59C3D',
+    blockchain: 'BTC',
+    delisted: 0,
+    isGeofenced: 0,
+  },
+  {
+    id: 267,
+    name: 'Ethereum',
+    humanType: 'Sweep to Main Account',
+    currencyType: 'address',
+    txFee: '0.01387773',
+    minConf: 12,
+    depositAddress: '',
+    disabled: 0,
+    frozen: 0,
+    depositDisabled: 0,
+    hexColor: '1C1CE1',
+    blockchain: 'ETH',
+    delisted: 0,
+    isGeofenced: 0,
+  },
+]
+
+
+export const POLONIEX_RAW_SYMBOL_RESPONSE: IPoloniexSymbolResponseSchema = {
+  LTC: POLONIEX_RAW_SYMBOL_INFO[0],
+  BTC: POLONIEX_RAW_SYMBOL_INFO[1],
+  ETH: POLONIEX_RAW_SYMBOL_INFO[2],
+}
+
 export const POLONIEX_RAW_SYMBOLS: IPoloniexSymbolSchema[] = [
   {
-    symbol: 'BTC',
-    name: 'Bitcoin',
+    ...POLONIEX_RAW_SYMBOL_INFO[0],
+    currency: 'LTC',
   },
   {
-    symbol: 'LTC',
-    name: 'Litecoin',
+    ...POLONIEX_RAW_SYMBOL_INFO[1],
+    currency: 'BTC',
   },
   {
-    symbol: 'ETH',
-    name: 'Ethereum',
+    ...POLONIEX_RAW_SYMBOL_INFO[2],
+    currency: 'ETH',
   },
 ]
 
