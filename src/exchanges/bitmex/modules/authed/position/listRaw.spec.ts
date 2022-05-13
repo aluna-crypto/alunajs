86815e3b7f6d3d3925246d63032b1eec2fbe9876
Sergio <sergio.uft@gmail.com>
2022-05-13 12:22:03 -0300

    add test suite for Bitmex position 'listRaw'

--- src/exchanges/bitmex/modules/authed/position/listRaw.spec.ts
+++ src/exchanges/bitmex/modules/authed/position/listRaw.spec.ts
@@ -1,11 +1,16 @@
 import { expect } from 'chai'
 
+import { PARSED_MARKETS } from '../../../../../../test/fixtures/parsedMarkets'
 import { mockHttp } from '../../../../../../test/mocks/exchange/Http'
+import { mockList } from '../../../../../../test/mocks/exchange/modules/mockList'
+import { AlunaHttpVerbEnum } from '../../../../../lib/enums/AlunaHtttpVerbEnum'
 import { IAlunaCredentialsSchema } from '../../../../../lib/schemas/IAlunaCredentialsSchema'
+import { IAlunaSettingsSchema } from '../../../../../lib/schemas/IAlunaSettingsSchema'
 import { BitmexAuthed } from '../../../BitmexAuthed'
 import { BitmexHttp } from '../../../BitmexHttp'
 import { getBitmexEndpoints } from '../../../bitmexSpecs'
 import { BITMEX_RAW_POSITIONS } from '../../../test/fixtures/bitmexPositions'
+import * as listMod from '../../public/market/list'
 
 
 
@@ -16,33 +21,44 @@ describe(__filename, () => {
     secret: 'secret',
   }
 
-  it('should list raw positions just fine', async () => {
-
-    // preparing data
-    const mockedRawPositions = BITMEX_RAW_POSITIONS
-
+  it('should list Bitmex raw positions just fine', async () => {
 
     // mocking
     const {
       publicRequest,
       authedRequest,
     } = mockHttp({ classPrototype: BitmexHttp.prototype })
-    authedRequest.returns(Promise.resolve(mockedRawPositions))
+
+    authedRequest.returns(Promise.resolve(BITMEX_RAW_POSITIONS))
+
+    const { list } = mockList({ module: listMod })
+    list.returns(Promise.resolve({ markets: PARSED_MARKETS }))
 
 
     // executing
-    const exchange = new BitmexAuthed({ credentials })
+    const settings: IAlunaSettingsSchema = {}
+    const exchange = new BitmexAuthed({ credentials, settings })
 
     const { rawPositions } = await exchange.position!.listRaw()
 
 
     // validating
-    expect(rawPositions).to.deep.eq(mockedRawPositions)
+    expect(rawPositions).to.deep.eq({
+      bitmexPositions: BITMEX_RAW_POSITIONS,
+      markets: PARSED_MARKETS,
+    })
+
+    expect(list.callCount).to.be.eq(1)
+    expect(list.firstCall.args[0]).to.deep.eq({
+      http: new BitmexHttp({}),
+    })
 
     expect(authedRequest.callCount).to.be.eq(1)
     expect(authedRequest.firstCall.args[0]).to.deep.eq({
+      verb: AlunaHttpVerbEnum.GET,
+      url: getBitmexEndpoints(settings).position.list,
       credentials,
-      url: getBitmexEndpoints(exchange.settings).position.list,
+      body: { filter: { isOpen: true } },
     })
 
     expect(publicRequest.callCount).to.be.eq(0)
