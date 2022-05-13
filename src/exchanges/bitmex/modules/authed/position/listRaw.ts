188cce9c915a1b1af2e981364a4a224442f22426
Sergio <sergio.uft@gmail.com>
2022-05-13 12:21:43 -0300

    implement Bitmex position 'listRaw'

--- src/exchanges/bitmex/modules/authed/position/listRaw.ts
+++ src/exchanges/bitmex/modules/authed/position/listRaw.ts
@@ -1,13 +1,17 @@
 import debug from 'debug'
 
 import { IAlunaExchangeAuthed } from '../../../../../lib/core/IAlunaExchange'
+import { AlunaHttpVerbEnum } from '../../../../../lib/enums/AlunaHtttpVerbEnum'
 import {
   IAlunaPositionListParams,
   IAlunaPositionListRawReturns,
 } from '../../../../../lib/modules/authed/IAlunaPositionModule'
 import { BitmexHttp } from '../../../BitmexHttp'
 import { getBitmexEndpoints } from '../../../bitmexSpecs'
-import { IBitmexPositionSchema } from '../../../schemas/IBitmexPositionSchema'
+import {
+  IBitmexPosition,
+  IBitmexPositionsSchema,
+} from '../../../schemas/IBitmexPositionSchema'
 
 
 
@@ -17,7 +21,7 @@ const log = debug('@alunajs:bitmex/position/listRaw')
 
 export const listRaw = (exchange: IAlunaExchangeAuthed) => async (
   params: IAlunaPositionListParams = {},
-): Promise<IAlunaPositionListRawReturns<IBitmexPositionSchema[]>> => {
+): Promise<IAlunaPositionListRawReturns<IBitmexPositionsSchema>> => {
 
   log('listing raw positions', params)
 
@@ -30,10 +34,19 @@ export const listRaw = (exchange: IAlunaExchangeAuthed) => async (
     http = new BitmexHttp(settings),
   } = params
 
-  const rawPositions = await http.authedRequest<IBitmexPositionSchema[]>({
+  const bitmexPositions = await http.authedRequest<IBitmexPosition[]>({
     credentials,
     url: getBitmexEndpoints(settings).position.list,
+    verb: AlunaHttpVerbEnum.GET,
+    body: { filter: { isOpen: true } },
   })
+
+  const { markets } = await exchange.market.list({ http })
+
+  const rawPositions: IBitmexPositionsSchema = {
+    bitmexPositions,
+    markets,
+  }
 
   const { requestWeight } = http
 
