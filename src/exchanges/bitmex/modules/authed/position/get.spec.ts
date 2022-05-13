55708e32a63548324499a7781f9fc7b7aadec6f8
Sergio <sergio.uft@gmail.com>
2022-05-13 12:26:11 -0300

    add test suite for Bitmex position 'get'

--- src/exchanges/bitmex/modules/authed/position/get.spec.ts
+++ src/exchanges/bitmex/modules/authed/position/get.spec.ts
@@ -1,5 +1,6 @@
 import { expect } from 'chai'
 
+import { PARSED_MARKETS } from '../../../../../../test/fixtures/parsedMarkets'
 import { PARSED_POSITIONS } from '../../../../../../test/fixtures/parsedPositions'
 import { mockGetRaw } from '../../../../../../test/mocks/exchange/modules/mockGetRaw'
 import { mockParse } from '../../../../../../test/mocks/exchange/modules/mockParse'
@@ -7,6 +8,7 @@ import { IAlunaPositionGetParams } from '../../../../../lib/modules/authed/IAlun
 import { IAlunaCredentialsSchema } from '../../../../../lib/schemas/IAlunaCredentialsSchema'
 import { BitmexAuthed } from '../../../BitmexAuthed'
 import { BitmexHttp } from '../../../BitmexHttp'
+import { IBitmexPositionSchema } from '../../../schemas/IBitmexPositionSchema'
 import { BITMEX_RAW_POSITIONS } from '../../../test/fixtures/bitmexPositions'
 import * as getRawMod from './getRaw'
 import * as parseMod from './parse'
@@ -23,44 +25,46 @@ describe(__filename, () => {
   it('should get position just fine', async () => {
 
     // preparing data
-    const mockedRawPosition = BITMEX_RAW_POSITIONS[0]
-    const mockedParsedPosition = PARSED_POSITIONS[0]
+    const bitmexPosition = BITMEX_RAW_POSITIONS[0]
+    const parsedPosition = PARSED_POSITIONS[0]
+    const market = PARSED_MARKETS[0]
 
-    const { id } = mockedRawPosition
+    const rawPosition: IBitmexPositionSchema = {
+      market,
+      bitmexPosition,
+    }
 
 
     // mocking
     const { getRaw } = mockGetRaw({ module: getRawMod })
-    getRaw.returns(Promise.resolve({ rawPosition: mockedRawPosition }))
+    getRaw.returns(Promise.resolve({ rawPosition }))
 
     const { parse } = mockParse({ module: parseMod })
-
-    parse.returns({ position: mockedParsedPosition })
+    parse.returns({ position: parsedPosition })
 
 
     // executing
     const exchange = new BitmexAuthed({ credentials })
 
     const params: IAlunaPositionGetParams = {
-      id,
-      symbolPair: mockedRawPosition[0],
+      symbolPair: bitmexPosition.symbol,
     }
 
     const { position } = await exchange.position!.get(params)
 
 
     // validating
-    expect(position).to.deep.eq(mockedParsedPosition)
+    expect(position).to.deep.eq(parsedPosition)
 
     expect(getRaw.callCount).to.be.eq(1)
     expect(getRaw.firstCall.args[0]).to.deep.eq({
-      id,
+      symbolPair: bitmexPosition.symbol,
       http: new BitmexHttp({}),
     })
 
     expect(parse.callCount).to.be.eq(1)
     expect(parse.firstCall.args[0]).to.deep.eq({
-      rawPosition: mockedRawPosition,
+      rawPosition,
     })
 
   })
