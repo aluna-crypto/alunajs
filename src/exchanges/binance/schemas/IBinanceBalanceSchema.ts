cf5caea4a16fe742cbf8298a6344ffcd4ee585fd
Sergio <sergio.uft@gmail.com>
2022-05-16 15:17:51 -0300

    add Binance margin balance schema

--- src/exchanges/binance/schemas/IBinanceBalanceSchema.ts
+++ src/exchanges/binance/schemas/IBinanceBalanceSchema.ts
@@ -1,5 +1,30 @@
+export interface IBinanceBalancesSchema {
+  spotBalances: IBinanceSpotBalanceSchema[]
+  marginBalances: IBinanceMarginBalanceSchema[]
+}
+
+
 export interface IBinanceBalanceSchema {
+  asset: string
+  available: number
+  total: number
+}
+
+
+
+export interface IBinanceSpotBalanceSchema {
   asset: string
   free: string
   locked: string
+}
+
+
+
+export interface IBinanceMarginBalanceSchema {
+  asset: string
+  free: string
+  locked: string
+  borrowed: string
+  interest: string
+  netAsset: string
 }
