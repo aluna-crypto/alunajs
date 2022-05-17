4d2089f47fea59713fd9b62abfebb3a688ebdd85
Sergio <sergio.uft@gmail.com>
2022-05-17 14:33:21 -0300

    update Binance order 'getRaw' test suite

--- src/exchanges/binance/modules/authed/order/getRaw.spec.ts
+++ src/exchanges/binance/modules/authed/order/getRaw.spec.ts
@@ -2,8 +2,12 @@ import { expect } from 'chai'
 
 import { mockHttp } from '../../../../../../test/mocks/exchange/Http'
 import { mockListRaw } from '../../../../../../test/mocks/exchange/modules/mockListRaw'
+import { AlunaAccountEnum } from '../../../../../lib/enums/AlunaAccountEnum'
 import { AlunaHttpVerbEnum } from '../../../../../lib/enums/AlunaHtttpVerbEnum'
+import { AlunaGenericErrorCodes } from '../../../../../lib/errors/AlunaGenericErrorCodes'
+import { IAlunaOrderCancelParams } from '../../../../../lib/modules/authed/IAlunaOrderModule'
 import { IAlunaCredentialsSchema } from '../../../../../lib/schemas/IAlunaCredentialsSchema'
+import { executeAndCatch } from '../../../../../utils/executeAndCatch'
 import { BinanceAuthed } from '../../../BinanceAuthed'
 import { BinanceHttp } from '../../../BinanceHttp'
 import { getBinanceEndpoints } from '../../../binanceSpecs'
@@ -20,7 +24,7 @@ describe(__filename, () => {
     secret: 'secret',
   }
 
-  it('should get a Binance raw order just fine', async () => {
+  it('should get a Binance raw order just fine (SPOT)', async () => {
 
     // preparing data
     const binanceOrder = BINANCE_RAW_ORDERS[0]
@@ -28,12 +32,16 @@ describe(__filename, () => {
 
     const { orderId, symbol } = binanceOrder
 
-    const id = orderId.toString()
-    const symbolPair = symbol
+    const params: IAlunaOrderCancelParams = {
+      id: orderId.toString(),
+      account: AlunaAccountEnum.SPOT,
+      symbolPair: symbol,
+    }
 
     const query = new URLSearchParams()
-    query.append('orderId', id)
-    query.append('symbol', symbolPair)
+    query.append('orderId', params.id)
+    query.append('symbol', params.symbolPair)
+
 
 
     // mocking
@@ -50,10 +58,7 @@ describe(__filename, () => {
     // executing
     const exchange = new BinanceAuthed({ credentials })
 
-    const { rawOrder } = await exchange.order.getRaw({
-      id,
-      symbolPair,
-    })
+    const { rawOrder } = await exchange.order.getRaw(params)
 
 
     // validating
@@ -72,6 +77,98 @@ describe(__filename, () => {
       weight: 2,
     })
 
+    expect(publicRequest.callCount).to.be.eq(0)
+
+  })
+
+  it('should get a Binance raw order just fine (MARGIN)', async () => {
+
+    // preparing data
+    const binanceOrder = BINANCE_RAW_ORDERS[0]
+    const rawSymbol = BINANCE_RAW_SYMBOLS[0]
+
+    const { orderId, symbol } = binanceOrder
+
+    const params: IAlunaOrderCancelParams = {
+      id: orderId.toString(),
+      account: AlunaAccountEnum.MARGIN,
+      symbolPair: symbol,
+    }
+
+    const query = new URLSearchParams()
+    query.append('orderId', params.id)
+    query.append('symbol', params.symbolPair)
+
+
+
+    // mocking
+    const {
+      publicRequest,
+      authedRequest,
+    } = mockHttp({ classPrototype: BinanceHttp.prototype })
+    authedRequest.returns(Promise.resolve(binanceOrder))
+
+    const { listRaw } = mockListRaw({ module: listRawMod })
+    listRaw.returns(Promise.resolve({ rawSymbols: [rawSymbol] }))
+
+
+    // executing
+    const exchange = new BinanceAuthed({ credentials })
+
+    const { rawOrder } = await exchange.order.getRaw(params)
+
+
+    // validating
+    expect(rawOrder).to.deep.eq({
+      binanceOrder,
+      rawSymbol,
+    })
+
+    expect(authedRequest.callCount).to.be.eq(1)
+
+    expect(authedRequest.firstCall.args[0]).to.deep.eq({
+      verb: AlunaHttpVerbEnum.GET,
+      credentials,
+      query,
+      url: getBinanceEndpoints(exchange.settings).order.margin,
+      weight: 10,
+    })
+
+    expect(publicRequest.callCount).to.be.eq(0)
+
+  })
+
+  it('should ensure order account is required to get a binance order', async () => {
+
+    // preparing data
+    const params: IAlunaOrderCancelParams = {
+      id: 'id',
+      symbolPair: 'symbolPair',
+    }
+
+
+    // mocking
+    const {
+      publicRequest,
+      authedRequest,
+    } = mockHttp({ classPrototype: BinanceHttp.prototype })
+    authedRequest.returns(Promise.resolve())
+
+
+    // executing
+    const exchange = new BinanceAuthed({ credentials })
+
+    const { error } = await executeAndCatch(() => exchange.order.getRaw(params))
+
+
+    // validating
+    const msg = 'Order account is required to cancel Binance orders'
+
+    expect(error!.code).to.deep.eq(AlunaGenericErrorCodes.PARAM_ERROR)
+    expect(error!.message).to.deep.eq(msg)
+    expect(error!.httpStatusCode).to.deep.eq(400)
+
+    expect(authedRequest.callCount).to.be.eq(0)
     expect(publicRequest.callCount).to.be.eq(0)
 
   })
