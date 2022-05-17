4995177ecf032e11f6e6eba22c775b780967a10d
LucianoPierdona <lucianompj1@gmail.com>
2022-05-17 20:39:57 -0300

    Add Ftx Key Schema and Fixtures

--- src/exchanges/ftx/test/fixtures/ftxKey.ts
+++ src/exchanges/ftx/test/fixtures/ftxKey.ts
@@ -2,8 +2,13 @@ import { IFtxKeySchema } from '../../schemas/IFtxKeySchema'
 
 
 
+const FTX_RAW_MOCKED_REST_KEY = {} as any
+
 export const FTX_KEY_PERMISSIONS: IFtxKeySchema = {
-  read: true,
-  trade: true,
-  withdraw: true,
+  ...FTX_RAW_MOCKED_REST_KEY,
+  readOnly: true,
+  withdrawalEnabled: true,
+  account: {
+    accountIdentifier: 1234,
+  },
 }
