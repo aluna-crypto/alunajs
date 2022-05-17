ef0a493b174aff1901a47e3f8d7082399e8a350f
Sergio <sergio.uft@gmail.com>
2022-05-17 14:33:11 -0300

    made Binance order 'getRaw' works for margin orders

--- src/exchanges/binance/modules/authed/order/getRaw.ts
+++ src/exchanges/binance/modules/authed/order/getRaw.ts
@@ -1,8 +1,11 @@
 import { debug } from 'debug'
 import { find } from 'lodash'
 
+import { AlunaError } from '../../../../../lib/core/AlunaError'
 import { IAlunaExchangeAuthed } from '../../../../../lib/core/IAlunaExchange'
+import { AlunaAccountEnum } from '../../../../../lib/enums/AlunaAccountEnum'
 import { AlunaHttpVerbEnum } from '../../../../../lib/enums/AlunaHtttpVerbEnum'
+import { AlunaGenericErrorCodes } from '../../../../../lib/errors/AlunaGenericErrorCodes'
 import {
   IAlunaOrderGetParams,
   IAlunaOrderGetRawReturns,
@@ -33,9 +36,41 @@ export const getRaw = (exchange: IAlunaExchangeAuthed) => async (
 
   const {
     id,
+    account,
     symbolPair,
     http = new BinanceHttp(settings),
   } = params
+
+  if (!account) {
+
+    const error = new AlunaError({
+      code: AlunaGenericErrorCodes.PARAM_ERROR,
+      message: 'Order account is required to cancel Binance orders',
+      httpStatusCode: 400,
+    })
+
+    log(error)
+
+    throw error
+
+  }
+
+  const orderEndpoints = getBinanceEndpoints(settings).order
+
+  let url: string
+  let weight: number
+
+  if (account === AlunaAccountEnum.SPOT) {
+
+    url = orderEndpoints.spot
+    weight = 2
+
+  } else {
+
+    url = orderEndpoints.margin
+    weight = 10
+
+  }
 
   const query = new URLSearchParams()
 
@@ -45,18 +80,16 @@ export const getRaw = (exchange: IAlunaExchangeAuthed) => async (
   const binanceOrder = await http.authedRequest<IBinanceOrderSchema>({
     credentials,
     verb: AlunaHttpVerbEnum.GET,
-    url: getBinanceEndpoints(settings).order.spot,
+    url,
     query,
-    weight: 2,
+    weight,
   })
 
   const { rawSymbols } = await exchange.symbol.listRaw({
     http,
   })
 
-  const rawSymbol = find(rawSymbols, {
-    symbol: symbolPair,
-  })
+  const rawSymbol = find(rawSymbols, { symbol: symbolPair })
 
   const rawOrder: IBinanceOrderResponseSchema = {
     binanceOrder,
