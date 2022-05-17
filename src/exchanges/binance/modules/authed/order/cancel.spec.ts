40c2f2f2cba738ccbf50a63c345411ffd055ed5d
Sergio <sergio.uft@gmail.com>
2022-05-17 14:32:57 -0300

    update Binance order 'cancel' test suite

--- src/exchanges/binance/modules/authed/order/cancel.spec.ts
+++ src/exchanges/binance/modules/authed/order/cancel.spec.ts
@@ -4,7 +4,9 @@ import { PARSED_ORDERS } from '../../../../../../test/fixtures/parsedOrders'
 import { mockHttp } from '../../../../../../test/mocks/exchange/Http'
 import { mockGet } from '../../../../../../test/mocks/exchange/modules/mockGet'
 import { AlunaError } from '../../../../../lib/core/AlunaError'
+import { AlunaAccountEnum } from '../../../../../lib/enums/AlunaAccountEnum'
 import { AlunaHttpVerbEnum } from '../../../../../lib/enums/AlunaHtttpVerbEnum'
+import { AlunaGenericErrorCodes } from '../../../../../lib/errors/AlunaGenericErrorCodes'
 import { AlunaOrderErrorCodes } from '../../../../../lib/errors/AlunaOrderErrorCodes'
 import { IAlunaOrderCancelParams } from '../../../../../lib/modules/authed/IAlunaOrderModule'
 import { IAlunaCredentialsSchema } from '../../../../../lib/schemas/IAlunaCredentialsSchema'
@@ -24,7 +26,7 @@ describe(__filename, () => {
     secret: 'secret',
   }
 
-  it('should cancel a Binance order just fine', async () => {
+  it('should cancel a Binance order just fine (SPOT)', async () => {
 
     // preparing data
     const mockedRawOrder = BINANCE_RAW_ORDERS[0]
@@ -34,7 +36,8 @@ describe(__filename, () => {
 
     const params: IAlunaOrderCancelParams = {
       id: orderId.toString(),
-      symbolPair: '',
+      account: AlunaAccountEnum.SPOT,
+      symbolPair: 'symbolPair',
     }
 
     const query = new URLSearchParams()
@@ -74,7 +77,65 @@ describe(__filename, () => {
       credentials,
       url: getBinanceEndpoints(exchange.settings).order.spot,
       query,
-      weight: 2,
+      weight: 1,
+    })
+
+    expect(publicRequest.callCount).to.be.eq(0)
+
+  })
+
+  it('should cancel a Binance order just fine (MARGIN)', async () => {
+
+    // preparing data
+    const mockedRawOrder = BINANCE_RAW_ORDERS[0]
+    const mockedParsedOrder = PARSED_ORDERS[0]
+
+    const { orderId } = mockedRawOrder
+
+    const params: IAlunaOrderCancelParams = {
+      id: orderId.toString(),
+      account: AlunaAccountEnum.MARGIN,
+      symbolPair: 'symbolPair',
+    }
+
+    const query = new URLSearchParams()
+    query.append('orderId', params.id)
+    query.append('symbol', params.symbolPair)
+
+
+    // mocking
+    const {
+      publicRequest,
+      authedRequest,
+    } = mockHttp({ classPrototype: BinanceHttp.prototype })
+
+
+    const { get } = mockGet({ module: getMod })
+
+    authedRequest.returns(Promise.resolve(mockedRawOrder))
+
+    get.returns(Promise.resolve({ order: mockedParsedOrder }))
+
+
+    // executing
+    const exchange = new BinanceAuthed({ credentials })
+
+    const { order } = await exchange.order.cancel(params)
+
+
+    // validating
+    expect(order).to.deep.eq(mockedParsedOrder)
+
+    expect(authedRequest.callCount).to.be.eq(1)
+
+
+
+    expect(authedRequest.firstCall.args[0]).to.deep.eq({
+      verb: AlunaHttpVerbEnum.DELETE,
+      credentials,
+      url: getBinanceEndpoints(exchange.settings).order.margin,
+      query,
+      weight: 10,
     })
 
     expect(publicRequest.callCount).to.be.eq(0)
@@ -87,6 +148,7 @@ describe(__filename, () => {
     const params: IAlunaOrderCancelParams = {
       id: 'id',
       symbolPair: 'symbolPair',
+      account: AlunaAccountEnum.SPOT,
     }
 
     const query = new URLSearchParams()
@@ -115,10 +177,7 @@ describe(__filename, () => {
 
     const {
       error: responseError,
-    } = await executeAndCatch(() => exchange.order.cancel({
-      id: params.id,
-      symbolPair: params.symbolPair,
-    }))
+    } = await executeAndCatch(() => exchange.order.cancel(params))
 
 
     // validating
@@ -131,9 +190,44 @@ describe(__filename, () => {
       credentials,
       query,
       url: getBinanceEndpoints(exchange.settings).order.spot,
-      weight: 2,
+      weight: 1,
     })
 
+    expect(publicRequest.callCount).to.be.eq(0)
+
+  })
+
+  it('should ensure order account is required to cancel binance orders', async () => {
+
+    // preparing data
+    const params: IAlunaOrderCancelParams = {
+      id: 'id',
+      symbolPair: 'symbolPair',
+    }
+
+
+    // mocking
+    const {
+      publicRequest,
+      authedRequest,
+    } = mockHttp({ classPrototype: BinanceHttp.prototype })
+    authedRequest.returns(Promise.reject())
+
+
+    // executing
+    const exchange = new BinanceAuthed({ credentials })
+
+    const { error } = await executeAndCatch(() => exchange.order.cancel(params))
+
+
+    // validating
+    const msg = 'Order account is required to cancel Binance orders'
+
+    expect(error!.code).to.deep.eq(AlunaGenericErrorCodes.PARAM_ERROR)
+    expect(error!.message).to.deep.eq(msg)
+    expect(error!.httpStatusCode).to.deep.eq(400)
+
+    expect(authedRequest.callCount).to.be.eq(0)
     expect(publicRequest.callCount).to.be.eq(0)
 
   })
