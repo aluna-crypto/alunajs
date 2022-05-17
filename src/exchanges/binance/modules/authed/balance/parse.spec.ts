955055d7cb72c1862f0d005bca0f7b7dd45485ba
Sergio <sergio.uft@gmail.com>
2022-05-17 13:49:55 -0300

    update Binance balance 'parse' test suite

--- src/exchanges/binance/modules/authed/balance/parse.spec.ts
+++ src/exchanges/binance/modules/authed/balance/parse.spec.ts
@@ -4,7 +4,10 @@ import { AlunaWalletEnum } from '../../../../../lib/enums/AlunaWalletEnum'
 import { IAlunaCredentialsSchema } from '../../../../../lib/schemas/IAlunaCredentialsSchema'
 import { mockTranslateSymbolId } from '../../../../../utils/mappings/translateSymbolId.mock'
 import { BinanceAuthed } from '../../../BinanceAuthed'
-import { BINANCE_RAW_SPOT_BALANCES } from '../../../test/fixtures/binanceBalances'
+import {
+  BINANCE_RAW_MARGIN_BALANCES,
+  BINANCE_RAW_SPOT_BALANCES,
+} from '../../../test/fixtures/binanceBalances'
 
 
 
@@ -15,7 +18,7 @@ describe(__filename, () => {
     secret: 'secret',
   }
 
-  it('should parse a Binance raw balance just fine', async () => {
+  it('should parse a Binance raw balance just fine (SPOT)', async () => {
 
     // preparing data
     const exchange = new BinanceAuthed({ credentials })
@@ -46,6 +49,42 @@ describe(__filename, () => {
     expect(balance.total).to.be.eq(total)
     expect(balance.symbolId).to.be.eq(asset)
     expect(balance.wallet).to.be.eq(AlunaWalletEnum.SPOT)
+
+    expect(balance.meta).to.be.eq(rawBalance)
+
+  })
+
+  it('should parse a Binance raw balance just fine (MARGIN)', async () => {
+
+    // preparing data
+    const exchange = new BinanceAuthed({ credentials })
+
+    const rawBalance = BINANCE_RAW_MARGIN_BALANCES[0]
+
+    const {
+      asset,
+      free,
+      locked,
+    } = rawBalance
+
+    const available = Number(free)
+    const total = available + Number(locked)
+
+    // mocking
+    const { translateSymbolId } = mockTranslateSymbolId()
+    translateSymbolId.returns(asset)
+
+    // executing
+    const { balance } = exchange.balance.parse({ rawBalance })
+
+
+    // validating
+    expect(balance).to.exist
+
+    expect(balance.available).to.be.eq(available)
+    expect(balance.total).to.be.eq(total)
+    expect(balance.symbolId).to.be.eq(asset)
+    expect(balance.wallet).to.be.eq(AlunaWalletEnum.MARGIN)
 
     expect(balance.meta).to.be.eq(rawBalance)
 
