38d08aaf6c93a2b44a200450dd681b912c1d3629
LucianoPierdona <lucianompj1@gmail.com>
2022-05-18 22:02:19 -0300

    Add Okx Balance schema and fixtures

--- src/exchanges/okx/schemas/IOkxBalanceSchema.ts
+++ src/exchanges/okx/schemas/IOkxBalanceSchema.ts
@@ -1,7 +1,38 @@
-// TODO: Describe balance interface for Okx exchange
 export interface IOkxBalanceSchema {
-  total: string
-  available: string
-  currencySymbol: string
-  // ...
+  availBal: string
+  availEq: string
+  cashBal: string
+  ccy: string
+  crossLiab: string
+  disEq: string
+  eq: string
+  eqUsd: string
+  frozenBal: string
+  interest: string
+  isoEq: string
+  isoLiab: string
+  isoUpl: string
+  liab: string
+  maxLoan: string
+  mgnRatio: string
+  notionalLever: string
+  ordFrozen: string
+  twap: string
+  uTime: string
+  upl: string
+  uplLiab: string
+  stgyEq: string
+}
+
+export interface IOkxBalanceListSchema {
+  adjEq: string
+  details: IOkxBalanceSchema[]
+  imr: string
+  isoEq: string
+  mgnRatio: string
+  mmr: string
+  notionalUsd: string
+  ordFroz: string
+  totalEq: string
+  uTime: string
 }
