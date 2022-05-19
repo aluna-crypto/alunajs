2b4780979e2b8281e7947f58fff10a6e9128be84
LucianoPierdona <lucianompj1@gmail.com>
2022-05-18 22:01:17 -0300

    Add Okx Symbol schema and fixtures

--- src/exchanges/okx/schemas/IOkxSymbolSchema.ts
+++ src/exchanges/okx/schemas/IOkxSymbolSchema.ts
@@ -1,6 +1,27 @@
-// TODO: Describe symbol interface for Okx exchange
+import { OkxSymbolStatusEnum } from '../enums/OkxSymbolStatusEnum'
+
+
+
 export interface IOkxSymbolSchema {
-  symbol: string
-  name: string
-  // ...
+  instType: string
+  instId: string
+  uly: string
+  category: string
+  baseCcy: string
+  quoteCcy: string
+  settleCcy: string
+  ctVal: string
+  ctMult: string
+  ctValCcy: string
+  optType: string
+  stk: string
+  listTime: string
+  expTime: string
+  lever: string
+  tickSz: string
+  lotSz: string
+  minSz: string
+  ctType: string
+  alias: string
+  state: OkxSymbolStatusEnum
 }
