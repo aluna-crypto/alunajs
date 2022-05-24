73fb5582162c2238bd29fb7d0252054021affdf8
LucianoPierdona <lucianompj1@gmail.com>
2022-05-24 18:24:53 -0300

    Add Huobi Order parse and test suite

--- src/exchanges/huobi/modules/authed/order/parse.spec.ts
+++ src/exchanges/huobi/modules/authed/order/parse.spec.ts
@@ -1,33 +1,178 @@
 import { expect } from 'chai'
 
 import { IAlunaCredentialsSchema } from '../../../../../lib/schemas/IAlunaCredentialsSchema'
+import { mockTranslateSymbolId } from '../../../../../utils/mappings/translateSymbolId.mock'
+import { translateOrderSideToAluna } from '../../../enums/adapters/huobiOrderSideAdapter'
+import { translateOrderStatusToAluna } from '../../../enums/adapters/huobiOrderStatusAdapter'
+import { translateOrderTypeToAluna } from '../../../enums/adapters/huobiOrderTypeAdapter'
+import { HuobiOrderSideEnum } from '../../../enums/HuobiOrderSideEnum'
+import { HuobiOrderTypeEnum } from '../../../enums/HuobiOrderTypeEnum'
 import { HuobiAuthed } from '../../../HuobiAuthed'
 import { HUOBI_RAW_ORDERS } from '../../../test/fixtures/huobiOrders'
+import { HUOBI_RAW_SYMBOLS } from '../../../test/fixtures/huobiSymbols'
 
 
 
-describe.skip(__filename, () => {
+describe(__filename, () => {
 
   const credentials: IAlunaCredentialsSchema = {
     key: 'key',
     secret: 'secret',
   }
 
-  it('should parse a Huobi raw order just fine', async () => {
+  it('should parse a Huobi raw limit order just fine', async () => {
 
     // preparing data
     const rawOrder = HUOBI_RAW_ORDERS[0]
+    const rawSymbol = HUOBI_RAW_SYMBOLS[0]
 
+    const rawOrderRequest = {
+      rawOrder,
+      rawSymbol,
+    }
+
+    const {
+      symbol,
+      price,
+      type,
+      'created-at': createdAt,
+      amount,
+      state,
+      id,
+    } = rawOrder
+
+    const {
+      bc: baseSymbolId,
+      qc: quoteSymbolId,
+    } = rawSymbol
+
+    const orderStatus = translateOrderStatusToAluna({
+      from: state,
+    })
+    const orderSide = type.split('-')[0] as HuobiOrderSideEnum
+    const orderType = type.split('-')[1] as HuobiOrderTypeEnum
+    const translatedOrderSide = translateOrderSideToAluna({
+      from: orderSide,
+    })
+    const translatedOrderType = translateOrderTypeToAluna({
+      from: orderType,
+    })
+    const orderAmount = Number(amount)
+    const rate = Number(price)
+    const total = orderAmount * rate
+
+    // mocking
     const exchange = new HuobiAuthed({ credentials })
 
-    const { order } = exchange.order.parse({ rawOrder })
+    const { translateSymbolId } = mockTranslateSymbolId()
 
+    translateSymbolId
+      .onFirstCall()
+      .returns(baseSymbolId)
+
+    translateSymbolId
+      .onSecondCall()
+      .returns(quoteSymbolId)
+
+    // executing
+    const { order } = exchange.order.parse({
+      rawOrder: rawOrderRequest,
+    })
 
     // validating
     expect(order).to.exist
 
-    // TODO: add expectations for everything
-    // expect(order).to.deep.eq(...)
+    expect(order.id).to.be.eq(id.toString())
+    expect(order.symbolPair).to.be.eq(symbol)
+    expect(order.baseSymbolId).to.be.eq(baseSymbolId)
+    expect(order.quoteSymbolId).to.be.eq(quoteSymbolId)
+    expect(order.total).to.be.eq(total)
+    expect(order.rate).to.be.eq(rate)
+    expect(order.amount).to.be.eq(orderAmount)
+    expect(order.type).to.be.eq(translatedOrderType)
+    expect(order.side).to.be.eq(translatedOrderSide)
+    expect(order.status).to.be.eq(orderStatus)
+    expect(order.placedAt.getTime()).to.be.eq(new Date(createdAt).getTime())
+    expect(order.canceledAt).to.be.ok
+    expect(order.filledAt).not.to.be.ok
+
+  })
+
+  it('should parse a Huobi raw market order just fine', async () => {
+
+    // preparing data
+    const rawOrder = HUOBI_RAW_ORDERS[1]
+    const rawSymbol = HUOBI_RAW_SYMBOLS[1]
+
+    const rawOrderRequest = {
+      rawOrder,
+      rawSymbol,
+    }
+
+    const {
+      symbol,
+      price,
+      type,
+      'created-at': createdAt,
+      amount,
+      state,
+      id,
+    } = rawOrder
+
+    const {
+      bc: baseSymbolId,
+      qc: quoteSymbolId,
+    } = rawSymbol
+
+    const orderStatus = translateOrderStatusToAluna({
+      from: state,
+    })
+    const orderSide = type.split('-')[0] as HuobiOrderSideEnum
+    const orderType = type.split('-')[1] as HuobiOrderTypeEnum
+    const translatedOrderSide = translateOrderSideToAluna({
+      from: orderSide,
+    })
+    const translatedOrderType = translateOrderTypeToAluna({
+      from: orderType,
+    })
+    const orderAmount = Number(amount)
+    const rate = Number(price)
+    const total = orderAmount * rate
+
+    // mocking
+    const exchange = new HuobiAuthed({ credentials })
+
+    const { translateSymbolId } = mockTranslateSymbolId()
+
+    translateSymbolId
+      .onFirstCall()
+      .returns(baseSymbolId)
+
+    translateSymbolId
+      .onSecondCall()
+      .returns(quoteSymbolId)
+
+    // executing
+    const { order } = exchange.order.parse({
+      rawOrder: rawOrderRequest,
+    })
+
+    // validating
+    expect(order).to.exist
+
+    expect(order.id).to.be.eq(id.toString())
+    expect(order.symbolPair).to.be.eq(symbol)
+    expect(order.baseSymbolId).to.be.eq(baseSymbolId)
+    expect(order.quoteSymbolId).to.be.eq(quoteSymbolId)
+    expect(order.total).to.be.eq(total)
+    expect(order.rate).to.be.eq(rate)
+    expect(order.amount).to.be.eq(orderAmount)
+    expect(order.type).to.be.eq(translatedOrderType)
+    expect(order.side).to.be.eq(translatedOrderSide)
+    expect(order.status).to.be.eq(orderStatus)
+    expect(order.placedAt.getTime()).to.be.eq(new Date(createdAt).getTime())
+    expect(order.filledAt).to.be.ok
+    expect(order.canceledAt).not.to.be.ok
 
   })
 
--- src/exchanges/huobi/modules/authed/order/parse.ts
+++ src/exchanges/huobi/modules/authed/order/parse.ts
@@ -1,52 +1,115 @@
 import { IAlunaExchangeAuthed } from '../../../../../lib/core/IAlunaExchange'
+import { AlunaAccountEnum } from '../../../../../lib/enums/AlunaAccountEnum'
+import { AlunaOrderStatusEnum } from '../../../../../lib/enums/AlunaOrderStatusEnum'
 import {
   IAlunaOrderParseParams,
   IAlunaOrderParseReturns,
 } from '../../../../../lib/modules/authed/IAlunaOrderModule'
 import { IAlunaOrderSchema } from '../../../../../lib/schemas/IAlunaOrderSchema'
 import { translateSymbolId } from '../../../../../utils/mappings/translateSymbolId'
-import { IHuobiOrderSchema } from '../../../schemas/IHuobiOrderSchema'
+import { translateOrderSideToAluna } from '../../../enums/adapters/huobiOrderSideAdapter'
+import { translateOrderStatusToAluna } from '../../../enums/adapters/huobiOrderStatusAdapter'
+import { translateOrderTypeToAluna } from '../../../enums/adapters/huobiOrderTypeAdapter'
+import { HuobiOrderSideEnum } from '../../../enums/HuobiOrderSideEnum'
+import { HuobiOrderTypeEnum } from '../../../enums/HuobiOrderTypeEnum'
+import { IHuobiOrderResponseSchema } from '../../../schemas/IHuobiOrderSchema'
 
 
 
 export const parse = (exchange: IAlunaExchangeAuthed) => (
-  params: IAlunaOrderParseParams<IHuobiOrderSchema>,
+  params: IAlunaOrderParseParams<IHuobiOrderResponseSchema>,
 ): IAlunaOrderParseReturns => {
 
 
-  const { rawOrder } = params
+  const { rawOrder: rawOrderResponse } = params
 
-  const { symbol } = rawOrder
+  const {
+    rawOrder,
+    rawSymbol,
+  } = rawOrderResponse
 
-  let [
-    baseSymbolId,
-    quoteSymbolId,
-  ] = symbol.split('/')
+  const {
+    symbol,
+    price,
+    type,
+    'created-at': createdAt,
+    amount,
+    state,
+    id,
+  } = rawOrder
+
+  let {
+    bc: baseSymbolId,
+    qc: quoteSymbolId,
+  } = rawSymbol
+
+  const {
+    settings,
+    id: exchangeId,
+  } = exchange
+
+  const { symbolMappings } = settings
 
   baseSymbolId = translateSymbolId({
     exchangeSymbolId: baseSymbolId,
-    symbolMappings: exchange.settings.symbolMappings,
+    symbolMappings,
   })
 
   quoteSymbolId = translateSymbolId({
     exchangeSymbolId: quoteSymbolId,
-    symbolMappings: exchange.settings.symbolMappings,
+    symbolMappings,
   })
 
-  // TODO: Implement proper parser
+  const orderStatus = translateOrderStatusToAluna({
+    from: state,
+  })
+
+  const placedAt = new Date(createdAt)
+  let filledAt: Date | undefined
+  let canceledAt: Date | undefined
+
+  if (orderStatus === AlunaOrderStatusEnum.CANCELED) {
+
+    canceledAt = new Date()
+
+  }
+
+  if (orderStatus === AlunaOrderStatusEnum.FILLED) {
+
+    filledAt = new Date()
+
+  }
+
+  const orderSide = type.split('-')[0] as HuobiOrderSideEnum
+  const orderType = type.split('-')[1] as HuobiOrderTypeEnum
+  const translatedOrderSide = translateOrderSideToAluna({
+    from: orderSide,
+  })
+  const translatedOrderType = translateOrderTypeToAluna({
+    from: orderType,
+  })
+  const orderAmount = Number(amount)
+  const rate = Number(price)
+  const total = orderAmount * rate
+
   const order: IAlunaOrderSchema = {
-    id: rawOrder.id,
-    symbolPair: rawOrder.id,
-    exchangeId: exchange.specs.id,
+    id: id.toString(),
+    symbolPair: symbol,
+    account: AlunaAccountEnum.SPOT,
+    exchangeId,
     baseSymbolId,
     quoteSymbolId,
-    // total: rawOrder.total,
-    // amount: rawOrder.amount,
-    // account: AlunaAccountEnum.MARGIN,
-    // status: rawOrder.status,
-    // side: rawOrder.side,
+    total,
+    placedAt,
+    canceledAt,
+    filledAt,
+    rate,
+    amount: orderAmount,
+    side: translatedOrderSide,
+    status: orderStatus,
+    type: translatedOrderType,
     meta: rawOrder,
-  } as any // TODO: Remove casting to any
+  }
 
   return { order }
 
