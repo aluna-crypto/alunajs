e651cd555dad88c33bdb0a1a843cf01610a4f04d
Sergio <sergio.uft@gmail.com>
2022-05-25 20:39:55 -0300

    add draft for Ftx position 'parseMany'

---
+++ src/exchanges/ftx/modules/authed/position/parseMany.ts
@@ -0,0 +1,44 @@
+import debug from 'debug'
+import { each } from 'lodash'
+
+import { IAlunaExchangeAuthed } from '../../../../../lib/core/IAlunaExchange'
+import {
+  IAlunaPositionParseManyParams,
+  IAlunaPositionParseManyReturns,
+} from '../../../../../lib/modules/authed/IAlunaPositionModule'
+import { IAlunaPositionSchema } from '../../../../../lib/schemas/IAlunaPositionSchema'
+
+
+
+const log = debug('alunajs:ftx/position/parseMany')
+
+
+
+export const parseMany = (exchange: IAlunaExchangeAuthed) => (
+  params: IAlunaPositionParseManyParams<any[]>,
+): IAlunaPositionParseManyReturns => {
+
+  const { rawPositions } = params
+
+  const positions: IAlunaPositionSchema[] = []
+
+  each(rawPositions, (rawPosition) => {
+
+    const { size } = rawPosition
+
+    // skipping closed positions
+    if (size === 0) {
+      return
+    }
+
+    const { position } = exchange.position!.parse({ rawPosition })
+
+    positions.push(position)
+
+  })
+
+  log(`parsed ${positions.length} positions`)
+
+  return { positions }
+
+}
