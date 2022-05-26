1ce92817c451a02378ab16416324832d014b63a2
Sergio <sergio.uft@gmail.com>
2022-05-26 20:24:56 -0300

    add schema 'IFtxTriggerOrderSchema'

--- src/exchanges/ftx/schemas/IFtxOrderSchema.ts
+++ src/exchanges/ftx/schemas/IFtxOrderSchema.ts
@@ -1,6 +1,7 @@
 import { FtxOrderSideEnum } from '../enums/FtxOrderSideEnum'
 import { FtxOrderStatusEnum } from '../enums/FtxOrderStatusEnum'
 import { FtxOrderTypeEnum } from '../enums/FtxOrderTypeEnum'
+import { FtxTriggerOrderTypeEnum } from '../enums/FtxTriggerOrderTypeEnum'
 
 
 
@@ -21,4 +22,32 @@ export interface IFtxOrderSchema {
   ioc: boolean
   postOnly: boolean
   clientId: number
+}
+
+
+
+export interface IFtxTriggerOrderSchema {
+  createdAt: string
+  filledSize: number
+  future: string
+  id: number
+  market: string
+  price: number | null
+  avgFillPrice: number | null
+  remainingSize: number
+  side: FtxOrderSideEnum
+  size: number
+  status: FtxOrderStatusEnum
+  type: FtxTriggerOrderTypeEnum
+  reduceOnly: boolean
+  ioc: boolean
+  postOnly: boolean
+  clientId: number
+  orderPrice: number | null
+  trailStart: number | null
+  trailValue: number | null
+  triggerPrice: number
+  triggeredAt: string | null
+  orderType: FtxOrderTypeEnum
+  retryUntilFilled: boolean
 }
