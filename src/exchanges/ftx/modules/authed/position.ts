df1163290601c5a3a26eccf7f36e268f7c767bcc
Sergio <sergio.uft@gmail.com>
2022-05-25 21:13:23 -0300

    add recent implemented methods to Ftx position module

--- src/exchanges/ftx/modules/authed/position.ts
+++ src/exchanges/ftx/modules/authed/position.ts
@@ -1,5 +1,8 @@
 import { IAlunaExchangeAuthed } from '../../../../lib/core/IAlunaExchange'
 import { IAlunaPositionModule } from '../../../../lib/modules/authed/IAlunaPositionModule'
+import { close } from './position/close'
+import { get } from './position/get'
+import { getRaw } from './position/getRaw'
 import { list } from './position/list'
 import { listRaw } from './position/listRaw'
 import { parse } from './position/parse'
@@ -10,12 +13,13 @@ import { parseMany } from './position/parseMany'
 export function position(exchange: IAlunaExchangeAuthed): IAlunaPositionModule {
 
   return {
-
+    get: get(exchange),
+    getRaw: getRaw(exchange),
     listRaw: listRaw(exchange),
     list: list(exchange),
     parse: parse(exchange),
     parseMany: parseMany(exchange),
-
-  } as any
+    close: close(exchange),
+  }
 
 }
