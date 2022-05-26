4d43a2e4fb9444c1679696dd8b730fcbab8c04b3
Sergio <sergio.uft@gmail.com>
2022-05-25 21:10:39 -0300

    add draft for Ftx position 'getRaw'

---
+++ src/exchanges/ftx/modules/authed/position/getRaw.ts
@@ -0,0 +1,60 @@
+import debug from 'debug'
+import { find } from 'lodash'
+
+import { AlunaError } from '../../../../../lib/core/AlunaError'
+import { IAlunaExchangeAuthed } from '../../../../../lib/core/IAlunaExchange'
+import { AlunaHttpVerbEnum } from '../../../../../lib/enums/AlunaHtttpVerbEnum'
+import { AlunaPositionErrorCodes } from '../../../../../lib/errors/AlunaPositionErrorCodes'
+import {
+  IAlunaPositionGetParams,
+  IAlunaPositionGetRawReturns,
+} from '../../../../../lib/modules/authed/IAlunaPositionModule'
+import { FtxHttp } from '../../../FtxHttp'
+import { getFtxEndpoints } from '../../../ftxSpecs'
+
+
+
+const log = debug('alunajs:bitfinex/position/getRaw')
+
+
+
+export const getRaw = (exchange: IAlunaExchangeAuthed) => async (
+  params: IAlunaPositionGetParams,
+): Promise<IAlunaPositionGetRawReturns<any>> => {
+
+  const {
+    settings,
+    credentials,
+  } = exchange
+
+  const {
+    symbolPair,
+    http = new FtxHttp(exchange.settings),
+  } = params
+
+  log('getting raw position', { symbolPair })
+
+  const response = await http.authedRequest<any[]>({
+    credentials,
+    url: getFtxEndpoints(settings).position.list,
+    verb: AlunaHttpVerbEnum.GET,
+  })
+
+  const rawPosition = find(response, { future: symbolPair })
+
+  if (!rawPosition) {
+    throw new AlunaError({
+      code: AlunaPositionErrorCodes.NOT_FOUND,
+      message: 'Position not found',
+      httpStatusCode: 400,
+    })
+  }
+
+  const { requestWeight } = http
+
+  return {
+    rawPosition,
+    requestWeight,
+  }
+
+}
