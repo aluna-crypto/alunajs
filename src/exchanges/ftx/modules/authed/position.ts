93e6cea5c7fd470e44cf02e0bdca4b39de23564c
Sergio <sergio.uft@gmail.com>
2022-05-25 22:05:52 -0300

    set up Ftx leverage methods on position module

--- src/exchanges/ftx/modules/authed/position.ts
+++ src/exchanges/ftx/modules/authed/position.ts
@@ -2,11 +2,13 @@ import { IAlunaExchangeAuthed } from '../../../../lib/core/IAlunaExchange'
 import { IAlunaPositionModule } from '../../../../lib/modules/authed/IAlunaPositionModule'
 import { close } from './position/close'
 import { get } from './position/get'
+import { getLeverage } from './position/getLeverage'
 import { getRaw } from './position/getRaw'
 import { list } from './position/list'
 import { listRaw } from './position/listRaw'
 import { parse } from './position/parse'
 import { parseMany } from './position/parseMany'
+import { setLeverage } from './position/setLeverage'
 
 
 
@@ -20,6 +22,8 @@ export function position(exchange: IAlunaExchangeAuthed): IAlunaPositionModule {
     parse: parse(exchange),
     parseMany: parseMany(exchange),
     close: close(exchange),
+    getLeverage: getLeverage(exchange),
+    setLeverage: setLeverage(exchange),
   }
 
 }
