91856acb227799c2ece88044203d76fd591efbb9
Sergio <sergio.uft@gmail.com>
2022-05-28 17:12:51 -0300

    made Ftx edit order test suite use macro test

--- src/exchanges/ftx/modules/authed/order/edit.spec.ts
+++ src/exchanges/ftx/modules/authed/order/edit.spec.ts
@@ -1,15 +1,8 @@
 import { expect } from 'chai'
 
-import { PARSED_ORDERS } from '../../../../../../test/fixtures/parsedOrders'
-import { mockHttp } from '../../../../../../test/mocks/exchange/Http'
-import { mockParse } from '../../../../../../test/mocks/exchange/modules/mockParse'
-import { AlunaAccountEnum } from '../../../../../lib/enums/AlunaAccountEnum'
-import { AlunaOrderSideEnum } from '../../../../../lib/enums/AlunaOrderSideEnum'
+import { testEditOrder } from '../../../../../../test/macros/testEditOrder'
 import { AlunaOrderTypesEnum } from '../../../../../lib/enums/AlunaOrderTypesEnum'
-import { IAlunaOrderEditParams } from '../../../../../lib/modules/authed/IAlunaOrderModule'
 import { IAlunaCredentialsSchema } from '../../../../../lib/schemas/IAlunaCredentialsSchema'
-import { editOrderParamsSchema } from '../../../../../utils/validation/schemas/editOrderParamsSchema'
-import { mockValidateParams } from '../../../../../utils/validation/validateParams.mock'
 import { FtxAuthed } from '../../../FtxAuthed'
 import { FtxHttp } from '../../../FtxHttp'
 import { getFtxEndpoints } from '../../../ftxSpecs'
@@ -18,80 +11,69 @@ import * as parseMod from './parse'
 
 
 
-describe(__filename, () => {
+describe.only(__filename, () => {
 
   const credentials: IAlunaCredentialsSchema = {
     key: 'key',
     secret: 'secret',
   }
 
-  it('should edit a Ftx order just fine', async () => {
+  testEditOrder({
+    ExchangeAuthed: FtxAuthed,
+    HttpClass: FtxHttp,
+    parseImportPath: parseMod,
+    authedRequestResponse: FTX_RAW_ORDERS[0],
+    credentials,
+    validateAuthedRequest: (params) => {
 
-    // preparing data
-    const http = new FtxHttp({})
+      const {
+        editParams,
+        stub,
+      } = params
 
-    const mockedRawOrder = FTX_RAW_ORDERS[0]
-    const mockedParsedOrder = PARSED_ORDERS[0]
+      const {
+        type,
+        amount,
+        rate,
+        limitRate,
+        stopRate,
+        id,
+      } = editParams
 
-    const { id } = mockedRawOrder
+      const body: Record<string, any> = {}
 
+      let url: string
 
-    // mocking
-    const {
-      authedRequest,
-      publicRequest,
-    } = mockHttp({ classPrototype: FtxHttp.prototype })
+      switch (type) {
 
-    authedRequest.returns(Promise.resolve(mockedRawOrder))
+        case AlunaOrderTypesEnum.STOP_LIMIT:
+          url = getFtxEndpoints({}).order.editTrigger(id)
+          body.triggerPrice = stopRate
+          body.orderPrice = limitRate
+          body.size = amount
+          break
 
-    const { parse } = mockParse({ module: parseMod })
+        case AlunaOrderTypesEnum.STOP_MARKET:
+          url = getFtxEndpoints({}).order.editTrigger(id)
+          body.triggerPrice = stopRate
+          body.size = amount
+          break
 
-    parse.returns({ order: mockedParsedOrder })
+        // Limit orders
+        default:
+          url = getFtxEndpoints({}).order.edit(id)
+          body.price = rate
+          body.size = amount
 
-    const { validateParamsMock } = mockValidateParams()
+      }
 
-    // executing
-    const exchange = new FtxAuthed({ credentials })
+      expect(stub.firstCall.args[0]).to.deep.eq({
+        url,
+        credentials,
+        body,
+      })
 
-    const params: IAlunaOrderEditParams = {
-      id: id.toString(),
-      symbolPair: '',
-      account: AlunaAccountEnum.SPOT,
-      amount: 0.01,
-      side: AlunaOrderSideEnum.BUY,
-      type: AlunaOrderTypesEnum.LIMIT,
-      rate: 0,
-    }
-
-    const {
-      order,
-      requestWeight,
-    } = await exchange.order.edit(params)
-
-
-    // validating
-    expect(order).to.deep.eq(mockedParsedOrder)
-
-    expect(requestWeight).to.deep.eq(http.requestWeight)
-
-    expect(authedRequest.callCount).to.be.eq(1)
-    expect(authedRequest.firstCall.args[0]).to.deep.eq({
-      url: getFtxEndpoints(exchange.settings).order.edit(id.toString()),
-      credentials,
-      body: {
-        price: 0,
-        size: 0.01,
-      },
-    })
-
-    expect(validateParamsMock.callCount).to.be.eq(1)
-    expect(validateParamsMock.firstCall.args[0]).to.deep.eq({
-      params,
-      schema: editOrderParamsSchema,
-    })
-
-    expect(publicRequest.callCount).to.be.eq(0)
-
+    },
   })
 
 })
