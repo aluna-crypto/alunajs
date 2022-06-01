7fa3c11174d0df731be0409b3d97f975a93cf517
Sergio <sergio.uft@gmail.com>
2022-06-01 11:23:23 -0300

    refacting some aluna schemas

--- src/lib/schemas/IAlunaExchangeSchema.ts
+++ src/lib/schemas/IAlunaExchangeSchema.ts
@@ -25,7 +25,7 @@ export interface IAlunaExchangeSchema {
 
 export interface IAlunaExchangeAccountSpecsSchema {
   type: AlunaAccountEnum
-  supported: boolean // supported by the exchange
+  supported: true // supported by the exchange
   implemented?: boolean // implemented by aluna
   wallet: AlunaWalletEnum
   orderTypes: IAlunaExchangeOrderSpecsSchema[] // TODO: check prop requirement
@@ -33,7 +33,7 @@ export interface IAlunaExchangeAccountSpecsSchema {
 
 export interface IAlunaExchangeOrderSpecsSchema {
   type: AlunaOrderTypesEnum
-  supported: boolean // supported by the exchange
+  supported: true // supported by the exchange
   implemented: boolean // implemented by aluna
   mode: AlunaFeaturesModeEnum
   options: IAlunaExchangeOrderOptionsSchema
