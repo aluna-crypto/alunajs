67dbeb7dc869fecd1aa42a293568654f39a8bfe2
Sergio <sergio.uft@gmail.com>
2022-06-02 13:52:17 -0300

    update Ftx market 'parse' test suite

--- src/exchanges/ftx/modules/public/market/parse.spec.ts
+++ src/exchanges/ftx/modules/public/market/parse.spec.ts
@@ -1,9 +1,14 @@
 import { expect } from 'chai'
-import { cloneDeep } from 'lodash'
+import {
+  clone,
+  cloneDeep,
+} from 'lodash'
 
 import { mockTranslateSymbolId } from '../../../../../utils/mappings/translateSymbolId.mock'
+import { FtxMarketTypeEnum } from '../../../enums/FtxMarketTypeEnum'
 import { Ftx } from '../../../Ftx'
 import { FTX_RAW_MARKETS } from '../../../test/fixtures/ftxMarket'
+import { mockSplitFtxSymbolPair } from './helpers/splitFtxSymbolPair.mock'
 
 
 
@@ -71,6 +76,88 @@ describe(__filename, () => {
     })
     expect(translateSymbolId.secondCall.args[0]).to.deep.eq({
       exchangeSymbolId: quoteCurrency,
+      symbolMappings: exchange.settings.symbolMappings,
+    })
+
+  })
+
+  it('should parse a Ftx raw future market just fine', async () => {
+
+    // preparing data
+    const rawMarket = clone(FTX_RAW_MARKETS[0])
+
+    rawMarket.name = 'BTC-PERP'
+    rawMarket.underlying = 'BTC'
+    rawMarket.type = FtxMarketTypeEnum.FUTURE
+
+    const expectedBaseSymbolId = 'BTC'
+    const expectedSymbolId = 'BTC'
+
+    const {
+      baseCurrency,
+      quoteCurrency,
+    } = rawMarket
+
+
+    // mocking
+    const { splitFtxSymbolPair } = mockSplitFtxSymbolPair()
+    splitFtxSymbolPair.returns({
+      baseSymbolId: expectedBaseSymbolId,
+      quoteSymbolId: expectedSymbolId,
+    })
+
+    const { translateSymbolId } = mockTranslateSymbolId()
+    translateSymbolId.onFirstCall().returns(baseCurrency)
+    translateSymbolId.onSecondCall().returns(quoteCurrency)
+
+
+    // executing
+    const exchange = new Ftx({})
+
+    const { market } = exchange.market.parse({
+      rawMarket,
+    })
+
+
+    // validating
+    expect(market).to.exist
+
+    expect(market.exchangeId).to.be.eq(exchange.specs.id)
+    expect(market.symbolPair).to.be.eq(rawMarket.name)
+    expect(market.baseSymbolId).to.be.eq(baseCurrency)
+    expect(market.quoteSymbolId).to.be.eq(quoteCurrency)
+
+    expect(market.ticker.high).to.be.eq(rawMarket.price)
+    expect(market.ticker.low).to.be.eq(rawMarket.price)
+    expect(market.ticker.bid).to.be.eq(rawMarket.bid)
+    expect(market.ticker.ask).to.be.eq(rawMarket.ask)
+    expect(market.ticker.last).to.be.eq(rawMarket.last)
+    expect(market.ticker.change).to.be.eq(rawMarket.change24h)
+    expect(market.ticker.baseVolume).to.be.eq(0)
+    expect(market.ticker.quoteVolume).to.be.eq(rawMarket.quoteVolume24h)
+    expect(market.ticker.date).to.be.ok
+
+    expect(market.leverageEnabled).to.be.ok
+    expect(market.derivativesEnabled).to.be.ok
+
+    expect(market.spotEnabled).not.to.be.ok
+    expect(market.marginEnabled).not.to.be.ok
+    expect(market.instrument).not.to.be.ok
+    expect(market.maxLeverage).not.to.be.ok
+
+    expect(splitFtxSymbolPair.callCount).to.be.eq(1)
+    expect(splitFtxSymbolPair.firstCall.args[0]).to.deep.eq({
+      market: rawMarket.name,
+    })
+
+
+    expect(translateSymbolId.callCount).to.be.eq(2)
+    expect(translateSymbolId.firstCall.args[0]).to.deep.eq({
+      exchangeSymbolId: expectedBaseSymbolId,
+      symbolMappings: exchange.settings.symbolMappings,
+    })
+    expect(translateSymbolId.secondCall.args[0]).to.deep.eq({
+      exchangeSymbolId: expectedBaseSymbolId,
       symbolMappings: exchange.settings.symbolMappings,
     })
 
